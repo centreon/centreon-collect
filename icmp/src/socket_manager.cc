568bb6b3c00a58d7885cdcbef56b8ed845629ff6
Matthieu Kermagoret <mkermagoret@merethis.com>
2011-09-14 10:54:30 +0200

    Refactoring of the ICMP connector.

--- icmp/src/socket_manager.cc
+++ icmp/src/socket_manager.cc
@@ -1,19 +1,19 @@
 /*
 ** Copyright 2011 Merethis
 **
-** This file is part of Centreon Engine.
+** This file is part of Centreon Connector ICMP.
 **
-** Centreon Engine is free software: you can redistribute it and/or
-** modify it under the terms of the GNU General Public License version 2
-** as published by the Free Software Foundation.
+** Centreon Connector ICMP is free software: you can redistribute it
+** and/or modify it under the terms of the GNU General Public License
+** version 2 as published by the Free Software Foundation.
 **
-** Centreon Engine is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Centreon Connector ICMP is distributed in the hope that it will be
+** useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ** General Public License for more details.
 **
 ** You should have received a copy of the GNU General Public License
-** along with Centreon Engine. If not, see
+** along with Centreon Connector ICMP. If not, see
 ** <http://www.gnu.org/licenses/>.
 */
 
