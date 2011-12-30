59d04a7f30ea69c38519f52307611b968a05766b
Matthieu Kermagoret <mkermagoret@merethis.com>
2011-12-30 12:22:45 +0100

    Connector SSH: added timeout class that will be used to notify checks when their timeout elapsed.

---
+++ ssh/test/checks/timeout/assignment.cc
@@ -0,0 +1,46 @@
+/*
+** Copyright 2011 Merethis
+**
+** This file is part of Centreon Connector SSH.
+**
+** Centreon Connector SSH is free software: you can redistribute it
+** and/or modify it under the terms of the GNU Affero General Public
+** License as published by the Free Software Foundation, either version
+** 3 of the License, or (at your option) any later version.
+**
+** Centreon Connector SSH is distributed in the hope that it will be
+** useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public
+** License along with Centreon Connector SSH. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include "com/centreon/connector/ssh/checks/check.hh"
+#include "com/centreon/connector/ssh/checks/timeout.hh"
+
+using namespace com::centreon::connector::ssh;
+
+/**
+ *  Check timeout's copy construction.
+ *
+ *  @return 0 on success.
+ */
+int main() {
+  // Base object.
+  checks::check c1;
+  checks::timeout t1(&c1);
+
+  // Copy object.
+  checks::check c2;
+  checks::timeout t2(&c2);
+
+  // Assignment.
+  t2 = t1;
+
+  // Return check result.
+  return ((t1.get_check() != &c1)
+          || (t2.get_check() != &c1));
+}
