74e7897b06f94a926797a04eeae5d43e69804236
Matthieu Kermagoret <mkermagoret@merethis.com>
2012-01-17 15:57:12 +0100

    Connector Perl: added orders parser.

---
+++ centreon-connector/perl/inc/com/centreon/connector/perl/orders/parser.hh
@@ -0,0 +1,65 @@
+/*
+** Copyright 2011-2012 Merethis
+**
+** This file is part of Centreon Connector Perl.
+**
+** Centreon Connector Perl is free software: you can redistribute it
+** and/or modify it under the terms of the GNU Affero General Public
+** License as published by the Free Software Foundation, either version
+** 3 of the License, or (at your option) any later version.
+**
+** Centreon Connector Perl is distributed in the hope that it will be
+** useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public
+** License along with Centreon Connector Perl. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CCCP_ORDERS_PARSER_HH
+#  define CCCP_ORDERS_PARSER_HH
+
+#  include <string>
+#  include "com/centreon/connector/perl/namespace.hh"
+#  include "com/centreon/connector/perl/orders/listener.hh"
+#  include "com/centreon/handle_listener.hh"
+
+CCCP_BEGIN()
+
+namespace              orders {
+  /**
+   *  @class parser parser.hh "com/centreon/connector/perl/orders/parser.hh"
+   *  @brief Parse orders.
+   *
+   *  Parse orders, generally issued by the monitoring engine. The
+   *  parser class can handle be registered with one handle at a time
+   *  and one listener.
+   */
+  class                parser : public handle_listener {
+  public:
+                       parser();
+                       parser(parser const& p);
+                       ~parser() throw ();
+    parser&            operator=(parser const& p);
+    void               error(handle& h);
+    std::string const& get_buffer() const throw ();
+    listener*          get_listener() const throw ();
+    void               listen(listener* l = NULL) throw ();
+    void               read(handle& h);
+    bool               want_read(handle& h);
+    bool               want_write(handle& h);
+
+  private:
+    void               _copy(parser const& p);
+    void               _parse(std::string const& cmd);
+
+    std::string        _buffer;
+    listener*          _listnr;
+  };
+}
+
+CCCP_END()
+
+#endif // !CCCP_ORDERS_PARSER_HH
---
+++ centreon-connector/perl/src/orders/parser.cc
@@ -0,0 +1,275 @@
+/*
+** Copyright 2011-2012 Merethis
+**
+** This file is part of Centreon Connector Perl.
+**
+** Centreon Connector Perl is free software: you can redistribute it
+** and/or modify it under the terms of the GNU Affero General Public
+** License as published by the Free Software Foundation, either version
+** 3 of the License, or (at your option) any later version.
+**
+** Centreon Connector Perl is distributed in the hope that it will be
+** useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public
+** License along with Centreon Connector Perl. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include "com/centreon/connector/perl/orders/parser.hh"
+#include "com/centreon/exceptions/basic.hh"
+#include "com/centreon/logging/logger.hh"
+
+using namespace com::centreon::connector::perl::orders;
+
+/**************************************
+*                                     *
+*           Public Methods            *
+*                                     *
+**************************************/
+
+/**
+ *  Default constructor.
+ */
+parser::parser() : _listnr(NULL) {}
+
+/**
+ *  Copy constructor.
+ *
+ *  @param[in] p Object to copy.
+ */
+parser::parser(parser const& p) : handle_listener(p) {
+  _copy(p);
+}
+
+/**
+ *  Destructor.
+ */
+parser::~parser() throw () {}
+
+/**
+ *  Assignment operator.
+ *
+ *  @param[in] p Object to copy.
+ *
+ *  @return This object.
+ */
+parser& parser::operator=(parser const& p) {
+  if (this != &p) {
+    handle_listener::operator=(p);
+    _copy(p);
+  }
+  return (*this);
+}
+
+/**
+ *  Got error event on handle.
+ *
+ *  @param[in] h Handle.
+ */
+void parser::error(handle& h) {
+  (void)h;
+  if (_listnr)
+    _listnr->on_error();
+  return ;
+}
+
+/**
+ *  Get unparsed buffer.
+ *
+ *  @return Unparsed buffer.
+ */
+std::string const& parser::get_buffer() const throw () {
+  return (_buffer);
+}
+
+/**
+ *  Get associated listener.
+ *
+ *  @return Listener if object has one, NULL otherwise.
+ */
+listener* parser::get_listener() const throw () {
+  return (_listnr);
+}
+
+/**
+ *  Change the listener.
+ *
+ *  @param[in] l Listener.
+ */
+void parser::listen(listener* l) throw () {
+  _listnr = l;
+  return ;
+}
+
+/**
+ *  Read data from handle.
+ *
+ *  @param[in] h Handle.
+ */
+void parser::read(handle& h) {
+  // Read data.
+  logging::debug(logging::medium) << "reading data for parsing";
+  char buffer[4096];
+  unsigned long rb(h.read(buffer, sizeof(buffer)));
+  logging::debug(logging::medium) << "read "
+    << rb << " bytes from handle";
+
+  // stdin's eof is reached.
+  if (!rb) {
+    logging::debug(logging::high) << "got eof on read handle";
+    if (_listnr)
+      _listnr->on_eof();
+  }
+  // Data was read.
+  else {
+    _buffer.append(buffer, rb);
+
+    // Find a command boundary.
+    char boundary[4];
+    memset(boundary, 0, sizeof(boundary));
+    size_t bound(_buffer.find(boundary, 0, sizeof(boundary)));
+
+    // Parse command.
+    while (bound != std::string::npos) {
+      logging::debug(logging::high)
+        << "got command boundary at offset " << bound;
+      bound += sizeof(boundary);
+      std::string cmd(_buffer.substr(0, bound));
+      _buffer.erase(0, bound);
+      bool error(false);
+      try {
+        _parse(cmd);
+      }
+      catch (std::exception const& e) {
+        logging::error(logging::low) << "orders parsing error: "
+          << e.what();
+        error = true;
+      }
+      catch (...) {
+        logging::error(logging::low) << "unknown orders parsing error";
+        error = true;
+      }
+      if (error && _listnr)
+        _listnr->on_error();
+      bound = _buffer.find(boundary, 0, sizeof(boundary));
+    }
+  }
+  return ;
+}
+
+/**
+ *  Do we want to read handle ?
+ *
+ *  @return Always true.
+ */
+bool parser::want_read(handle& h) {
+  (void)h;
+  return (true);
+}
+
+/**
+ *  Do we want to write to handle ?
+ *
+ *  @return Always false (class just parse).
+ */
+bool parser::want_write(handle& h) {
+  (void)h;
+  return (false);
+}
+
+/**************************************
+*                                     *
+*           Private Methods           *
+*                                     *
+**************************************/
+
+/**
+ *  Copy internal data members.
+ *
+ *  @param[in] p Object to copy.
+ */
+void parser::_copy(parser const& p) {
+  _buffer = p._buffer;
+  _listnr = p._listnr;
+  return ;
+}
+
+/**
+ *  @brief Parse a command.
+ *
+ *  It is the caller's responsibility to ensure that the command given
+ *  to parse is terminated with 4 \0.
+ *
+ *  @param[in] cmd Command to parse.
+ */
+void parser::_parse(std::string const& cmd) {
+  // Get command ID.
+  size_t pos(cmd.find('\0'));
+  unsigned int id(strtoul(cmd.c_str(), NULL, 10));
+  ++pos;
+
+  // Process each command as necessary.
+  switch (id) {
+  case 0: // Version query.
+    if (_listnr)
+      _listnr->on_version();
+    break ;
+  case 2: // Execute query.
+    {
+      // Note: no need to check npos because cmd is
+      //       terminated with at least 4 \0.
+
+      // Find command ID.
+      size_t end(cmd.find('\0', pos));
+      char* ptr(NULL);
+      unsigned long long cmd_id(strtoull(cmd.c_str() + pos, &ptr, 10));
+      if (!cmd_id || *ptr)
+        throw (basic_error() << "invalid execution request received:" \
+                    " bad command ID (" << cmd.c_str() + pos << ")");
+      pos = end + 1;
+      // Find timeout value.
+      end = cmd.find('\0', pos);
+      time_t timeout(static_cast<time_t>(strtoull(
+        cmd.c_str() + pos,
+        &ptr,
+        10)));
+      if (*ptr)
+        throw (basic_error() << "invalid execution request received:" \
+                    " bad timeout (" << cmd.c_str() + pos << ")");
+      timeout += time(NULL);
+      pos = end + 1;
+      // Find start time.
+      end = cmd.find('\0', pos);
+      strtoull(cmd.c_str() + pos, &ptr, 10);
+      if (*ptr)
+        throw (basic_error() << "invalid execution request received:" \
+                    " bad start time (" << cmd.c_str() + pos << ")");
+      pos = end + 1;
+      // Find command to execute.
+      end = cmd.find('\0', pos);
+      std::string cmdline(cmd.substr(pos, end - pos));
+
+      // Notify listener.
+      if (_listnr)
+        _listnr->on_execute(
+          cmd_id,
+          timeout,
+          cmdline);
+    }
+    break ;
+  case 4: // Quit query.
+    if (_listnr)
+      _listnr->on_quit();
+    break ;
+  default:
+    throw (basic_error() << "invalid command received (ID "
+             << id << ")");
+  };
+  return ;
+}
