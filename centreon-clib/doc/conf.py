4775235274d0a286f63d9543b61f303863626920
Dorian Guillois <dguillois@merethis.com>
2013-01-07 11:53:12 +0100

    Update copyright for 2013.

--- centreon-clib/doc/conf.py
+++ centreon-clib/doc/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Centreon Clib'
-copyright = u'2012, Merethis'
+copyright = u'2013, Merethis'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -95,7 +95,7 @@ if on_centreon_rtd:
     # The theme to use for HTML and HTML Help pages.  See the documentation for
     # a list of builtin themes.
     html_theme = 'centreon'
-    
+
     # Theme options are theme-specific and customize the look and feel of a theme
     # further.  For a list of options available for each theme, see the
     # documentation.
