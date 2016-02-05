949830f390037bb999bb1d46a0c83208f63ea29b
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-02-05 16:21:46 +0100

    containers: the running container is now immediately starting to monitor when starting up.

--- containers/centos6/run.sh
+++ containers/centos6/run.sh
@@ -2,6 +2,12 @@
 
 service mysql start
 service httpd start
+service cbd start
+service centengine start
+service centcore start
 bash
+service centcore stop
+service centengine stop
+service cbd stop
 service httpd stop
 service mysql stop
