1aea9d4a7d182f3d0d8e8b98ca60357854ff9de1
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-02-12 14:44:32 +0100

    jobs: add monitoring-engine-package shell job.

---
+++ jobs/monitoring-engine-package.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Pull monitoring-build-dependencies container.
+docker pull 10.24.11.199:5000/monitoring-build-dependencies:centos6
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Get Centreon Engine sources.
+if [ \! -d centreon-engine ] ; then
+  git clone https://github.com/centreon/centreon-engine
+fi
+
+# Get version.
+cmakelists=centreon-engine/build/CMakeLists.txt
+major=`grep 'set(CENTREON_ENGINE_MAJOR' "$cmakelists" | cut -d ' ' -f 2 | cut -d ')' -f 1`
+minor=`grep 'set(CENTREON_ENGINE_MINOR' "$cmakelists" | cut -d ' ' -f 2 | cut -d ')' -f 1`
+patch=`grep 'set(CENTREON_ENGINE_PATCH' "$cmakelists" | cut -d ' ' -f 2 | cut -d ')' -f 1`
+export VERSION="$major.$minor.$patch"
+
+# Get release.
+cd centreon-engine
+commit=`git log -1 "$GIT_COMMIT" --pretty=format:%h`
+now=`date +%s`
+export RELEASE="$now.$commit"
+
+# Create source tarball.
+git archive --prefix="centreon-engine-$VERSION/" "$GIT_BRANCH" | gzip > "../input/centreon-engine-$VERSION.tar.gz"
+cd ..
+
+# Retrieve spec file.
+if [ \! -d packaging-centreon-engine ] ; then
+  git clone http://gitbot:gitbot@git.merethis.net/packaging-centreon-engine.git
+else
+  cd packaging-centreon-engine
+  git pull
+  cd ..
+fi
+cp packaging-centreon-engine/rpm/centreon-engine.spectemplate input/
+
+# Build RPMs.
+docker-rpm-builder dir 10.24.11.199:5000/monitoring-build-dependencies:centos6 input output
+
+# Copy files to server.
+CES_VERSION='3.3'
+FILES='output/x86_64/*.rpm'
+scp $FILES "root@srvi-ces-repository.merethis.net:/srv/repos/standard/$CES_VERSION/testing/x86_64/RPMS"
+ssh "root@srvi-ces-repository.merethis.net" createrepo "root@srvi-ces-repository.merethis.net:/srv/repos/standard/$CES_VERSION/testing/x86_64/"
