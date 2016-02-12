21d83e2186d5cdaf935f44b41160d22c8ce32cad
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-02-12 17:06:56 +0100

    jobs: add monitoring-bundle shell job.

---
+++ jobs/monitoring-bundle.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# CentOS 6 running image.
+cd centreon-build/containers/centos6
+docker build -t monitoring-running:centos6 -f running.Dockerfile .
+docker tag monitoring-running:centos6 10.24.11.199:5000/monitoring-running:centos6
+docker push 10.24.11.199:5000/monitoring-running:centos6
+
+# CentOS 7 running image.
+cd ../centos7
+docker build -t monitoring-running:centos7 -f running.Dockerfile .
+docker tag monitoring-running:centos7 10.24.11.199:5000/monitoring-running:centos7
+docker push 10.24.11.199:5000/monitoring-running:centos7
