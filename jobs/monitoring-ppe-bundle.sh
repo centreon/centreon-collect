570626b658231faebe5f2b7034bdebf8f5f59123
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-02-24 09:48:26 +0100

    jobs: add PPE bundling shell job.

---
+++ jobs/monitoring-ppe-bundle.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Pull monitoring-running image.
+docker pull 10.24.11.199:5000/monitoring-running:centos6
+docker tag -f 10.24.11.199:5000/monitoring-running:centos6 monitoring-running:centos6
+
+# Fetch PPE sources.
+scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.merethis.net:/tmp/centreon-export.tar.gz" .
+tar xzf centreon-export.tar.gz
+
+# CentOS 6 running image.
+cd centreon-build/containers/centos6
+rm -rf centreon-export
+cp ../../../centreon-export/www/modules/centreon-export .
+docker build -t monitoring-ppe-running:centos6 -f ppe-running.Dockerfile
+docker tag -f monitoring-ppe-running:centos6 10.24.11.199:5000/monitoring-ppe-running:centos6
+docker push 10.24.11.199:5000/monitoring-ppe-running:centos6
