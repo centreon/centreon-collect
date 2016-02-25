c168e8617996bc87971cb519f340c5505d312570
afouille <afouille@merethis.net>
2016-02-25 16:19:28 +0100

    Fix.

--- centreon-build/containers/centos6/lm/unittest-lm.sh
+++ centreon-build/containers/centos6/lm/unittest-lm.sh
@@ -3,4 +3,5 @@
 # Run unit tests.
 cd /usr/local/src/centreon-license-manager/
 composer install
-vendor/bin/phing -logfile /tmp/centreon-license-manager.xml unittest
+vendor/bin/phing unittest
+mv build/phpunit.xml /tmp/centreon-license-manager.xml
