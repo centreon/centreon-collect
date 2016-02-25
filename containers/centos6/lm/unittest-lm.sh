c1a20c88f946fc91d6afb17982380751fcd5e58b
afouille <afouille@merethis.net>
2016-02-25 16:29:46 +0100

    Add auth github.

--- containers/centos6/lm/unittest-lm.sh
+++ containers/centos6/lm/unittest-lm.sh
@@ -2,6 +2,7 @@
 
 # Run unit tests.
 cd /usr/local/src/centreon-license-manager/
+composer config --global github-oauth.github.com "2cf4c72854f10e4ef54ef5dde7cd41ab474fff71"
 composer install
 vendor/bin/phing unittest
 mv build/phpunit.xml /tmp/centreon-license-manager.xml
