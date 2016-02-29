a2cb939700d5ef9ab738c24ecac0086405dba7f1
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-02-29 14:28:38 +0100

    jobs: update Centreon Web jobs to match new documented rules.

--- centreon-build/jobs/web/mon-web-acceptance.sh
+++ centreon-build/jobs/web/mon-web-acceptance.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Pull monitoring-running container.
-docker pull ci.int.centreon.com:5000/monitoring-web:centos6
+docker pull ci.int.centreon.com:5000/mon-web:centos6
 
 # Start a container.
-containerid=`docker run -d -t -p 80 ci.int.centreon.com:5000/monitoring-web:centos6`
+containerid=`docker run -d -t -p 80 ci.int.centreon.com:5000/mon-web:centos6`
 port=`docker port "$containerid" 80 | cut -d : -f 2`
 
 # Copy acceptance tests from container to host.
