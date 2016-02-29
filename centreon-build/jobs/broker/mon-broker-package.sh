bf92121949fe6ac95cf68e25a9c2cb50da7931fb
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-02-29 14:34:15 +0100

    jobs: update Centreon Broker jobs to match new documented rules.

--- centreon-build/jobs/broker/mon-broker-package.sh
+++ centreon-build/jobs/broker/mon-broker-package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Pull monitoring-build-dependencies container.
-docker pull 10.24.11.199:5000/monitoring-build-dependencies:centos6
+# Pull mon-build-dependencies container.
+docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos6
 
 # Create input and output directories for docker-rpm-builder.
 rm -rf input
@@ -42,7 +42,7 @@ fi
 cp packaging-centreon-broker/rpm/centreon-broker.spectemplate input/
 
 # Build RPMs.
-docker-rpm-builder dir 10.24.11.199:5000/monitoring-build-dependencies:centos6 input output
+docker-rpm-builder dir ci.int.centreon.com:5000/mon-build-dependencies:centos6 input output
 
 # Copy files to server.
 CES_VERSION='3.0'
--- centreon-build/jobs/broker/mon-broker-unittest.sh
+++ centreon-build/jobs/broker/mon-broker-unittest.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Launch monitoring-unittest container.
-docker pull 10.24.11.199:5000/monitoring-unittest:centos6
-containerid=`docker create 10.24.11.199:5000/monitoring-unittest:centos6 /usr/local/bin/unittest-broker`
+# Launch mon-unittest container.
+docker pull ci.int.centreon.com:5000/mon-unittest:centos6
+containerid=`docker create ci.int.centreon.com:5000/mon-unittest:centos6 /usr/local/bin/unittest-broker`
 
 # Copy sources to container.
 docker cp centreon-broker "$containerid:/usr/local/src/centreon-broker"
