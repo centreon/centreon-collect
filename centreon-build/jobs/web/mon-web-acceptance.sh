f7f0345cde6364baf0a3fccc4611a93815c800f9
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-01 17:17:53 +0100

    jobs: use CENTREON_WEB_IMAGE in mon-web-acceptance.

--- centreon-build/jobs/web/mon-web-acceptance.sh
+++ centreon-build/jobs/web/mon-web-acceptance.sh
@@ -11,6 +11,7 @@ port=`docker port "$containerid" 80 | cut -d : -f 2`
 cd centreon-web
 
 # Run acceptance tests.
+export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-web:centos6
 /opt/behat/vendor/bin/behat --strict --format junit --out ../mon-web-acceptance.xml
 
 # Stop container.
