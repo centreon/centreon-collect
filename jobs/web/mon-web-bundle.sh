fe2b43c266a2b03d878397d6d65c9ed40a266842
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-01 09:04:09 +0100

    jobs: disable CentOS 7 in mon-web-bundle job.

--- jobs/web/mon-web-bundle.sh
+++ jobs/web/mon-web-bundle.sh
@@ -9,9 +9,9 @@ docker build -t ci.int.centreon.com:5000/mon-web:centos6 -f web.centos6.Dockerfi
 docker push ci.int.centreon.com:5000/mon-web:centos6
 
 # Pull mon-dependencies images.
-docker pull ci.int.centreon.com:5000/mon-dependencies:centos7
+#docker pull ci.int.centreon.com:5000/mon-dependencies:centos7
 
 # CentOS 7 main image.
-cd ../centos7
-docker build -t ci.int.centreon.com:5000/mon-web:centos7 -f web.centos7.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-web:centos7
+#cd ../centos7
+#docker build -t ci.int.centreon.com:5000/mon-web:centos7 -f web.centos7.Dockerfile .
+#docker push ci.int.centreon.com:5000/mon-web:centos7
