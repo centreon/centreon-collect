7b0e16f30bfc4b34a539d03e333d30a474738378
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-02 10:43:46 +0100

    jobs: properly set JUnit output file for mon-web-acceptance and mon-ppe-acceptance.

--- jobs/ppe/mon-ppe-acceptance.sh
+++ jobs/ppe/mon-ppe-acceptance.sh
@@ -10,7 +10,7 @@ port=`docker port "$containerid" 80 | cut -d : -f 2`
 # Run acceptance tests.
 export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-ppe:centos6
 cd centreon-export/www/modules/centreon-export
-/opt/behat/vendor/bin/behat --strict --format=junit --out=../../../../mon-ppe-acceptance.xml
+/opt/behat/vendor/bin/behat --strict --format=junit --out="$WORKSPACE/mon-ppe-acceptance.xml"
 
 # Stop container.
 docker stop "$containerid"
