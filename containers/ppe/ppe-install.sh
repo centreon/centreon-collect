1397975a4c767eb9539f82708048fe33efc9b1f0
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-02 17:13:08 +0100

    containers: use proper path for module installation helper script in PPE image.

--- containers/ppe/ppe-install.sh
+++ containers/ppe/ppe-install.sh
@@ -2,6 +2,6 @@
 
 service mysql start
 service httpd start
-./install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-export
+/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-export
 service httpd stop
 service mysql stop
