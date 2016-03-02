7372f758fd22f7620c02693ed0025e0438dcb48a
afouille <afouille@merethis.net>
2016-03-02 10:18:40 +0100

    Add recursion to cp.

--- centreon-build/jobs/lm/mon-lm-bundle.sh
+++ centreon-build/jobs/lm/mon-lm-bundle.sh
@@ -11,7 +11,7 @@ tar xzf centreon-license-manager.tar.gz
 # CentOS 6 running image.
 cd centreon-build/containers/lm/
 rm -rf centreon-license-manager
-cp ../../../centreon-license-manager/www/modules/centreon-license-manager .
+cp -R ../../../centreon-license-manager/www/modules/centreon-license-manager .
 docker build -t mon-lm-running:centos6 -f lm-running.Dockerfile
 docker tag -f mon-lm-running:centos6 10.24.11.199:5000/mon-lm-running:centos6
 docker push 10.24.11.199:5000/mon-lm-running:centos6
