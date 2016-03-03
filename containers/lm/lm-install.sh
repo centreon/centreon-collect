f32534086ab4258c12b15e2da90a56abdaa4f193
afouille <afouille@merethis.net>
2016-03-03 11:26:04 +0100

    Small fix.

--- containers/lm/lm-install.sh
+++ containers/lm/lm-install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+service mysql start
+service httpd start
 cd /usr/share/centreon/www/modules/centreon-license-manager/frontend/app
 npm install
 npm install -g gulp
 gulp
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
+service httpd stop
+service mysql stop
