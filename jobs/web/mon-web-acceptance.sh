35107a0ca32023eb108c3e11130a71c1982c707d
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-03 15:44:37 +0100

    jobs: add PhantomJS check routine before running acceptance tests.

--- jobs/web/mon-web-acceptance.sh
+++ jobs/web/mon-web-acceptance.sh
@@ -7,6 +7,12 @@ docker pull ci.int.centreon.com:5000/mon-web:centos6
 containerid=`docker run -d -t -p 80 ci.int.centreon.com:5000/mon-web:centos6`
 port=`docker port "$containerid" 80 | cut -d : -f 2`
 
+# Check that phantomjs is running.
+nc -w 0 localhost 4444
+if [ "$?" -ne 0 ] ; then
+  screen -d -m phantomjs --webdriver=4444
+fi
+
 # Run acceptance tests.
 export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-web:centos6
 rm -rf xunit-reports
