0a2699ec9f25a6cf90570566aa20d4f4d92892b1
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-04 14:58:05 +0100

    jobs: use the origin/2.7.x branch of Centreon Plugins to package Centreon Web.

--- centreon-build/jobs/web/mon-web-package.sh
+++ centreon-build/jobs/web/mon-web-package.sh
@@ -69,7 +69,7 @@ mkdir "../centreon-$VERSION"
 git archive "$GIT_BRANCH" | tar -C "../centreon-$VERSION" -x
 cd ../centreon-plugins
 # We should use "$GIT_BRANCH" instead of 2.7.x. However nothing seems to work as expected.
-git archive --prefix=plugins/ "2.7.x" | tar -C "../centreon-$VERSION" -x
+git archive --prefix=plugins/ "origin/2.7.x" | tar -C "../centreon-$VERSION" -x
 cd ..
 tar czf "input/centreon-$VERSION.tar.gz" "centreon-$VERSION"
 
