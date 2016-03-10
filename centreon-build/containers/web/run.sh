67d6eb3f8c92a5af03e3a928a7a16de1223bd29d
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-10 17:09:59 +0100

    containers: set httpd as the last daemon to run in mon-web image.

--- centreon-build/containers/web/run.sh
+++ centreon-build/containers/web/run.sh
@@ -4,13 +4,13 @@
 set -x
 
 service mysql start
-service httpd start
 service cbd start
 service centengine start
 service centcore start
+service httpd start
 bash
+service httpd stop
 service centcore stop
 service centengine stop
 service cbd stop
-service httpd stop
 service mysql stop
