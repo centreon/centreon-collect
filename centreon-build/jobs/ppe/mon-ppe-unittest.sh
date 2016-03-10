732f04671fbbcd7fa21e128977ffd2a6c0984441
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-10 16:33:07 +0100

    jobs: install Centreon Export dependencies with composer in mon-ppe-unittest.

--- centreon-build/jobs/ppe/mon-ppe-unittest.sh
+++ centreon-build/jobs/ppe/mon-ppe-unittest.sh
@@ -7,15 +7,14 @@ set -x
 docker pull ci.int.centreon.com:5000/mon-unittest:centos6
 containerid=`docker create ci.int.centreon.com:5000/mon-unittest:centos6 /usr/local/bin/unittest-ppe`
 
-# Copy sources to container.
-if [ \! -d centreon-web ] ; then
-  git clone https://github.com/centreon/centreon centreon-web
-fi
-cd centreon-web
-git checkout origin/2.8.x
+# Install project dependencies.
+cd centreon-export
+composer install
+composer update
 cd ..
-docker cp centreon-web "$containerid:/usr/local/src/centreon-web"
-docker cp centreon-export/www/modules/centreon-export "$containerid:/usr/local/src/centreon-web/www/modules/centreon-export"
+
+# Copy sources to container.
+docker cp centreon-export "$containerid:/usr/local/src/centreon-export"
 
 # Run unit tests.
 docker start -a "$containerid"
