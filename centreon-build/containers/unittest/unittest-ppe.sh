05eee6ab64f474d2e539fb2f313efc87ea89f651
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-10 16:48:33 +0100

    containers: fix path of bootstrap file in PPE test script of mon-unittest.

--- centreon-build/containers/unittest/unittest-ppe.sh
+++ centreon-build/containers/unittest/unittest-ppe.sh
@@ -6,4 +6,4 @@ set -x
 # Run unit tests.
 rm -f /tmp/centreon-export.xml
 cd /usr/local/src/centreon-export
-phpunit --bootstrap tests/bootstrap.php --log-junit /tmp/centreon-export.xml tests
+phpunit --bootstrap www/modules/centreon-export/tests/bootstrap.php --log-junit /tmp/centreon-export.xml tests
