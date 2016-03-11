f214d5582ef6208d2c2defaf73d26ca48eed238c
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-11 16:27:35 +0100

    containers: provide Clover coverage file in PPE unit test script of mon-unittest.

--- containers/unittest/unittest-ppe.sh
+++ containers/unittest/unittest-ppe.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 # Run unit tests.
-rm -f /tmp/centreon-export.xml
+rm -f /tmp/centreon-ppe_ut.xml
+rm -f /tmp/centreon-ppe_coverage.xml
 cd /usr/local/src/centreon-export/www/modules/centreon-export/tests
-phpunit --bootstrap bootstrap.php --log-junit /tmp/centreon-export.xml .
+phpunit --bootstrap bootstrap.php --log-junit /tmp/centreon-ppe_ut.xml --coverage-clover /tmp/centreon-ppe_coverage.xml .
