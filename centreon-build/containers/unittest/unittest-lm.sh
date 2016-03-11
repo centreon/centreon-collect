20709dba242f5683ae6e31378fb1a19d8dd033bd
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-11 12:21:32 +0100

    containers: update LM unit test script in mon-unittest image.

--- centreon-build/containers/unittest/unittest-lm.sh
+++ centreon-build/containers/unittest/unittest-lm.sh
@@ -4,8 +4,11 @@ set -e
 set -x
 
 # Run unit tests.
+rm -f /tmp/centreon-license-manager_ut.xml
+rm -f /tmp/centreon-license-manager_coverage.xml
 cd /usr/local/src/centreon-license-manager/
 composer config --global github-oauth.github.com "2cf4c72854f10e4ef54ef5dde7cd41ab474fff71"
 composer install
 vendor/bin/phing unittest
-mv build/phpunit.xml /tmp/centreon-license-manager.xml
+mv build/phpunit.xml /tmp/centreon-license-manager_ut.xml
+mv build/coverage.xml /tmp/centreon-license-manager_coverage.xml
