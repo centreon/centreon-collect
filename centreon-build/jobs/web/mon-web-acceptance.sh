c64f93a54820f6f2e5a42f8b4b17136f3500667f
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-16 15:49:14 +0100

    jobs: fix script of mon-web-acceptance.

--- centreon-build/jobs/web/mon-web-acceptance.sh
+++ centreon-build/jobs/web/mon-web-acceptance.sh
@@ -21,7 +21,7 @@ fi
 export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-web:centos6
 rm -rf xunit-reports
 cd centreon-web
-alreadyset=`grep ci.int.centreon.com < composer.json`
+alreadyset=`grep ci.int.centreon.com < composer.json || true`
 if [ -z "$alreadyset" ] ; then
   sed -i 's#    "require-dev": {#    "repositories": [\n      { "type": "composer", "url": "http://ci.int.centreon.com" }\n    ],\n    "config": {\n        "secure-http": false\n    },\n    "require-dev": {#g' composer.json
 fi
