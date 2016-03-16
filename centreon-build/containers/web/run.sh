9721c40cb106f38eb6a50551fa1cf05dc469d378
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-16 09:08:32 +0100

    containers: update mon-web container to make it wait until DB server is available.

--- centreon-build/containers/web/run.sh
+++ centreon-build/containers/web/run.sh
@@ -4,6 +4,15 @@
 set -x
 
 service mysql start
+while true ; do
+  retval=`mysql centreon -e 'SELECT * FROM nagios_server' > /dev/null ; echo $?`
+  if [ "$retval" = 0 ] ; then
+    break ;
+  else
+    echo 'DB server is not yet available.'
+    sleep 1
+  fi
+done
 service cbd start
 service centengine start
 service centcore start
