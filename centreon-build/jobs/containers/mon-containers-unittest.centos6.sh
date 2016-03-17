248b1a32eff559729f7cdf27b42425a33f65beb7
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-17 14:55:50 +0100

    jobs: fix mon-containers-unittest jobs.

--- centreon-build/jobs/containers/mon-containers-unittest.centos6.sh
+++ centreon-build/jobs/containers/mon-containers-unittest.centos6.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed s/@CENTOS_VERSION@/6/g < centreon-build/containers/unittest.Dockerfile.in > centreon-build/containers/unittest.centos6.Dockerfile
-docker build -t ci.int.centreon.com:5000/mon-unittest:centos6 -f centreon-build/containers/unittest.centos6.Dockerfile centreon-build/containers
+sed s/@CENTOS_VERSION@/6/g < centreon-build/containers/unittest/unittest.Dockerfile.in > centreon-build/containers/unittest/unittest.centos6.Dockerfile
+docker build -t ci.int.centreon.com:5000/mon-unittest:centos6 -f centreon-build/containers/unittest/unittest.centos6.Dockerfile centreon-build/containers
 docker push ci.int.centreon.com:5000/mon-unittest:centos6
--- centreon-build/jobs/containers/mon-containers-unittest.centos7.sh
+++ centreon-build/jobs/containers/mon-containers-unittest.centos7.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed s/@CENTOS_VERSION@/7/g < centreon-build/containers/unittest.Dockerfile.in > centreon-build/containers/unittest.centos7.Dockerfile
-docker build -t ci.int.centreon.com:5000/mon-unittest:centos7 -f centreon-build/containers/unittest.centos7.Dockerfile centreon-build/containers
+sed s/@CENTOS_VERSION@/7/g < centreon-build/containers/unittest/unittest.Dockerfile.in > centreon-build/containers/unittest/unittest.centos7.Dockerfile
+docker build -t ci.int.centreon.com:5000/mon-unittest:centos7 -f centreon-build/containers/unittest/unittest.centos7.Dockerfile centreon-build/containers
 docker push ci.int.centreon.com:5000/mon-unittest:centos7
