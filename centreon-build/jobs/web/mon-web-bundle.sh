70bc372db72080add5b7329779ae732bb574ea37
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-18 10:03:19 +0100

    containers: provide a template Dockerfile for mon-web images.

--- centreon-build/jobs/web/mon-web-bundle.sh
+++ centreon-build/jobs/web/mon-web-bundle.sh
@@ -13,7 +13,10 @@ CENTOS_VERSION="$1"
 # Pull mon-dependencies image.
 docker pull ci.int.centreon.com:5000/mon-dependencies:centos$CENTOS_VERSION
 
-# CentOS main image.
+# Prepare Dockerfile.
 cd centreon-build/containers
+sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < web/web.Dockerfile.in > web/web.centos$CENTOS_VERSION.Dockerfile
+
+# Build image.
 docker build --no-cache -t ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION -f web/web.centos$CENTOS_VERSION.Dockerfile .
 docker push ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
