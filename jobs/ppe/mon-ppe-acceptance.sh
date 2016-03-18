cc0d996e7c61f5b8b4f0e05fd4bd5351f2e987a4
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-18 15:32:50 +0100

    jobs: update mon-ppe-acceptance.
    Do not run a container directly in acceptance job. And use new
    environment variable CENTREON_PPE_IMAGE.

--- jobs/ppe/mon-ppe-acceptance.sh
+++ jobs/ppe/mon-ppe-acceptance.sh
@@ -13,10 +13,6 @@ CENTOS_VERSION="$1"
 # Pull mon-ppe image.
 docker pull ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
 
-# Start a container.
-containerid=`docker run -d -t -p 80 ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION`
-port=`docker port "$containerid" 80 | cut -d : -f 2`
-
 # Check that phantomjs is running.
 export PHANTOMJS_RUNNING=1
 nc -w 0 localhost 4444 || export PHANTOMJS_RUNNING=0 || true
@@ -25,12 +21,10 @@ if [ "$PHANTOMJS_RUNNING" -ne 1 ] ; then
 fi
 
 # Run acceptance tests.
-export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
+export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
+export CENTREON_PPE_IMAGE=ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
 rm -rf xunit-reports
 cd centreon-export
 composer install
 composer update
 /opt/behat/vendor/bin/behat --strict --format=junit --out="../xunit-reports"
-
-# Stop container.
-docker stop "$containerid"
