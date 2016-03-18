ba46db290fddf5bd6d0acd267357e588df65f91f
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-18 14:55:27 +0100

    jobs: add CentOS version argument to shell jobs of PPE.

--- centreon-build/jobs/ppe/mon-ppe-acceptance.sh
+++ centreon-build/jobs/ppe/mon-ppe-acceptance.sh
@@ -3,11 +3,18 @@
 set -e
 set -x
 
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <6|7>"
+  exit 1
+fi
+CENTOS_VERSION="$1"
+
 # Pull mon-ppe image.
-docker pull ci.int.centreon.com:5000/mon-ppe:centos6
+docker pull ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
 
 # Start a container.
-containerid=`docker run -d -t -p 80 ci.int.centreon.com:5000/mon-ppe:centos6`
+containerid=`docker run -d -t -p 80 ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION`
 port=`docker port "$containerid" 80 | cut -d : -f 2`
 
 # Check that phantomjs is running.
@@ -18,7 +25,7 @@ if [ "$PHANTOMJS_RUNNING" -ne 1 ] ; then
 fi
 
 # Run acceptance tests.
-export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-ppe:centos6
+export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
 rm -rf xunit-reports
 cd centreon-export
 composer install
--- centreon-build/jobs/ppe/mon-ppe-package.sh
+++ centreon-build/jobs/ppe/mon-ppe-package.sh
@@ -3,11 +3,18 @@
 set -e
 set -x
 
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <6|7>"
+  exit 1
+fi
+CENTOS_VERSION="$1"
+
 # Generate archive of Centreon PPE.
 cd centreon-export
-git archive --prefix="centreon-export/" "$GIT_BRANCH" | gzip > "../centreon-export.tar.gz"
+git archive --prefix="centreon-export/" "$GIT_BRANCH" | gzip > "../centreon-export.centos$CENTOS_VERSION.tar.gz"
 cd ..
 
 # Copy files to server.
-FILES="centreon-export.tar.gz"
+FILES="centreon-export.cento$CENTOS_VERSION.tar.gz"
 scp -o StrictHostKeyChecking=no $FILES "root@srvi-ces-repository.merethis.net:/tmp/"
