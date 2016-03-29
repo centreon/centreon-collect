b04d313135df2cd9168b9c33c4922dc6fa2b7ece
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-29 17:04:41 +0200

    containers: fix LM install script that used systemctl instead of direct process launch.

--- containers/lm/lm-install-centos7.sh
+++ containers/lm/lm-install-centos7.sh
@@ -3,12 +3,12 @@
 set -e
 set -x
 
-systemctl mysql start
-systemctl httpd start
+service mysql start
+httpd
 cd /usr/share/centreon/www/modules/centreon-license-manager/frontend/app
 npm install
 npm install -g gulp
 gulp
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
-systemctl httpd stop
-systemctl mysql stop
+# Stop httpd. killall ?
+service mysql stop
