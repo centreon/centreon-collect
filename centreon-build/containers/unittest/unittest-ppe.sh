cddfd59a021625c39c16d8114de405e43bc321d6
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-03-29 13:18:52 +0200

    containers: update PPE unit test script that now use phing in mon-unittest images.

--- centreon-build/containers/unittest/unittest-ppe.sh
+++ centreon-build/containers/unittest/unittest-ppe.sh
@@ -6,5 +6,9 @@ set -x
 # Run unit tests.
 rm -f /tmp/centreon-ppe_ut.xml
 rm -f /tmp/centreon-ppe_coverage.xml
-cd /usr/local/src/centreon-export/www/modules/centreon-export/tests
-phpunit --bootstrap bootstrap.php --log-junit /tmp/centreon-ppe_ut.xml --coverage-clover /tmp/centreon-ppe_coverage.xml .
+cd /usr/local/src/centreon-export/
+composer config --global github-oauth.github.com "2cf4c72854f10e4ef54ef5dde7cd41ab474fff71"
+composer install
+vendor/bin/phing unittest
+mv build/phpunit.xml /tmp/centreon-ppe_ut.xml
+mv build/coverage.xml /tmp/centreon-ppe_coverage.xml
