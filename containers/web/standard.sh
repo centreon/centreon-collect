a23f457ccde9435056fd542db66bbfa20226d68c
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-01 17:18:00 +0200

    containers: use proper SQL file when building mon-web images.

--- containers/web/standard.sh
+++ containers/web/standard.sh
@@ -6,7 +6,7 @@ set -x
 service mysql start
 httpd -k start
 # Temporary fix to provide default setup while we're working on free plugin packs.
-mysql centreon < /tmp/default.sql
+mysql centreon < /tmp/standard.sql
 centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
 centreon -d -u admin -p centreon -a CFGMOVE -v 1
 httpd -k stop
