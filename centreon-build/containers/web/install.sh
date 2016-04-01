7ea6ecbdc60a8f794a384f368a5c59acdac58ff2
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-01 14:38:24 +0200

    containers: install default monitoring objects manually in mon-web images.

--- centreon-build/containers/web/install.sh
+++ centreon-build/containers/web/install.sh
@@ -13,6 +13,8 @@ cat ../../../../autoinstall.php createDbUser.php | php
 cat ../../../../autoinstall.php insertBaseConf.php | php
 cat ../../../../autoinstall.php configFileSetup.php | php
 rm -rf /usr/share/centreon/www/install
+# Temporary fix to provide default setup while we're working on free plugin packs.
+mysql centreon < /tmp/default.sql
 centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
 centreon -d -u admin -p centreon -a CFGMOVE -v 1
 httpd -k stop
