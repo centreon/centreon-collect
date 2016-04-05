879f7af2cf9791128b624c5f6b940fa5e67fbb66
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-05 14:44:55 +0200

    containers: add middleware unit test script to mon-unittest images.

---
+++ containers/unittest/unittest-middleware.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Run unit tests.
+rm -f /tmp/centreon-middleware_ut.xml
+rm -f /tmp/centreon-middleware_coverage.xml
+cd /usr/local/src/centreon-imp-portal-api/
+gulp test:xunit
+
+# Copy reports.
+mv xunit.xml /tmp/centreon-middleware_ut.xml
+mv build/reports/coverage.xml /tmp/centreon-middleware_coverage.xml
