9a3ecb15ab345188554dc5608b6b044ad1b2be75
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-07 09:27:51 +0200

    containers: install OpenLDAP in mon-middleware images.

--- centreon-build/containers/middleware/run.sh
+++ centreon-build/containers/middleware/run.sh
@@ -4,8 +4,11 @@ set -e
 set -x
 
 service mysql start
+service slapd start
+ldapadd -f /tmp/ldap.ldif -D 'cn=Manager,dc=centreon,dc=com' -w centreon || true
 /etc/init.d/redis_6379 start
 cd /usr/local/src/centreon-imp-portal-api
 gulp serve
 /etc/init.d/redis_6379 stop
+service slapd stop
 service mysql stop
