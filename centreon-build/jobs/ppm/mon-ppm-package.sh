41d9ba0f4255ac2c4afcf04bcceee3ee55ea06cd
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-07 18:23:05 +0200

    jobs: add PPM packaging job.

---
+++ centreon-build/jobs/ppm/mon-ppm-package.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <6|7>"
+  exit 1
+fi
+CENTOS_VERSION="$1"
+
+# Generate archive of Centreon PPM.
+cd centreon-import
+git archive --prefix="centreon-import/" "$GIT_BRANCH" | gzip > "../centreon-import.centos$CENTOS_VERSION.tar.gz"
+cd ..
+
+# Copy files to server.
+FILES="centreon-import.centos$CENTOS_VERSION.tar.gz"
+scp -o StrictHostKeyChecking=no $FILES "root@srvi-ces-repository.merethis.net:/tmp/"
