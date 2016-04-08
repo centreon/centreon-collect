a1c90d3bb834f1c4a93d5cfb126d21aa72fe1eb0
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-08 15:47:12 +0200

    containers: disable configuration tweaking of slapd daemon in mon-middleware images.

--- containers/middleware/run.sh
+++ containers/middleware/run.sh
@@ -5,11 +5,11 @@ set -x
 
 echo '10.30.2.27 support.centreon.com' >> /etc/hosts
 service mysql start
-service slapd start
+screen -d -m slapd -u ldap -h ldap:// -F /etc/openldap/slapd.d/
 ldapadd -f /tmp/ldap.ldif -D 'cn=Manager,dc=centreon,dc=com' -w centreon || true
 /etc/init.d/redis_6379 start
 cd /usr/local/src/centreon-imp-portal-api
 gulp serve
 /etc/init.d/redis_6379 stop
-service slapd stop
+killall -TERM slapd
 service mysql stop
