88d3d2f2bfc30a1fa142ea16371db42792a5b023
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-08 11:50:45 +0200

    jobs: update mon-ppm-bundle job to match PPM module name (centreon-pp-manager).

--- centreon-build/containers/ppm/install.sh
+++ centreon-build/containers/ppm/install.sh
@@ -5,6 +5,6 @@ set -x
 
 service mysql start
 httpd -k start
-/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-import
+/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
 httpd -k stop
 service mysql stop
