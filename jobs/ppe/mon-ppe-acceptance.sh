08f0fc22ae7fb1fbb5f054544532f33d4365269e
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-13 11:58:16 +0200

    jobs: update mon-ppe-acceptance job to use the new Behat ContainerExtension.

--- jobs/ppe/mon-ppe-acceptance.sh
+++ jobs/ppe/mon-ppe-acceptance.sh
@@ -5,13 +5,18 @@ set -x
 
 # Check arguments.
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
 
-# Pull mon-ppe image.
-docker pull ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
+# Pull images.
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
+PPE_IMAGE=ci.int.centreon.com:5000/mon-ppe:$DISTRIB
+PPE1_IMAGE=ci.int.centreon.com:5000/mon-ppe1:$DISTRIB
+docker pull $WEB_IMAGE
+docker pull $PPE_IMAGE
+docker pull $PPE1_IMAGE
 
 # Check that phantomjs is running.
 export PHANTOMJS_RUNNING=1
@@ -21,12 +26,13 @@ if [ "$PHANTOMJS_RUNNING" -ne 1 ] ; then
 fi
 
 # Run acceptance tests.
-export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
-export CENTREON_PPE_IMAGE=ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
-export CENTREON_PPE1_IMAGE=ci.int.centreon.com:5000/mon-ppe1:centos$CENTOS_VERSION
 rm -rf xunit-reports
 mkdir xunit-reports
 cd centreon-export
 composer install
 composer update
+alreadyset=`grep ci.int.centreon.com < behat.yml || true`
+if [ -z "$alreadyset" ] ; then
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      images:\n        web: '$WEB_IMAGE'\n        ppe: '$PPE_IMAGE'\n        ppe1: '$PPE1_IMAGE'#g' behat.yml
+fi
 ls features/*.feature | parallel /opt/behat/vendor/bin/behat --strict --format=junit --out="../xunit-reports/{/.}" "{}"
