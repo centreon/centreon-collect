74c9a0d64917dcc9178b9049138d14a16fbf152e
afouille <afouille@merethis.net>
2016-04-13 11:11:58 +0200

    Add mon-containers-web-stable.

---
+++ jobs/containers/mon-containers-web-stable.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ "$#" -lt 2 ] ; then
+  echo "USAGE: $0 <6|7>"
+  exit 1
+fi
+CENTOS_VERSION="$1"
+
+# Pull mon-dependencies image.
+docker pull ci.int.centreon.com:5000/mon-dependencies:centos$CENTOS_VERSION
+
+# Prepare Dockerfiles.
+cd centreon-build/containers
+sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < web/stable.Dockerfile.in > web/stable.centos$CENTOS_VERSION.Dockerfile
+
+# Build 'stable' image.
+docker build --no-cache -t ci.int.centreon.com:5000/mon-web-stable:centos$CENTOS_VERSION -f web/stable.centos$CENTOS_VERSION.Dockerfile .
+docker push ci.int.centreon.com:5000/mon-web-stable:centos$CENTOS_VERSION
