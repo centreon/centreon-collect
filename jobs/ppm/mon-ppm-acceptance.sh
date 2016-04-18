c6a0d3a16f3b87e3969e2ff66bedf890b9f23177
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-18 11:23:27 +0200

    jobs: update mon-ppm-acceptance to use Docker Compose.

--- jobs/ppm/mon-ppm-acceptance.sh
+++ jobs/ppm/mon-ppm-acceptance.sh
@@ -12,7 +12,9 @@ DISTRIB="$1"
 
 # Pull images.
 PPM_IMAGE=ci.int.centreon.com:5000/mon-ppm:$DISTRIB
+MIDDLEWARE_IMAGE=ci.int.centreon.com:5000/mon-middleware:$DISTRIB
 docker pull $PPM_IMAGE
+docker pull $MIDDLEWARE_IMAGE
 
 # Check that phantomjs is running.
 export PHANTOMJS_RUNNING=1
@@ -21,15 +23,19 @@ if [ "$PHANTOMJS_RUNNING" -ne 1 ] ; then
   screen -d -m phantomjs --webdriver=4444
 fi
 
-# Run acceptance tests.
-export CENTREON_PPM_IMAGE=ci.int.centreon.com:5000/mon-ppm:centos$CENTOS_VERSION
-rm -rf xunit-reports
-mkdir xunit-reports
+# Prepare Docker compose file.
 cd centreon-import
+sed -e 's#@WEB_IMAGE@#'$PPM_IMAGE'#g' -e 's#@MIDDLEWARE_IMAGE@#'$MIDDLEWARE_IMAGE'#g' < `dirname $0`/../../containers/middleware/docker-compose-web.yml.in > docker-compose-ppm.yml
+
+# Prepare behat.yml.
+alreadyset=`grep docker-compose-ppm.yml < behat.yml || true`
+if [ -z "$alreadyset" ] ; then
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      ppm: docker-compose-ppm.yml#g' behat.yml
+fi
+
+# Run acceptance tests.
+rm -rf ../xunit-reports
+mkdir ../xunit-reports
 composer install
 composer update
-alreadyset=`grep ci.int.centreon.com < behat.yml || true`
-if [ -z "$alreadyset" ] ; then
-  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      images:\n        ppm: '$PPM_IMAGE'#g' behat.yml
-fi
 ls features/*.feature | parallel /opt/behat/vendor/bin/behat --strict --format=junit --out="../xunit-reports/{/.}" "{}"
