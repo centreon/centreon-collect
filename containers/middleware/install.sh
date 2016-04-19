810889b0f11c421a5f6e0412c4c1cd69b5f66350
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-19 13:32:44 +0200

    containers: add some Plugin Packs to mon-middleware images.

--- containers/middleware/install.sh
+++ containers/middleware/install.sh
@@ -6,5 +6,6 @@ set -x
 service mysql start
 mysql -e 'CREATE DATABASE imp'
 mysql imp < /usr/local/src/centreon-imp-portal-api/database/create.sql
+mysql imp < /usr/local/src/imp.sql
 mysql -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'centreon'"
 service mysql stop
