ed9ce2d22465880a2c504a4efc6333ac3696d9d5
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-19 15:48:58 +0200

    jobs: copy License Manager sources when building mon-ppm images.

--- jobs/ppm/mon-ppm-bundle.sh
+++ jobs/ppm/mon-ppm-bundle.sh
@@ -17,6 +17,10 @@ docker pull ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
 scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.merethis.net:/tmp/centreon-import.centos$CENTOS_VERSION.tar.gz" .
 tar xzf centreon-import.centos$CENTOS_VERSION.tar.gz
 
+# Fetch LM sources.
+scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.merethis.net:/tmp/centreon-license-manager.centos$CENTOS_VERSION.tar.gz" .
+tar xzf centreon-license-manager.centos$CENTOS_VERSION.tar.gz
+
 # Prepare Dockerfile.
 cd centreon-build/containers
 sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < ppm/ppm.Dockerfile.in > ppm/ppm.centos$CENTOS_VERSION.Dockerfile
@@ -24,5 +28,6 @@ sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < ppm/ppm.Dockerfile.in > ppm/ppm.cen
 # CentOS PPM image.
 rm -rf centreon-pp-manager
 cp -r ../../centreon-import/www/modules/centreon-pp-manager .
+cp -r ../../centreon-license-manager/www/modules/centreon-license-manager .
 docker build --no-cache -t ci.int.centreon.com:5000/mon-ppm:centos$CENTOS_VERSION -f ppm/ppm.centos$CENTOS_VERSION.Dockerfile .
 docker push ci.int.centreon.com:5000/mon-ppm:centos$CENTOS_VERSION
