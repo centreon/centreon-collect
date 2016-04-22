45b126280dd5465db31a8f5aaf5e79684f2315b2
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-22 14:32:15 +0200

    jobs: update mon-lm-acceptance to use middleware images.

--- jobs/lm/mon-lm-acceptance.sh
+++ jobs/lm/mon-lm-acceptance.sh
@@ -13,12 +13,14 @@ DISTRIB="$1"
 # Pull images.
 WEBDRIVER_IMAGE=ci.int.centreon.com:5000/mon-phantomjs:latest
 LM_IMAGE=ci.int.centreon.com:5000/mon-lm:$DISTRIB
+MIDDLEWARE_IMAGE=ci.int.centreon.com:5000/mon-middleware:$DISTRIB
 docker pull $WEBDRIVER_IMAGE
 docker pull $LM_IMAGE
+docker pull $MIDDLEWARE_IMAGE
 
 # Prepare Docker Compose file.
 cd centreon-license-manager
-sed 's#@WEB_IMAGE@#'$LM_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose.yml.in > docker-compose-lm.yml
+sed -e 's#@WEB_IMAGE@#'$LM_IMAGE'#g' -e 's#@MIDDLEWARE_IMAGE@#'$MIDDLEWARE_IMAGE'#g' < `dirname $0`/../../containers/middleware/docker-compose-web.yml.in > docker-compose-lm.yml
 
 # Prepare behat.yml.
 alreadyset=`grep docker-compose-lm.yml < behat.yml || true`
