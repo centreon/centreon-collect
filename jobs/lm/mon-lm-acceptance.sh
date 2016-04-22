5887ae5541ee5926ff24aded7ac2c9a00fcd843f
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-22 13:44:39 +0200

    jobs: update mon-lm-acceptance job.

--- jobs/lm/mon-lm-acceptance.sh
+++ jobs/lm/mon-lm-acceptance.sh
@@ -5,31 +5,30 @@ set -x
 
 # Check arguments.
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
 
-# Pull mon-lm image.
-docker pull ci.int.centreon.com:5000/mon-lm:centos$CENTOS_VERSION
+# Pull images.
+WEBDRIVER_IMAGE=ci.int.centreon.com:5000/mon-phantomjs:latest
+LM_IMAGE=ci.int.centreon.com:5000/mon-lm:$DISTRIB
+docker pull $WEBDRIVER_IMAGE
+docker pull $LM_IMAGE
 
-# Check that phantomjs is running.
-export PHANTOMJS_RUNNING=1
-nc -w 0 localhost 4444 || export PHANTOMJS_RUNNING=0 || true
-if [ "$PHANTOMJS_RUNNING" -ne 1 ] ; then
-  screen -d -m phantomjs --webdriver=4444
+# Prepare Docker Compose file.
+cd centreon-license-manager
+sed 's#@WEB_IMAGE@#'$LM_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose.yml.in > docker-compose-lm.yml
+
+# Prepare behat.yml.
+alreadyset=`grep docker-compose-lm.yml < behat.yml || true`
+if [ -z "$alreadyset" ] ; then
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      lm: docker-compose-lm.yml#g' behat.yml
 fi
 
 # Run acceptance tests.
-export CENTREON_WEB_IMAGE=ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
-export CENTREON_PPE_IMAGE=ci.int.centreon.com:5000/mon-lm:centos$CENTOS_VERSION
-rm -rf xunit-reports
-mkdir xunit-reports
-cd centreon-license-manager
-alreadyset=`grep ci.int.centreon.com < composer.json || true`
-if [ -z "$alreadyset" ] ; then
-  sed -i 's#    "require-dev": {#    "repositories": [\n      { "type": "composer", "url": "http://ci.int.centreon.com" }\n    ],\n    "config": {\n        "secure-http": false\n    },\n    "require-dev": {#g' composer.json
-fi
+rm -rf ../xunit-reports
+mkdir ../xunit-reports
 composer install
 composer update
 ls features/*.feature | parallel /opt/behat/vendor/bin/behat --strict --format=junit --out="../xunit-reports/{/.}" "{}"
