6b6d95dbc77bdc6b9e1a313875f7a3bc4b275d1a
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-04-25 09:10:25 +0200

    jobs: update mon-lm-bundle job to match changes to mon-lm images.

--- centreon-build/jobs/lm/mon-lm-bundle.sh
+++ centreon-build/jobs/lm/mon-lm-bundle.sh
@@ -5,21 +5,25 @@ set -x
 
 # Check arguments.
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
 
-# Pull monitoring-running image.
-docker pull ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
+# Pull images.
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
+docker pull $WEB_IMAGE
 
 # Fetch LM sources.
-scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com:/tmp/centreon-license-manager.centos$CENTOS_VERSION.tar.gz" .
-tar xzf centreon-license-manager.centos$CENTOS_VERSION.tar.gz
+scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com:/tmp/centreon-license-manager.$DISTRIB.tar.gz" .
+tar xzf centreon-license-manager.$DISTRIB.tar.gz
 
-# CentOS 6 running image.
+# Prepare Dockerfile.
+sed "s/@DISTRIB@/$DISTRIB/g" < lm/lm.Dockerfile.in > lm/lm.$DISTRIB.Dockerfile
+
+# Build image.
 cd centreon-build/containers/
 rm -rf centreon-license-manager
 cp -R ../../centreon-license-manager/www/modules/centreon-license-manager .
-docker build --no-cache -t ci.int.centreon.com:5000/mon-lm:centos$CENTOS_VERSION -f lm/lm.centos$CENTOS_VERSION.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-lm:centos$CENTOS_VERSION
+docker build --no-cache -t ci.int.centreon.com:5000/mon-lm:$DISTRIB -f lm/lm.$DISTRIB.Dockerfile .
+docker push ci.int.centreon.com:5000/mon-lm:$DISTRIB
