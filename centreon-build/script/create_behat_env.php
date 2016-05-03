bc6956ea6469e8b9c5193c08c54445aaea874558
afouille <afouille@merethis.net>
2016-05-03 11:13:27 +0200

    Don't use docker cp, it is for losers (also docker cp is not disponible for old dockers).

--- centreon-build/script/create_behat_env.php
+++ centreon-build/script/create_behat_env.php
@@ -6,6 +6,12 @@ if (!isset($opts["p"]) || !isset($opts["s"]) || !isset($opts["a"])) {
   echo "usage: create_behat_env [-d a_docker_machine] -f[feature_file] -p project_name -a centos6|centos7 -s source_directory\n";
   return (0);
 }
+$centreon_build_directory = realpath('centreon-build');
+$source_directory = realpath($opts['s']);
+$source_directory_name = $opts['s'];
+// Remove ./ as the docker mount dislikes this.
+if (0 === strpos($source_directory_name, './'))
+  $source_directory_name = substr($source_directory_name, strlen('./'));
 
 // Create tmp directory.
 echo "creating tmp directory...\n";
@@ -17,17 +23,12 @@ echo "creating behat docker image...\n";
 copy('./centreon-build/containers/behat/behat.Dockerfile', "$tmp_directory/behat.Dockerfile");
 exec("docker build -t behat -f $tmp_directory/behat.Dockerfile $tmp_directory");
 echo "starting bethat docker image...\n";
-exec("docker run -v /var/run/docker.sock:/var/run/docker.sock -ti -d behat", $output, $return_var);
+exec("docker run -v /var/run/docker.sock:/var/run/docker.sock -v $centreon_build_directory:/tmp/centreon-build -v $source_directory:/tmp/$source_directory_name -ti -d behat", $output, $return_var);
 if ($return_var != 0) {
   echo "couldn't launch behat: " . implode('\n', $output) . "\n";
   return (-1);
 }
 $id = $output[0];
-
-// Copy files
-echo "copying files to docker...\n";
-exec("docker cp ./centreon-build $id:/tmp/");
-exec("docker cp " . $opts['s'] . " $id:/tmp/");
 
 // Launch acceptance script.
 echo "starting acceptance script...\n";
