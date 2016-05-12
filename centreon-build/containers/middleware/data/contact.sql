cd060dc8339662a1d9aef1d4e32b8fc8b6c88079
Kevin Duret <kduret@centreon.com>
2016-05-12 17:38:53 +0200

    remove column from imp data

--- centreon-build/containers/middleware/data/contact.sql
+++ centreon-build/containers/middleware/data/contact.sql
@@ -1,4 +1,4 @@
 INSERT INTO `company` VALUES (1,'Centreon','1','7a272af06ea1885f510ba0ad76bee1bc',1);
-INSERT INTO `subscription` VALUES (1,1,'2016-02-16 00:00:00','2016-05-16 00:00:00','IMP_6_MONTH',1,0,99.99,NULL,'IMP_6_MONTH',NULL,'2016-05-02 10:50:53','9999-12-12 00:00:00');
+INSERT INTO `subscription` VALUES (1,1,'2016-02-16 00:00:00','2016-05-16 00:00:00','IMP_6_MONTH',1,0,99.99,NULL,'IMP_6_MONTH','2016-05-02 10:50:53','9999-12-12 00:00:00');
 INSERT INTO `contact` VALUES (1,'centreon','centreon','centreon@centreon.com','Administrateur','Centreon',1,1,1,'1');
 
