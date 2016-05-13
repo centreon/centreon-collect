bbec307631b8bb34ecb6b7de8131226be73a650b
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-05-13 15:33:35 +0200

    containers: properly install LM module in mon-ppm images.

--- containers/ppm/install.sh
+++ containers/ppm/install.sh
@@ -5,6 +5,10 @@ set -x
 
 service mysql start
 httpd -k start
+cd /usr/share/centreon/www/modules/centreon-license-manager/frontend/app
+npm install
+npm install -g gulp
+gulp
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
 httpd -k stop
