e802308e5f6a5279e3a15eeb93be7078f0b266ed
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-05-16 16:38:09 +0200

    containers: create license directory in mon-web-fresh images.

--- containers/web/fresh.sh
+++ containers/web/fresh.sh
@@ -13,6 +13,7 @@ cat ../../../../autoinstall.php createDbUser.php | php
 cat ../../../../autoinstall.php insertBaseConf.php | php
 cat ../../../../autoinstall.php configFileSetup.php | php
 rm -rf /usr/share/centreon/www/install
+mkdir /etc/centreon/license.d
 centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
 centreon -d -u admin -p centreon -a CFGMOVE -v 1
 httpd -k stop
