77523d642c4151d15cda662d3a89d0e2a63e0c08
Kevin Duret <kduret@centreon.com>
2016-05-18 15:21:41 +0200

    avoid duplicate install of ppm

--- containers/ppm/install.sh
+++ containers/ppm/install.sh
@@ -5,11 +5,6 @@ set -x
 
 service mysql start
 httpd -k start
-cd /usr/share/centreon/www/modules/centreon-license-manager/frontend/app
-npm install
-npm install -g gulp
-gulp
-/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
 httpd -k stop
 service mysql stop
