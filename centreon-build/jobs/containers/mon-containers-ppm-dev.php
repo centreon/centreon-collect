995a3bd4ee8c5ec7695253046b44be76347c3af9
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-05-20 16:27:42 +0200

    jobs: convert mon-ppm-dev creation script to PHP.

---
+++ centreon-build/jobs/containers/mon-containers-ppm-dev.php
@@ -0,0 +1,47 @@
+<?php
+
+// Copy directory recursively
+function xcopy($source, $dest) {
+    exec("cp -r '$source' '$dest'", $output, $return);
+    if ($return == 0)
+        return (true);
+    else
+        return (false);
+}
+
+// Remove directory recursively.
+function xrmdir($dir) {
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir."/".$object))
+                    xrmdir($dir."/".$object);
+                else
+                    unlink($dir."/".$object);
+            }
+        }
+        rmdir($dir);
+    }
+}
+
+# Check arguments.
+$centreon_build_dir = dirname(__FILE__) . '/../..';
+if ($argc <= 1) {
+    echo "USAGE: $0 <centos6|centos7>\n";
+    exit(1);
+}
+$distrib = $argv[1];
+
+# Prepare Dockerfile.
+$content = file_get_contents($centreon_build_dir . '/containers/ppm/ppm-dev.Dockerfile.in');
+$content = str_replace('@DISTRIB@', $distrib, $content);
+$dockerfile = $centreon_build_dir . '/containers/ppm/ppm-dev.' . $distrib . '.Dockerfile';
+file_put_contents($dockerfile, $content);
+
+# Build middleware image.
+xrmdir($centreon_build_dir . '/containers/centreon-pp-manager');
+xcopy('www/modules/centreon-pp-manager', $centreon_build_dir . '/containers/centreon-pp-manager');
+passthru('docker build -t mon-ppm-dev:' . $distrib . ' -f ' . $dockerfile . ' ' . $centreon_build_dir . '/containers');
+
+?>
