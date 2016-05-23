6b288756d04b70861f6088257d6ac9654e7f54aa
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-05-23 09:31:36 +0200

    containers: speed up creation of mon-web-dev images.

--- centreon-build/containers/web/dev.sh
+++ centreon-build/containers/web/dev.sh
@@ -4,11 +4,6 @@ set -e
 set -x
 
 service mysql start
-httpd -k start
-cd /tmp/centreon/
-bash install.sh -i -f /tmp/vars
 php /tmp/update-centreon.php -c /etc/centreon/centreon.conf.php
-centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
-centreon -d -u admin -p centreon -a CFGMOVE -v 1
-httpd -k stop
 service mysql stop
+rm -rf /usr/share/centreon/www/install
