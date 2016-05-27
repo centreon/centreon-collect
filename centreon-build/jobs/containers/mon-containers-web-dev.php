67ab770c112190f16ad255c0e359db5643b04692
afouille <afouille@merethis.net>
2016-05-27 15:59:12 +0200

    Get engine/broker packets in web-dev.

--- centreon-build/jobs/containers/mon-containers-web-dev.php
+++ centreon-build/jobs/containers/mon-containers-web-dev.php
@@ -3,6 +3,7 @@
 // Load Centreon Build library.
 $centreon_build_dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
 require_once($centreon_build_dir . DIRECTORY_SEPARATOR . 'script' . DIRECTORY_SEPARATOR . 'common.php');
+require_once($centreon_build_dir . DIRECTORY_SEPARATOR . 'script' . DIRECTORY_SEPARATOR . 'get_packets.php');
 
 # Check arguments.
 if ($argc <= 1) {
@@ -16,6 +17,9 @@ $content = file_get_contents(xpath($centreon_build_dir . '/containers/web/dev.Do
 $content = str_replace('@DISTRIB@', $distrib, $content);
 $dockerfile = xpath($centreon_build_dir . '/containers/web/dev.' . $distrib . '.Dockerfile');
 file_put_contents($dockerfile, $content);
+
+# Get engine/broker
+getPackets();
 
 # Build web image.
 xrmdir(xpath($centreon_build_dir . '/containers/centreon'));
