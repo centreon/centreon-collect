9a812184c7b75cb346335eb6dc3eba0aaea17b22
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-05-30 12:00:10 +0200

    script: install latest Engine and Broker packages to mon-web-dev.

--- jobs/containers/mon-containers-web-dev.php
+++ jobs/containers/mon-containers-web-dev.php
@@ -3,7 +3,7 @@
 // Load Centreon Build library.
 $centreon_build_dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
 require_once($centreon_build_dir . DIRECTORY_SEPARATOR . 'script' . DIRECTORY_SEPARATOR . 'common.php');
-require_once($centreon_build_dir . DIRECTORY_SEPARATOR . 'script' . DIRECTORY_SEPARATOR . 'get_packets.php');
+require_once($centreon_build_dir . DIRECTORY_SEPARATOR . 'script' . DIRECTORY_SEPARATOR . 'get_packages.php');
 
 # Check arguments.
 if ($argc <= 1) {
@@ -18,8 +18,8 @@ $content = str_replace('@DISTRIB@', $distrib, $content);
 $dockerfile = xpath($centreon_build_dir . '/containers/web/dev.' . $distrib . '.Dockerfile');
 file_put_contents($dockerfile, $content);
 
-# Get engine/broker
-getPackets();
+# Get Engine and Broker packages.
+getPackages($distrib);
 
 # Build web image.
 xrmdir(xpath($centreon_build_dir . '/containers/centreon'));
