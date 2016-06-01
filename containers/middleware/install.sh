f4506c6c14d425011ff1de5589757c19903f6bcd
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-01 17:48:01 +0200

    containers: use new JSON import script in mon-middleware images.

--- containers/middleware/install.sh
+++ containers/middleware/install.sh
@@ -7,6 +7,7 @@ service mysql start
 mysql -e 'DROP DATABASE IF EXISTS imp'
 mysql -e 'CREATE DATABASE imp'
 mysql imp < /usr/local/src/centreon-imp-portal-api/database/create.sql
-php /usr/local/src/json2sql.php /usr/local/src/imp.sql | mysql imp
+mysql imp < /usr/local/src/contact.sql
+php /usr/local/src/json2sql.php
 mysql -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'centreon'"
 service mysql stop
