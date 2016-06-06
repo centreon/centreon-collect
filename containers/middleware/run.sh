2dca62fb9cda7043ba9d303b0ffa700084dacc2a
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-06 14:49:32 +0200

    containers: make mon-middleware image (yes single image) use Ubuntu as base.

--- containers/middleware/run.sh
+++ containers/middleware/run.sh
@@ -6,7 +6,7 @@ set -x
 echo '10.30.2.62 support.centreon.com' >> /etc/hosts
 echo '10.24.11.73 crm.int.centreon.com' >> /etc/hosts
 service mysql start
-screen -d -m slapd -u ldap -h ldap:// -F /etc/openldap/slapd.d/
+screen -d -m slapd -u openldap -h ldap:// -F /etc/ldap/slapd.d/
 set +e
 started=1
 while [ "$started" -ne 0 ] ; do
