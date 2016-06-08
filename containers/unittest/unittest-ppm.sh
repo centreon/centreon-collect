5845c51903eb19f92ecbf555e5e008f5246e6861
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-08 13:36:27 +0200

    containers: make unit test script continue even if a test failed.

--- containers/unittest/unittest-ppm.sh
+++ containers/unittest/unittest-ppm.sh
@@ -9,6 +9,6 @@ rm -f /tmp/centreon-ppm_coverage.xml
 cd /usr/local/src/centreon-import/
 composer config --global github-oauth.github.com "2cf4c72854f10e4ef54ef5dde7cd41ab474fff71"
 composer install
-vendor/bin/phing unittest
+vendor/bin/phing unittest || true
 mv build/phpunit.xml /tmp/centreon-ppm_ut.xml
 mv build/coverage.xml /tmp/centreon-ppm_coverage.xml
