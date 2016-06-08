87a372f09f89a49173d85d55e005898c03d465a8
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-08 13:48:29 +0200

    containers: disable shell error checking in unit test scripts of mon-unittest images.

--- containers/unittest/unittest-lm.sh
+++ containers/unittest/unittest-lm.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -e
 set -x
 
 # Run unit tests.
@@ -9,6 +8,6 @@ rm -f /tmp/centreon-license-manager_coverage.xml
 cd /usr/local/src/centreon-license-manager/
 composer config --global github-oauth.github.com "2cf4c72854f10e4ef54ef5dde7cd41ab474fff71"
 composer install
-vendor/bin/phing unittest || true
+vendor/bin/phing unittest
 mv build/phpunit.xml /tmp/centreon-license-manager_ut.xml
 mv build/coverage.xml /tmp/centreon-license-manager_coverage.xml
