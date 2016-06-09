1cc632021f44dbb493c94d233797b5a9de2c5514
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-09 15:38:12 +0200

    containers: update mon-ppm images to use RPMs.

--- containers/ppm/install.sh
+++ containers/ppm/install.sh
@@ -5,6 +5,7 @@ set -x
 
 service mysql start
 httpd -k start
+/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
 httpd -k stop
 service mysql stop
