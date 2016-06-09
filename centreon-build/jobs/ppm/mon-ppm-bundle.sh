b27ad4748155d2ceed0b1ed5439164d5eea98950
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-09 15:38:29 +0200

    jobs: update mon-ppm-bundle to use RPMs.

--- centreon-build/jobs/ppm/mon-ppm-bundle.sh
+++ centreon-build/jobs/ppm/mon-ppm-bundle.sh
@@ -5,29 +5,19 @@ set -x
 
 # Check arguments.
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
 
-# Pull Centreon Web image.
-docker pull ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
-
-# Fetch PPM sources.
-scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com:/tmp/centreon-import.centos$CENTOS_VERSION.tar.gz" .
-tar xzf centreon-import.centos$CENTOS_VERSION.tar.gz
-
-# Fetch LM sources.
-scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com:/tmp/centreon-license-manager.centos$CENTOS_VERSION.tar.gz" .
-tar xzf centreon-license-manager.centos$CENTOS_VERSION.tar.gz
+# Pull images.
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
+docker pull $WEB_IMAGE
 
 # Prepare Dockerfile.
 cd centreon-build/containers
-sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < ppm/ppm.Dockerfile.in > ppm/ppm.centos$CENTOS_VERSION.Dockerfile
+sed "s/@DISTRIB@/$DISTRIB/g" < ppm/ppm.Dockerfile.in > ppm/ppm.$DISTRIB.Dockerfile
 
-# CentOS PPM image.
-rm -rf centreon-pp-manager
-cp -r ../../centreon-import/www/modules/centreon-pp-manager .
-cp -r ../../centreon-license-manager/www/modules/centreon-license-manager .
-docker build --no-cache -t ci.int.centreon.com:5000/mon-ppm:centos$CENTOS_VERSION -f ppm/ppm.centos$CENTOS_VERSION.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-ppm:centos$CENTOS_VERSION
+# Build image.
+docker build --no-cache -t ci.int.centreon.com:5000/mon-ppm:$DISTRIB -f ppm/ppm.$DISTRIB.Dockerfile .
+docker push ci.int.centreon.com:5000/mon-ppm:$DISTRIB
