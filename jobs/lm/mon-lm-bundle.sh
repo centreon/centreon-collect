068d2c0d31530e11e18f8b8e9bdec52dbb66fa8a
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-09 14:24:06 +0200

    jobs: update mon-lm-bundle to use RPMs of centreon-license-manager.

--- jobs/lm/mon-lm-bundle.sh
+++ jobs/lm/mon-lm-bundle.sh
@@ -14,16 +14,10 @@ DISTRIB="$1"
 WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
 docker pull $WEB_IMAGE
 
-# Fetch LM sources.
-scp -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com:/tmp/centreon-license-manager.$DISTRIB.tar.gz" .
-tar xzf centreon-license-manager.$DISTRIB.tar.gz
-
 # Prepare Dockerfile.
 cd centreon-build/containers/
 sed "s/@DISTRIB@/$DISTRIB/g" < lm/lm.Dockerfile.in > lm/lm.$DISTRIB.Dockerfile
 
 # Build image.
-rm -rf centreon-license-manager
-cp -R ../../centreon-license-manager/www/modules/centreon-license-manager .
 docker build --no-cache -t ci.int.centreon.com:5000/mon-lm:$DISTRIB -f lm/lm.$DISTRIB.Dockerfile .
 docker push ci.int.centreon.com:5000/mon-lm:$DISTRIB
