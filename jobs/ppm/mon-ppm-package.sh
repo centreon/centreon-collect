8fed1137365b4bf2d8f064fccefe3857263261c9
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-09 16:57:24 +0200

    jobs: update mon-ppm-package to build RPMs.

--- jobs/ppm/mon-ppm-package.sh
+++ jobs/ppm/mon-ppm-package.sh
@@ -5,16 +5,61 @@ set -x
 
 # Check arguments.
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
+
+# Create input and output directories.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Get version.
+cd centreon-import
+VERSION=`grep mod_release www/modules/centreon-pp-manager/conf.php | cut -d '"' -f 4`
+export VERSION="$VERSION"
+
+# Get release.
+commit=`git log -1 "$GIT_COMMIT" --pretty=format:%h`
+now=`date +%s`
+export RELEASE="$now.$commit"
 
 # Generate archive of Centreon PPM.
-cd centreon-import
-git archive --prefix="centreon-import/" "$GIT_BRANCH" | gzip > "../centreon-import.centos$CENTOS_VERSION.tar.gz"
+git archive --prefix="centreon-pp-manager-$VERSION/" "$GIT_BRANCH" | gzip > "../centreon-pp-manager-$VERSION.tar.gz"
 cd ..
 
+# Encrypt source archive.
+if [ "$DISTRIB" = "centos6" ] ; then
+  phpversion=53
+elif [ "$DISTRIB" = "centos7" ] ; then
+  phpversion=54
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+curl -F "file=@centreon-pp-manager-$VERSION.tar.gz" -F "version=$phpversion" -F 'modulename=centreon-pp-manager' -F 'needlicense=0' 'http://encode.int.centreon.com/api/' -o "input/centreon-pp-manager-$VERSION-php$phpversion.tar.gz"
+
+# Pull latest build dependencies.
+BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
+docker pull "$BUILD_IMG"
+
+# Build RPMs.
+cp centreon-import/packaging/centreon-pp-manager.spectemplate input
+docker-rpm-builder dir "$BUILD_IMG" input output
+
 # Copy files to server.
-FILES="centreon-import.centos$CENTOS_VERSION.tar.gz"
-scp -o StrictHostKeyChecking=no $FILES "root@srvi-ces-repository.int.centreon.com:/tmp/"
+if [ "$DISTRIB" = 'centos6' ] ; then
+  CES_VERSION='3'
+elif [ "$DISTRIB" = 'centos7' ] ; then
+  CES_VERSION='4'
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/$CES_VERSION/unstable/noarch/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../clean_repositories.sh "root@srvi-ces-repository.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" sh -c "'chmod +x $DESTFILE ; $DESTFILE ; rm $DESTFILE ; createrepo /srv/repos/standard/$CES_VERSION/unstable/noarch/'"
