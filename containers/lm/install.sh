1a57e17dc45899463b23e13eb610db423d510dfe
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-09 14:24:59 +0200

    containers: use RPMs of centreon-license-manager when building mon-lm images.

--- containers/lm/install.sh
+++ containers/lm/install.sh
@@ -3,13 +3,8 @@
 set -e
 set -x
 
-sed -i "s#define('CENTREON_IMP_API_URL', '');#define('CENTREON_IMP_API_URL', 'http://middleware:3000/api');#g" /usr/share/centreon/www/modules/centreon-license-manager/centreon-license-manager.conf.php
 service mysql start
 httpd -k start
-cd /usr/share/centreon/www/modules/centreon-license-manager/frontend/app
-npm install
-npm install -g gulp
-gulp
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
 httpd -k stop
 service mysql stop
