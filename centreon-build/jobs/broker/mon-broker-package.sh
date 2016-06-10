5ab3aba9f90802c07fd65e85cb25cc39af73d2bf
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-10 14:34:41 +0200

    jobs: properly execute updaterepo on remote server.

--- centreon-build/jobs/broker/mon-broker-package.sh
+++ centreon-build/jobs/broker/mon-broker-package.sh
@@ -64,4 +64,4 @@ FILES='output/x86_64/*.rpm'
 scp -o StrictHostKeyChecking=no $FILES "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/$CES_VERSION/unstable/x86_64/RPMS"
 DESTFILE=`ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" mktemp`
 scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "root@srvi-ces-repository.int.centreon.com:$DESTFILE"
-ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" $DESTFILE $CES_VERSION x86_64
+ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" sh $DESTFILE $CES_VERSION x86_64
