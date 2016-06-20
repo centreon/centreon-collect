afc4d20efb1a1248d100c12a914c31c7a7b9cde6
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-20 17:26:22 +0200

    jobs: fix sed usage in mon-middleware-delivery.

--- centreon-build/jobs/middleware/mon-middleware-delivery.sh
+++ centreon-build/jobs/middleware/mon-middleware-delivery.sh
@@ -8,7 +8,7 @@ MIDDLEWARE_IMAGE=ci.int.centreon.com:5000/mon-middleware:latest
 docker pull $MIDDLEWARE_IMAGE
 
 # Generate Docker Compose file.
-sed "s/@MIDDLEWARE_IMAGE@/$MIDDLEWARE_IMAGE/g" < `dirname $0`/../../containers/middleware/docker-compose-standalone.yml.in > /opt/middleware/docker-compose.yml
+sed "s#@MIDDLEWARE_IMAGE@#$MIDDLEWARE_IMAGE#g" < `dirname $0`/../../containers/middleware/docker-compose-standalone.yml.in > /opt/middleware/docker-compose.yml
 
 # Update container.
 docker-compose up -d
