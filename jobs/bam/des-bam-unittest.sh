a60b13af668d59dae4b8ce2355783e1e3b3884ae
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-29 14:38:52 +0200

    jobs: enable style check report in mon-bam-unittest.

--- jobs/bam/des-bam-unittest.sh
+++ jobs/bam/des-bam-unittest.sh
@@ -3,4 +3,27 @@
 set -e
 set -x
 
-# Run Centreon BAM unit tests.
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Launch mon-unittest container.
+UT_IMAGE=ci.int.centreon.com:5000/mon-unittest:$DISTRIB
+docker pull $UT_IMAGE
+containerid=`docker create $UT_IMAGE /usr/local/bin/unittest-bam`
+
+# Copy sources to container.
+docker cp centreon-bam "$containerid:/usr/local/src/centreon-bam"
+
+# Run unit tests.
+docker start -a "$containerid"
+#docker cp "$containerid:/tmp/centreon-bam_ut.xml" centreon-bam_ut.xml
+#docker cp "$containerid:/tmp/centreon-bam_coverage.xml" centreon-bam_coverage.xml
+docker cp "$containerid:/tmp/centreon-bam_codestyle.xml" centreon-bam_codestyle.xml
+
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
