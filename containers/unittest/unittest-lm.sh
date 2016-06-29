26899b0ddbfa6008f34abae3ad27bb7924aebc61
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-06-29 14:31:41 +0200

    containers: update mon-unittest scripts to provide code style reports.

--- containers/unittest/unittest-lm.sh
+++ containers/unittest/unittest-lm.sh
@@ -2,14 +2,23 @@
 
 set -x
 
-# Run unit tests.
+# Remove old reports.
 rm -f /tmp/centreon-license-manager_ut.xml
 rm -f /tmp/centreon-license-manager_coverage.xml
+rm -f /tmp/centreon-license-manager_codestyle.xml
+
+# Install dependencies.
 cd /usr/local/src/centreon-license-manager/
 composer config --global github-oauth.github.com "2cf4c72854f10e4ef54ef5dde7cd41ab474fff71"
 composer install
+
+# Run unit tests.
 vendor/bin/phing unittest
+
+# Check code style.
 vendor/bin/phing codestyle
+
+# Move reports to expected places.
 mv build/phpunit.xml /tmp/centreon-license-manager_ut.xml
 mv build/coverage.xml /tmp/centreon-license-manager_coverage.xml
 mv build/checkstyle.xml /tmp/centreon-license-manager_codestyle.xml
--- containers/unittest/unittest-ppm.sh
+++ containers/unittest/unittest-ppm.sh
@@ -2,12 +2,23 @@
 
 set -x
 
-# Run unit tests.
+# Remove old reports.
 rm -f /tmp/centreon-ppm_ut.xml
 rm -f /tmp/centreon-ppm_coverage.xml
+rm -f /tmp/centreon-ppm_codestyle.xml
+
+# Install dependencies.
 cd /usr/local/src/centreon-import/
 composer config --global github-oauth.github.com "2cf4c72854f10e4ef54ef5dde7cd41ab474fff71"
 composer install
+
+# Run unit tests.
 vendor/bin/phing unittest
+
+# Check code style.
+vendor/bin/phing codestyle
+
+# Move reports to expected places.
 mv build/phpunit.xml /tmp/centreon-ppm_ut.xml
 mv build/coverage.xml /tmp/centreon-ppm_coverage.xml
+mv build/checkstyle.xml /tmp/centreon-ppm_codestyle.xml
