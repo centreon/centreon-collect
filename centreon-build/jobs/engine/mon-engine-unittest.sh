dfeeb59f0e51d6a50daec4b5397e1869e18d15f8
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-07-05 10:22:52 +0200

    jobs: merge the two unit test scripts in a single mon-engine-unittest.sh script.

---
+++ centreon-build/jobs/engine/mon-engine-unittest.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Remove old report files.
+rm -f ut.xml
+
+# Launch mon-unittest container.
+UNITTEST_IMAGE=ci.int.centreon.com:5000/mon-unittest:$DISTRIB
+docker pull $UNITTEST_IMAGE
+containerid=`docker create $UNITTEST_IMAGE /usr/local/bin/unittest-engine`
+
+# Copy sources to container.
+docker cp centreon-engine "$containerid:/usr/local/src/centreon-engine"
+
+# Run unit tests.
+docker start -a "$containerid"
+docker cp "$containerid:/tmp/ut.xml" ut.xml
+
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
