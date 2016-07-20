7576b6a3089ecf9113c0356be0fa177bce438409
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-07-20 08:57:44 +0200

    jobs: update packaging jobs to push in a /dev/ directory instead of a specific version.

--- jobs/web/mon-web-package.sh
+++ jobs/web/mon-web-package.sh
@@ -98,9 +98,9 @@ docker-rpm-builder dir ci.int.centreon.com:5000/mon-build-dependencies:centos$CE
 
 # Copy files to server.
 if [ "$CENTOS_VERSION" = 6 ] ; then
-  REPO='standard/3/el6/unstable/noarch'
+  REPO='standard/dev/el6/unstable/noarch'
 else
-  REPO='standard/3/el7/unstable/noarch'
+  REPO='standard/dev/el7/unstable/noarch'
 fi
 FILES='output/noarch/*.rpm'
 scp -o StrictHostKeyChecking=no $FILES "root@srvi-ces-repository.int.centreon.com:/srv/repos/$REPO/RPMS"
