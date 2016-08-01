f39e2dc302f0360c2e72b386d07330863b4a05cb
Kevin Duret <kduret@centreon.com>
2016-08-01 14:45:32 +0200

    temporary commit to fix clapi config generation

--- containers/web/dev.sh
+++ containers/web/dev.sh
@@ -7,3 +7,9 @@ service mysql start
 php /tmp/update-centreon.php -c /etc/centreon/centreon.conf.php
 service mysql stop
 rm -rf /usr/share/centreon/www/install
+
+# Temporary fix due tu clapi bug : waiting new centreon stable version
+rm -rf /etc/centreon-engine/*
+rm -rf /etc/centreon-broker/poller*
+rm -rf /etc/centreon-broker/central*
+
