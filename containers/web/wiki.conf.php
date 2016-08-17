b45f394a6aac8759d91c7b2285e38fe8846dd75d
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-08-17 12:23:33 +0200

    containers: add valid wiki config file in mon-web images.

---
+++ containers/web/wiki.conf.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*
+	 * MySQL Database Connexion
+	 */
+	$db_name = "wikidb";
+	$db_user = "centreon";
+	$db_password = "centreon";
+	$db_host = "mediawiki";
+	$db_prefix = "";
+
+	/*
+	 * Wiki URL without a / at the end
+	 */
+	$WikiURL = "http://mediawiki";
+    $CentreonURL = "http://localhost/centreon";
+
+	$etc_centreon = "/etc/centreon/";
+	$log_centreon = "/var/log/centreon/";
