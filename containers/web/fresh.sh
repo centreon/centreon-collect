7f499905f8ae7a006060f0bae0e2c24d875d633f
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-08-19 18:16:12 +0200

    containers: do not non-existent installUtilsDb.php when building mon-web-fresh.

--- containers/web/fresh.sh
+++ containers/web/fresh.sh
@@ -8,7 +8,6 @@ httpd -k start
 cd /usr/share/centreon/www/install/steps/process
 cat ../../../../autoinstall.php installConfigurationDb.php | php
 cat ../../../../autoinstall.php installStorageDb.php | php
-cat ../../../../autoinstall.php installUtilsDb.php | php
 cat ../../../../autoinstall.php createDbUser.php | php
 cat ../../../../autoinstall.php insertBaseConf.php | php
 cat ../../../../autoinstall.php configFileSetup.php | php
