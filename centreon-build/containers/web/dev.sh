ae1ba66d73caac476b36141bee1b52ab2b1ff213
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-08-31 12:04:47 +0200

    containers: improve cron task support when building mon-web-dev image.

--- centreon-build/containers/web/dev.sh
+++ centreon-build/containers/web/dev.sh
@@ -9,6 +9,9 @@ mysql centreon < /tmp/kb.sql
 service mysql stop
 rm -rf /usr/share/centreon/www/install
 
+# Temporary fix, should be removed when Centreon Web 2.8.0 is released.
+mkdir -p /var/cache/centreon/backup || true
+
 # Temporary fix due tu clapi bug : waiting new centreon stable version
 rm -rf /etc/centreon-engine/*
 rm -rf /etc/centreon-broker/poller*
