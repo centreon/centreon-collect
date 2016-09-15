bf229a4354c0aabab2ed2ad12585d7f5451ce482
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-09-15 14:31:45 +0200

    jobs: store acceptance logs in workspace in all acceptance jobs.

--- jobs/middleware/mon-middleware-acceptance.sh
+++ jobs/middleware/mon-middleware-acceptance.sh
@@ -19,12 +19,16 @@ sed 's#@MIDDLEWARE_IMAGE@#'$MIDDLEWARE_IMAGE'#g' < `dirname $0`/../../containers
 # Prepare behat.yml.
 alreadyset=`grep docker-compose-middleware.yml < behat.yml || true`
 if [ -z "$alreadyset" ] ; then
-  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      middleware: docker-compose-middleware.yml#g' behat.yml
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      middleware: docker-compose-middleware.yml#g' behat.yml
 fi
 
 # Run acceptance tests.
 rm -rf ../xunit-reports
 mkdir ../xunit-reports
+rm -rf ../acceptance-logs-wip
+mkdir ../acceptance-logs-wip
 composer install
 composer update
 ls features/*.feature | parallel -j 1 /opt/behat/vendor/bin/behat --strict --format=junit --out="../xunit-reports/{/.}" "{}"
+rm -rf ../acceptance-logs
+mv ../acceptance-logs-wip ../acceptance-logs
