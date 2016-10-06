796508d244d2fa1160467bfffb26ad1b4707beab
Kevin Duret <kduret@centreon.com>
2016-10-06 16:03:02 +0200

    add acceptance test to ci

---
+++ jobs/automation/mon-automation-acceptance.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Pull images.
+WEBDRIVER_IMAGE=ci.int.centreon.com:5000/mon-phantomjs:latest
+PPM_IMAGE=ci.int.centreon.com:5000/mon-automation:$DISTRIB
+docker pull $WEBDRIVER_IMAGE
+docker pull $PPM_IMAGE
+
+# Prepare Docker compose file.
+cd centreon-automation
+sed -e 's#@WEB_IMAGE@#'$PPM_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose.yml.in > docker-compose-automation.yml
+
+# Prepare behat.yml.
+alreadyset=`grep docker-compose-automation.yml < behat.yml || true`
+if [ -z "$alreadyset" ] ; then
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      automation: docker-compose-automation.yml#g' behat.yml
+fi
+
+# Run acceptance tests.
+rm -rf ../xunit-reports
+mkdir ../xunit-reports
+rm -rf ../acceptance-logs-wip
+mkdir ../acceptance-logs-wip
+composer install
+composer update
+ls features/*.feature | parallel /opt/behat/vendor/bin/behat --strict --format=junit --out="../xunit-reports/{/.}" "{}"
+rm -rf ../acceptance-logs
+mv ../acceptance-logs-wip ../acceptance-logs
