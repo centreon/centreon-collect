d91ff50ef6070a8cc4debf3c6938356e88c390da
Laurent Pinsivy <lpinsivy@centreon.com>
2016-10-13 10:19:58 +0100

    Add jobs to generate Centreon Plugins RPMS

---
+++ jobs/plugins/centreon/common/lock.pm
@@ -0,0 +1,149 @@
+
+package centreon::common::lock;
+
+use strict;
+use warnings;
+
+sub new {
+    my ($class, $name, %options) = @_;
+    my %defaults = (name => $name, pid => $$, timeout => 10);
+    my $self = {%defaults, %options};
+
+    bless $self, $class;
+    return $self;
+}
+
+sub is_set {
+    die "Not implemented";
+}
+
+sub set {
+    my $self = shift;
+
+    for (my $i = 0; $i < $self->{timeout}; $i++) {
+        return if (!$self->is_set());
+        sleep 1;
+    }
+
+    die "Failed to set lock for $self->{name}";
+}
+
+package centreon::common::lock::file;
+
+use base qw(centreon::common::lock);
+
+sub new {
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+
+    if (!defined $self->{storagedir}) {
+        die "Can't build lock, required arguments not provided";
+    }
+    bless $self, $class;
+    $self->{pidfile} = "$self->{storagedir}/$self->{name}.lock";
+    return $self;
+}
+
+sub is_set {
+    return -e shift->{pidfile};
+}
+
+sub set {
+    my $self = shift;
+
+    $self->SUPER::set();
+    open LOCK, ">", $self->{pidfile};
+    print LOCK $self->{pid};
+    close LOCK;
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    if (defined $self->{pidfile} && -e $self->{pidfile}) {
+        unlink $self->{pidfile};
+    }
+}
+
+package centreon::common::lock::sql;
+
+use base qw(centreon::common::lock);
+
+sub new {
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+
+    if (!defined $self->{dbc}) {
+        die "Can't build lock, required arguments not provided";
+    }
+    bless $self, $class;
+    $self->{launch_time} = time();
+    return $self;
+}
+
+sub is_set {
+    my $self = shift;
+    my ($status, $sth) = $self->{dbc}->query(
+        "SELECT id,running,pid,time_launch FROM cron_operation WHERE name LIKE '$self->{name}'"
+    );
+    
+    return 1 if ($status == -1);
+    my $data = $sth->fetchrow_hashref();
+
+    if (!defined $data->{id}) {
+        $self->{not_created_yet} = 1;
+        $self->{previous_launch_time} = 0;
+        return 0;
+    }
+    $self->{id} = $data->{id};
+	my $pid = defined($data->{pid}) ? $data->{pid} : -1;
+    $self->{previous_launch_time} = $data->{time_launch};
+    if (defined $data->{running} && $data->{running} == 1) {
+        my $line = `ps -ef | grep -v grep | grep -- $pid | grep $self->{name}`;
+        return 0 if !length $line;
+        return 1;
+    }
+    return 0;
+}
+
+sub set {
+    my $self = shift;
+    my $status;
+
+    $self->SUPER::set();
+    if (defined $self->{not_created_yet}) {
+        $status = $self->{dbc}->do(<<"EOQ");
+INSERT INTO cron_operation
+(name, system, activate)
+VALUES ('$self->{name}', '1', '1')
+EOQ
+        goto error if $status == -1;
+        $self->{id} = $self->{dbc}->last_insert_id();
+        return;
+    }
+    $status = $self->{dbc}->do(<<"EOQ");
+UPDATE cron_operation
+SET running = '1', time_launch = '$self->{launch_time}', pid = '$self->{pid}'
+WHERE id = '$self->{id}'
+EOQ
+    goto error if $status == -1;
+    return;
+
+  error:
+    die "Failed to set lock for $self->{name}";
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    if (defined $self->{dbc}) {
+        my $exectime = time() - $self->{launch_time};
+        $self->{dbc}->do(<<"EOQ");
+UPDATE cron_operation
+SET last_execution_time = '$exectime'
+WHERE id = '$self->{id}'
+EOQ
+    }
+}
+
+1;
---
+++ jobs/plugins/centreon/script.pm
@@ -0,0 +1,121 @@
+
+package centreon::script;
+
+use strict;
+use warnings;
+use FindBin;
+use Getopt::Long;
+use Pod::Usage;
+use centreon::common::logger;
+use centreon::common::db;
+use centreon::common::lock;
+
+use vars qw($centreon_config);
+use vars qw($mysql_user $mysql_passwd $mysql_host $mysql_database_oreon $mysql_database_ods $mysql_database_ndo);
+
+$SIG{__DIE__} = sub {
+    return unless defined $^S and $^S == 0; # Ignore errors in eval
+    my $error = shift;
+    print "Error: $error";
+    exit 1;
+};
+
+sub new {
+    my ($class, $name, %options) = @_;
+    my %defaults = 
+      (
+       config_file => "/etc/centreon/conf.pm",
+       log_file => undef,
+       centreon_db_conn => 0,
+       centstorage_db_conn => 0,
+       severity => "info",
+       noconfig => 0,
+       noroot => 0
+      );
+    my $self = {%defaults, %options};
+
+    bless $self, $class;
+    $self->{name} = $name;
+    $self->{logger} = centreon::common::logger->new();
+    $self->{options} = {
+        "config=s" => \$self->{config_file},
+        "logfile=s" => \$self->{log_file},
+        "severity=s" => \$self->{severity},
+        "help|?" => \$self->{help}
+    };
+    return $self;
+}
+
+sub init {
+    my $self = shift;
+
+    if (defined $self->{log_file}) {
+        $self->{logger}->file_mode($self->{log_file});
+    }
+    $self->{logger}->severity($self->{severity});
+
+    if ($self->{noroot} == 1) {
+        # Stop exec if root
+        if ($< == 0) {
+            $self->{logger}->writeLogError("Can't execute script as root.");
+            die("Quit");
+        }
+    }
+    
+    if ($self->{centreon_db_conn}) {
+        $self->{cdb} = centreon::common::db->new
+          (db => $self->{centreon_config}->{centreon_db},
+           host => $self->{centreon_config}->{db_host},
+           user => $self->{centreon_config}->{db_user},
+           password => $self->{centreon_config}->{db_passwd},
+           logger => $self->{logger});
+        $self->{lock} = centreon::common::lock::sql->new($self->{name}, dbc => $self->{cdb});
+        $self->{lock}->set();
+    }
+    if ($self->{centstorage_db_conn}) {
+        $self->{csdb} = centreon::common::db->new
+          (db => $self->{centreon_config}->{centstorage_db},
+           host => $self->{centreon_config}->{db_host},
+           user => $self->{centreon_config}->{db_user},
+           password => $self->{centreon_config}->{db_passwd},
+           logger => $self->{logger});
+    }
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    if (defined $self->{cdb}) {
+        $self->{cdb}->disconnect();
+    }
+    if (defined $self->{csdb}) {
+        $self->{csdb}->disconnect();
+    }
+}
+
+sub add_options {
+    my ($self, %options) = @_;
+
+    $self->{options} = {%{$self->{options}}, %options};
+}
+
+sub parse_options {
+    my $self = shift;
+
+    Getopt::Long::Configure('bundling');
+    die "Command line error" if !GetOptions(%{$self->{options}});
+    pod2usage(-exitval => 1, -input => $FindBin::Bin . "/" . $FindBin::Script) if $self->{help};
+    if ($self->{noconfig} == 0) {
+        require $self->{config_file};
+        $self->{centreon_config} = $centreon_config;
+    }
+}
+
+sub run {
+    my $self = shift;
+
+    $self->parse_options();
+    $self->init();
+}
+
+1;
