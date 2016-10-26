907f56cb7efb727f61cab980661dc7d76324a216
Kevin Duret <kduret@centreon.com>
2016-10-26 14:33:01 +0200

    use module installation script for dev bam docker

--- containers/bam/dev.sh
+++ containers/bam/dev.sh
@@ -6,6 +6,6 @@ set -x
 
 service mysql start
 httpd -k start
-/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-automation -u
+/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-bam-server -u
 httpd -k stop
 service mysql stop
