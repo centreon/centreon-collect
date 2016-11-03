16b9e8583f207f99d8879f3e83d28e774820955c
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-03 08:54:21 +0100

    jobs: add centreon-packs-package job.

---
+++ centreon-build/jobs/packs/centreon-packs-package.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Pull mon-build-dependencies container.
+docker pull ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Get Centreon Web sources.
+if [ \! -d centreon-export ] ; then
+  git clone http://git.int.centreon.com/centreon-export
+fi
+
+# Generate spec file.
+./centreon-export/packaging/spec.sh centreon-packs.zip > input/centreon-packs.spec
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB input output
+
+# Copy files to server.
+if [ "$CENTOS_VERSION" = 6 ] ; then
+  REPO='standard/dev/el6/unstable/noarch'
+else
+  REPO='standard/dev/el7/unstable/noarch'
+fi
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "root@srvi-ces-repository.int.centreon.com:/srv/repos/$REPO/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "root@srvi-ces-repository.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" sh $DESTFILE $REPO
