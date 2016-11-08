2560c8e168d821c39119a16ba82bb78b3bba7fea
Kevin Duret <kduret@centreon.com>
2016-11-08 11:51:01 +0100

    install old plugin packs in ppm1 docker

--- containers/ppm/ppm1-install.sh
+++ containers/ppm/ppm1-install.sh
@@ -6,5 +6,7 @@ set -x
 service mysql start
 httpd -k start
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
+yum --disablerepo='*' --enablerepo='ces-standard-stable*' --enablerepo='plugin-packs*' install -y --nogpgcheck ces-pack-Base-Generic
+yum --disablerepo='*' --enablerepo='ces-standard-stable*' --enablerepo='plugin-packs*' install -y --nogpgcheck ces-pack-*
 httpd -k stop
 service mysql stop
