d8dd2383663749981a83acbc8755b9dcffc0b3e8
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-09 17:11:19 +0100

    containers: update mon-ppm1 image.

--- centreon-build/containers/ppm/ppm1-install.sh
+++ centreon-build/containers/ppm/ppm1-install.sh
@@ -6,7 +6,6 @@ set -x
 service mysql start
 httpd -k start
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
-yum --disablerepo='*' --enablerepo='ces-standard-stable*' --enablerepo='plugin-packs*' install -y --nogpgcheck ces-pack-Base-Generic
-yum --disablerepo='*' --enablerepo='ces-standard-stable*' --enablerepo='plugin-packs*' install -y --nogpgcheck ces-pack-* ces-plugins-*
+yum --disablerepo='ces-standard-unstable*' install -y --nogpgcheck ces-pack-* ces-plugins-*
 httpd -k stop
 service mysql stop
