321a8f7f5f77fae9cb35873d06d55f0c553b126e
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-09 15:01:36 +0100

    containers: install generic-active-host template in mon-automation images.

--- centreon-build/containers/automation/install.sh
+++ centreon-build/containers/automation/install.sh
@@ -8,6 +8,8 @@ httpd -k start
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-automation
+# Temporary fix, until base-generic PP can be installed with Clapi.
+mysql centreon -e "UPDATE host SET host_name='generic-active-host', host_alias='generic-active-host' WHERE host_name='generic-host'"
 centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
 centreon -d -u admin -p centreon -a CFGMOVE -v 1
 httpd -k stop
