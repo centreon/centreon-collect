a6b44d90ac010ec40750e8daf6cbffeff1b29734
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-09 17:13:42 +0100

    jobs: update mon-containers-ppm1.

--- jobs/containers/mon-containers-ppm1.sh
+++ jobs/containers/mon-containers-ppm1.sh
@@ -3,22 +3,17 @@
 set -e
 set -x
 
-# Check arguments.
-if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
-  exit 1
-fi
-CENTOS_VERSION="$1"
+DISTRIB="centos6"
 
 # Pull Centreon dependencies.
-docker pull ci.int.centreon.com:5000/mon-dependencies:centos$CENTOS_VERSION
+docker pull ci.int.centreon.com:5000/mon-dependencies:$DISTRIB
 
 # Prepare Dockerfile.
 cd centreon-build/containers
-sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < ppm/ppm1.Dockerfile.in > ppm/ppm1.centos$CENTOS_VERSION.Dockerfile
+sed "s/@DISTRIB@/$DISTRIB/g" < ppm/ppm1.Dockerfile.in > ppm/ppm1.$DISTRIB.Dockerfile
 
 # CentOS PPM image.
 rm -rf centreon-pp-manager
 cp -r ../../centreon-import/www/modules/centreon-pp-manager .
-docker build --no-cache -t ci.int.centreon.com:5000/mon-ppm1:centos$CENTOS_VERSION -f ppm/ppm1.centos$CENTOS_VERSION.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-ppm1:centos$CENTOS_VERSION
+docker build --no-cache -t ci.int.centreon.com:5000/mon-ppm1:$DISTRIB -f ppm/ppm1.$DISTRIB.Dockerfile .
+docker push ci.int.centreon.com:5000/mon-ppm1:$DISTRIB
