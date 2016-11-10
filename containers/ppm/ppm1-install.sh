169445dc34fd0c424d0f420b2a87af33992457cc
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-10 15:25:39 +0100

    containers: fix installation of ces-pack (that insert in DB) in mon-ppm1.

--- containers/ppm/ppm1-install.sh
+++ containers/ppm/ppm1-install.sh
@@ -6,5 +6,6 @@ set -x
 service mysql start
 httpd -k start
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
+yum install --disablerepo='ces-*-unstable*' -y --nogpgcheck 'ces-pack-*'
 httpd -k stop
 service mysql stop
