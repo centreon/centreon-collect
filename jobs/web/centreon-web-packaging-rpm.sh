72676d246f22e9c27afcfe0a6c043faa1eb0e2d9
Kevin Duret <kduret@centreon.com>
2016-11-14 16:49:20 +0100

    use tag to generate centreon rpm

--- jobs/web/centreon-web-packaging-rpm.sh
+++ jobs/web/centreon-web-packaging-rpm.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 # Check arguments.
-if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$BRANCH" ] ; then
-  echo "You need to specify VERSION, RELEASE and BRANCH environment variables."
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION, RELEASE environment variables."
   exit 1
 fi
 
@@ -21,11 +21,6 @@ mkdir output-centos6
 rm -rf output-centos7
 mkdir output-centos7
 
-# Get Centreon Web sources.
-if [ \! -d centreon-web ] ; then
-  git clone https://github.com/centreon/centreon centreon-web
-fi
-
 # Get Centreon Plugins sources.
 if [ \! -d centreon-plugins ] ; then
   git clone https://github.com/centreon/centreon-plugins.git
@@ -33,7 +28,7 @@ fi
 
 # Create source tarball.
 cd centreon-web
-git checkout --detach "$BRANCH"
+git checkout --detach "tags/$VERSION"
 rm -rf "../centreon-$VERSION"
 mkdir "../centreon-$VERSION"
 git archive HEAD | tar -C "../centreon-$VERSION" -x
@@ -79,7 +74,7 @@ docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5
 # Copy files to server.
 FILES_CENTOS6='output-centos6/noarch/*.rpm'
 FILES_CENTOS7='output-centos7/noarch/*.rpm'
-scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/dev/el6/testing/noarch/RPMS"
-scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/dev/el7/testing/noarch/RPMS"
-ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" createrepo /srv/repos/standard/dev/el6/testing/noarch
-ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" createrepo /srv/repos/standard/dev/el7/testing/noarch
+scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/3.4/el6/stable/noarch/RPMS"
+scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/3.4/el7/stable/noarch/RPMS"
+ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" createrepo /srv/repos/standard/3.4/el6/stable/noarch
+ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" createrepo /srv/repos/standard/3.4/el7/stable/noarch
