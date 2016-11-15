59013b6182f0e7af8a7f5289d5ff18d8b13ffda7
Kevin Duret <kduret@centreon.com>
2016-11-15 10:02:00 +0100

    add generic job to build widgets

---
+++ jobs/widgets/centreon-widget-packaging-rpm.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$NAME" ] ; then
+  echo "You need to specify VERSION, RELEASE, NAME environment variables."
+  exit 1
+fi
+
+# Pull mon-build-dependencies containers.
+docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos6
+docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos7
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output-centos6
+mkdir output-centos6
+rm -rf output-centos7
+mkdir output-centos7
+
+# Create source tarball.
+if [ \! -d $NAME ] ; then
+  git clone https://github.com/Centreon-Widgets/$NAME.git $NAME
+else
+  cd $NAME
+  git pull
+  cd ..
+fi
+cd $NAME
+git checkout --detach "tags/$VERSION"
+rm -rf "../$NAME-$VERSION"
+mkdir "../$NAME-$VERSION"
+git archive HEAD | tar -C "../$NAME-$VERSION" -x
+cd ..
+tar czf "input/$NAME-$VERSION.tar.gz" "$NAME-$VERSION"
+
+# Retrieve spec file.
+if [ \! -d packaging-centreon-web ] ; then
+  git clone http://gitbot:gitbot@git.int.centreon.com/packaging-centreon packaging-centreon-web
+else
+  cd packaging-centreon-web
+  git pull
+  cd ..
+fi
+cp packaging-centreon-web/rpm/widgets/centreon-widget.spectemplate input/
+
+widget_sub_directory= `echo "$NAME" | sed 's/centreon-widget-//'`
+export WIDGETSUBDIR = $widget_sub_directory
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos6 input output-centos6
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos7 input output-centos7
+
+# Copy files to server.
+FILES_CENTOS6='output-centos6/noarch/*.rpm'
+FILES_CENTOS7='output-centos7/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/3.4/el6/unstable/noarch/RPMS"
+scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "root@srvi-ces-repository.int.centreon.com:/srv/repos/standard/3.4/el7/unstable/noarch/RPMS"
+ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" createrepo /srv/repos/standard/3.4/el6/unstable/noarch
+ssh -o StrictHostKeyChecking=no "root@srvi-ces-repository.int.centreon.com" createrepo /srv/repos/standard/3.4/el7/unstable/noarch
