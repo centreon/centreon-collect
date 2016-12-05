4bf49e7c3f69b1d5cdaa5fe0d99340f5fc052cd9
Kevin Duret <kduret@centreon.com>
2016-12-05 15:23:19 +0100

    add squid container for lm and ppm

--- centreon-build/jobs/lm/mon-lm-acceptance.sh
+++ centreon-build/jobs/lm/mon-lm-acceptance.sh
@@ -14,18 +14,21 @@ DISTRIB="$1"
 WEBDRIVER_IMAGE=ci.int.centreon.com:5000/mon-phantomjs:latest
 LM_IMAGE=ci.int.centreon.com:5000/mon-lm:$DISTRIB
 MIDDLEWARE_IMAGE=ci.int.centreon.com:5000/mon-middleware:latest
+SQUID_IMAGE=ci.int.centreon.com:5000/mon-squid:latest
 docker pull $WEBDRIVER_IMAGE
 docker pull $LM_IMAGE
 docker pull $MIDDLEWARE_IMAGE
+docker pull $SQUID_IMAGE
 
 # Prepare Docker Compose file.
 cd centreon-license-manager
 sed -e 's#@WEB_IMAGE@#'$LM_IMAGE'#g' -e 's#@MIDDLEWARE_IMAGE@#'$MIDDLEWARE_IMAGE'#g' < `dirname $0`/../../containers/middleware/docker-compose-web.yml.in > docker-compose-lm.yml
+sed -e 's#@WEB_IMAGE@#'$LM_IMAGE'#g' < `dirname $0`/../../containers/squid/docker-compose.yml.in > docker-compose-ppm-squid.yml
 
 # Prepare behat.yml.
 alreadyset=`grep docker-compose-lm.yml < behat.yml || true`
 if [ -z "$alreadyset" ] ; then
-  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      lm: docker-compose-lm.yml#g' behat.yml
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      lm: docker-compose-lm.yml\n      lm_squid: docker-compose-lm-squid.yml#g' behat.yml
 fi
 
 # Run acceptance tests.
--- centreon-build/jobs/ppm/mon-ppm-acceptance.sh
+++ centreon-build/jobs/ppm/mon-ppm-acceptance.sh
@@ -14,19 +14,22 @@ DISTRIB="$1"
 WEBDRIVER_IMAGE=ci.int.centreon.com:5000/mon-phantomjs:latest
 PPM_IMAGE=ci.int.centreon.com:5000/mon-ppm:$DISTRIB
 PPM1_IMAGE=ci.int.centreon.com:5000/mon-ppm1:$DISTRIB
+SQUID_IMAGE=ci.int.centreon.com:5000/mon-squid:latest
 docker pull $WEBDRIVER_IMAGE
 docker pull $PPM_IMAGE
 docker pull $PPM1_IMAGE
+docker pull $SQUID_IMAGE
 
 # Prepare Docker compose file.
 cd centreon-import
 sed -e 's#@WEB_IMAGE@#'$PPM_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose.yml.in > docker-compose-ppm.yml
 sed -e 's#@WEB_IMAGE@#'$PPM1_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose.yml.in > docker-compose-ppm1.yml
+sed -e 's#@WEB_IMAGE@#'$PPM_IMAGE'#g' < `dirname $0`/../../containers/squid/docker-compose.yml.in > docker-compose-ppm-squid.yml
 
 # Prepare behat.yml.
 alreadyset=`grep docker-compose-ppm.yml < behat.yml || true`
 if [ -z "$alreadyset" ] ; then
-  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      ppm: docker-compose-ppm.yml\n      ppm1: docker-compose-ppm1.yml#g' behat.yml
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      ppm: docker-compose-ppm.yml\n      ppm1: docker-compose-ppm1.yml\n      ppm_squid: docker-compose-ppm-squid.yml#g' behat.yml
 fi
 
 # Filter tags
