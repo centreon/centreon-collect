72338e024a170feaf929e87944b732a3fc6b72d4
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-05 15:59:01 +0100

    jobs: do not use cache when building mon-build-dependencies images.

--- jobs/containers/mon-containers-build-dependencies.centos6.sh
+++ jobs/containers/mon-containers-build-dependencies.centos6.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sed s/@CENTOS_VERSION@/6/g < centreon-build/containers/build-dependencies.Dockerfile.in > centreon-build/containers/build-dependencies.centos6.Dockerfile
-docker build -t ci.int.centreon.com:5000/mon-build-dependencies:centos6 -f centreon-build/containers/build-dependencies.centos6.Dockerfile centreon-build/containers
+docker build --no-cache -t ci.int.centreon.com:5000/mon-build-dependencies:centos6 -f centreon-build/containers/build-dependencies.centos6.Dockerfile centreon-build/containers
 docker push ci.int.centreon.com:5000/mon-build-dependencies:centos6
