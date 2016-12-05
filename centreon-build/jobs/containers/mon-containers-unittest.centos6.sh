4754f9bc1f5a4a238e2d70c86b3ad1758061fb22
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-05 16:36:02 +0100

    jobs: update mon-unittest creation scripts.

--- centreon-build/jobs/containers/mon-containers-unittest.centos6.sh
+++ centreon-build/jobs/containers/mon-containers-unittest.centos6.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sed s/@CENTOS_VERSION@/6/g < centreon-build/containers/unittest/unittest.Dockerfile.in > centreon-build/containers/unittest/unittest.centos6.Dockerfile
-docker build -t ci.int.centreon.com:5000/mon-unittest:centos6 -f centreon-build/containers/unittest/unittest.centos6.Dockerfile centreon-build/containers
+docker pull ci.int.centreon.com:5000/mon-dependencies:centos6
+docker build --no-cache -t ci.int.centreon.com:5000/mon-unittest:centos6 -f centreon-build/containers/unittest/unittest.centos6.Dockerfile centreon-build/containers
 docker push ci.int.centreon.com:5000/mon-unittest:centos6
