f2eae450617e41eb790f4d5e18bce1bf32f532ec
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-06 08:51:52 +0100

    jobs: use packages in mon-ppe-bundle.

--- centreon-build/jobs/ppe/mon-ppe-bundle.sh
+++ centreon-build/jobs/ppe/mon-ppe-bundle.sh
@@ -5,24 +5,18 @@ set -x
 
 # Check arguments.
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
 
 # Pull Centreon Web image.
-docker pull ci.int.centreon.com:5000/mon-web:centos$CENTOS_VERSION
-
-# Fetch PPE sources.
-scp -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com:/srv/sources/centreon-export.centos$CENTOS_VERSION.tar.gz" .
-tar xzf centreon-export.centos$CENTOS_VERSION.tar.gz
+docker pull ci.int.centreon.com:5000/mon-web:$DISTRIB
 
 # Prepare Dockerfile.
 cd centreon-build/containers
-sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < ppe/ppe.Dockerfile.in > ppe/ppe.centos$CENTOS_VERSION.Dockerfile
+sed "s/@DISTRIB@/$DISTRIB/g" < ppe/ppe.Dockerfile.in > ppe/ppe.$DISTRIB.Dockerfile
 
-# CentOS PPE image.
-rm -rf centreon-export
-cp -r ../../centreon-export/www/modules/centreon-export .
-docker build --no-cache -t ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION -f ppe/ppe.centos$CENTOS_VERSION.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-ppe:centos$CENTOS_VERSION
+# Build image.
+docker build --no-cache -t ci.int.centreon.com:5000/mon-ppe:$DISTRIB -f ppe/ppe.$DISTRIB.Dockerfile .
+docker push ci.int.centreon.com:5000/mon-ppe:$DISTRIB
