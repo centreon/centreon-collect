f7883e7ced94557927e135a80ba83c2ed20a643c
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-06 08:49:29 +0100

    jobs: build RPM packages in mon-ppe-package.

--- jobs/ppe/mon-ppe-package.sh
+++ jobs/ppe/mon-ppe-package.sh
@@ -5,16 +5,61 @@ set -x
 
 # Check arguments.
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
 
-# Generate archive of Centreon PPE.
+# Create input and output directories.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Get version.
 cd centreon-export
-git archive --prefix="centreon-export/" "$GIT_BRANCH" | gzip > "../centreon-export.centos$CENTOS_VERSION.tar.gz"
+VERSION=`grep mod_release www/modules/centreon-export/conf.php | cut -d '"' -f 4`
+export VERSION="$VERSION"
+
+# Get release.
+commit=`git log -1 "$GIT_COMMIT" --pretty=format:%h`
+now=`date +%s`
+export RELEASE="$now.$commit"
+
+# Generate archive of Centreon Export.
+git archive --prefix="centreon-export-$VERSION/" "$GIT_BRANCH" | gzip > "../centreon-export-$VERSION.tar.gz"
 cd ..
 
+# Encrypt source archive.
+if [ "$DISTRIB" = "centos6" ] ; then
+  phpversion=53
+elif [ "$DISTRIB" = "centos7" ] ; then
+  phpversion=54
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+curl -F "file=@centreon-export-$VERSION.tar.gz" -F "version=$phpversion" -F 'modulename=centreon-export' -F 'needlicense=0' 'http://encode.int.centreon.com/api/' -o "input/centreon-export-$VERSION-php$phpversion.tar.gz"
+
+# Pull latest build dependencies.
+BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
+docker pull "$BUILD_IMG"
+
+# Build RPMs.
+cp centreon-export/packaging/centreon-export.spectemplate input
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG" input output
+
 # Copy files to server.
-FILES="centreon-export.centos$CENTOS_VERSION.tar.gz"
-scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/sources/"
+if [ "$DISTRIB" = 'centos6' ] ; then
+  REPO='internal/el6/noarch'
+elif [ "$DISTRIB" = 'centos7' ] ; then
+  REPO='internal/el7/noarch'
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
