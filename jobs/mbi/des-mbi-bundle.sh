ae00155089a139061ea96da1e52f44314e539ef6
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-06 17:05:23 +0100

    jobs: add des-mbi-bundle job.

---
+++ jobs/mbi/des-mbi-bundle.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Pull base image.
+DEP_IMAGE=ci.int.centreon.com:5000/mon-dependencies:$DISTRIB
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
+docker pull $DEP_IMAGE
+docker pull $WEB_IMAGE
+
+# Prepare Dockerfiles.
+cd centreon-build/containers
+sed "s/@DISTRIB@/$DISTRIB/g" < mbi/web.Dockerfile.in > mbi/web.$DISTRIB.Dockerfile
+
+# Build image.
+docker build --no-cache -t ci.int.centreon.com:5000/des-mbi-web:$DISTRIB -f mbi/web.$DISTRIB.Dockerfile .
+docker push ci.int.centreon.com:5000/des-mbi-web:$DISTRIB
