c4c8838a7e1236900b988974bc17bca2a1491caf
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-06 14:21:45 +0100

    jobs: add des-mbi-package.

---
+++ jobs/mbi/des-mbi-package.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Create input and output directories.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Get version.
+cd centreon-bi-server
+VERSION=`grep mod_release www/modules/centreon-bi-server/conf.php | cut -d '"' -f 4`
+export VERSION="$VERSION"
+
+# Get release.
+commit=`git log -1 "$GIT_COMMIT" --pretty=format:%h`
+now=`date +%s`
+export RELEASE="$now.$commit"
+
+# Generate archive of Centreon MBI.
+git archive --prefix="centreon-bi-server-$VERSION/" "$GIT_BRANCH" | gzip > "../centreon-bi-server-$VERSION.tar.gz"
+cd ..
+
+# Encrypt source archive.
+if [ "$DISTRIB" = "centos6" ] ; then
+  phpversion=53
+elif [ "$DISTRIB" = "centos7" ] ; then
+  phpversion=54
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+curl -F "file=@centreon-bi-server-$VERSION.tar.gz" -F "version=$phpversion" -F 'modulename=centreon-bi-server' -F 'needlicense=0' 'http://encode.int.centreon.com/api/' -o "input/centreon-bi-server-$VERSION-php$phpversion.tar.gz"
+
+# Pull latest build dependencies.
+BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
+docker pull "$BUILD_IMG"
+
+# Build RPMs.
+cp centreon-bi-server/packaging/centreon-license-manager.spectemplate input
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG" input output
+
+# Copy files to server.
+if [ "$DISTRIB" = 'centos6' ] ; then
+  REPO='internal/el6/noarch'
+elif [ "$DISTRIB" = 'centos7' ] ; then
+  REPO='internal/el7/noarch'
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
