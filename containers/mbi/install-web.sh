a9c578c7dc280cc6c080d296cbb2d380fbb89b70
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-07 08:52:38 +0100

    containers: set MBI configuration in mon-mbi-web.

--- containers/mbi/install-web.sh
+++ containers/mbi/install-web.sh
@@ -11,7 +11,9 @@ httpd -k start
 /tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-bi-server
 
 # Configure Centreon MBI.
-# XXX
+mysql -e "UPDATE mod_bi_options SET opt_value='mbi' WHERE opt_key='cbis.host'" centreon
+mysql -e "UPDATE mod_bi_options SET opt_value='true' WHERE opt_key='etl.host.dedicated'" centreon
+mysql -e "UPDATE mod_bi_options SET opt_value='mbi' WHERE opt_key='reportingDB.ip'" centreon
 
 # Grant privileges on database to Centreon MBI.
 /usr/share/centreon/www/modules/centreon-bi-server/tools/centreonMysqlRights.pl
