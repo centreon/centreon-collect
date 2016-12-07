fc91fe423525e4998b720ca59e0aef3155d3dd36
Kevin Duret <kduret@centreon.com>
2016-12-07 15:04:19 +0100

    add lemonldap docker (sso)

---
+++ jobs/containers/mon-containers-lemonldap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Pull base image.
+docker pull debian:jessie
+
+# Build image.
+cd centreon-build/containers
+docker build -t ci.int.centreon.com:5000/mon-lemonldap:latest -f lemonldap/Dockerfile .
+
+# Push image
+docker push ci.int.centreon.com:5000/mon-lemonldap:latest
