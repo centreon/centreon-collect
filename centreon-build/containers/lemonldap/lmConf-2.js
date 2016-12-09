4fd66b6fa8384275a42f744dfa83f8840973315a
Kevin Duret <kduret@centreon.com>
2016-12-09 13:56:06 +0100

    fix in lemonldap configuration

--- centreon-build/containers/lemonldap/lmConf-2.js
+++ centreon-build/containers/lemonldap/lmConf-2.js
@@ -7,25 +7,23 @@
     "CAS_proxiedServices": {},
     "cfgAuthorIP": "10.40.1.188",
     "vhostOptions": {
-    "centreon.centreon.com": {
-        "vhostAliases": "10.30.2.4"
-    },
-    "manager.centreon.com": {
-        "vhostMaintenance": 0,
+        "centreon.centreon.com": {},
+        "manager.centreon.com": {
+            "vhostMaintenance": 0,
             "vhostHttps": -1
-    }
-},
+        }
+    },
     "exportedVars": {
-    "UA": "HTTP_USER_AGENT"
-},
+        "UA": "HTTP_USER_AGENT"
+    },
     "ldapBase": "dc=centreon,dc=com",
     "oidcServiceMetaDataAuthnContext": {},
     "exportedHeaders": {
-    "centreon.centreon.com": {
-        "Auth-User": "$uid"
+        "centreon.centreon.com": {
+            "Auth-User": "$uid"
+        },
+        "manager.centreon.com": {}
     },
-    "manager.centreon.com": {}
-},
     "cfgDate": 1481278558,
     "ldapExportedVars": {},
     "sessionDataToRemember": {},
@@ -33,9 +31,9 @@
     "facebookExportedVars": {},
     "samlSPMetaDataOptions": null,
     "post": {
-    "centreon.centreon.com": {},
-    "manager.centreon.com": {}
-},
+        "centreon.centreon.com": {},
+        "manager.centreon.com": {}
+    },
     "ldapVersion": 3,
     "casAttributes": {},
     "registerDB": "Demo",
@@ -46,11 +44,11 @@
     "cookieName": "lemonldap",
     "casStorageOptions": {},
     "macros": {
-    "_whatToTrace": "$_auth eq 'SAML' ? \"$_user\\@$_idpConfKey\" : \"$_user\""
-},
+        "_whatToTrace": "$_auth eq 'SAML' ? \"$_user\\@$_idpConfKey\" : \"$_user\""
+    },
     "reloadUrls": {
-    "reload.centreon.com": "http://reload.centreon.com/reload"
-},
+        "reload.centreon.com": "http://reload.centreon.com/reload"
+    },
     "samlIDPMetaDataOptions": null,
     "oidcOPMetaDataJWKS": null,
     "userDB": "LDAP",
@@ -62,8 +60,8 @@
     "oidcStorageOptions": {},
     "grantSessionRules": {},
     "notificationStorageOptions": {
-    "dirName": "/var/lib/lemonldap-ng/notifications"
-},
+        "dirName": "/var/lib/lemonldap-ng/notifications"
+    },
     "portalSkinRules": {},
     "mailUrl": "http://auth.centreon.com/mail.pl",
     "slaveExportedVars": {},
@@ -76,10 +74,10 @@
     "dbiExportedVars": {},
     "ldapPort": 389,
     "globalStorageOptions": {
-    "generateModule": "Lemonldap::NG::Common::Apache::Session::Generate::SHA256",
+        "generateModule": "Lemonldap::NG::Common::Apache::Session::Generate::SHA256",
         "Directory": "/var/lib/lemonldap-ng/sessions",
         "LockDirectory": "/var/lib/lemonldap-ng/sessions/lock"
-},
+    },
     "oidcRPMetaDataOptions": null,
     "whatToTrace": "_whatToTrace",
     "ldapServer": "ldap://openldap",
@@ -87,23 +85,23 @@
     "cfgNum": 5,
     "loginHistoryEnabled": 1,
     "demoExportedVars": {
-    "uid": "uid",
+        "uid": "uid",
         "mail": "mail",
         "cn": "cn"
-},
+    },
     "persistentStorageOptions": {
-    "Directory": "/var/lib/lemonldap-ng/psessions",
+        "Directory": "/var/lib/lemonldap-ng/psessions",
         "LockDirectory": "/var/lib/lemonldap-ng/psessions/lock"
-},
+    },
     "oidcOPMetaDataOptions": null,
     "managerPassword": "centreon",
     "localSessionStorageOptions": {
-    "default_expires_in": 600,
+        "default_expires_in": 600,
         "namespace": "lemonldap-ng-sessions",
         "directory_umask": "007",
         "cache_depth": 3,
         "cache_root": "/tmp"
-},
+    },
     "ldapAuthnLevel": 2,
     "openIdExportedVars": {},
     "authentication": "LDAP",
@@ -112,101 +110,101 @@
     "ldapTimeout": 120,
     "oidcRPMetaDataExportedVars": null,
     "locationRules": {
-    "manager.centreon.com": {
-        "(?#Configuration)^/(manager\\.html|conf/)": "$uid eq \"dwho\"",
-            "(?#Notifications)/notifications": "$uideq \"dwho\" or $uid eq \"rtyler\"",
+        "manager.centreon.com": {
+            "(?#Configuration)^/(manager\\.html|conf/)": "$uid eq \"dwho\"",
+            "(?#Notifications)/notifications": "$uid eq \"dwho\" or $uid eq \"rtyler\"",
             "(?#Sessions)/sessions": "$uid eq \"dwho\" or $uid eq \"rtyler\"",
             "default": "$uid eq \"dwho\""
-    },
-    "centreon.centreon.com": {
-        "default": "accept",
+        },
+        "centreon.centreon.com": {
+            "default": "accept",
             "^/logout": "logout_sso"
-    }
-},
+        }
+    },
     "applicationList": {
-    "1sample": {
-        "test1": {
-            "options": {
-                "logo": "demo.png",
+        "1sample": {
+            "test1": {
+                "options": {
+                    "logo": "demo.png",
                     "name": "Application Test 1",
                     "description": "A simple application displaying authenticated user",
                     "uri": "http://test1.centreon.com/",
                     "display": "auto"
+                },
+                "type": "application"
             },
-            "type": "application"
-        },
-        "type": "category",
+            "type": "category",
             "test2": {
-            "type": "application",
+                "type": "application",
                 "options": {
-                "display": "auto",
+                    "display": "auto",
                     "uri": "http://test2.centreon.com/",
                     "description": "The same simple application displaying authenticated user",
                     "name": "Application Test 2",
                     "logo": "thumbnail.png"
-            }
+                }
+            },
+            "catname": "Sample applications"
         },
-        "catname": "Sample applications"
-    },
-    "2administration": {
-        "notifications": {
-            "type": "application",
+        "2administration": {
+            "notifications": {
+                "type": "application",
                 "options": {
-                "logo": "database.png",
+                    "logo": "database.png",
                     "description": "Explore WebSSO notifications",
                     "name": "Notifications explorer",
                     "uri": "http://manager.centreon.com/notifications.html",
                     "display": "auto"
-            }
-        },
-        "type": "category",
+                }
+            },
+            "type": "category",
             "catname": "Administration",
             "manager": {
-            "type": "application",
+                "type": "application",
                 "options": {
-                "name": "WebSSO Manager",
+                    "name": "WebSSO Manager",
                     "description": "Configure LemonLDAP::NG WebSSO",
                     "uri": "http://manager.centreon.com/manager.html",
                     "display": "auto",
                     "logo": "configure.png"
-            }
-        },
-        "sessions": {
-            "type": "application",
+                }
+            },
+            "sessions": {
+                "type": "application",
                 "options": {
-                "description": "Explore WebSSO sessions",
+                    "description": "Explore WebSSO sessions",
                     "name": "Sessions explorer",
                     "display": "auto",
                     "uri": "http://manager.centreon.com/sessions.html",
                     "logo": "database.png"
+                }
             }
-        }
-    },
-    "3documentation": {
-        "localdoc": {
-            "type": "application",
+        },
+        "3documentation": {
+            "localdoc": {
+                "type": "application",
                 "options": {
-                "display": "on",
+                    "display": "on",
                     "uri": "http://manager.centreon.com/doc/",
                     "name": "Local documentation",
                     "description": "Documentation supplied with LemonLDAP::NG",
                     "logo": "help.png"
-            }
-        },
-        "type": "category",
+                }
+            },
+            "type": "category",
             "officialwebsite": {
-            "options": {
-                "logo": "network.png",
+                "options": {
+                    "logo": "network.png",
                     "uri": "http://lemonldap-ng.org/",
                     "display": "on",
                     "description": "Official LemonLDAP::NG Website",
                     "name": "Offical Website"
+                },
+                "type": "application"
             },
-            "type": "application"
-        },
-        "catname": "Documentation"
-    }
-},
+            "catname": "Documentation"
+        }
+    },
     "samlIDPMetaDataExportedAttributes": null,
     "key": "p7J7d7w\"z=laOo'h",
     "webIDExportedVars": {},
