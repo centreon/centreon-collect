ec3a2a5c2e47f4a43b73859f7e8fa287cd30a2df
Loic Laurent <loic.lau@gmail.com>
2016-12-12 16:49:07 +0100

    set login/pass of mediawiki

--- centreon-build/containers/web/kb.sql
+++ centreon-build/containers/web/kb.sql
@@ -4,11 +4,15 @@ DELETE FROM options where `key` IN (
   'kb_db_user',
   'kb_db_password',
   'kb_db_prefix',
-  'kb_wiki_url');
+  'kb_wiki_url',
+  'kb_wiki_account',
+  'kb_wiki_password');
 INSERT INTO options (`key`, `value`) VALUES
   ('kb_db_name', 'wikidb'),
   ('kb_db_host', 'mediawiki'),
   ('kb_db_user', 'centreon'),
   ('kb_db_password', 'centreon'),
   ('kb_db_prefix', ''),
-  ('kb_wiki_url', 'http://mediawiki');
+  ('kb_wiki_url', 'http://mediawiki'),
+  ('kb_wiki_account', 'WikiSysop'),
+  ('kb_wiki_password', 'centreon');
