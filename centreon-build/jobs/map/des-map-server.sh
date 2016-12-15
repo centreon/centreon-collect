1bdf294e54cc771de72f7286a853ffa753c10bfb
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-12-15 09:59:18 +0100

    jobs: update repository after RPM copy in des-map-server.

--- centreon-build/jobs/map/des-map-server.sh
+++ centreon-build/jobs/map/des-map-server.sh
@@ -6,3 +6,9 @@ set -x
 # This is run once the Maven build terminated.
 scp -o StrictHostKeyChecking=no centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat6/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/internal/el6/noarch/RPMS/
 scp -o StrictHostKeyChecking=no centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/internal/el7/noarch/RPMS/
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE 'internal/el6/noarch'
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE 'internal/el7/noarch'
