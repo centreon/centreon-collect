4923fa61411c5266d103ac29093877904d37e05f
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-01-17 15:17:02 +0100

    jobs: add mon-web-package for pipeline.

---
+++ jobs/web/pipeline/mon-web-package.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <6|7>"
+  exit 1
+fi
+CENTOS_VERSION="$1"
+
+# Pull mon-build-dependencies container.
+docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos$CENTOS_VERSION
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Retrieve spec file.
+if [ \! -d packaging-centreon-web ] ; then
+  git clone http://gitbot:gitbot@git.int.centreon.com/packaging-centreon packaging-centreon-web
+else
+  cd packaging-centreon-web
+  git pull
+  cd ..
+fi
+cp packaging-centreon-web/rpm/centreon.spectemplate input/
+
+# Retrieve additional sources.
+cp packaging-centreon-web/src/* input
+
+# Retrieve sources.
+cd input
+wget "http://srvi-repo.int.centreon.com/sources/internal/centreon-web-$VERSION-$RELEASE/centreon-$VERSION.tar.gz"
+cd ..
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos$CENTOS_VERSION input output
+
+# Copy files to server.
+if [ "$CENTOS_VERSION" = 6 ] ; then
+  REPO='internal/el6/noarch'
+else
+  REPO='internal/el7/noarch'
+fi
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
