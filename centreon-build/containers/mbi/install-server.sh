d591c889e2bd3676f1b2065587ca0de1801c6e8d
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-01-17 09:13:28 +0100

    containers: update install script of des-mbi.

--- centreon-build/containers/mbi/install-server.sh
+++ centreon-build/containers/mbi/install-server.sh
@@ -10,14 +10,16 @@ service mysql start
 mysql -e 'CREATE DATABASE centreon'
 mysql -e 'CREATE DATABASE centreon_storage'
 
-# Grant reporting user on the databases (extracted from install.sh).
-REPORTING_DB_USER='centreon'
-REPORTING_DB_PWD='centreon'
+# Grant privileges to users on the databases (extracted from install.sh).
+REPORTING_DB_USER='centreonbi'
+REPORTING_DB_PWD='centreonbi'
+MONITORING_DB_USER='centreonbi'
+MONITORING_DB_PWD='centreonbi'
 mysql -e "GRANT file ON *.* TO '$REPORTING_DB_USER'@'localhost' IDENTIFIED BY '$REPORTING_DB_PWD'"
 mysql -e "GRANT ALL PRIVILEGES ON centreon.* to '$REPORTING_DB_USER'@'localhost' IDENTIFIED BY '$REPORTING_DB_PWD'" centreon
 mysql -e "GRANT ALL PRIVILEGES ON centreon_storage.* to '$REPORTING_DB_USER'@'localhost' IDENTIFIED BY '$REPORTING_DB_PWD'" centreon_storage
-mysql -e "GRANT SELECT ON centreon.* TO '$MONITORING_DB_USER'@'$MONITORING_DB_IP' IDENTIFIED BY '$MONITORING_DB_PWD'" centreon
-mysql -e "GRANT SELECT ON centreon_storage.* TO '$MONITORING_DB_USER'@'$MONITORING_DB_IP' IDENTIFIED BY '$MONITORING_DB_PWD'" centreon_storage
+mysql -e "GRANT SELECT ON centreon.* TO '$MONITORING_DB_USER'@'%' IDENTIFIED BY '$MONITORING_DB_PWD'" centreon
+mysql -e "GRANT SELECT ON centreon_storage.* TO '$MONITORING_DB_USER'@'%' IDENTIFIED BY '$MONITORING_DB_PWD'" centreon_storage
 
 # Stop services.
 service mysql stop
