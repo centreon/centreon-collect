aabd31153f39a60c775dc50c866c4b89dab06f5a
Kevin Duret <kduret@centreon.com>
2017-02-01 15:44:52 +0100

    add openldap container for acceptance tests

--- centreon-build/jobs/web/mon-web-acceptance.sh
+++ centreon-build/jobs/web/mon-web-acceptance.sh
@@ -15,11 +15,13 @@ WEBDRIVER_IMAGE=ci.int.centreon.com:5000/mon-phantomjs:latest
 WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
 WEB_FRESH_IMAGE=ci.int.centreon.com:5000/mon-web-fresh:$DISTRIB
 MEDIAWIKI_IMAGE=ci.int.centreon.com:5000/mon-mediawiki:latest
+OPENLDAP_IMAGE=ci.int.centreon.com:5000/mon-openldap:latest
 INFLUXDB_IMAGE=influxdb:latest
 docker pull $WEBDRIVER_IMAGE
 docker pull $WEB_IMAGE
 docker pull $WEB_FRESH_IMAGE
 docker pull $MEDIAWIKI_IMAGE
+docker pull $OPENLDAP_IMAGE
 docker pull $INFLUXDB_IMAGE
 
 # Prepare Docker Compose file.
@@ -27,12 +29,13 @@ cd centreon-web
 sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose.yml.in > docker-compose-web.yml
 sed 's#@WEB_IMAGE@#'$WEB_FRESH_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose.yml.in > docker-compose-web-fresh.yml
 sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../../containers/mediawiki/docker-compose.yml.in > docker-compose-web-kb.yml
+sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../../containers/openldap/docker-compose.yml.in > docker-compose-web-openldap.yml
 sed 's#@WEB_IMAGE@#'$WEB_IMAGE'#g' < `dirname $0`/../../containers/web/docker-compose-influxdb.yml.in > docker-compose-web-influxdb.yml
 
 # Prepare Behat.yml
 alreadyset=`grep docker-compose-web.yml < behat.yml || true`
 if [ -z "$alreadyset" ] ; then
-  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      web: docker-compose-web.yml\n      web_fresh: docker-compose-web-fresh.yml\n      web_kb: docker-compose-web-kb.yml\n      web_influxdb: docker-compose-web-influxdb.yml#g' behat.yml
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      web: docker-compose-web.yml\n      web_fresh: docker-compose-web-fresh.yml\n      web_kb: docker-compose-web-kb.yml\n      web_openldap: docker-compose-web-openldap.yml\n      web_influxdb: docker-compose-web-influxdb.yml#g' behat.yml
 fi
 
 # Run acceptance tests.
