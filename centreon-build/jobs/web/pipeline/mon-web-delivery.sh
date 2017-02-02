1863599d5259aa97dfd9b3983735786a808ee2a7
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-02 10:57:17 +0100

    jobs: update mon-web-delivery script.

--- centreon-build/jobs/web/pipeline/mon-web-delivery.sh
+++ centreon-build/jobs/web/pipeline/mon-web-delivery.sh
@@ -11,11 +11,23 @@ fi
 
 # Set Docker images as latest.
 REGISTRY='ci.int.centreon.com:5000'
-docker tag "$REGISTRY/mon-web-fresh-$VERSION-$RELEASE:centos6" "$REGISTRY/mon-web-fresh:centos6"
-docker tag "$REGISTRY/mon-web-fresh-$VERSION-$RELEASE:centos7" "$REGISTRY/mon-web-fresh:centos7"
-docker tag "$REGISTRY/mon-web-$VERSION-$RELEASE:centos6" "$REGISTRY/mon-web:centos6"
-docker tag "$REGISTRY/mon-web-$VERSION-$RELEASE:centos7" "$REGISTRY/mon-web:centos7"
+FRESH_CENTOS6="$REGISTRY/mon-web-fresh-$VERSION-$RELEASE:centos6"
+FRESH_CENTOS7="$REGISTRY/mon-web-fresh-$VERSION-$RELEASE:centos7"
+STANDARD_CENTOS6="$REGISTRY/mon-web-$VERSION-$RELEASE:centos6"
+STANDARD_CENTOS7="$REGISTRY/mon-web-$VERSION-$RELEASE:centos7"
+
+docker pull "$FRESH_CENTOS6"
+docker tag "$FRESH_CENTOS6" "$REGISTRY/mon-web-fresh:centos6"
 docker push "$REGISTRY/mon-web-fresh:centos6"
+
+docker pull "$FRESH_CENTOS7"
+docker tag "$FRESH_CENTOS7" "$REGISTRY/mon-web-fresh:centos7"
 docker push "$REGISTRY/mon-web-fresh:centos7"
+
+docker pull "$STANDARD_CENTOS6"
+docker tag "$STANDARD_CENTOS6" "$REGISTRY/mon-web:centos6"
 docker push "$REGISTRY/mon-web:centos6"
+
+docker pull "$STANDARD_CENTOS7"
+docker tag "$STANDARD_CENTOS7" "$REGISTRY/mon-web:centos7"
 docker push "$REGISTRY/mon-web:centos7"
