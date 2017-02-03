a5df43d16937341a6a9f2b9f727178b118c6caed
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-03 16:15:03 +0100

    containers: install all widgets in mon-web-widgets.

--- containers/web/widgets.sh
+++ containers/web/widgets.sh
@@ -5,7 +5,7 @@ set -x
 
 service mysql start
 httpd -k start
-for w in global-health graph-monitoring hostgroup-monitoring httploader service-monitoring servicegroup-monitoring ; do
+for w in engine-status global-health graph-monitoring grid-map host-monitoring hostgroup-monitoring httploader live-top10-cpu-usage live-top10-memory-usage service-monitoring servicegroup-monitoring tactical-overview ; do
   php /tmp/install-centreon-widget.php -c /etc/centreon/centreon.conf.php -w $w
 done
 httpd -k stop
