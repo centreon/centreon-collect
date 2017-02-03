0fa492617470bf9090bb6afd7b8bb4fef76a1a9d
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-03 10:45:36 +0100

    jobs: remove old source tarball in Centreon Web pipeline jobs.

--- jobs/web/pipeline/mon-web-unittest.sh
+++ jobs/web/pipeline/mon-web-unittest.sh
@@ -15,6 +15,7 @@ fi
 DISTRIB="$1"
 
 # Fetch sources.
+rm -f "centreon-$VERSION.tar.gz"
 wget "http://srvi-repo.int.centreon.com/sources/internal/centreon-web-$VERSION-$RELEASE/centreon-$VERSION.tar.gz"
 tar xzf "centreon-$VERSION.tar.gz"
 
