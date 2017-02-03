9f5de7241012b9fa4924f2598a0dd8d062404f4e
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-03 17:23:50 +0100

    jobs: update pipeline mon-web-bundle to work with chained images.

--- jobs/web/pipeline/mon-web-bundle.sh
+++ jobs/web/pipeline/mon-web-bundle.sh
@@ -9,33 +9,36 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+  echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
-CENTOS_VERSION="$1"
+DISTRIB="$1"
 
-# Pull mon-dependencies image.
-docker pull ci.int.centreon.com:5000/mon-dependencies:centos$CENTOS_VERSION
+# Target images.
+BASE_IMG="ci.int.centreon.com:5000/mon-dependencies:$DISTRIB"
+FRESH_IMG="ci.int.centreon.com:5000/mon-web-fresh-$VERSION-$RELEASE:$DISTRIB"
+STANDARD_IMG="ci.int.centreon.com:5000/mon-web-$VERSION-$RELEASE:$DISTRIB"
+WIDGETS_IMG="ci.int.centreon.com:5000/mon-web-$VERSION-$RELEASE:$DISTRIB"
+
+# Pull base image.
+docker pull "$BASE_IMG"
 
 # Prepare Dockerfiles.
-rm -rf centreon-build
-cp -r /opt/centreon-build .
-cd centreon-build/containers
-sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < web/fresh.Dockerfile.in > web/fresh.centos$CENTOS_VERSION.Dockerfile
-sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < web/standard.Dockerfile.in > web/standard.centos$CENTOS_VERSION.Dockerfile
-sed "s/@CENTOS_VERSION@/$CENTOS_VERSION/g" < web/widgets.Dockerfile.in > web/widgets.centos$CENTOS_VERSION.Dockerfile
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+sed "s#@BASE_IMAGE@#$BASE_IMG#g" < web/fresh.Dockerfile.in > web/fresh.Dockerfile
+sed "s#@BASE_IMAGE@#$FRESH_IMG#g" < web/standard.Dockerfile.in > web/standard.Dockerfile
+sed "s#@BASE_IMAGE@#$STANDARD_IMG#g" < web/widgets.Dockerfile.in > web/widgets.Dockerfile
 
 # Build 'fresh' image.
-FRESH_IMG="ci.int.centreon.com:5000/mon-web-fresh-$VERSION-$RELEASE:centos$CENTOS_VERSION"
-docker build --no-cache --ulimit 'nofile=40000' -t "$FRESH_IMG" -f web/fresh.centos$CENTOS_VERSION.Dockerfile .
+docker build --no-cache --ulimit 'nofile=40000' -t "$FRESH_IMG" -f web/fresh.Dockerfile .
 docker push "$FRESH_IMG"
 
 # Build 'standard' image.
-STANDARD_IMG="ci.int.centreon.com:5000/mon-web-$VERSION-$RELEASE:centos$CENTOS_VERSION"
-docker build --no-cache -t "$STANDARD_IMG" -f web/standard.centos$CENTOS_VERSION.Dockerfile .
+docker build --no-cache -t "$STANDARD_IMG" -f web/standard.Dockerfile .
 docker push "$STANDARD_IMG"
 
 # Build 'widgets' image.
-WIDGETS_IMG="ci.int.centreon.com:5000/mon-web-$VERSION-$RELEASE:centos$CENTOS_VERSION"
-docker build --no-cache -t "$WIDGETS_IMG" -f web/widgets.centos$CENTOS_VERSION.Dockerfile .
+docker build --no-cache -t "$WIDGETS_IMG" -f web/widgets.Dockerfile .
 docker push "$WIDGETS_IMG"
