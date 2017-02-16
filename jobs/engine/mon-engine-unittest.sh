006e50ddd63627d8ef9aaad1d4f64b36b959920f
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-16 09:15:49 +0100

    jobs: update mon-engine-unittest, pipeline style.

--- jobs/engine/mon-engine-unittest.sh
+++ jobs/engine/mon-engine-unittest.sh
@@ -4,6 +4,10 @@ set -e
 set -x
 
 # Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
 if [ "$#" -lt 1 ] ; then
   echo "USAGE: $0 <centos6|centos7>"
   exit 1
@@ -13,13 +17,18 @@ DISTRIB="$1"
 # Remove old report files.
 rm -f ut.xml
 
+# Fetch sources.
+rm -f "centreon-engine-$VERSION.tar.gz"
+wget "http://srvi-repo.int.centreon.com/sources/internal/centreon-engine-$VERSION-$RELEASE/centreon-engine-$VERSION.tar.gz"
+tar xzf "centreon-engine-$VERSION.tar.gz"
+
 # Launch mon-unittest container.
 UNITTEST_IMAGE=ci.int.centreon.com:5000/mon-unittest:$DISTRIB
 docker pull $UNITTEST_IMAGE
 containerid=`docker create $UNITTEST_IMAGE /usr/local/bin/unittest-engine`
 
 # Copy sources to container.
-docker cp centreon-engine "$containerid:/usr/local/src/centreon-engine"
+docker cp "centreon-engine-$VERSION" "$containerid:/usr/local/src/centreon-engine"
 
 # Run unit tests.
 docker start -a "$containerid"
