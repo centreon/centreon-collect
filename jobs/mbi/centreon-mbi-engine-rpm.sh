8b4f98b61f62457305befafff70e38f3b3a0e722
bcournaud <bcournaud@centreon.com>
2017-02-17 09:26:01 +0100

    jobs: update mbi-engine job

--- jobs/mbi/centreon-mbi-engine-rpm.sh
+++ jobs/mbi/centreon-mbi-engine-rpm.sh
@@ -10,7 +10,10 @@ export VERSION="$VERSION"
 PRODUCT_NAME="centreon-bi-engine"
 PRODUCT_NAME_FULL="$PRODUCT_NAME-$VERSION-$RELEASE"
 SPECS_NAME="$PRODUCT_NAME_FULL.spec"
-ARCHIVE_NAME="$PRODUCT_NAME_FULL.tar.gz"
+
+# Get release
+cd $WORKSPACE/centreon-bi-engine
+ARCHIVE_NAME="$PRODUCT_NAME_FULL-$RELEASE.tar.gz"
 
 # Clean workspace
 rm -rf $WORKSPACE/build/
@@ -18,29 +21,13 @@ rm -rf $WORKSPACE/build/
 # Create build folders
 mkdir -p $WORKSPACE/build/bin/external_lib/
 
-# Build BIRT library
-BIRT_ZIP_NAME="birt-runtime-osgi-4_4_2-20150217"
-BIRT_NAME="birt-runtime-osgi-4_4_2"
-
-# Extract BIRT report engine
-rm -rf $BIRT_NAME
-wget http://srvi-repo.int.centreon.com/sources/mbi/stable/$BIRT_ZIP_NAME.zip
-unzip $BIRT_ZIP_NAME.zip
-cp -R $BIRT_NAME/ReportEngine/ $WORKSPACE/build/
-
 cd $WORKSPACE
 
 # Copy maven built files
 cp -R $WORKSPACE/centreon-bi-engine/com.merethis.bi.cbis/deploy/* $WORKSPACE/build/
 cp -R $WORKSPACE/centreon-bi-engine/com.merethis.bi.cbis/com.merethis.bi.cbis.engine/target/*.jar $WORKSPACE/build/bin/cbis.jar
-cp -R $WORKSPACE/centreon-bi-engine/com.merethis.bi.cbis/com.merethis.bi.cbis.engine/target/cbis_lib/*.jar $WORKSPACE/build/bin/
-
-# Replace MySQL connector with MariaDB connector
-rm -rf $WORKSPACE/build/bin/cbis_lib/mysql-connector*
-
-# Add MariaDB connector to BIRT installation
-cp $WORKSPACE/build/bin/mariadb-java-client-*.jar $WORKSPACE/build/ReportEngine/plugins/org.eclipse.birt.report.data.oda.jdbc_*/drivers/
-
+mkdir $WORKSPACE/build/bin/cbis_lib/
+cp -R $WORKSPACE/centreon-bi-engine/com.merethis.bi.cbis/com.merethis.bi.cbis.engine/target/cbis_lib/*.jar $WORKSPACE/build/bin/cbis_lib/
 
 # Copy all files into a correct name folder
 mv $WORKSPACE/build $WORKSPACE/$PRODUCT_NAME-$VERSION/
@@ -54,37 +41,38 @@ rm -rf $WORKSPACE/$PRODUCT_NAME-$VERSION
 cp $WORKSPACE/centreon-bi-engine/RPM-SPECS/$PRODUCT_NAME.spec $SPECS_NAME
 
 # Change spec version, release and source numbers
-sed -i -e  "s/^Release:.*/Release: $RELEASE%{?dist}/g" "$SPECS_NAME"
-sed -i -e  "s/^Source0:.*/Source0:%{name}-%{version}.tar.gz/g" "$SPECS_NAME"
+ sed -i -e  "s/^Release:.*/Release: $RELEASE%{?dist}/g" "$SPECS_NAME"
+ sed -i -e  "s/^Source0:.*/Source0:%{name}-%{version}-$RELEASE.tar.gz/g" "$SPECS_NAME"
 
-# Create input and output directories for Docker build
-rm -rf input
-rm -rf output-centos6
-rm -rf output-centos7
-mkdir input
-mkdir output-centos6
-mkdir output-centos7
+ # Create input and output files for Docker to build
+ rm -rf input
+ mkdir input
 
-# Move tarball and spec to build into input/ folder
-mv $WORKSPACE/$ARCHIVE_NAME input/
-mv $SPECS_NAME input/
+ rm -rf output6
+ rm -rf output7
+ 
+ mkdir output6
+ mkdir output7
+
+ # Move tarball and spec to build into input/ folder
+ mv $WORKSPACE/$ARCHIVE_NAME input/
+ mv $SPECS_NAME input/
 
 # Pull latest build dependencies.
-BUILD_IMG_CENTOS_6="ci.int.centreon.com:5000/mon-build-dependencies:centos6"
-BUILD_IMG_CENTOS_7="ci.int.centreon.com:5000/mon-build-dependencies:centos7"
+ BUILD_IMG_CENTOS6="ci.int.centreon.com:5000/mon-build-dependencies:centos6"
+ BUILD_IMG_CENTOS7="ci.int.centreon.com:5000/mon-build-dependencies:centos7"
+ docker pull "$BUILD_IMG_CENTOS6"
+ docker pull "$BUILD_IMG_CENTOS7"
 
-docker pull "$BUILD_IMG_CENTOS_6"
-docker pull "$BUILD_IMG_CENTOS_7"
-
-# Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG_CENTOS_6" input output-centos6
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG_CENTOS_7" input output-centos7
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG6" input output6
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG7" input output7
 
 # Copy files to server.
-FILES_CENTOS6='output-centos6/noarch/*.rpm'
-FILES_CENTOS7='output-centos7/noarch/*.rpm'
+  REPO6='internal/el6/noarch'
+  REPO7='internal/el7/noarch'
 
-scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/mbi/3.4/el6/$REPO/noarch/RPMS"
-scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/mbi/3.4/el7/$REPO/noarch/RPMS"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/mbi/3.4/el6/$REPO/noarch
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/mbi/3.4/el7/$REPO/noarch
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
