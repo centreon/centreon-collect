31b89a66db69dd95c870a5cdae41ab9c8f4840ee
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-23 10:26:14 +0100

    jobs: add mon-poller-display-package.

---
+++ centreon-build/jobs/poller-display/mon-poller-display-package.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Project.
+PROJECT=centreon-poller-display
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Pull mon-build-dependencies container.
+docker pull ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Retrieve spec file.
+cd input
+cp `dirname $0`/../../packaging/poller-display/centreon-poller-display.spectemplate .
+
+# Retrieve sources.
+wget "http://srvi-repo.int.centreon.com/sources/internal/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+cd ..
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB input output
+
+# Copy files to server.
+if [ "$DISTRIB" = "centos6" ] ; then
+  REPO='internal/el6/noarch'
+else
+  REPO='internal/el7/noarch'
+fi
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
