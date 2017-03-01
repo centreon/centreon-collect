b2e87f295b64a26647a1dd2ff6f8b26a84849b22
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-01 11:20:33 +0100

    jobs: update centreon-broker-packaging-rpm to build only current stable version.

--- centreon-build/jobs/broker/centreon-broker-packaging-rpm.sh
+++ centreon-build/jobs/broker/centreon-broker-packaging-rpm.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 # Check arguments.
-if [ -z "$COMMIT" -o -z "$REPO" ] ; then
-  echo "You need to specify COMMIT and REPO environment variables."
+if [ -z "$COMMIT" -o -z "$RELEASE" ] ; then
+  echo "You need to specify COMMIT and RELEASE environment variables."
   exit 1
 fi
 
@@ -43,10 +43,7 @@ else
   git pull
   cd ..
 fi
-cd packaging-centreon-broker
-git checkout --detach "origin/$VERSION"
-cd ..
-cp packaging-centreon-broker/rpm/centreon-broker.spec input/
+cp packaging-centreon-broker/rpm/centreon-broker.spectemplate input/
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos6 input output-centos6
@@ -55,7 +52,7 @@ docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5
 # Copy files to server.
 FILES_CENTOS6='output-centos6/x86_64/*.rpm'
 FILES_CENTOS7='output-centos7/x86_64/*.rpm'
-scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el6/$REPO/x86_64/RPMS"
-scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el7/$REPO/x86_64/RPMS"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/standard/3.4/el6/$REPO/x86_64
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/standard/3.4/el7/$REPO/x86_64
+scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el6/testing/x86_64/RPMS"
+scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el7/testing/x86_64/RPMS"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/standard/3.4/el6/testing/x86_64
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/standard/3.4/el7/testing/x86_64
