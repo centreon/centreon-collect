c1613a2a4d7df39e2680672ba8b18ceb4138a5e3
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-03 16:54:15 +0100

    jobs: add testing to stable.

---
+++ centreon-build/jobs/testing-to-stable.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Global parameters.
+SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
+BASE_DIR='/srv/yum'
+
+# Check arguments.
+if [ -z "$PROJECT" -o -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify PROJECT, VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Get RPM list.
+case "$PROJECT" in
+  centreon-web)
+    DIR='/standard/3.4'
+    ARCH='noarch'
+    RPMS="centreon
+          centreon-base-config-centreon-engine
+          centreon-common
+          centreon-lang-fr_FR
+          centreon-installed
+          centreon-perl-libs
+          centreon-plugin-meta
+          centreon-plugins
+          centreon-poller-centreon-engine
+          centreon-trap
+          centreon-web"
+    ;;
+esac
+
+# Move all RPMs to stable.
+for rpm in $RPMS ; do
+  $SSH_REPO mv "$BASE_DIR/$DIR/el6/testing/$ARCH/RPMS/$rpm-$VERSION-$RELEASE.el6.$ARCH.rpm" "$BASE_DIR/$DIR/el6/stable/$ARCH/RPMS"
+  $SSH_REPO mv "$BASE_DIR/$DIR/el7/testing/$ARCH/RPMS/$rpm-$VERSION-$RELEASE.el7.centos.$ARCH.rpm" "$BASE_DIR/$DIR/el7/stable/$ARCH/RPMS"
+done
+
+# Create repo.
+$SSH_REPO createrepo "$BASE_DIR/$DIR/el6/stable/$ARCH"
+$SSH_REPO createrepo "$BASE_DIR/$DIR/el7/stable/$ARCH"
+$SSH_REPO createrepo "$BASE_DIR/$DIR/el6/testing/$ARCH"
+$SSH_REPO createrepo "$BASE_DIR/$DIR/el7/testing/$ARCH"
