ebb3f377db103ddea36927451905913c6c8bd92a
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-06 12:11:37 +0100

    jobs: make mon-poller-display-source more generic.

--- centreon-build/jobs/poller-display/mon-poller-display-source.sh
+++ centreon-build/jobs/poller-display/mon-poller-display-source.sh
@@ -4,16 +4,18 @@ set -e
 set -x
 
 #
-# This script will generate Centreon Poller Display sources from the
-# local clone of Centreon Poller Display repository
-# (centreon-poller-display directory). These sources will then be pushed
-# to the internal repository (srvi-repo) and used in downstream jobs,
-# thanks to the property file generated at the end of the script.
+# This script will generate sources from the local clone of the project
+# repository. These sources will then be pushed to the internal
+# repository (srvi-repo) and used in downstream jobs, thanks to the
+# property file generated at the end of the script.
 #
 
+# Project.
+PROJECT=centreon-poller-display
+
 # Get version.
-cd centreon-poller-display
-VERSION=`grep mod_release www/modules/centreon-poller-display/conf.php | cut -d '"' -f 4`
+cd "$PROJECT"
+VERSION=`grep mod_release www/modules/$PROJECT/conf.php | cut -d '"' -f 4`
 export VERSION="$VERSION"
 
 # Get release.
@@ -25,18 +27,18 @@ export RELEASE="$now.$COMMIT"
 COMMITTER=`git show --format='%cN <%cE>' HEAD | head -n 1`
 
 # Create source tarball.
-git archive --prefix="centreon-poller-display-$VERSION/" HEAD | gzip > "../centreon-poller-display-$VERSION.tar.gz"
+git archive --prefix="$PROJECT-$VERSION/" HEAD | gzip > "../$PROJECT-$VERSION.tar.gz"
 cd ..
 
 # Send it to srvi-repo.
-FILES="centreon-poller-display-$VERSION.tar.gz"
-DEST="/srv/sources/internal/centreon-poller-display-$VERSION-$RELEASE"
+FILES="$PROJECT-$VERSION.tar.gz"
+DEST="/srv/sources/internal/$PROJECT-$VERSION-$RELEASE"
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mkdir -p "$DEST"
 scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:$DEST"
 
 # Generate properties files for downstream jobs.
 cat > source.properties << EOF
-PROJECT=centreon-poller-display
+PROJECT=$PROJECT
 VERSION=$VERSION
 RELEASE=$RELEASE
 COMMIT=$COMMIT
