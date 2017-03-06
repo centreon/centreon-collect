8e925d806747eb2a4401a6bb564af7d6e82ac2ed
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-06 13:25:42 +0100

    jobs: make mon-ppm-bundle pipeline-compatible.

--- jobs/ppm/mon-ppm-bundle.sh
+++ jobs/ppm/mon-ppm-bundle.sh
@@ -4,6 +4,10 @@ set -e
 set -x
 
 # Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
 if [ "$#" -lt 1 ] ; then
   echo "USAGE: $0 <centos6|centos7>"
   exit 1
@@ -15,9 +19,12 @@ WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
 docker pull $WEB_IMAGE
 
 # Prepare Dockerfile.
-cd centreon-build/containers
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
 sed "s/@DISTRIB@/$DISTRIB/g" < ppm/ppm.Dockerfile.in > ppm/ppm.$DISTRIB.Dockerfile
 
 # Build image.
-docker build --no-cache -t ci.int.centreon.com:5000/mon-ppm:$DISTRIB -f ppm/ppm.$DISTRIB.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-ppm:$DISTRIB
+PPM_IMAGE="ci.int.centreon.com:5000/mon-ppm-$VERSION-$RELEASE:$DISTRIB"
+docker build --no-cache -t "$PPM_IMAGE" -f ppm/ppm.$DISTRIB.Dockerfile .
+docker push "$PPM_IMAGE"
