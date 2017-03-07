725a2c003ab20620732a264b07cda32bfd06cef0
Kevin Duret <kduret@centreon.com>
2017-03-07 18:02:41 +0100

    enable sql queries in standard container

---
+++ containers/web/enable_sql_logs.sql
@@ -0,0 +1,3 @@
+SET global log_output = 'FILE';
+SET global general_log_file='/var/lib/mysql/queries.log';
+SET global general_log = 1;
--- containers/web/standard.sh
+++ containers/web/standard.sh
@@ -7,6 +7,7 @@ service mysql start
 httpd -k start
 # Temporary fix to provide default setup while we're working on free plugin packs.
 mysql centreon < /tmp/standard.sql
+mysql centreon < /tmp/enable_sql_logs.sql
 centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
 centreon -d -u admin -p centreon -a CFGMOVE -v 1
 httpd -k stop
