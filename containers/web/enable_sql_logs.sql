aff956de682eb4d349be1358eeb51812cd44ea02
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-08 15:52:59 +0100

    containers: set MySQL server logging with config file instead of session variables.

--- containers/web/enable_sql_logs.sql
+++
@@ -1,5 +0,0 @@
-SET global log_output = 'FILE';
-SET global general_log_file = '/var/lib/mysql/queries.log';
-SET global general_log = 1;
-SET global slow_query_log_file = '/var/lib/mysql/slow_queries.log';
-SET global slow_query_log = 1;
