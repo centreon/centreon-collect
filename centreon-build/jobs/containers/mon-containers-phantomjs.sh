0fe47113598d9a0276193f0959bc7bac302f0809
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-09 09:11:25 +0100

    containers: move phantomjs.Dockerfile to the webdrivers subdirectory.

--- centreon-build/jobs/containers/mon-containers-phantomjs.sh
+++ centreon-build/jobs/containers/mon-containers-phantomjs.sh
@@ -8,7 +8,7 @@ docker pull centos:7
 
 # Build image.
 cd centreon-build/containers
-docker build -t ci.int.centreon.com:5000/mon-phantomjs:latest -f phantomjs.Dockerfile .
+docker build -t ci.int.centreon.com:5000/mon-phantomjs:latest -f webdrivers/phantomjs.Dockerfile .
 
 # Push image.
 docker push ci.int.centreon.com:5000/mon-phantomjs:latest
