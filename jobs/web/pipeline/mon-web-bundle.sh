0188d308c32645498aec43929f362a0a9c670cf8
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-14 10:17:30 +0100

    jobs: update mon-web-bundle to push create -wip images.

--- jobs/web/pipeline/mon-web-bundle.sh
+++ jobs/web/pipeline/mon-web-bundle.sh
@@ -15,10 +15,14 @@ fi
 DISTRIB="$1"
 
 # Target images.
-BASE_IMG="ci.int.centreon.com:5000/mon-dependencies:$DISTRIB"
-FRESH_IMG="ci.int.centreon.com:5000/mon-web-fresh-$VERSION-$RELEASE:$DISTRIB"
-STANDARD_IMG="ci.int.centreon.com:5000/mon-web-$VERSION-$RELEASE:$DISTRIB"
-WIDGETS_IMG="ci.int.centreon.com:5000/mon-web-widgets-$VERSION-$RELEASE:$DISTRIB"
+REGISTRY="ci.int.centreon.com:5000"
+BASE_IMG="$REGISTRY/mon-dependencies:$DISTRIB"
+FRESH_IMG="$REGISTRY/mon-web-fresh-$VERSION-$RELEASE:$DISTRIB"
+FRESH_WIP_IMG="$REGISTRY/mon-web-fresh-wip:$DISTRIB"
+STANDARD_IMG="$REGISTRY/mon-web-$VERSION-$RELEASE:$DISTRIB"
+STANDARD_WIP_IMG="$REGISTRY/mon-web-wip:$DISTRIB"
+WIDGETS_IMG="$REGISTRY/mon-web-widgets-$VERSION-$RELEASE:$DISTRIB"
+WIDGETS_WIP_IMG="$REGISTRY/mon-web-widgets-wip:$DISTRIB"
 
 # Pull base image.
 docker pull "$BASE_IMG"
@@ -34,11 +38,17 @@ sed "s#@BASE_IMAGE@#$STANDARD_IMG#g" < web/widgets.Dockerfile.in > web/widgets.D
 # Build 'fresh' image.
 docker build --no-cache --ulimit 'nofile=40000' -t "$FRESH_IMG" -f web/fresh.Dockerfile .
 docker push "$FRESH_IMG"
+docker tag "$FRESH_IMG" "$FRESH_WIP_IMG"
+docker push "$FRESH_WIP_IMG"
 
 # Build 'standard' image.
 docker build --no-cache -t "$STANDARD_IMG" -f web/standard.Dockerfile .
 docker push "$STANDARD_IMG"
+docker tag "$STANDARD_IMG" "$STANDARD_WIP_IMG"
+docker push "$STANDARD_WIP_IMG"
 
 # Build 'widgets' image.
 docker build --no-cache -t "$WIDGETS_IMG" -f web/widgets.Dockerfile .
 docker push "$WIDGETS_IMG"
+docker tag "$WIDGETS_IMG" "$WIDGETS_WIP_IMG"
+docker push "$WIDGETS_WIP_IMG"
