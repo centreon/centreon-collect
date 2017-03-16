0225e989614d06de0f3590b9349d729eea1d8d42
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-16 14:02:13 +0100

    jobs: use new /stable/ additional source directory for Centreon Web packaging jobs.

--- jobs/web/centreon-web-testing.sh
+++ jobs/web/centreon-web-testing.sh
@@ -68,7 +68,7 @@ cd ..
 cp packaging-centreon-web/rpm/centreon.spectemplate input/
 
 # Retrieve additional sources.
-cp packaging-centreon-web/src/* input
+cp packaging-centreon-web/src/stable/* input
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos6 input output-centos6
