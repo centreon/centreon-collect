b4491dcd6adcc3c4264ce9f9cde29d542b777717
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-17 14:56:37 +0100

    jobs: make sources available on download.centreon.com in centreon-web-current-stable.

--- jobs/web/centreon-web-current-stable.sh
+++ jobs/web/centreon-web-current-stable.sh
@@ -17,7 +17,9 @@ SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
 $SSH_REPO mv "/srv/sources/standard/testing/$PROJECT-$VERSION-$RELEASE" "/srv/sources/standard/stable/"
 
 # Put sources online.
+SRCHASH=$SSH_REPO md5sum "/srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
 $SSH_REPO aws s3 cp --acl public-read "/srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz" "s3://centreon-download/public/centreon/$PROJECT-$VERSION.tar.gz"
+curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=1"
 
 # Move RPMs to the stable repository.
 `dirname $0`/../testing-to-stable.sh
