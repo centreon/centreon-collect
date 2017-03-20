b0df16eb5f9cff5cfc5f08dc227967d8f08923c3
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-20 10:32:48 +0100

    Revert "jobs: add --recursive flag when uploading sources to S3."
    This reverts commit 06a9d565389809d76f64cbd91f69802bea390f2c.

--- jobs/bam/centreon-bam-current-stable.sh
+++ jobs/bam/centreon-bam-current-stable.sh
@@ -19,7 +19,7 @@ $SSH_REPO mv "/srv/sources/bam/testing/$PROJECT-$VERSION-$RELEASE" "/srv/sources
 # Put sources online.
 SRCHASH=`$SSH_REPO "cat /srv/sources/bam/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php{53,54}.tar.gz | md5sum | cut -d ' ' -f 1"`
 for phpversion in 53 54 ; do
-  $SSH_REPO aws s3 cp --acl public-read --recursive "/srv/sources/bam/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php$phpversion.tar.gz" "s3://centreon-download/enterprises/centreon-bam/centreon-bam-3.5/centreon-bam-$VERSION/$SRCHASH/$PROJECT-$VERSION.tar.gz"
+  $SSH_REPO aws s3 cp --acl public-read "/srv/sources/bam/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php$phpversion.tar.gz" "s3://centreon-download/enterprises/centreon-bam/centreon-bam-3.5/centreon-bam-$VERSION/$SRCHASH/$PROJECT-$VERSION.tar.gz"
 done
 
 # Move RPMs to the stable repository.
