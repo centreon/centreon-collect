f200e074bf918abbb678cec8b0bbbfdc0558c13e
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-20 17:51:45 +0100

    jobs: fix centreon-bam-current-testing.

--- jobs/bam/centreon-bam-current-testing.sh
+++ jobs/bam/centreon-bam-current-testing.sh
@@ -25,13 +25,13 @@ mkdir output-centos6
 rm -rf output-centos7
 mkdir output-centos7
 
-# Get version.
-cd centreon-bam
-export VERSION=`grep mod_release www/modules/centreon-bam-server/conf.php | cut -d '"' -f 4`
-
 # Create source tarball.
+cd centreon-bam
 git checkout --detach "$COMMIT"
 git archive --prefix="$PROJECT-$VERSION" HEAD | gzip > "../$PROJECT-$VERSION.tar.gz"
+
+# Get version.
+export VERSION=`grep mod_release www/modules/centreon-bam-server/conf.php | cut -d '"' -f 4`
 cd ..
 
 # Encrypt source tarballs.
