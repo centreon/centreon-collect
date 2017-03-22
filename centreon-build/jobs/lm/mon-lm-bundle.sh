5ec899dd051926c326734e6d8a71b4c19015d0de
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-03-22 09:27:44 +0100

    jobs: make mon-lm-bundle pipeline-compatible.

--- centreon-build/jobs/lm/mon-lm-bundle.sh
+++ centreon-build/jobs/lm/mon-lm-bundle.sh
@@ -4,6 +4,10 @@ set -e
 set -x
 
 # Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
 if [ "$#" -lt 1 ] ; then
   echo "USAGE: $0 <centos6|centos7>"
   exit 1
@@ -15,9 +19,16 @@ WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
 docker pull $WEB_IMAGE
 
 # Prepare Dockerfile.
-cd centreon-build/containers/
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
 sed "s/@DISTRIB@/$DISTRIB/g" < lm/lm.Dockerfile.in > lm/lm.$DISTRIB.Dockerfile
 
 # Build image.
-docker build --no-cache -t ci.int.centreon.com:5000/mon-lm:$DISTRIB -f lm/lm.$DISTRIB.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-lm:$DISTRIB
+REGISTRY="ci.int.centreon.com:5000"
+LM_IMAGE="$REGISTRY/mon-lm-$VERSION-$RELEASE:$DISTRIB"
+LM_WIP_IMAGE="$REGISTRY/mon-lm-wip:$DISTRIB"
+docker build --no-cache -t "$LM_IMAGE" -f lm/lm.$DISTRIB.Dockerfile .
+docker push "$LM_IMAGE"
+docker tag "$LM_IMAGE" "$LM_WIP_IMAGE"
+docker push "$LM_WIP_IMAGE"
