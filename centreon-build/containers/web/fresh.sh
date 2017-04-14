e0854989b882b43042112574c91c7d2d586782fa
Kevin Duret <kduret@centreon.com>
2017-04-14 15:03:10 +0200

    modify fresh container due to isntall refacto

--- centreon-build/containers/web/fresh.sh
+++ centreon-build/containers/web/fresh.sh
@@ -6,12 +6,13 @@ set -x
 service mysql start
 httpd -k start
 cd /usr/share/centreon/www/install/steps/process
-cat ../../../../autoinstall.php installConfigurationDb.php | php
-cat ../../../../autoinstall.php installStorageDb.php | php
-cat ../../../../autoinstall.php createDbUser.php | php
-cat ../../../../autoinstall.php insertBaseConf.php | php
-cat ../../../../autoinstall.php configFileSetup.php | php
-cat ../../../../autoinstall.php partitionTables.php | php
+php configFileSetup.php
+php installConfigurationDb.php
+php installStorageDb.php
+php createDbUser.php
+php insertBaseConf.php
+php partitionTables.php
+curl -d "modules[]=centreon-license-manager&modules[]=centreon-license-manager" http://localhost/centreon/install/steps/process/process_step8.php
 rm -rf /usr/share/centreon/www/install
 mysql -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'centreon'"
 centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
