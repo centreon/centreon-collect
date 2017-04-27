8d4d27edd6942cc2835054b60ae2eedc58779313
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-04-27 11:00:01 +0200

    jobs: generate documentation on doc-dev in Centreon Map Server testing job.

--- jobs/map/centreon-map-server-endoflife-testing-post.sh
+++ jobs/map/centreon-map-server-endoflife-testing-post.sh
@@ -5,11 +5,14 @@ set -x
 
 # This is run once the Maven build terminated.
 
+# Copy files to internal repository.
 FILE_CENTOS6=centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat6/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm
 FILE_CENTOS7=centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm
-
 scp -o StrictHostKeyChecking=no $FILE_CENTOS6 ubuntu@srvi-repo.int.centreon.com:/srv/yum/map/3.4/el6/testing/noarch/RPMS/
 scp -o StrictHostKeyChecking=no $FILE_CENTOS7 ubuntu@srvi-repo.int.centreon.com:/srv/yum/map/3.4/el7/testing/noarch/RPMS/
-
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/map/3.4/el6/testing/noarch
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/map/3.4/el7/testing/noarch
+
+# Generate doc on internal server.
+SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
+$SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-map-4 -V master -p'"
