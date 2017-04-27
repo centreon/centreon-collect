83c19954d3d644569d59cdcc1ccf61ef8427cd9b
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-04-27 11:06:19 +0200

    jobs: handle COMMIT variable in Centreon Map Server release jobs.

--- centreon-build/jobs/map/centreon-map-server-endoflife-testing-pre.sh
+++ centreon-build/jobs/map/centreon-map-server-endoflife-testing-pre.sh
@@ -7,7 +7,16 @@ set -x
 # This is run before the Maven build.
 #
 
+# Check arguments.
+if [ -z "$COMMIT" -o -z "$RELEASE" ] ; then
+  echo "You need to specify COMMIT and RELEASE environment variables."
+  exit 1
+fi
+
+# Checkout commit.
 cd centreon-studio-server
+git checkout --detach "$COMMIT"
+
 # Tweak pom files to add release version.
 sed -i 's/<project.release>1/<project.release>'"$RELEASE"'/g' com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat6/pom.xml
 sed -i 's/<project.release>1/<project.release>'"$RELEASE"'/g' com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/pom.xml
