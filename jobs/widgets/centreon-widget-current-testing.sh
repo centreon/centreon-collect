ffc4bc6c739cf836c104550f9c89c68d48b9b126
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-05-22 10:59:31 +0200

    jobs: update centreon-widget-current-testing.

--- jobs/widgets/centreon-widget-current-testing.sh
+++ jobs/widgets/centreon-widget-current-testing.sh
@@ -22,16 +22,16 @@ rm -rf output-centos7
 mkdir output-centos7
 
 # Create source tarball.
-NAME="centreon-widget-$WIDGET"
-rm -rf "$NAME"
-git clone "https://github.com/Centreon-Widgets/$NAME"
-cd "$NAME"
+PROJECT="centreon-widget-$WIDGET"
+rm -rf "$PROJECT"
+git clone "https://github.com/Centreon-Widgets/$PROJECT"
+cd "$PROJECT"
 git checkout --detach "$COMMIT"
-rm -rf "../$NAME-$VERSION"
-mkdir "../$NAME-$VERSION"
-git archive HEAD | tar -C "../$NAME-$VERSION" -x
+rm -rf "../$PROJECT-$VERSION"
+mkdir "../$PROJECT-$VERSION"
+git archive HEAD | tar -C "../$PROJECT-$VERSION" -x
 cd ..
-tar czf "input/$NAME-$VERSION.tar.gz" "$NAME-$VERSION"
+tar czf "input/$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
 
 # Retrieve spec file.
 if [ \! -d packaging-centreon-web ] ; then
@@ -48,6 +48,8 @@ docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5
 docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos7 input output-centos7
 
 # Copy files to server.
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mkdir -p "/srv/sources/standard/testing/$PROJECT-$VERSION-$RELEASE"
+scp -o StrictHostKeyChecking=no "input/$PROJECT-$VERSION.tar.gz" "ubuntu@srvi-repo.int.centreon.com:/srv/sources/standard/testing/$PROJECT-$VERSION-$RELEASE/"
 FILES_CENTOS6='output-centos6/noarch/*.rpm'
 FILES_CENTOS7='output-centos7/noarch/*.rpm'
 scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el6/testing/noarch/RPMS"
