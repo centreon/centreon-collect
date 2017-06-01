3113a833efc4c3b2e8ad9d90f15fdc7f3f0538bb
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-01 15:06:46 +0200

    jobs: update 2.9 mon-web-acceptance.

--- jobs/web/2.9/mon-web-acceptance.sh
+++ jobs/web/2.9/mon-web-acceptance.sh
@@ -32,7 +32,7 @@ docker pull $INFLUXDB_IMAGE
 
 # Fetch sources.
 rm -rf "centreon-$VERSION" "centreon-$VERSION.tar.gz"
-wget "http://srvi-repo.int.centreon.com/sources/internal/centreon-web-$VERSION-$RELEASE/centreon-$VERSION.tar.gz"
+get_internal_source "web/centreon-web-$VERSION-$RELEASE/centreon-$VERSION.tar.gz"
 tar xzf "centreon-$VERSION.tar.gz"
 cd "centreon-$VERSION"
 
