cba12934f4fa03e97db831fbb3e0d89acbc1b04d
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-02 10:30:32 +0200

    jobs: do not use -wip images in 2.8.

--- centreon-build/jobs/web/2.8/mon-web-bundle.sh
+++ centreon-build/jobs/web/2.8/mon-web-bundle.sh
@@ -23,11 +23,8 @@ fi
 REGISTRY="ci.int.centreon.com:5000"
 BASE_IMG="$REGISTRY/mon-dependencies:$DISTRIB"
 FRESH_IMG="$REGISTRY/mon-web-fresh-$VERSION-$RELEASE:$DISTRIB"
-FRESH_WIP_IMG="$REGISTRY/mon-web-fresh-wip:$DISTRIB"
 STANDARD_IMG="$REGISTRY/mon-web-$VERSION-$RELEASE:$DISTRIB"
-STANDARD_WIP_IMG="$REGISTRY/mon-web-wip:$DISTRIB"
 WIDGETS_IMG="$REGISTRY/mon-web-widgets-$VERSION-$RELEASE:$DISTRIB"
-WIDGETS_WIP_IMG="$REGISTRY/mon-web-widgets-wip:$DISTRIB"
 
 # Pull base image.
 docker pull "$BASE_IMG"
@@ -44,17 +41,11 @@ sed "s#@CENTOS_VERSION@#$CENTOS_VERSION#g;s#@SUBDIR@#web/centreon-web-$VERSION-$
 # Build 'fresh' image.
 docker build --no-cache --ulimit 'nofile=40000' -t "$FRESH_IMG" -f web/fresh.Dockerfile .
 docker push "$FRESH_IMG"
-docker tag "$FRESH_IMG" "$FRESH_WIP_IMG"
-docker push "$FRESH_WIP_IMG"
 
 # Build 'standard' image.
 docker build --no-cache -t "$STANDARD_IMG" -f web/standard.Dockerfile .
 docker push "$STANDARD_IMG"
-docker tag "$STANDARD_IMG" "$STANDARD_WIP_IMG"
-docker push "$STANDARD_WIP_IMG"
 
 # Build 'widgets' image.
 docker build --no-cache -t "$WIDGETS_IMG" -f web/widgets.Dockerfile .
 docker push "$WIDGETS_IMG"
-docker tag "$WIDGETS_IMG" "$WIDGETS_WIP_IMG"
-docker push "$WIDGETS_WIP_IMG"
