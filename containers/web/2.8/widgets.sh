c341a3060920926e45824edaba611f30536f71ac
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-02 10:10:23 +0200

    containers: use 2.8 widget installation script in mon-web-widgets.

--- containers/web/2.8/widgets.sh
+++ containers/web/2.8/widgets.sh
@@ -6,7 +6,7 @@ set -x
 service mysql start
 httpd -k start
 for w in graph-monitoring host-monitoring hostgroup-monitoring httploader live-top10-cpu-usage live-top10-memory-usage service-monitoring servicegroup-monitoring tactical-overview ; do
-  php /tmp/install-centreon-widget.php -b /usr/share/centreon/bootstrap.php -w $w
+  php /tmp/install-centreon-widget.php -c /etc/centreon/centreon.conf.php -w $w
 done
 httpd -k stop
 service mysql stop
