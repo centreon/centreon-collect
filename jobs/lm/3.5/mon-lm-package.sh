7483a11dc97f1eb0837bc93e5d06a878404faf1e
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-07 16:26:14 +0200

    jobs: split LM jobs in 3.4 and 3.5 directories.

---
+++ jobs/lm/3.5/mon-lm-package.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Project.
+PROJECT=centreon-license-manager
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Fetch sources.
+rm -f "$PROJECT-$VERSION.tar.gz"
+wget "http://srvi-repo.int.centreon.com/sources/internal/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+
+# Create input and output directories.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Encrypt source archive.
+if [ "$DISTRIB" = "centos6" ] ; then
+  phpversion=53
+elif [ "$DISTRIB" = "centos7" ] ; then
+  phpversion=54
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+curl -F "file=@$PROJECT-$VERSION.tar.gz" -F "version=$phpversion" -F "modulename=$PROJECT" -F 'needlicense=0' 'http://encode.int.centreon.com/api/' -o "input/$PROJECT-$VERSION-php$phpversion.tar.gz"
+
+# Pull latest build dependencies.
+BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
+docker pull "$BUILD_IMG"
+
+# Build RPMs.
+cp "$PROJECT-$VERSION/packaging/$PROJECT.spectemplate" input
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG" input output
+
+# Copy files to server.
+if [ "$DISTRIB" = 'centos6' ] ; then
+  REPO='internal/el6/noarch'
+elif [ "$DISTRIB" = 'centos7' ] ; then
+  REPO='internal/el7/noarch'
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+FILES='output/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
+DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
+scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
