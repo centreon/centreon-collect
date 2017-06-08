5a688dd9d7f3b459874a232988c56ea6a9fb3a42
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-08 10:00:26 +0200

    jobs: update mon-web-package.

--- jobs/common.sh
+++ jobs/common.sh
@@ -29,9 +29,10 @@ put_internal_source () {
 # Internal RPMs.
 
 put_internal_rpms () {
-  DIR="/srv/yum/internal/$1/$2/$3"
-  NEWDIR="$4"
+  DIR="/srv/yum/internal/$1/$2/$3/$4"
+  NEWDIR="$5"
   REPO="internal/$1/$2"
+  shift
   shift
   shift
   shift
@@ -46,18 +47,4 @@ put_internal_rpms () {
   done
   scp "$UPDATEREPODIR/updaterepo.sh" "$REPO_CREDS:$DESTFILE"
   ssh "$REPO_CREDS" sh $DESTFILE $REPO
-}
-
-# Private RPMs.
-
-put_private_rpms () {
-  DIR="/srv/yum/internal/$1/private/$2"
-  NEWDIR="$3"
-  shift
-  shift
-  shift
-  ssh "$REPO_CREDS" mkdir -p "$DIR/$NEWDIR"
-  scp "$@" "$REPO_CREDS:$DIR/$NEWDIR"
-  ssh "$REPO_CREDS" createrepo "$DIR/$NEWDIR"
-  clean_directory "$DIR"
 }
