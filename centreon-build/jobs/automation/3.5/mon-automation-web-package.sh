8c41c10a3897bce68892af520a5e3fae2161f6d4
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-09 17:19:58 +0200

    jobs: another fix to mon-automation-web-package.

--- centreon-build/jobs/automation/3.5/mon-automation-web-package.sh
+++ centreon-build/jobs/automation/3.5/mon-automation-web-package.sh
@@ -39,7 +39,7 @@ else
   echo "Unsupported distribution $DISTRIB."
   exit 1
 fi
-curl -F "file=@$PROJECT-$VERSION.tar.gz" -F "version=$phpversion" -F 'modulename=$PROJECT' -F 'needlicense=0' 'http://encode.int.centreon.com/api/' -o "input/$PROJECT-$VERSION-php$phpversion.tar.gz"
+curl -F "file=@$PROJECT-$VERSION.tar.gz" -F "version=$phpversion" -F "modulename=$PROJECT" -F 'needlicense=0' 'http://encode.int.centreon.com/api/' -o "input/$PROJECT-$VERSION-php$phpversion.tar.gz"
 
 # Pull latest build dependencies.
 BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
