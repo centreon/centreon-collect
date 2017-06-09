428018557d473783bfbd7603226d6a76381a0ab9
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-09 14:48:29 +0200

    jobs: add 3.4 CI scripts for poller-display.

---
+++ jobs/poller-display/3.4/mon-poller-display-delivery.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Set Docker images as latest.
+REGISTRY='ci.int.centreon.com:5000'
+POLLER_CENTOS6="$REGISTRY/mon-poller-display-$VERSION-$RELEASE:centos6"
+POLLER_CENTOS7="$REGISTRY/mon-poller-display-$VERSION-$RELEASE:centos7"
+CENTRAL_CENTOS6="$REGISTRY/mon-poller-display-central-$VERSION-$RELEASE:centos6"
+CENTRAL_CENTOS7="$REGISTRY/mon-poller-display-central-$VERSION-$RELEASE:centos7"
+
+docker pull "$POLLER_CENTOS6"
+docker tag "$POLLER_CENTOS6" "$REGISTRY/mon-poller-display-3.4:centos6"
+docker push "$REGISTRY/mon-poller-display-3.4:centos6"
+
+docker pull "$POLLER_CENTOS7"
+docker tag "$POLLER_CENTOS7" "$REGISTRY/mon-poller-display-3.4:centos7"
+docker push "$REGISTRY/mon-poller-display-3.4:centos7"
+
+docker pull "$CENTRAL_CENTOS6"
+docker tag "$CENTRAL_CENTOS6" "$REGISTRY/mon-poller-display-central-3.4:centos6"
+docker push "$REGISTRY/mon-poller-display-central-3.4:centos6"
+
+docker pull "$CENTRAL_CENTOS7"
+docker tag "$CENTRAL_CENTOS7" "$REGISTRY/mon-poller-display-central-3.4:centos7"
+docker push "$REGISTRY/mon-poller-display-central-3.4:centos7"
