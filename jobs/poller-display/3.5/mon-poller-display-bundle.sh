0ba916189db2d340de8e278c119be7091c799e77
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-09 14:35:27 +0200

    jobs: update mon-poller-display-bundl.

--- jobs/poller-display/3.5/mon-poller-display-bundle.sh
+++ jobs/poller-display/3.5/mon-poller-display-bundle.sh
@@ -34,8 +34,8 @@ docker pull "$WEB_STANDARD_IMG"
 rm -rf centreon-build-containers
 cp -r /opt/centreon-build/containers centreon-build-containers
 cd centreon-build-containers
-sed "s#@BASE_IMAGE@#$WEB_FRESH_IMG#g" < poller-display/poller.Dockerfile.in > poller-display/poller.Dockerfile
-sed "s#@BASE_IMAGE@#$WEB_STANDARD_IMG#g" < poller-display/central.Dockerfile.in > poller-display/central.Dockerfile
+sed "s#@BASE_IMAGE@#$WEB_FRESH_IMG#g" < poller-display/3.5/poller.Dockerfile.in > poller-display/poller.Dockerfile
+sed "s#@BASE_IMAGE@#$WEB_STANDARD_IMG#g" < poller-display/3.5/central.Dockerfile.in > poller-display/central.Dockerfile
 
 # Build 'poller' image.
 docker build --no-cache -t "$POLLER_IMG" -f poller-display/poller.Dockerfile .
