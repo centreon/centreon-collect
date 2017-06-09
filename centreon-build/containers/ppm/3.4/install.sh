85b7a125f8a9ba4e55aa0099310596385ae15281
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-09 14:00:54 +0200

    containers: update installation of PPM in mon-ppm-3.4.

--- centreon-build/containers/ppm/3.4/install.sh
+++ centreon-build/containers/ppm/3.4/install.sh
@@ -5,7 +5,7 @@ set -x
 
 service mysql start
 httpd -k start
-/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-license-manager
-/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-pp-manager
+/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-license-manager
+/tmp/install-centreon-module.php -c /etc/centreon/centreon.conf.php -m centreon-pp-manager
 httpd -k stop
 service mysql stop
