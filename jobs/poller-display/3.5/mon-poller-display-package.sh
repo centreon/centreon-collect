226a37909781920ac43b09415ee148430c58ed59
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-09 14:34:37 +0200

    jobs: update mon-poller-display-package.

--- jobs/poller-display/3.5/mon-poller-display-package.sh
+++ jobs/poller-display/3.5/mon-poller-display-package.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+. `dirname $0`/../../common.sh
+
 # Project.
 PROJECT=centreon-poller-display
 
@@ -28,23 +30,19 @@ mkdir output
 
 # Retrieve spec file.
 cd input
-cp `dirname $0`/../../packaging/poller-display/centreon-poller-display.spectemplate .
+cp `dirname $0`/../../../packaging/poller-display/centreon-poller-display.spectemplate .
 
 # Retrieve sources.
-wget "http://srvi-repo.int.centreon.com/sources/internal/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+get_internal_source "poller-display/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
 cd ..
 
 # Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB input output
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB input output
 
 # Copy files to server.
 if [ "$DISTRIB" = "centos6" ] ; then
-  REPO='internal/el6/noarch'
+  DISTRIB='el6'
 else
-  REPO='internal/el7/noarch'
+  DISTRIB='el7'
 fi
-FILES='output/noarch/*.rpm'
-scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
-DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
-scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
+put_internal_rpms "3.5" "$DISTRIB" "noarch" "poller-display" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
