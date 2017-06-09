768e70b32128eddfd0f97fbead5336947c545291
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-09 13:32:59 +0200

    jobs: update mon-ppm-bundle.

--- jobs/ppm/3.4/mon-ppm-bundle.sh
+++ jobs/ppm/3.4/mon-ppm-bundle.sh
@@ -15,20 +15,17 @@ fi
 DISTRIB="$1"
 
 # Pull images.
-WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web-3.4:$DISTRIB
 docker pull $WEB_IMAGE
 
 # Prepare Dockerfile.
 rm -rf centreon-build-containers
 cp -r /opt/centreon-build/containers centreon-build-containers
 cd centreon-build-containers
-sed "s/@DISTRIB@/$DISTRIB/g" < ppm/ppm.Dockerfile.in > ppm/ppm.$DISTRIB.Dockerfile
+sed "s/@DISTRIB@/$DISTRIB/g" < ppm/3.4/ppm.Dockerfile.in > ppm/ppm.Dockerfile
 
 # Build image.
 REGISTRY="ci.int.centreon.com:5000"
 PPM_IMAGE="$REGISTRY/mon-ppm-$VERSION-$RELEASE:$DISTRIB"
-PPM_WIP_IMAGE="$REGISTRY/mon-ppm-wip:$DISTRIB"
-docker build --no-cache -t "$PPM_IMAGE" -f ppm/ppm.$DISTRIB.Dockerfile .
+docker build --no-cache -t "$PPM_IMAGE" -f ppm/ppm.Dockerfile .
 docker push "$PPM_IMAGE"
-docker tag "$PPM_IMAGE" "$PPM_WIP_IMAGE"
-docker push "$PPM_WIP_IMAGE"
--- jobs/ppm/3.5/mon-ppm-bundle.sh
+++ jobs/ppm/3.5/mon-ppm-bundle.sh
@@ -22,13 +22,13 @@ docker pull $WEB_IMAGE
 rm -rf centreon-build-containers
 cp -r /opt/centreon-build/containers centreon-build-containers
 cd centreon-build-containers
-sed "s/@DISTRIB@/$DISTRIB/g" < ppm/ppm.Dockerfile.in > ppm/ppm.$DISTRIB.Dockerfile
+sed "s/@DISTRIB@/$DISTRIB/g" < ppm/3.5/ppm.Dockerfile.in > ppm/ppm.Dockerfile
 
 # Build image.
 REGISTRY="ci.int.centreon.com:5000"
 PPM_IMAGE="$REGISTRY/mon-ppm-$VERSION-$RELEASE:$DISTRIB"
 PPM_WIP_IMAGE="$REGISTRY/mon-ppm-wip:$DISTRIB"
-docker build --no-cache -t "$PPM_IMAGE" -f ppm/ppm.$DISTRIB.Dockerfile .
+docker build --no-cache -t "$PPM_IMAGE" -f ppm/ppm.Dockerfile .
 docker push "$PPM_IMAGE"
 docker tag "$PPM_IMAGE" "$PPM_WIP_IMAGE"
 docker push "$PPM_WIP_IMAGE"
