150b2aa3d28c803ebb67505f93a9234b2ec42c09
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-09 13:35:48 +0200

    jobs: update mon-ppm-delivery.

--- centreon-build/jobs/ppm/3.4/mon-ppm-delivery.sh
+++ centreon-build/jobs/ppm/3.4/mon-ppm-delivery.sh
@@ -15,9 +15,9 @@ PPM_CENTOS6="$REGISTRY/mon-ppm-$VERSION-$RELEASE:centos6"
 PPM_CENTOS7="$REGISTRY/mon-ppm-$VERSION-$RELEASE:centos7"
 
 docker pull "$PPM_CENTOS6"
-docker tag "$PPM_CENTOS6" "$REGISTRY/mon-ppm:centos6"
-docker push "$REGISTRY/mon-ppm:centos6"
+docker tag "$PPM_CENTOS6" "$REGISTRY/mon-ppm-3.4:centos6"
+docker push "$REGISTRY/mon-ppm-3.4:centos6"
 
 docker pull "$PPM_CENTOS7"
-docker tag "$PPM_CENTOS7" "$REGISTRY/mon-ppm:centos7"
-docker push "$REGISTRY/mon-ppm:centos7"
+docker tag "$PPM_CENTOS7" "$REGISTRY/mon-ppm-3.4:centos7"
+docker push "$REGISTRY/mon-ppm-3.4:centos7"
