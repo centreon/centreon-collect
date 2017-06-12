0d5df2b352c651d5dd69cbbab4162e59ae190772
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-12 11:50:32 +0200

    jobs: update des-map-web-package.

--- centreon-build/jobs/map/des-map-web-package.sh
+++ centreon-build/jobs/map/des-map-web-package.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+. `dirname $0`/../../common.sh
+
 # Project.
 PROJECT=centreon-map-web
 PACKAGE=centreon-map4-web-client
@@ -18,37 +20,33 @@ if [ "$#" -lt 1 ] ; then
 fi
 DISTRIB="$1"
 
-# Fetch sources.
-rm -f "$PACKAGE-$VERSION.tar.gz"
-wget "http://srvi-repo.int.centreon.com/sources/internal/$PROJECT-$VERSION-$RELEASE/$PACKAGE-$VERSION.tar.gz"
-tar xzf "$PACKAGE-$VERSION.tar.gz"
-
 # Create input and output directories.
 rm -rf input
 mkdir input
 rm -rf output
 mkdir output
 
+# Fetch sources.
+cd input
+get_internal_source "map-web/$PACKAGE-$VERSION-$RELEASE/$PACKAGE-$VERSION.tar.gz"
+get_internal_source "map-web/$PACKAGE-$VERSION-$RELEASE/$PACKAGE.spectemplate"
+cd ..
+
 # Pull latest build dependencies.
 BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
 docker pull "$BUILD_IMG"
 
 # Build RPMs.
-cp "$PACKAGE-$VERSION.tar.gz" input
-wget -O "input/$PACKAGE.spectemplate" "http://srvi-repo.int.centreon.com/sources/internal/$PROJECT-$VERSION-$RELEASE/$PACKAGE.spectemplate"
 docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG" input output
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos6' ] ; then
-  REPO='internal/el6/noarch'
+  DISTRIB='el6'
 elif [ "$DISTRIB" = 'centos7' ] ; then
-  REPO='internal/el7/noarch'
+  DISTRIB='el7'
 else
   echo "Unsupported distribution $DISTRIB."
   exit 1
 fi
-FILES='output/noarch/*.rpm'
-scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
-DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
-scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
+put_internal_rpms "3.4" "$DISTRIB" "noarch" "map-web" "$PACKAGE-$VERSION-$RELEASE" output/noarch/*.rpm
+put_internal_rpms "3.5" "$DISTRIB" "noarch" "map-web" "$PACKAGE-$VERSION-$RELEASE" output/noarch/*.rpm
