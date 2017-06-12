39ce6971816fbab3968e38ecc8a2fb9ca7cc7a82
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-12 12:20:29 +0200

    jobs: update des-map-server-post.

--- jobs/map/des-map-server-post.sh
+++ jobs/map/des-map-server-post.sh
@@ -3,12 +3,12 @@
 set -e
 set -x
 
+. `dirname $0`/../common.sh
+
 # This is run once the Maven build terminated.
-scp -o StrictHostKeyChecking=no centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat6/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/internal/el6/noarch/RPMS/
-scp -o StrictHostKeyChecking=no centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/internal/el7/noarch/RPMS/
-DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
-scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE 'internal/el6/noarch'
-DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
-scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE 'internal/el7/noarch'
+FILES_TOMCAT6='centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat6/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm'
+FILES_TOMCAT7='centreon-studio-server/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm'
+put_internal_rpms "3.4" "el6" "noarch" "map-server" "" $FILES_TOMCAT6
+put_internal_rpms "3.4" "el7" "noarch" "map-server" "" $FILES_TOMCAT7
+put_internal_rpms "3.5" "el6" "noarch" "map-server" "" $FILES_TOMCAT6
+put_internal_rpms "3.5" "el7" "noarch" "map-server" "" $FILES_TOMCAT7
