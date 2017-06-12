70de14df564ad63983a62c75b21ff21a59647d64
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-12 14:40:54 +0200

    jobs: update des-map-web-package.

--- jobs/map/3.5/des-map-web-package.sh
+++ jobs/map/3.5/des-map-web-package.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-. `dirname $0`/../common.sh
+. `dirname $0`/../../common.sh
 
 # Project.
 PROJECT=centreon-map-web
@@ -37,7 +37,7 @@ BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
 docker pull "$BUILD_IMG"
 
 # Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG" input output
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos6' ] ; then
@@ -48,5 +48,4 @@ else
   echo "Unsupported distribution $DISTRIB."
   exit 1
 fi
-put_internal_rpms "3.4" "$DISTRIB" "noarch" "map-web" "$PACKAGE-$VERSION-$RELEASE" output/noarch/*.rpm
 put_internal_rpms "3.5" "$DISTRIB" "noarch" "map-web" "$PACKAGE-$VERSION-$RELEASE" output/noarch/*.rpm
