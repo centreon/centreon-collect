d112fc9e20b1289cc5879dde0c3b67f8ac3986a9
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-12 14:46:18 +0200

    jobs: update des-map-bundle.

--- jobs/map/3.5/des-map-bundle.sh
+++ jobs/map/3.5/des-map-bundle.sh
@@ -4,25 +4,40 @@ set -e
 set -x
 
 # Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
 if [ "$#" -lt 1 ] ; then
   echo "USAGE: $0 <centos6|centos7>"
   exit 1
 fi
 DISTRIB="$1"
+if [ "$DISTRIB" = "centos6" ] ; then
+  CENTOS_VERSION=6
+else
+  CENTOS_VERSION=7
+fi
 
 # Pull base image.
-DEP_IMAGE=ci.int.centreon.com:5000/mon-dependencies:$DISTRIB
-WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
+REGISTRY="ci.int.centreon.com:5000"
+DEP_IMAGE="$REGISTRY/mon-dependencies:$DISTRIB"
+BASE_IMAGE="$REGISTRY/mon-web:$DISTRIB"
+SERVER_IMAGE="$REGISTRY/des-map-server:$DISTRIB"
+WEB_IMAGE="$REGISTRY/des-map-web:$DISTRIB"
 docker pull $DEP_IMAGE
-docker pull $WEB_IMAGE
+docker pull $BASE_IMAGE
 
 # Prepare Dockerfiles.
-cd centreon-build/containers
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
 sed "s/@DISTRIB@/$DISTRIB/g" < map/server.Dockerfile.in > map/server.$DISTRIB.Dockerfile
-sed "s/@DISTRIB@/$DISTRIB/g" < map/web.Dockerfile.in > map/web.$DISTRIB.Dockerfile
+sed "s/@BASE_IMAGE@/$BASE_IMAGE/g" < map/web.Dockerfile.in > map/web.$DISTRIB.Dockerfile
+sed "s/@VERSION@/3.5/g;s/@DISTRIB@/el$CENTOS_VERSION/g" < repo/centreon-internal.repo.in > repo/centreon-internal.repo
 
 # Build image.
-docker build --no-cache -t ci.int.centreon.com:5000/des-map-server:$DISTRIB -f map/server.$DISTRIB.Dockerfile .
-docker build --no-cache -t ci.int.centreon.com:5000/des-map-web:$DISTRIB -f map/web.$DISTRIB.Dockerfile .
-docker push ci.int.centreon.com:5000/des-map-server:$DISTRIB
-docker push ci.int.centreon.com:5000/des-map-web:$DISTRIB
+docker build --no-cache -t "$SERVER_IMAGE" -f map/server.$DISTRIB.Dockerfile .
+docker build --no-cache -t "$WEB_IMAGE" -f map/web.$DISTRIB.Dockerfile .
+docker push "$SERVER_IMAGE"
+docker push "$WEB_IMAGE"
