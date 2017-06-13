f60d9aaffa31186e9a5a2368de36093fe27c6b84
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-13 14:44:08 +0200

    jobs: add 3.4 Centreon BAM CI scripts.

---
+++ jobs/bam/3.4/des-bam-bundle.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Pull base image.
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web-3.4:$DISTRIB
+docker pull $WEB_IMAGE
+
+# Prepare Dockerfiles.
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+sed "s/@DISTRIB@/$DISTRIB/g" < bam/3.4/Dockerfile.in > bam/Dockerfile
+
+# Build image.
+REGISTRY="ci.int.centreon.com:5000"
+BAM_IMAGE="$REGISTRY/des-bam-$VERSION-$RELEASE:$DISTRIB"
+BAM_WIP_IMAGE="$REGISTRY/des-bam-3.4-wip:$DISTRIB"
+docker build --no-cache -t "$BAM_IMAGE" -f bam/Dockerfile .
+docker push "$BAM_IMAGE"
+docker tag "$BAM_IMAGE" "$BAM_WIP_IMAGE"
+docker push "$BAM_WIP_IMAGE"
