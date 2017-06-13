46fefb3e1832ef47c97746bbd53c971fd5e02d40
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-13 14:44:08 +0200

    jobs: add 3.4 Centreon BAM CI scripts.

---
+++ centreon-build/jobs/bam/3.4/des-bam-acceptance.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=centreon-bam-server
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Pull images.
+REGISTRY="ci.int.centreon.com:5000"
+WEBDRIVER_IMAGE=selenium/standalone-chrome:latest
+BAM_IMAGE="$REGISTRY/des-bam-$VERSION-$RELEASE:$DISTRIB"
+docker pull $WEBDRIVER_IMAGE
+docker pull $BAM_IMAGE
+
+# Get sources.
+rm -rf "$PROJECT-$VERSION" "$PROJECT-$VERSION.tar.gz"
+get_internal_source "bam/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+cd "$PROJECT-$VERSION"
+
+# Prepare Docker Compose file.
+sed 's#@WEB_IMAGE@#'$BAM_IMAGE'#g' < `dirname $0`/../../../containers/web/3.4/docker-compose.yml.in > docker-compose-bam.yml
+
+# Prepare Behat.yml
+alreadyset=`grep docker-compose-bam.yml < behat.yml || true`
+if [ -z "$alreadyset" ] ; then
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs-wip\n      bam: docker-compose-bam.yml#g' behat.yml
+fi
+
+# Run acceptance tests.
+rm -rf ../xunit-reports
+mkdir ../xunit-reports
+rm -rf ../acceptance-logs
+mkdir ../acceptance-logs
+composer install
+composer update
+ls features/*.feature | parallel ./vendor/bin/behat --strict --format=junit --out="../xunit-reports/{/.}" "{}"
---
+++ centreon-build/jobs/bam/3.4/des-bam-package.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=centreon-bam-server
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Create input and output directories.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Fetch sources
+rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+get_internal_source "bam/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+
+# Encrypt source archive.
+if [ "$DISTRIB" = "centos6" ] ; then
+  phpversion=53
+elif [ "$DISTRIB" = "centos7" ] ; then
+  phpversion=54
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+curl -F "file=@centreon-bam-server-$VERSION.tar.gz" -F "version=$phpversion" -F 'modulename=centreon-bam-server' 'http://encode.int.centreon.com/api/' -o "input/centreon-bam-server-$VERSION-php$phpversion.tar.gz"
+
+# Pull latest build dependencies.
+BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
+docker pull "$BUILD_IMG"
+
+# Build RPMs.
+cp "$PROJECT-$VERSION/packaging/$PROJECT.spectemplate" input
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
+
+# Copy files to server.
+if [ "$DISTRIB" = 'centos6' ] ; then
+  DISTRIB='el6'
+elif [ "$DISTRIB" = 'centos7' ] ; then
+  DISTRIB='el7'
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
+put_internal_rpms "3.4" "$DISTRIB" "noarch" "bam" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
