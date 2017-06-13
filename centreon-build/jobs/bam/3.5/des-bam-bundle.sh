f3fb3fb3c74fdb6899e0cb188623e9faed72dea3
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-13 14:38:42 +0200

    jobs: update des-bam-bundle.

--- centreon-build/jobs/bam/3.5/des-bam-bundle.sh
+++ centreon-build/jobs/bam/3.5/des-bam-bundle.sh
@@ -4,6 +4,10 @@ set -e
 set -x
 
 # Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
 if [ "$#" -lt 1 ] ; then
   echo "USAGE: $0 <centos6|centos7>"
   exit 1
@@ -11,13 +15,20 @@ fi
 DISTRIB="$1"
 
 # Pull base image.
-WEB_IMAGE=ci.int.centreon.com:5000/mon-web:$DISTRIB
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web-3.5:$DISTRIB
 docker pull $WEB_IMAGE
 
 # Prepare Dockerfiles.
-cd centreon-build/containers
-sed "s/@DISTRIB@/$DISTRIB/g" < bam/Dockerfile.in > bam/$DISTRIB.Dockerfile
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+sed "s/@DISTRIB@/$DISTRIB/g" < bam/3.5/Dockerfile.in > bam/Dockerfile
 
 # Build image.
-docker build --no-cache -t ci.int.centreon.com:5000/des-bam:$DISTRIB -f bam/$DISTRIB.Dockerfile .
-docker push ci.int.centreon.com:5000/des-bam:$DISTRIB
+REGISTRY="ci.int.centreon.com:5000"
+BAM_IMAGE="$REGISTRY/des-bam-$VERSION-$RELEASE:$DISTRIB"
+BAM_WIP_IMAGE="$REGISTRY/des-bam-3.5-wip:$DISTRIB"
+docker build --no-cache -t "$BAM_IMAGE" -f bam/Dockerfile .
+docker push "$BAM_IMAGE"
+docker tag "$BAM_IMAGE" "$BAM_WIP_IMAGE"
+docker push "$BAM_WIP_IMAGE"
