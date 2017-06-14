4528429b5d5432e740b77e351bb65b3fe67adcc6
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-14 11:12:29 +0200

    jobs: update mon-ppe-unittest.

--- centreon-build/jobs/ppe/3.5/mon-ppe-unittest.sh
+++ centreon-build/jobs/ppe/3.5/mon-ppe-unittest.sh
@@ -3,25 +3,40 @@
 set -e
 set -x
 
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=centreon-export
+
 # Check arguments.
-if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <6|7>"
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
   exit 1
 fi
-CENTOS_VERSION="$1"
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+get_internal_source "ppe/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
 
 # Launch mon-unitttest container.
-docker pull ci.int.centreon.com:5000/mon-unittest:centos$CENTOS_VERSION
-containerid=`docker create ci.int.centreon.com:5000/mon-unittest:centos$CENTOS_VERSION /usr/local/bin/unittest-ppe`
+UT_IMAGE=ci.int.centreon.com:5000/mon-unittest:$DISTRIB
+docker pull $UT_IMAGE
+containerid=`docker create $UT_IMAGE /usr/local/bin/unittest-phing $PROJECT`
 
 # Copy sources to container.
-docker cp centreon-export "$containerid:/usr/local/src/centreon-export"
+docker cp "$PROJECT-$VERSION" "$containerid:/usr/local/src/$PROJECT"
 
 # Run unit tests.
 docker start -a "$containerid"
-docker cp "$containerid:/tmp/centreon-ppe_ut.xml" centreon-ppe_ut.xml
-docker cp "$containerid:/tmp/centreon-ppe_coverage.xml" centreon-ppe_coverage.xml
-docker cp "$containerid:/tmp/centreon-ppe_codestyle.xml" centreon-ppe_codestyle.xml
+docker cp "$containerid:/tmp/ut.xml" ut.xml
+docker cp "$containerid:/tmp/coverage.xml" coverage.xml
+docker cp "$containerid:/tmp/codestyle.xml" codestyle.xml
 
 # Stop container.
 docker stop "$containerid"
