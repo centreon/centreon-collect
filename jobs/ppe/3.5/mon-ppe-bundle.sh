7c4f861d9b78e88b1583ad8546b4505ddd70c81b
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-06-14 11:20:08 +0200

    jobs: update mon-ppe-bundle.

--- jobs/ppe/3.5/mon-ppe-bundle.sh
+++ jobs/ppe/3.5/mon-ppe-bundle.sh
@@ -4,6 +4,10 @@ set -e
 set -x
 
 # Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
 if [ "$#" -lt 1 ] ; then
   echo "USAGE: $0 <centos6|centos7>"
   exit 1
@@ -11,12 +15,20 @@ fi
 DISTRIB="$1"
 
 # Pull Centreon Web image.
-docker pull ci.int.centreon.com:5000/mon-web:$DISTRIB
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web-3.5:$DISTRIB
+docker pull $WEB_IMAGE
 
 # Prepare Dockerfile.
-cd centreon-build/containers
-sed "s/@DISTRIB@/$DISTRIB/g" < ppe/ppe.Dockerfile.in > ppe/ppe.$DISTRIB.Dockerfile
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+sed "s/@DISTRIB@/$DISTRIB/g" < ppe/3.5/ppe.Dockerfile.in > ppe/Dockerfile
 
 # Build image.
-docker build --no-cache -t ci.int.centreon.com:5000/mon-ppe:$DISTRIB -f ppe/ppe.$DISTRIB.Dockerfile .
-docker push ci.int.centreon.com:5000/mon-ppe:$DISTRIB
+REGISTRY="ci.int.centreon.com:5000"
+PPE_IMAGE="$REGISTRY/mon-ppe-$VERSION-$RELEASE:$DISTRIB"
+PPE_WIP_IMAGE="$REGISTRY/mon-ppe-3.5-wip:$DISTRIB"
+docker build --no-cache -t "$PPE_IMAGE" -f ppe/Dockerfile .
+docker push "$PPE_IMAGE"
+docker tag "$PPE_IMAGE" "$PPE_WIP_IMAGE"
+docker push "$PPE_WIP_IMAGE"
