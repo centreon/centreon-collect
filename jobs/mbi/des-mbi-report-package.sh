a22ba05752e130ac3666ca19c9ac2f01534c3104
Etienne Gautier <egautier@centreon.com>
2017-06-28 13:28:00 +0200

    Publish RPMs to 3.4 and 3.5 for project not impacted by PDO

--- jobs/mbi/des-mbi-report-package.sh
+++ jobs/mbi/des-mbi-report-package.sh
@@ -40,15 +40,19 @@ docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG" input ou
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos6' ] ; then
-  REPO='internal/el6/noarch'
+  REPO_34='internal/3.4/el6/noarch'
+  REPO_35='internal/3.5/el6/noarch'
 elif [ "$DISTRIB" = 'centos7' ] ; then
-  REPO='internal/el7/noarch'
+  REPO_34='internal/3.5/el7/noarch'
+  REPO_35='internal/3.5/el7/noarch'
 else
   echo "Unsupported distribution $DISTRIB."
   exit 1
 fi
 FILES='output/noarch/*.rpm'
-scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO/RPMS"
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO_34/RPMS"
+scp -o StrictHostKeyChecking=no $FILES "ubuntu@srvi-repo.int.centreon.com:/srv/yum/$REPO_35/RPMS"
 DESTFILE=`ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mktemp`
 scp -o StrictHostKeyChecking=no `dirname $0`/../updaterepo.sh "ubuntu@srvi-repo.int.centreon.com:$DESTFILE"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO_34
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" sh $DESTFILE $REPO_35
