ed0aa0873980656196ecb01b95c9e49c557add8b
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-17 12:21:48 +0200

    jobs: update mon-containers-dependencies.

--- jobs/containers/mon-containers-dependencies.sh
+++ jobs/containers/mon-containers-dependencies.sh
@@ -6,17 +6,28 @@ set -x
 . `dirname $0`/../common.sh
 
 # Check arguments.
-if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <distrib>"
-  exit 1
-fi
+case "$1" in
+  centos6)
+    BASE_IMAGE="centos:6"
+    ;;
+  centos7)
+    BASE_IMAGE="centos:7"
+    ;;
+  debian9)
+    BASE_IMAGE="debian:9"
+    ;;
+  *)
+    echo "USAGE: $0 <distrib>"
+    exit 1
+    ;;
+esac
 DISTRIB="$1"
 
 # Prepare Dockerfile.
 rm -rf centreon-build-containers
 cp -r /opt/centreon-build/containers centreon-build-containers
 cd centreon-build-containers
-sed "s/@DISTRIB@/$DISTRIB/g" < dependencies/Dockerfile.in > dependencies/Dockerfile
+sed "s/@DISTRIB@/$DISTRIB/g" "s/@BASE_IMAGE@/$BASE_IMAGE/g" < dependencies/Dockerfile.in > dependencies/Dockerfile
 
 # Build image.
 DEP_IMG="ci.int.centreon.com:5000/mon-dependencies:$DISTRIB"
