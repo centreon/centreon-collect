9d5a8d3f24af101608a8fa5c4a2fd8f0d992a311
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-17 14:16:31 +0200

    jobs: rework mon-containers-unittest build script (only one now).

---
+++ jobs/containers/mon-containers-unittest.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Check arguments.
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <distrib>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Prepare Dockerfile.
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+sed "s/@DISTRIB@/$DISTRIB/g" < unittest/Dockerfile.in > unittest/Dockerfile
+
+# Build image.
+UNITTEST_IMG="ci.int.centreon.com:5000/mon-unittest:$DISTRIB"
+docker pull "ci.int.centreon.com:5000/mon-dependencies:$DISTRIB"
+docker build --no-cache -t "$UNITTEST_IMG" -f unittest/Dockerfile .
+docker push "$UNITTEST_IMG"
