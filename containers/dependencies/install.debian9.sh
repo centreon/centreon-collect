cc97e2d32df28a5605c2bd1866dba8ed2f204215
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-17 13:53:55 +0200

    containers: final fix to Node.js install on Debian 9 for mon-dependencies.

--- containers/dependencies/install.debian9.sh
+++ containers/dependencies/install.debian9.sh
@@ -11,7 +11,7 @@ apt-get update
 apt-get install curl netcat-openbsd
 
 # Install Node.js repository.
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
 # Install dependencies.
 xargs apt-get install < /tmp/dependencies.txt
