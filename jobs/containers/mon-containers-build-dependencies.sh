daa2272ecfc2fafc8e6dfea3e9124592d6f8ab28
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-17 15:21:55 +0200

    jobs: rework mon-containers-build-dependencies build script (only one now).

---
+++ jobs/containers/mon-containers-build-dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Check arguments.
+case "$1" in
+  centos6)
+    BASE_IMAGE="alanfranz/drb-epel-6-x86-64"
+    ;;
+  centos7)
+    BASE_IMAGE="alanfranz/drb-epel-7-x86-64"
+    ;;
+  debian9)
+    BASE_IMAGE="debian:9"
+    ;;
+  *)
+    echo "USAGE: $0 <distrib>"
+    exit 1
+    ;;
+esac
+DISTRIB="$1"
+
+# Prepare Dockerfile.
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+sed -e "s/@DISTRIB@/$DISTRIB/g" -e "s/@BASE_IMAGE@/$BASE_IMAGE/g" < build-dependencies/Dockerfile.in > build-dependencies/Dockerfile
+
+# Build image.
+BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
+docker pull "$BASE_IMAGE"
+docker build --no-cache -t "$BUILD_IMG" -f build-dependencies/Dockerfile .
+docker push "$BUILD_IMG"
