67d35c1c701afc88cfe6fa93e1cc05624b89778a
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-19 12:17:42 +0200

    jobs: add Debian 9 specific script for Centreon Broker packaging job.

---
+++ jobs/broker/3.4/mon-broker-package.debian9.sh
@@ -0,0 +1,22 @@
+# Prepare build directory.
+rm -rf debuildir
+mkdir debuildir
+cp "$PROJECT-$VERSION.tar.gz" "debuildir/$PROJECT_$VERSION.orig.tar.gz"
+cd debuildir
+tar xzf "$PROJECT_$VERSION.orig.tar.gz"
+cp -r "../packaging-$PROJECT/debian" "$PROJECT-$VERSION/"
+sed -e "s/@VERSION@/$VERSION/g" -e "s/@RELEASE@/$RELEASE/g" < debian/changelog.in > debian/changelog
+cd ..
+
+# Launch debuild.
+containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies:debian9 cd /usr/local/src/debuildir && debuild -us -uc -i`
+docker cp debuildir "$containerid:/usr/local/src/debuildir"
+docker start "$containerid"
+
+# Send package to repository.
+docker cp "$containerid:/usr/local/src/debuildir/$PROJECT_$VERSION-$RELEASE_amd64.deb" .
+# XXX
+
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
