dba4ef100cb8561796315b2cc790231287fc1094
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-20 09:57:58 +0200

    jobs: rewrite mon-engine-package.

--- jobs/engine/3.4/mon-engine-package.sh
+++ jobs/engine/3.4/mon-engine-package.sh
@@ -14,7 +14,7 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <centos6|centos7>"
+  echo "USAGE: $0 <centos6|centos7|debian9>"
   exit 1
 fi
 DISTRIB="$1"
@@ -22,28 +22,8 @@ DISTRIB="$1"
 # Pull mon-build-dependencies container.
 docker pull ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB
 
-# Create input and output directories for docker-rpm-builder.
-rm -rf input
-mkdir input
-rm -rf output
-mkdir output
-
-# Retrieve spectemplate and additional source files.
-cp `dirname $0`/../../../packaging/engine/* input/
-
 # Retrieve sources.
-cd input
 get_internal_source "engine/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
-cd ..
 
-# Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB input output
-
-# Copy files to server.
-if [ "$DISTRIB" = 'centos6' ] ; then
-  DISTRIB='el6'
-else
-  DISTRIB='el7'
-fi
-put_internal_rpms "3.4" "$DISTRIB" "x86_64" "engine" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
-put_internal_rpms "3.5" "$DISTRIB" "x86_64" "engine" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
+# Run distribution-dependent script.
+. `dirname $0`/mon-engine-package.$DISTRIB.sh
