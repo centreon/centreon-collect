8d031b00871eccdb945e01a78cc9ced6aa90570a
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-20 14:15:33 +0200

    jobs: rework 3.5 mon-web-package to introduce Debian 9 packages.

---
+++ centreon-build/jobs/web/3.5/mon-web-package.centos7.sh
@@ -0,0 +1,16 @@
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Retrieve sources.
+mv "centreon-$VERSION.tar.gz" input/
+cp -r packaging-centreon-web/rpm/centreon-dev.spectemplate input/
+cp -r packaging-centreon-web/src/dev/* input/
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos7 input output
+
+# Publish RPMs.
+put_internal_rpms "3.5" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
