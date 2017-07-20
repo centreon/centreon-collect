bf2728990d8d4bca440acd02a67e1e620e8c3441
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-20 14:15:33 +0200

    jobs: rework 3.5 mon-web-package to introduce Debian 9 packages.

--- jobs/web/3.5/mon-web-package.sh
+++ jobs/web/3.5/mon-web-package.sh
@@ -5,13 +5,16 @@ set -x
 
 . `dirname $0`/../../common.sh
 
+# Project.
+PROJECT=centreon-web
+
 # Check arguments.
 if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   echo "You need to specify VERSION and RELEASE environment variables."
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <centos6|centos7>"
+  echo "USAGE: $0 <centos6|centos7|debian9>"
   exit 1
 fi
 DISTRIB="$1"
@@ -19,13 +22,11 @@ DISTRIB="$1"
 # Pull mon-build-dependencies container.
 docker pull ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB
 
-# Create input and output directories for docker-rpm-builder.
-rm -rf input
-mkdir input
-rm -rf output
-mkdir output
+# Retrieve sources.
+get_internal_source "web/$PROJECT-$VERSION-$RELEASE/centreon-$VERSION.tar.gz"
+get_internal_source "web/$PROJECT-$VERSION-$RELEASE/centreon-web-$VERSION.tar.gz"
 
-# Retrieve spec file.
+# Retrieve packaging files.
 if [ \! -d packaging-centreon-web ] ; then
   git clone http://gitbot:gitbot@git.int.centreon.com/packaging-centreon packaging-centreon-web
 else
@@ -33,23 +34,6 @@ else
   git pull
   cd ..
 fi
-cp packaging-centreon-web/rpm/centreon-dev.spectemplate input/
 
-# Retrieve additional sources.
-cp packaging-centreon-web/src/dev/* input
-
-# Retrieve sources.
-cd input
-get_internal_source "web/centreon-web-$VERSION-$RELEASE/centreon-$VERSION.tar.gz"
-cd ..
-
-# Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB input output
-
-# Copy files to server.
-if [ "$DISTRIB" = "centos6" ] ; then
-  DISTRIB='el6'
-else
-  DISTRIB='el7'
-fi
-put_internal_rpms "3.5" "$DISTRIB" "noarch" "web" "centreon-web-$VERSION-$RELEASE" output/noarch/*.rpm
+# Run distribution-dependent script.
+. `dirname $0`/mon-web-package.$DISTRIB.sh
