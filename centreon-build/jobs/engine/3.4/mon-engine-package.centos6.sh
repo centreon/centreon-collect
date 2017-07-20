4edfc6ad93ad7ee0d9ee4b2a3ee53beef64fc9da
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-20 09:57:58 +0200

    jobs: rewrite mon-engine-package.

---
+++ centreon-build/jobs/engine/3.4/mon-engine-package.centos6.sh
@@ -0,0 +1,16 @@
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Retrieve source tarball, spectemplate and additional source files.
+mv "$PROJECT-$VERSION.tar.gz" input/
+cp `dirname $0`/../../../packaging/engine/* input/
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos6 input output
+
+# Publish RPMs.
+put_internal_rpms "3.4" "el6" "x86_64" "engine" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
+put_internal_rpms "3.5" "el6" "x86_64" "engine" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
