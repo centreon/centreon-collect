fc399d87dc969255e0d319407bfb3f98e13056cf
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-07-20 15:10:06 +0200

    containers: install missing Debian 9 package php-curl in mon-build-dependencies and mon-unittest images.

--- containers/unittest/install.debian9.sh
+++ containers/unittest/install.debian9.sh
@@ -10,7 +10,7 @@ xargs apt-get install < /tmp/build-dependencies.txt
 apt-get install phpunit php-xdebug
 
 # Install Composer.
-apt-get install php php-cli php-mbstring php-xml
+apt-get install php php-cli php-curl php-mbstring php-xml
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
