beb44d27996824ae77c4d8431325167136248b7d
bcournaud <bcournaud@centreon.com>
2017-07-21 15:29:17 +0200

    - temporary change map desktop stable build

--- jobs/map/centreon-map-client-endoflife-stable.sh
+++ jobs/map/centreon-map-client-endoflife-stable.sh
@@ -11,15 +11,21 @@ if [ -z "$VERSION" ] ; then
   exit 1
 fi
 
-#IFS='.' read -ra VERSION_TABLE <<< "$VERSION"
+IFS='.' read -a VERSION_TABLE <<< "${VERSION}"
 
-#export MAJOR=${VERSION_TABLE[0]}
-#export MINOR=${VERSION_TABLE[1]}
-#export BUGFIX=${VERSION_TABLE[2]}
+#VERSION_TABLE=$( ${VERSION//./ }))
 
-export MAJOR=4
-export MINOR=2
-export BUGFIX=0
+
+export MAJOR=${VERSION_TABLE[0]}
+export MINOR=${VERSION_TABLE[1]}
+export BUGFIX=${VERSION_TABLE[2]}
+
+echo $MAJOR
+echo $MINOR
+echo $BUGFIX
+#export MAJOR=4
+#export MINOR=2
+#export BUGFIX=0
 
 # Move artifacts to the stable directory.
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
@@ -29,21 +35,21 @@ $SSH_REPO mv "/srv/sources/map/testing/$PROJECT-$VERSION*" "/srv/sources/map/sta
 # BCO: /!\ This works only for version 4.2.X
 # The url is specific for this version. We have to change it for new minor versions
 
-BASE_INTERNAL_URL='/srv/sources/map/stable/$PROJECT-$VERSION/'
-BASE_EXTERNAL_URL='s3://centreon-download/enterprises/centreon-map/centreon-map-4.2/centreon-map-4.2/9ae03a4457fa0ce578379a4e0c8b51f2/'
+BASE_INTERNAL_URL="/srv/sources/map/stable/$PROJECT-$VERSION/"
+BASE_EXTERNAL_URL="s3://centreon-download/enterprises/centreon-map/centreon-map-4.2/centreon-map-4.2/9ae03a4457fa0ce578379a4e0c8b51f2/"
 
 # Copy MacOS .tar.gz version
 PRODUCT_NAME='Centreon-Map4.product-macosx.cocoa.x86_64.tar.gz'
 $SSH_REPO aws s3 cp --acl public-read "$BASE_INTERNAL_URL/$PRODUCT_NAME" "$BASE_EXTERNAL_URL/$PRODUCT_NAME"
 
 # Copy Windows .exe
-PRODUCT_NAME='centreon-map4-desktop-client-$VERSION-SNAPSHOT-x86_64.exe'
-EXTERNAL_PRODUCT_NAME='centreon-map4-desktop-client-$VERSION-x86_64.exe'
+PRODUCT_NAME="centreon-map4-desktop-client-$VERSION-SNAPSHOT-x86_64.exe"
+EXTERNAL_PRODUCT_NAME="centreon-map4-desktop-client-$VERSION-x86_64.exe"
 $SSH_REPO aws s3 cp --acl public-read "$BASE_INTERNAL_URL/$PRODUCT_NAME" "$BASE_EXTERNAL_URL/$EXTERNAL_PRODUCT_NAME"
 
 # Copy Ubuntu .deb
-PRODUCT_NAME='centreon-map4-desktop-client-$VERSION-SNAPSHOT-x86_64.deb'
-EXTERNAL_PRODUCT_NAME='centreon-map4-desktop-client-$VERSION-x86_64.deb'
+PRODUCT_NAME="centreon-map4-desktop-client-$VERSION-SNAPSHOT-x86_64.deb"
+EXTERNAL_PRODUCT_NAME="centreon-map4-desktop-client-$VERSION-x86_64.deb"
 $SSH_REPO aws s3 cp --acl public-read "$BASE_INTERNAL_URL/$PRODUCT_NAME" "$BASE_EXTERNAL_URL/$EXTERNAL_PRODUCT_NAME"
 
 # Copy p2 artifacts to remote server.
