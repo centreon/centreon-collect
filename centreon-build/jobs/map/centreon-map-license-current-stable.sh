8f2cef059d771b5cc5e5f93cc0667f1bde91b10f
bcournaud <bcournaud@centreon.com>
2017-07-31 15:23:19 +0200

    jobs: map: change map license generator folder

--- centreon-build/jobs/map/centreon-map-license-current-stable.sh
+++ centreon-build/jobs/map/centreon-map-license-current-stable.sh
@@ -6,14 +6,14 @@ set -x
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
 
 # Clean previous releases
-$SSH_REPO rm -rf /srv/map-license/
-$SSH_REPO mkdir /srv/map-license/
+$SSH_REPO rm -rf /srv/p2/license/
+$SSH_REPO mkdir /srv/p2/license/
 
 # Copy installer
 INSTALLER_NAME="centreon-studio-license/com.centreon.studio.license.packaging.nsis.x86_64/target/*.exe"
-scp -o StrictHostKeyChecking=no $INSTALLER_NAME "ubuntu@srvi-repo.int.centreon.com:/srv/map-license/"
+scp -o StrictHostKeyChecking=no $INSTALLER_NAME "ubuntu@srvi-repo.int.centreon.com:/srv/p2/license/"
 
 # Copy plugins for automatic update
-$SSH_REPO mkdir /srv/map-license/p2/
-scp -o StrictHostKeyChecking=no -r /com/centreon.studio.license.product/target/repository/* "ubuntu@srvi-repo.int.centreon.com:/srv/map-license/p2/"
+$SSH_REPO mkdir /srv/p2/license/p2/
+scp -o StrictHostKeyChecking=no -r /com/centreon.studio.license.product/target/repository/* "ubuntu@srvi-repo.int.centreon.com:/srv/p2/license/p2/"
 
