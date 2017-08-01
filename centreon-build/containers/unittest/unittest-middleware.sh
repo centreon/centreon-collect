eaa016186390515ca0157c378af5e203f969c503
Kevin Duret <kduret@centreon.com>
2017-08-01 13:48:45 +0200

    fix path for gulp

--- centreon-build/containers/unittest/unittest-middleware.sh
+++ centreon-build/containers/unittest/unittest-middleware.sh
@@ -7,7 +7,7 @@ rm -f /tmp/centreon-middleware_ut.xml
 rm -f /tmp/centreon-middleware_coverage.xml
 cd /usr/local/src/centreon-imp-portal-api/
 npm install
-gulp test:xunit
+./node_modules/gulp/bin/gulp.js test:xunit
 
 # Copy reports.
 mv xunit.xml /tmp/centreon-middleware_ut.xml
