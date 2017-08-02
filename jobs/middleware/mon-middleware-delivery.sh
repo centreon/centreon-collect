82a94f05d861e9cb6782dab4d451ca1974bc2a18
Kevin Duret <kduret@centreon.com>
2017-08-02 15:34:19 +0200

    update delivery job of middleware

--- jobs/middleware/mon-middleware-delivery.sh
+++ jobs/middleware/mon-middleware-delivery.sh
@@ -3,9 +3,17 @@
 set -e
 set -x
 
-# Pull new image.
-MIDDLEWARE_IMAGE=ci.int.centreon.com:5000/mon-middleware:latest
-docker pull $MIDDLEWARE_IMAGE
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Set Docker images as latest.
+REGISTRY='ci.int.centreon.com:5000'
+docker pull "$REGISTRY/mon-middleware-$VERSION-$RELEASE:latest"
+docker tag "$REGISTRY/mon-middleware-$VERSION-$RELEASE:latest" "$REGISTRY/mon-middleware:latest"
+docker push "$REGISTRY/mon-middleware:latest"
 
 # Generate Docker Compose file.
 sed -e "s#@MIDDLEWARE_IMAGE@#$MIDDLEWARE_IMAGE#g" -e 's/3000/3000:3000/g' -e 's/3306/33060:3306/g' < `dirname $0`/../../containers/middleware/docker-compose-standalone.yml.in > /opt/middleware/docker-compose.yml
