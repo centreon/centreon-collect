62f350ca6c7b8973f31e2e03ab5ed956300b4096
bcournaud <bcournaud@centreon.com>
2017-08-08 12:07:14 +0200

    jobs: map: fix license generator script

--- jobs/map/centreon-map-license-current-stable.sh
+++ jobs/map/centreon-map-license-current-stable.sh
@@ -10,7 +10,7 @@ $SSH_REPO rm -rf /srv/p2/license/
 $SSH_REPO mkdir /srv/p2/license/
 
 # Copy installer
-INSTALLER_NAME="centreon-studio-decktop-client/com.centreon.studio.license.packaging.nsis.x86_64/target/*.exe"
+INSTALLER_NAME="centreon-studio-desktop-client/com.centreon.studio.license.packaging.nsis.x86_64/target/*.exe"
 scp -o StrictHostKeyChecking=no $INSTALLER_NAME "ubuntu@srvi-repo.int.centreon.com:/srv/p2/license/"
 
 # Copy plugins for automatic update
