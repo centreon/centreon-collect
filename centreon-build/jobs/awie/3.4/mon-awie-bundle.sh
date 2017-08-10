e5149be949033e5ba6d7fae5b986787b1abf0f60
loiclau <loic.lau@gmail.com>
2017-08-10 09:31:11 +0200

    * add api-web-import-export

---
+++ centreon-build/jobs/awie/3.4/mon-awie-bundle.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos6|centos7>"
+  exit 1
+fi
+DISTRIB="$1"
+
+# Pull Centreon Web image.
+WEB_IMAGE=ci.int.centreon.com:5000/mon-ppm-3.4:$DISTRIB
+docker pull $WEB_IMAGE
+
+# Prepare Dockerfile.
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+sed "s/@DISTRIB@/$DISTRIB/g" < awie/3.4/awie.Dockerfile.in > awie/Dockerfile
+
+# Build image.
+REGISTRY="ci.int.centreon.com:5000"
+AWIE_IMAGE="$REGISTRY/mon-awie-$VERSION-$RELEASE:$DISTRIB"
+AWIE_WIP_IMAGE="$REGISTRY/mon-awie-3.4-wip:$DISTRIB"
+docker build --no-cache -t "$AWIE_IMAGE" -f awie/Dockerfile .
+docker push "$AWIE_IMAGE"
+docker tag "$AWIE_IMAGE" "$AWIE_WIP_IMAGE"
+docker push "$AWIE_WIP_IMAGE"
