4f922703ed41ff4cf1c8b357b02a3b4b97fb06c6
Kevin Duret <kduret@centreon.com>
2017-08-11 10:51:03 +0200

    improve local acceptance test for bam (get packages)

--- script/bam-dev.php
+++ script/bam-dev.php
@@ -3,6 +3,7 @@
 // Load Centreon Build library.
 $centreon_build_dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..';
 require_once($centreon_build_dir . DIRECTORY_SEPARATOR . 'script' . DIRECTORY_SEPARATOR . 'common.php');
+require_once($centreon_build_dir . DIRECTORY_SEPARATOR . 'script' . DIRECTORY_SEPARATOR . 'get_packages.php');
 
 # Check arguments.
 if ($argc <= 2) {
