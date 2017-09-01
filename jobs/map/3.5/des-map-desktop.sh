e50f24454a2af02a7c9822ffbe8d12a278283801
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-09-01 09:59:43 +0200

    jobs: fix relative source paths in des-map-desktop and des-map-server.

--- jobs/map/3.5/des-map-desktop.sh
+++ jobs/map/3.5/des-map-desktop.sh
@@ -20,7 +20,7 @@ get_internal_source "map/$PROJECT-$VERSION-$RELEASE/$PROJECT-desktop-$VERSION.ta
 tar xzf "$PROJECT-desktop-$VERSION.tar.gz"
 
 # Build with Maven.
-cd "$PROJECT-desktop-$VERSION/desktop"
+cd "$PROJECT-desktop-$VERSION"
 mvn -f com.centreon.studio.client.parent/pom.xml clean install
 
 # Find version.
@@ -38,12 +38,12 @@ ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mkdir -p $pa
 scp -o StrictHostKeyChecking=no -r centreon-studio-desktop-client/com.centreon.studio.client.product/target/repository/* "ubuntu@srvi-repo.int.centreon.com":$path
 
 # Move all installers to an install folder.
-rm -rf ../../installs
-mkdir ../../installs
-cp com.centreon.studio.client.packaging.deb.amd64/target/*.deb ../../installs/
-cp com.centreon.studio.client.packaging.nsis.x86_64/target/*.exe ../../installs/
-cp com.centreon.studio.client.product/target/products/Centreon-Map4.product-macosx.cocoa.x86_64.tar.gz ../../installs/
-cd ../..
+rm -rf ../installs
+mkdir ../installs
+cp com.centreon.studio.client.packaging.deb.amd64/target/*.deb ../installs/
+cp com.centreon.studio.client.packaging.nsis.x86_64/target/*.exe ../installs/
+cp com.centreon.studio.client.product/target/products/Centreon-Map4.product-macosx.cocoa.x86_64.tar.gz ../installs/
+cd ..
 
 # Copy installers to remote repository
 scp -o StrictHostKeyChecking=no -r installs/* "ubuntu@srvi-repo.int.centreon.com:/srv/p2/unstable/"
