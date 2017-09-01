996308bd8bdaa8562e03b611e1949a9e869e3e3d
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-09-01 09:22:10 +0200

    jobs: rewrite des-map-server.

---
+++ jobs/map/3.5/des-map-server.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-map
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Fetch sources.
+rm -rf "$PROJECT-server-$VERSION.tar.gz" "$PROJECT-server-$VERSION"
+get_internal_source "map/$PROJECT-server-$VERSION-$RELEASE/$PROJECT-server-$VERSION.tar.gz"
+tar xzf "$PROJECT-server-$VERSION.tar.gz"
+
+# Tweak pom files.
+cd "$PROJECT-server-$VERSION/server"
+sed -i 's/<project.release>1/<project.release>'"$RELEASE"'/g' com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat6/pom.xml
+sed -i 's/<project.release>1/<project.release>'"$RELEASE"'/g' com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/pom.xml
+
+# Build with Maven.
+mvn -f com.centreon.studio.server.parent/pom.xml clean install
+
+# This is run once the Maven build terminated.
+FILES_TOMCAT6='com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat6/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm'
+FILES_TOMCAT7='com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/target/rpm/centreon-map4-server/RPMS/noarch/*.rpm'
+put_internal_rpms "3.4" "el6" "noarch" "map" "" $FILES_TOMCAT6
+put_internal_rpms "3.4" "el7" "noarch" "map" "" $FILES_TOMCAT7
+put_internal_rpms "3.5" "el6" "noarch" "map" "" $FILES_TOMCAT6
+put_internal_rpms "3.5" "el7" "noarch" "map" "" $FILES_TOMCAT7
