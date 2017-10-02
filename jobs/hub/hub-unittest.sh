127eb0caf55cd50d1885047cc27e0915ca4e039e
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-10-02 11:20:09 +0200

    jobs: add hub-unittest.

---
+++ jobs/hub/hub-unittest.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-hub-ui
+
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+get_internal_source "middleware/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+
+# Run unit tests.
+cd "$PROJECT-$VERSION"
+npm install
+npm run client:test:unit
