258ca5f0157dbfba75f75b8498bdcee4d01b3aab
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-10-13 11:13:34 +0200

    jobs: add hub-bundle.

---
+++ centreon-build/jobs/hub/hub-bundle.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-hub-ui
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Prepare build directory.
+rm -rf centreon-build-containers
+cp -r /opt/centreon-build/containers centreon-build-containers
+cd centreon-build-containers
+get_internal_source "hub/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-release.tar.gz"
+rm -rf "$PROJECT-$VERSION-release" "$PROJECT"
+tar xzf "$PROJECT-$VERSION-release.tar.gz"
+mv "$PROJECT-$VERSION-release" "$PROJECT"
+
+# Pull base image.
+docker pull httpd:latest
+
+# Build image.
+REGISTRY="ci.int.centreon.com:5000"
+HUB_IMAGE="$REGISTRY/hub-$VERSION-$RELEASE:latest"
+HUB_WIP_IMAGE="$REGISTRY/hub-wip:latest"
+docker build --no-cache -t "$HUB_IMAGE" -f hub/Dockerfile .
+docker push "$HUB_IMAGE"
+docker tag "$HUB_IMAGE" "$HUB_WIP_IMAGE"
+docker push "$HUB_WIP_IMAGE"
