6e3b9bd3915ce6edef218c69d997707afb542a64
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-10-24 15:19:11 +0200

    jobs: add hub-acceptance.

---
+++ jobs/hub/hub-acceptance.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-hub-ui
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Pull images.
+REGISTRY="ci.int.centreon.com:5000"
+HUB_IMAGE="$REGISTRY/hub-$VERSION-$RELEASE:latest"
+MIDDLEWARE_IMAGE="$REGISTRY/mon-middleware:latest"
+REDIS_IMAGE=redis:latest
+OPENLDAP_IMAGE="$REGISTRY/mon-openldap:latest"
+docker pull $HUB_IMAGE
+docker pull $MIDDLEWARE_IMAGE
+docker pull $REDIS_IMAGE
+docker pull $OPENLDAP_IMAGE
+
+# Get sources.
+rm -rf "$PROJECT-$VERSION" "$PROJECT-$VERSION.tar.gz"
+get_internal_source "hub/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+cd "$PROJECT-$VERSION"
+
+# Prepare Docker Compose file.
+sed -e 's#@WEB_IMAGE@#'$HUB_IMAGE'#g' < `dirname $0`/../../containers/middleware/docker-compose-web.yml.in > docker-compose-hub.yml
+
+# Prepare behat.yml.
+alreadyset=`grep docker-compose-middleware.yml < behat.yml || true`
+if [ -z "$alreadyset" ] ; then
+  sed -i 's#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:#    Centreon\\Test\\Behat\\Extensions\\ContainerExtension:\n      log_directory: ../acceptance-logs\n      hub: docker-compose-hub.yml#g' behat.yml
+fi
+
+# Run acceptance tests.
+rm -rf ../xunit-reports
+mkdir ../xunit-reports
+rm -rf ../acceptance-logs
+mkdir ../acceptance-logs
+composer install
+ls features/*.feature | parallel ./vendor/bin/behat --format=pretty --out=std --format=junit --out="../xunit-reports/{/.}" "{}" || true
