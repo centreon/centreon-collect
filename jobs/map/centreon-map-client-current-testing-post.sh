90730867df5bfed47e650bc877b174981b2af5db
bertrand cournaud <bcournaud@centreon.com>
2017-11-06 16:00:57 +0100

    jobs: map: Add documentation

--- jobs/map/centreon-map-client-current-testing-post.sh
+++ jobs/map/centreon-map-client-current-testing-post.sh
@@ -5,9 +5,11 @@ set -x
 
 #
 # This is run once the Maven build terminated.
+# The generated installers and plugins will be copied into the internal repository
+# (http://srvi-repo.int.centreon.com).
 #
 
-# Find version.
+# Get the version number from the .deb filename (the version is in the filename).
 filename='desktop/com.centreon.studio.client.packaging.deb.amd64/target/*.deb'
 VERSION=$(echo $filename | grep -Po '(?<=-client-)[0-9.]+')
 major=`echo $VERSION | cut -d . -f 1`
@@ -18,12 +20,12 @@ if [ -z "$major" -o -z "$minor" -o -z "$patch" ] ; then
   exit 1
 fi
 
-# Recreate p2 directory.
+# On the remote repository, we delete the existing p2 folder and recreate it.
 path="/srv/p2/testing/$major/$minor/"
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" rm -rf $path
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mkdir -p $path
 
-# Copy modules.
+# Copy all modules into the corresponding p2 folder in the remote repository.
 scp -o StrictHostKeyChecking=no -r desktop/com.centreon.studio.client.product/target/repository/* "ubuntu@srvi-repo.int.centreon.com":$path
 
 # Move all installers to an install folder.
