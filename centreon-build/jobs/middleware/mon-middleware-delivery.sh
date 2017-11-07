72e5bda6419a88069206f962af784399d4991a3e
Kevin Duret <kduret@centreon.com>
2017-11-07 13:16:43 +0100

    chore(hub): add middleware container with dataset

--- centreon-build/jobs/middleware/mon-middleware-delivery.sh
+++ centreon-build/jobs/middleware/mon-middleware-delivery.sh
@@ -15,6 +15,11 @@ docker pull "$REGISTRY/mon-middleware-$VERSION-$RELEASE:latest"
 docker tag "$REGISTRY/mon-middleware-$VERSION-$RELEASE:latest" "$REGISTRY/mon-middleware:latest"
 docker push "$REGISTRY/mon-middleware:latest"
 
+# Build middleware dataset image
+MIDDLEWARE_DATASET_IMAGE="$REGISTRY/mon-middleware-dataset:latest"
+docker build --no-cache -t "$MIDDLEWARE_DATASET_IMAGE" -f middleware/middleware-dataset.Dockerfile .
+docker push "$MIDDLEWARE_DATASET_IMAGE"
+
 # Generate Docker Compose file.
 sed -e "s#@MIDDLEWARE_IMAGE@#$REGISTRY/mon-middleware:latest#g" -e 's/3000/3000:3000/g' -e 's/3306/3306:3306/g' < `dirname $0`/../../containers/middleware/docker-compose-standalone.yml.in > docker-compose.yml
 
