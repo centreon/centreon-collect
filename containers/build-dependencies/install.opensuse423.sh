231077705b56c431be95e22440afdc709be42103
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-12-13 10:37:09 +0100

    fix(containers): fix installation of mon-build-dependencies:opensuse423.

--- containers/build-dependencies/install.opensuse423.sh
+++ containers/build-dependencies/install.opensuse423.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 # Install required build dependencies for all Centreon projects.
-xargs zypper install --downloadonly < /tmp/build-dependencies.txt
+xargs zypper install --download-only < /tmp/build-dependencies.txt
