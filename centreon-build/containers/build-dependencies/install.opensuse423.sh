604be4f349c7a9bee3718f75b80f3da9640f4982
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-12-13 14:05:07 +0100

    fix(containers): install fake yum-builddep in mon-build-dependencies:opensuse423.

--- centreon-build/containers/build-dependencies/install.opensuse423.sh
+++ centreon-build/containers/build-dependencies/install.opensuse423.sh
@@ -5,3 +5,10 @@ set -x
 
 # Install required build dependencies for all Centreon projects.
 xargs zypper --non-interactive install --download-only < /tmp/build-dependencies.txt
+
+# Install fake yum-builddep binary to install build dependencies.
+cat > /usr/local/bin/yum-builddep <<EOF
+#!/bin/sh
+zypper --non-interactive install `rpmspec -q --buildrequires $3`
+EOF
+chmod +x /usr/local/bin/yum-builddep
