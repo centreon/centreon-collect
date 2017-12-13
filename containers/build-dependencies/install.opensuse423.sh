caf024ce931b78f238a9a050103d44512e26ea46
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-12-13 14:12:24 +0100

    fix(containers): fix installation of fake yum-builddep.

--- containers/build-dependencies/install.opensuse423.sh
+++ containers/build-dependencies/install.opensuse423.sh
@@ -9,6 +9,6 @@ xargs zypper --non-interactive install --download-only < /tmp/build-dependencies
 # Install fake yum-builddep binary to install build dependencies.
 cat > /usr/local/bin/yum-builddep <<EOF
 #!/bin/sh
-zypper --non-interactive install `rpmspec -q --buildrequires $3`
+zypper --non-interactive install \`rpmspec -q --buildrequires \$3\`
 EOF
 chmod +x /usr/local/bin/yum-builddep
