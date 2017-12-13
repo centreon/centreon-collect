25cbf4fa61118b2b7584f700ec8dda2e2ba1567d
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-12-13 10:44:46 +0100

    fix(containers): use non-interactive mode of zypper.

--- centreon-build/containers/build-dependencies/install.opensuse423.sh
+++ centreon-build/containers/build-dependencies/install.opensuse423.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 # Install required build dependencies for all Centreon projects.
-xargs zypper install --download-only < /tmp/build-dependencies.txt
+xargs zypper --non-interactive install --download-only < /tmp/build-dependencies.txt
