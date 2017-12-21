fb9fa3bd4b9f20e01639f300a849ac96613ede56
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-12-21 11:11:30 +0100

    fix(jobs): generate CentOS 6 and 7 ISO from centreon-iso-current-testing.

--- jobs/iso/centreon-iso-current-testing.sh
+++ jobs/iso/centreon-iso-current-testing.sh
@@ -9,8 +9,12 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 
+##
+## CENTOS 6
+##
+
 # Launch container.
-ISO_IMAGE=ci.int.centreon.com:5000/mon-build-iso:latest
+ISO_IMAGE=ci.int.centreon.com:5000/mon-build-iso:centos6
 docker pull $ISO_IMAGE
 containerid=`docker create --privileged $ISO_IMAGE /usr/local/bin/container.sh $VERSION`
 
@@ -25,3 +29,24 @@ docker rm "$containerid"
 
 # Send to srvi-repo.
 scp -o StrictHostKeyChecking=no ces.iso "ubuntu@srvi-repo.int.centreon.com:/srv/iso/centreon-$VERSION.$RELEASE-el6-x86_64.iso"
+
+##
+## CENTOS 7
+##
+
+# Launch container.
+ISO_IMAGE=ci.int.centreon.com:5000/mon-build-iso:centos7
+docker pull $ISO_IMAGE
+containerid=`docker create --privileged $ISO_IMAGE /usr/local/bin/container.sh $VERSION`
+
+# Copy construction scripts to container.
+docker cp `dirname $0`/../../containers/iso/centos7/addon "$containerid:/tmp/addon"
+docker start -a "$containerid"
+docker cp "$containerid:/tmp/ces.iso" "ces.iso"
+
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
+
+# Send to srvi-repo.
+scp -o StrictHostKeyChecking=no ces.iso "ubuntu@srvi-repo.int.centreon.com:/srv/iso/centreon-$VERSION.$RELEASE-el7-x86_64.iso"
