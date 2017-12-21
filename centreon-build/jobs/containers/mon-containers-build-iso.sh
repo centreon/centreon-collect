d9bf87412dee98852223485e30e4eb20e790f2f8
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-12-21 10:45:31 +0100

    fix(job): allow mon-containers-build-iso to build centos6 et centos7 flavors of the image.

--- centreon-build/jobs/containers/mon-containers-build-iso.sh
+++ centreon-build/jobs/containers/mon-containers-build-iso.sh
@@ -3,12 +3,32 @@
 set -e
 set -x
 
-# Pull base image.
-docker pull centos:7.4.1708
+# CentOS 6.
+if [ "$1" = "centos6" ] ; then
+  # Pull base image.
+  docker pull ubuntu:latest
 
-# Build image.
-cd `dirname $0`/../../containers
-docker build -t ci.int.centreon.com:5000/mon-build-iso:centos7 -f iso/centos7/build-iso.Dockerfile .
+  # Build image.
+  cd `dirname $0`/../../containers
+  docker build -t ci.int.centreon.com:5000/mon-build-iso:centos6 -f iso/centos7/build-iso.Dockerfile .
 
-# Push image.
-docker push ci.int.centreon.com:5000/mon-build-iso:centos7
+  # Push image.
+  docker push ci.int.centreon.com:5000/mon-build-iso:centos6
+
+# CentOS 7.
+elif [ "$1" = "centos7" ] ; then
+  # Pull base image.
+  docker pull centos:7.4.1708
+
+  # Build image.
+  cd `dirname $0`/../../containers
+  docker build -t ci.int.centreon.com:5000/mon-build-iso:centos7 -f iso/centos7/build-iso.Dockerfile .
+
+  # Push image.
+  docker push ci.int.centreon.com:5000/mon-build-iso:centos7
+
+# Unknown distrib.
+else
+  echo "Unsupported distribution $1"
+  exit 1
+fi
