c6450034213cd4adc6d885abd16d70a8808689df
Guillaume28 <Guillaume28@users.noreply.github.com>
2017-12-21 10:40:27 +0100

    fix(iso): improve ISO generation for CentOS 7

--- centreon-build/jobs/containers/mon-containers-build-iso.sh
+++ centreon-build/jobs/containers/mon-containers-build-iso.sh
@@ -4,11 +4,11 @@ set -e
 set -x
 
 # Pull base image.
-docker pull ubuntu:latest
+docker pull centos:7.4.1708
 
 # Build image.
 cd `dirname $0`/../../containers
-docker build -t ci.int.centreon.com:5000/mon-build-iso:latest -f iso/build-iso.Dockerfile .
+docker build -t ci.int.centreon.com:5000/mon-build-iso:centos7 -f iso/centos7/build-iso.Dockerfile .
 
 # Push image.
-docker push ci.int.centreon.com:5000/mon-build-iso:latest
+docker push ci.int.centreon.com:5000/mon-build-iso:centos7
