7f91b83fdba9c45bdfbb1587df8522e40cbf6ee9
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-01-08 12:09:02 +0100

    feat(containers): add new mon-build-dependencies:raspbian9.

--- centreon-build/jobs/containers/mon-containers-build-dependencies.sh
+++ centreon-build/jobs/containers/mon-containers-build-dependencies.sh
@@ -16,6 +16,9 @@ case "$1" in
   debian9)
     BASE_IMAGE="debian:9"
     ;;
+  raspbian9)
+    BASE_IMAGE="ci.int.centreon.com:5000/mon-build-dependencies:debian9"
+    ;;
   *)
     echo "USAGE: $0 <distrib>"
     exit 1
