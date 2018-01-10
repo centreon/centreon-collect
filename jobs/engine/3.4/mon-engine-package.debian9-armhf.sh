f63378eb712aabd92b2fea579df052effa7f8b55
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-01-10 10:05:10 +0100

    feat(jobs): add debian9-armhf job in mon-engine-package.

---
+++ jobs/engine/3.4/mon-engine-package.debian9-armhf.sh
@@ -0,0 +1,22 @@
+# Prepare build directory.
+rm -rf debuildir
+mkdir debuildir
+cp "${PROJECT}-${VERSION}.tar.gz" "debuildir/${PROJECT}_${VERSION}.orig.tar.gz"
+cd debuildir
+tar xzf "${PROJECT}_${VERSION}.orig.tar.gz"
+cp -r `dirname $0`/../../../packaging/engine/debian "${PROJECT}-${VERSION}/"
+sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "${PROJECT}-${VERSION}/debian/changelog.in" > "${PROJECT}-${VERSION}/debian/changelog"
+cd ..
+
+# Launch debuild.
+containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies:debian9 sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && export CC=arm-linux-gnueabihf-gcc && export CXX=arm-linux-gnueabihf-g++ && dpkg-buildpackage -us -uc -d -aarmhf"`
+docker cp debuildir "$containerid:/usr/local/src/debuildir"
+docker start -a "$containerid"
+
+# Send package to repository.
+docker cp "$containerid:/usr/local/src/debuildir/${PROJECT}_${VERSION}-${RELEASE}_armhf.deb" .
+put_internal_debs "3.5" "stretch" *.deb
+
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
