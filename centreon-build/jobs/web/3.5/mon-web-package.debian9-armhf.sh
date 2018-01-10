e5dd88e0108c329868b30de715c17e8ea7b7c274
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-01-10 17:48:10 +0100

    fix(jobs): update debian9-armhf packaging in mon-web-package.

--- centreon-build/jobs/web/3.5/mon-web-package.debian9-armhf.sh
+++ centreon-build/jobs/web/3.5/mon-web-package.debian9-armhf.sh
@@ -1,16 +1,16 @@
 # Prepare build directory.
 rm -rf debuildir
 mkdir debuildir
-cp "${PROJECT}-${VERSION}.tar.gz" "debuildir/${PROJECT}_${VERSION}.orig.tar.gz"
+cp "centreon-${VERSION}.tar.gz" "debuildir/centreon_${VERSION}.orig.tar.gz"
 cd debuildir
-tar xzf "${PROJECT}_${VERSION}.orig.tar.gz"
-cp -r "../packaging-${PROJECT}/debian/3.5" "${PROJECT}-${VERSION}/debian"
-cp -r "../packaging-${PROJECT}/src/dev" "${PROJECT}-${VERSION}/debian/src"
-sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "${PROJECT}-${VERSION}/debian/changelog.in" > "${PROJECT}-${VERSION}/debian/changelog"
+tar xzf "centreon_${VERSION}.orig.tar.gz"
+cp -r "../packaging-${PROJECT}/debian/3.5" "centreon-${VERSION}/debian"
+cp -r "../packaging-${PROJECT}/src/dev" "centreon-${VERSION}/debian/src"
+sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "centreon-${VERSION}/debian/changelog.in" > "centreon-${VERSION}/debian/changelog"
 cd ..
 
 # Launch debuild.
-containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies:debian9-armhf sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && export CC=arm-linux-gnueabihf-gcc && export CXX=arm-linux-gnueabihf-g++ && dpkg-buildpackage -us -uc -d -aarmhf"`
+containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies:debian9-armhf sh -c "cd /usr/local/src/debuildir/centreon-${VERSION} && export CC=arm-linux-gnueabihf-gcc && export CXX=arm-linux-gnueabihf-g++ && dpkg-buildpackage -us -uc -d -aarmhf"`
 docker cp debuildir "$containerid:/usr/local/src/debuildir"
 docker start -a "$containerid"
 
