1b3fdf4799cd4baf61006a9eab39c050d353e602
Kevin Duret <kduret@centreon.com>
2018-01-24 16:04:17 +0100

    chore(test): fix npm commands to run tests on hub ui

--- centreon-build/jobs/hub/unittest.sh
+++ centreon-build/jobs/hub/unittest.sh
@@ -8,9 +8,9 @@ cd /usr/local/src/centreon-hub-ui
 npm install
 
 # Run unit tests.
-npm run client:test:build
-npm run client:lint
+npm run test:build
+npm run lint
 
 # Build release.
 sed -i 's#http://localhost:3000/api#http://middleware:3000/api#g' config.js
-npm run client:build
+npm run build:dev
