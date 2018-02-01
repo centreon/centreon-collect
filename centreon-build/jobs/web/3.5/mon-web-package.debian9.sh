07ed72eaff8622a6ec8db418160c3d0e494b81da
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-02-01 14:06:22 +0100

    fix(jobs): fix package naming (from centreon-web to centreon) in mon-web-package.debian9.

--- centreon-build/jobs/web/3.5/mon-web-package.debian9.sh
+++ centreon-build/jobs/web/3.5/mon-web-package.debian9.sh
@@ -1,22 +1,23 @@
 # Prepare build directory.
 rm -rf debuildir
 mkdir debuildir
-cp "${PROJECT}-${VERSION}.tar.gz" "debuildir/${PROJECT}_${VERSION}.orig.tar.gz"
 cd debuildir
-tar xzf "${PROJECT}_${VERSION}.orig.tar.gz"
-cp -r "../packaging-${PROJECT}/debian/3.5" "${PROJECT}-${VERSION}/debian"
-cp -r "../packaging-${PROJECT}/src/dev" "${PROJECT}-${VERSION}/debian/src"
-sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "${PROJECT}-${VERSION}/debian/changelog.in" > "${PROJECT}-${VERSION}/debian/changelog"
+cp "../centreon-${VERSION}.tar.gz" "centreon_${VERSION}.orig.tar.gz"
+tar xzf "centreon_${VERSION}.orig.tar.gz"
+cp -r "../packaging-${PROJECT}/debian/3.5" "centreon-${VERSION}/debian"
+cp -r "../packaging-${PROJECT}/src/dev" "centreon-${VERSION}/debian/src"
+sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "centreon-${VERSION}/debian/changelog.in" > "centreon-${VERSION}/debian/changelog"
 cd ..
 
 # Launch debuild.
-containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies:debian9 sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && debuild -us -uc -i"`
+containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies:debian9 sh -c "cd /usr/local/src/debuildir/centreon-${VERSION} && debuild -us -uc -i"`
 docker cp debuildir "$containerid:/usr/local/src/debuildir"
 docker start -a "$containerid"
 
 # Send package to repository.
-docker cp "$containerid:/usr/local/src/debuildir/${PROJECT}_${VERSION}-${RELEASE}_amd64.deb" .
-put_internal_debs "3.5" "stretch" *.deb
+rm -rf debuildir
+docker cp "$containerid:/usr/local/src/debuildir" .
+put_internal_debs "3.5" "stretch" debuildir/*.deb
 
 # Stop container.
 docker stop "$containerid"
