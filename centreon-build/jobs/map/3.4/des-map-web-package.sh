9c725f16e6b4bf59ba24174a53205dae3e137580
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-02-28 14:42:17 +0100

    fix(jobs): Centreon Map is currently built for Centreon 3.4.

--- centreon-build/jobs/map/3.4/des-map-web-package.sh
+++ centreon-build/jobs/map/3.4/des-map-web-package.sh
@@ -6,12 +6,12 @@ set -x
 . `dirname $0`/../../common.sh
 
 # Project.
-PROJECT=centreon-map-web
+PROJECT=centreon-map
 PACKAGE=centreon-map4-web-client
 
 # Check arguments.
-if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
-  echo "You need to specify VERSION and RELEASE environment variables."
+if [ -z "$VERSION" -o -z "$VERSIONWEB" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION, VERSIONWEB and RELEASE environment variables."
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
@@ -28,8 +28,8 @@ mkdir output
 
 # Fetch sources.
 cd input
-get_internal_source "map-web/$PACKAGE-$VERSION-$RELEASE/$PACKAGE-$VERSION.tar.gz"
-get_internal_source "map-web/$PACKAGE-$VERSION-$RELEASE/$PACKAGE.spectemplate"
+get_internal_source "map/$PROJECT-$VERSION-$RELEASE/$PACKAGE-$VERSIONWEB.tar.gz"
+get_internal_source "map/$PROJECT-$VERSION-$RELEASE/$PACKAGE.spectemplate"
 cd ..
 
 # Pull latest build dependencies.
@@ -37,7 +37,10 @@ BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
 docker pull "$BUILD_IMG"
 
 # Build RPMs.
+OLDVERSION="$VERSION"
+export VERSION="$VERSIONWEB"
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
+export VERSION="$OLDVERSION"
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos6' ] ; then
@@ -48,4 +51,5 @@ else
   echo "Unsupported distribution $DISTRIB."
   exit 1
 fi
-put_internal_rpms "3.4" "$DISTRIB" "noarch" "map-web" "$PACKAGE-$VERSION-$RELEASE" output/noarch/*.rpm
+put_internal_rpms "3.4" "$DISTRIB" "noarch" "map" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
+put_internal_rpms "3.5" "$DISTRIB" "noarch" "map" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
