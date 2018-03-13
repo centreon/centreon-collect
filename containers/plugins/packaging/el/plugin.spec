b6b2af95eae3fd4e782a39d3c530032d65b2575a
Kevin Duret <duret.kevin@gmail.com>
2018-03-13 14:43:52 +0100

    chore(build): add debian package to centreon plugins job (#30)
    * begin build of debian package of plugins
    
    * improve debian package of centreon plugins
    
    * chore(build): update centreon plugins job
    
    * chore(build): add commit to centreon plugins build scriot

---
+++ containers/plugins/packaging/el/plugin.spec
@@ -0,0 +1,32 @@
+Name:           @NAME@
+Version:        @VERSION@
+Release:        @RELEASE@%{?dist}
+Summary:        Centreon plugin
+Group:          Development/Libraries
+License:        ASL 2.0
+URL:            https://www.centreon.com/
+BuildArch:      noarch
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+AutoReqProv:    no
+
+Requires: @REQUIRES@
+
+@CUSTOM_PKG_DATA@
+
+%description
+
+%prep
+
+%build
+
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}/usr/lib/centreon/plugins/
+%{__install} -m 775 @PATH@/plugin/@PLUGIN_NAME@.current %{buildroot}@PLUGIN_PATH@/@PLUGIN_NAME@
+
+%clean
+rm -rf \%{buildroot}
+
+%files
+%defattr(-,root,root,-)
+@PLUGIN_PATH@
--- jobs/plugins/centreon-plugins-current-testing.sh
+++ jobs/plugins/centreon-plugins-current-testing.sh
@@ -5,18 +5,45 @@ set -x
 
 . `dirname $0`/../common.sh
 
+# Build images.
+cd centreon-build/containers
+
+docker pull ci.int.centreon.com:5000/mon-build-dependencies:debian9-armhf
+docker build -t ci.int.centreon.com:5000/centreon-plugins:debian9-armhf -f plugins/debian.Dockerfile .
+docker push ci.int.centreon.com:5000/centreon-plugins:debian9-armhf
+
+docker pull centos:7
+docker build -t ci.int.centreon.com:5000/centreon-plugins:centos7 -f plugins/centos.Dockerfile .
+docker push ci.int.centreon.com:5000/centreon-plugins:centos7
+
 # Build RPMs.
-docker pull ci.int.centreon.com:5000/centreon-plugins:latest
-containerid=`docker create ci.int.centreon.com:5000/centreon-plugins:latest`
+containerid=`docker create ci.int.centreon.com:5000/centreon-plugins:centos7`
 docker start -a "$containerid"
 
 # Publish RPMs.
-rm -rf noarch
-docker cp "$containerid:/script/build/rpmbuild/RPMS/noarch" .
-scp noarch/*.el6.*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el6/testing/noarch/RPMS/
+rm -rf el6
+rm -rf el7
+docker cp "$containerid:/script/centreon-plugins/el6" .
+docker cp "$containerid:/script/centreon-plugins/el7" .
+
+scp el6/*.el6.*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el6/testing/noarch/RPMS/
 ssh ubuntu@srvi-repo.int.centreon.com createrepo /srv/yum/standard/3.4/el6/testing/noarch
-scp noarch/*.el7.*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el7/testing/noarch/RPMS/
+scp el7/*.el7.*.rpm ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/3.4/el7/testing/noarch/RPMS/
 ssh ubuntu@srvi-repo.int.centreon.com createrepo /srv/yum/standard/3.4/el7/testing/noarch
+
+# Cleanup.
+docker rm "$containerid"
+
+
+# Build RPMs.
+containerid=`docker create ci.int.centreon.com:5000/centreon-plugins:debian9-armhf`
+docker start -a "$containerid"
+
+# Publish RPMs.
+rm -rf armhf
+docker cp "$containerid:/script/centreon-plugins/armhf" .
+
+put_internal_debs "3.5" "stretch" armhf/*.deb
 
 # Cleanup.
 docker rm "$containerid"
