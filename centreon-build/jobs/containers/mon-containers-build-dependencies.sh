c182919842d3eaf2281a2ce7b4576fcc6f02d2a3
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-05-28 15:18:13 +0200

    feat(containers): version flavored mon-build-dependencies (mon-build-dependencies-3.4).

--- centreon-build/jobs/containers/mon-containers-build-dependencies.sh
+++ centreon-build/jobs/containers/mon-containers-build-dependencies.sh
@@ -6,7 +6,8 @@ set -x
 . `dirname $0`/../common.sh
 
 # Check arguments.
-case "$1" in
+VERSION="$1"
+case "$2" in
   centos6)
     BASE_IMAGE="alanfranz/drb-epel-6-x86-64"
     ;;
@@ -27,16 +28,16 @@ case "$1" in
     exit 1
     ;;
 esac
-DISTRIB="$1"
+DISTRIB="$2"
 
 # Prepare Dockerfile.
 rm -rf centreon-build-containers
 cp -r /opt/centreon-build/containers centreon-build-containers
 cd centreon-build-containers
-sed -e "s#@DISTRIB@#$DISTRIB#g" -e "s#@BASE_IMAGE@#$BASE_IMAGE#g" < build-dependencies/Dockerfile.in > build-dependencies/Dockerfile
+sed -e "s#@DISTRIB@#$DISTRIB#g" -e "s#@BASE_IMAGE@#$BASE_IMAGE#g" < "build-dependencies/$VERSION/Dockerfile.in" > build-dependencies/Dockerfile
 
 # Build image.
-BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies:$DISTRIB"
+BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies-$VERSION:$DISTRIB"
 docker pull "$BASE_IMAGE"
 docker build --no-cache -t "$BUILD_IMG" -f build-dependencies/Dockerfile .
 docker push "$BUILD_IMG"
