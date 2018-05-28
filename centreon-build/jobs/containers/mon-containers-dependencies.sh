ad393733096669bea834aa12faf969b158677e24
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-05-28 11:20:13 +0200

    feat(containers): version flavored mon-dependencies (mon-dependencies-3.4).

--- centreon-build/jobs/containers/mon-containers-dependencies.sh
+++ centreon-build/jobs/containers/mon-containers-dependencies.sh
@@ -6,7 +6,8 @@ set -x
 . `dirname $0`/../common.sh
 
 # Check arguments.
-case "$1" in
+VERSION="$1"
+case "$2" in
   centos6)
     BASE_IMAGE="centos:6"
     ;;
@@ -21,16 +22,16 @@ case "$1" in
     exit 1
     ;;
 esac
-DISTRIB="$1"
+DISTRIB="$2"
 docker pull "$BASE_IMAGE"
 
 # Prepare Dockerfile.
 rm -rf centreon-build-containers
 cp -r /opt/centreon-build/containers centreon-build-containers
 cd centreon-build-containers
-sed -e "s/@DISTRIB@/$DISTRIB/g" -e "s/@BASE_IMAGE@/$BASE_IMAGE/g" < dependencies/Dockerfile.in > dependencies/Dockerfile
+sed -e "@VERSION@/$VERSION/g" -e "s/@DISTRIB@/$DISTRIB/g" -e "s/@BASE_IMAGE@/$BASE_IMAGE/g" < dependencies/Dockerfile.in > dependencies/Dockerfile
 
 # Build image.
-DEP_IMG="ci.int.centreon.com:5000/mon-dependencies:$DISTRIB"
+DEP_IMG="ci.int.centreon.com:5000/mon-dependencies-$VERSION:$DISTRIB"
 docker build --no-cache -t "$DEP_IMG" -f dependencies/Dockerfile .
 docker push "$DEP_IMG"
