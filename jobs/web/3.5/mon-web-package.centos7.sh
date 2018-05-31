41bfecfd1c83384ea3fa6ab95b6555c65ee5c12f
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-05-31 12:38:34 +0200

    fix(jobs): copy proper source files in mon-web-package.

--- jobs/web/3.5/mon-web-package.centos7.sh
+++ jobs/web/3.5/mon-web-package.centos7.sh
@@ -7,7 +7,7 @@ mkdir output
 # Retrieve sources.
 mv "centreon-$VERSION.tar.gz" input/
 cp -r packaging-centreon-web/rpm/centreon-3.5.spectemplate input/
-cp -r packaging-centreon-web/src/dev/* input/
+cp -r packaging-centreon-web/src/3.5/* input/
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.5:centos7 input output
