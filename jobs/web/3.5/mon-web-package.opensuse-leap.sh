777828e34bbdbedbd3ebb4b1e9d8e0d35211cf1a
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-05-31 09:39:51 +0200

    feat(jobs): use version flavored mon-package for Centreon Web packaging.

--- jobs/web/3.5/mon-web-package.opensuse-leap.sh
+++ jobs/web/3.5/mon-web-package.opensuse-leap.sh
@@ -10,7 +10,7 @@ cp -r packaging-centreon-web/rpm/centreon-dev.spectemplate input/
 cp -r packaging-centreon-web/src/dev/* input/
 
 # Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:opensuse423 input output
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.5:opensuse-leap input output
 
 # Publish RPMs.
-put_internal_rpms "3.5" "os423" "noarch" "web" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
+put_internal_rpms "3.5" "leap" "noarch" "web" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
