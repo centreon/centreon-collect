26ed8e4eb4175db8cb04d181c9066d4c104ac8e4
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-01 09:34:36 +0200

    feat(jobs): use version flavored mon-build-dependencies images in mon-broker-package job.

--- jobs/broker/3.4/mon-broker-package.centos7.sh
+++ jobs/broker/3.4/mon-broker-package.centos7.sh
@@ -9,7 +9,7 @@ mv "$PROJECT-$VERSION.tar.gz" input/
 cp packaging-centreon-broker/rpm/centreon-broker.spectemplate input/
 
 # Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos7 input output
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.5:centos7 input output
 
 # Publish RPMs.
 put_internal_rpms "3.4" "el7" "x86_64" "broker" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
