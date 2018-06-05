bcb1593a73e7aea6af7566b3072f952e3fd171cb
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-05 11:23:11 +0200

    fix(jobs): CentOS 6 is not supported for 3.5 version of mon-autodisco-delivery.

--- centreon-build/jobs/autodisco/3.5/mon-autodisco-delivery.sh
+++ centreon-build/jobs/autodisco/3.5/mon-autodisco-delivery.sh
@@ -11,7 +11,7 @@ fi
 
 # Set Docker images as latest.
 REGISTRY='ci.int.centreon.com:5000'
-for distrib in centos6 centos7 ; do
+for distrib in centos7 ; do
   docker pull "$REGISTRY/mon-autodisco-$VERSION-$RELEASE:$distrib"
   docker tag "$REGISTRY/mon-autodisco-$VERSION-$RELEASE:$distrib" "$REGISTRY/mon-autodisco-3.5:$distrib"
   docker push "$REGISTRY/mon-autodisco-3.5:$distrib"
