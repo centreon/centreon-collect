d9bd14840fc903bae39f38dc08df312d44e13f4e
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-05 15:07:18 +0200

    feat(jobs): update mon-awie-* jobs to properly handle 3.4 and 3.5 versions.

--- centreon-build/jobs/awie/3.5/mon-awie-bundle.sh
+++ centreon-build/jobs/awie/3.5/mon-awie-bundle.sh
@@ -9,25 +9,25 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <centos6|centos7|...>"
+  echo "USAGE: $0 <centos7|...>"
   exit 1
 fi
 DISTRIB="$1"
 
 # Pull Centreon Web image.
-WEB_IMAGE=ci.int.centreon.com:5000/mon-web-3.4:$DISTRIB
+WEB_IMAGE=ci.int.centreon.com:5000/mon-web-3.5:$DISTRIB
 docker pull $WEB_IMAGE
 
 # Prepare Dockerfile.
 rm -rf centreon-build-containers
 cp -r /opt/centreon-build/containers centreon-build-containers
 cd centreon-build-containers
-sed "s/@DISTRIB@/$DISTRIB/g" < awie/3.4/awie.Dockerfile.in > awie/Dockerfile
+sed "s/@DISTRIB@/$DISTRIB/g" < awie/3.5/awie.Dockerfile.in > awie/Dockerfile
 
 # Build image.
 REGISTRY="ci.int.centreon.com:5000"
 AWIE_IMAGE="$REGISTRY/mon-awie-$VERSION-$RELEASE:$DISTRIB"
-AWIE_WIP_IMAGE="$REGISTRY/mon-awie-3.4-wip:$DISTRIB"
+AWIE_WIP_IMAGE="$REGISTRY/mon-awie-3.5-wip:$DISTRIB"
 docker build --no-cache -t "$AWIE_IMAGE" -f awie/Dockerfile .
 docker push "$AWIE_IMAGE"
 docker tag "$AWIE_IMAGE" "$AWIE_WIP_IMAGE"
