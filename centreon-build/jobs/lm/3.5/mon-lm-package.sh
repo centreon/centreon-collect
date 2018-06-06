d5ae3fd7db0fb447e627091a47e8f43eb8f6e207
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-06 10:50:11 +0200

    fix(jobs): remove source encryption from 3.5 mon-lm-package.

--- centreon-build/jobs/lm/3.5/mon-lm-package.sh
+++ centreon-build/jobs/lm/3.5/mon-lm-package.sh
@@ -30,21 +30,15 @@ mkdir input
 rm -rf output
 mkdir output
 
-# Encrypt source archive.
-if [ "$DISTRIB" = "centos7" ] ; then
-  phpversion=54
-else
-  echo "Unsupported distribution $DISTRIB."
-  exit 1
-fi
-curl -F "file=@$PROJECT-$VERSION.tar.gz" -F "version=$phpversion" -F "modulename=$PROJECT" -F 'needlicense=0' 'http://encode.int.centreon.com/api/' -o "input/$PROJECT-$VERSION-php$phpversion.tar.gz"
+# Retrieve sources.
+cp "$PROJECT-$VERSION.tar.gz" input/
+cp "$PROJECT-$VERSION/packaging/$PROJECT.spectemplate" input/
 
 # Pull latest build dependencies.
 BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies-3.5:$DISTRIB"
 docker pull "$BUILD_IMG"
 
 # Build RPMs.
-cp "$PROJECT-$VERSION/packaging/$PROJECT.spectemplate" input
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
 
 # Copy files to server.
