9090bbaa8eb43504e64ace7c17cb0eba1032900b
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-06 10:02:33 +0200

    fix(jobs): minor updates to mon-broker-* and mon-engine-* jobs.

--- centreon-build/jobs/engine/3.4/mon-engine-package.opensuse423.sh
+++ centreon-build/jobs/engine/3.4/mon-engine-package.opensuse423.sh
@@ -12,4 +12,4 @@ cp -r `dirname $0`/../../../packaging/engine/* input/
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.5:opensuse-leap input output
 
 # Publish RPMs.
-put_internal_rpms "3.5" "os423" "x86_64" "engine" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
+put_internal_rpms "3.5" "leap" "x86_64" "engine" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
