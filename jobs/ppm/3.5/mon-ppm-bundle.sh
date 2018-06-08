e112a014d09e607e63808c5212ce63405760178f
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-08 11:26:55 +0200

    fix(containers): update mon-ppm images to properly use base image mon-lm.

--- jobs/ppm/3.5/mon-ppm-bundle.sh
+++ jobs/ppm/3.5/mon-ppm-bundle.sh
@@ -15,27 +15,28 @@ fi
 DISTRIB="$1"
 
 # Pull images.
-WEB_IMAGE=ci.int.centreon.com:5000/mon-web-3.5:$DISTRIB
-docker pull $WEB_IMAGE
-
-# Prepare Dockerfile.
-rm -rf centreon-build-containers
-cp -r `dirname $0`/../../../containers centreon-build-containers
-cd centreon-build-containers
-sed "s/@DISTRIB@/$DISTRIB/g" < ppm/3.5/ppm.Dockerfile.in > ppm/ppm.Dockerfile
-
-# Build images.
 REGISTRY="ci.int.centreon.com:5000"
 PPM_IMAGE="$REGISTRY/mon-ppm-$VERSION-$RELEASE:$DISTRIB"
 PPM_WIP_IMAGE="$REGISTRY/mon-ppm-3.5-wip:$DISTRIB"
+PPM_AUTODISCO_IMAGE="$REGISTRY/mon-ppm-autodisco-$VERSION-$RELEASE:$DISTRIB"
+PPM_AUTODISCO_WIP_IMAGE="$REGISTRY/mon-ppm-autodisco-3.5-wip:$DISTRIB"
+WEB_IMAGE="$REGISTRY/mon-lm-3.5:$DISTRIB"
+docker pull $WEB_IMAGE
+
+# Prepare Dockerfiles.
+rm -rf centreon-build-containers
+cp -r `dirname $0`/../../../containers centreon-build-containers
+cd centreon-build-containers
+sed -e "s#@BASE_IMAGE@#$WEB_IMAGE#g" -e "s#@DISTRIB@#$DISTRIB#g" < ppm/3.5/ppm.Dockerfile.in > ppm/ppm.Dockerfile
+sed -e "s#@BASE_IMAGE@#$PPM_IMAGE#g" -e "s#@DISTRIB@#$DISTRIB#g" < ppm/3.5/ppm-autodisco.Dockerfile.in > ppm/ppm-autodisco.Dockerfile
+
+# Build ppm image.
 docker build --no-cache -t "$PPM_IMAGE" -f ppm/ppm.Dockerfile .
 docker push "$PPM_IMAGE"
 docker tag "$PPM_IMAGE" "$PPM_WIP_IMAGE"
 docker push "$PPM_WIP_IMAGE"
 
-sed -e "s#@IMAGE@#$PPM_IMAGE#g" -e "s#@DISTRIB@#$DISTRIB#g" < ppm/3.5/ppm-autodisco.Dockerfile.in > ppm/ppm-autodisco.Dockerfile
-PPM_AUTODISCO_IMAGE="$REGISTRY/mon-ppm-autodisco-$VERSION-$RELEASE:$DISTRIB"
-PPM_AUTODISCO_WIP_IMAGE="$REGISTRY/mon-ppm-autodisco-3.5-wip:$DISTRIB"
+# Build ppm-autodisco image.
 docker build --no-cache -t "$PPM_AUTODISCO_IMAGE" -f ppm/ppm-autodisco.Dockerfile .
 docker push "$PPM_AUTODISCO_IMAGE"
 docker tag "$PPM_AUTODISCO_IMAGE" "$PPM_AUTODISCO_WIP_IMAGE"
