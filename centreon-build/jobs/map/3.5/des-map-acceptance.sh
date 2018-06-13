32ef72b8ed3f5051541e4e6276198e897bf5afa6
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-13 10:10:00 +0200

    feat(jobs): update des-map-* jobs for Centreon 3.5 compatibility.

---
+++ centreon-build/jobs/map/3.5/des-map-acceptance.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
---
+++ centreon-build/jobs/map/3.5/des-map-delivery.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Tag and push images.
+REGISTRY='ci.int.centreon.com:5000'
+for image in des-map-server des-map-web ; do
+  for distrib in centos7 ; do
+    docker pull "$REGISTRY/$image-$VERSION-$RELEASE:$distrib"
+    docker tag "$REGISTRY/$image-$VERSION-$RELEASE:$distrib" "$REGISTRY/$image-3.4:$distrib"
+    docker push "$REGISTRY/$image-3.4:$distrib"
+  done
+done
