4352c6c36651ba2aa4b48d3a7bae45db6664002a
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-20 17:14:02 +0200

    feat(jobs): update mon-web* jobs to use 18.9 version instead of 3.5.

--- jobs/web/18.9/mon-web-bundle.sh
+++ jobs/web/18.9/mon-web-bundle.sh
@@ -9,25 +9,21 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <centos6|centos7>"
+  echo "USAGE: $0 <centos7|...>"
   exit 1
 fi
 DISTRIB="$1"
-if [ "$DISTRIB" = "centos6" ] ; then
-  CENTOS_VERSION=6
-else
-  CENTOS_VERSION=7
-fi
+CENTOS_VERSION=7
 
 # Target images.
 REGISTRY="ci.int.centreon.com:5000"
-BASE_IMG="$REGISTRY/mon-dependencies-3.5:$DISTRIB"
+BASE_IMG="$REGISTRY/mon-dependencies-18.9:$DISTRIB"
 FRESH_IMG="$REGISTRY/mon-web-fresh-$VERSION-$RELEASE:$DISTRIB"
-FRESH_WIP_IMG="$REGISTRY/mon-web-fresh-3.5-wip:$DISTRIB"
+FRESH_WIP_IMG="$REGISTRY/mon-web-fresh-18.9-wip:$DISTRIB"
 STANDARD_IMG="$REGISTRY/mon-web-$VERSION-$RELEASE:$DISTRIB"
-STANDARD_WIP_IMG="$REGISTRY/mon-web-3.5-wip:$DISTRIB"
+STANDARD_WIP_IMG="$REGISTRY/mon-web-18.9-wip:$DISTRIB"
 WIDGETS_IMG="$REGISTRY/mon-web-widgets-$VERSION-$RELEASE:$DISTRIB"
-WIDGETS_WIP_IMG="$REGISTRY/mon-web-widgets-3.5-wip:$DISTRIB"
+WIDGETS_WIP_IMG="$REGISTRY/mon-web-widgets-18.9-wip:$DISTRIB"
 
 # Pull base image.
 docker pull "$BASE_IMG"
@@ -36,10 +32,10 @@ docker pull "$BASE_IMG"
 rm -rf centreon-build-containers
 cp -r `dirname $0`/../../../containers centreon-build-containers
 cd centreon-build-containers
-sed "s#@BASE_IMAGE@#$BASE_IMG#g;s#@CENTOS_VERSION@#$CENTOS_VERSION#g" < web/3.5/fresh.Dockerfile.in > web/fresh.Dockerfile
-sed "s#@BASE_IMAGE@#$FRESH_IMG#g" < web/3.5/standard.Dockerfile.in > web/standard.Dockerfile
-sed "s#@BASE_IMAGE@#$STANDARD_IMG#g" < web/3.5/widgets.Dockerfile.in > web/widgets.Dockerfile
-sed "s#@VERSION@#3.5#g;s#@DISTRIB@#el$CENTOS_VERSION#g" < repo/centreon-internal.repo.in > repo/centreon-internal.repo
+sed "s#@BASE_IMAGE@#$BASE_IMG#g;s#@CENTOS_VERSION@#$CENTOS_VERSION#g" < web/18.9/fresh.Dockerfile.in > web/fresh.Dockerfile
+sed "s#@BASE_IMAGE@#$FRESH_IMG#g" < web/18.9/standard.Dockerfile.in > web/standard.Dockerfile
+sed "s#@BASE_IMAGE@#$STANDARD_IMG#g" < web/18.9/widgets.Dockerfile.in > web/widgets.Dockerfile
+sed "s#@VERSION@#18.9#g;s#@DISTRIB@#el$CENTOS_VERSION#g" < repo/centreon-internal.repo.in > repo/centreon-internal.repo
 
 # Build 'fresh' image.
 docker build --no-cache --ulimit 'nofile=40000' -t "$FRESH_IMG" -f web/fresh.Dockerfile .
