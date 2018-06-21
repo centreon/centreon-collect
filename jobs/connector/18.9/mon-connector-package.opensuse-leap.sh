0c5169138457283f4a54073eea80dc358f259e2d
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-21 13:49:23 +0200

    feat(jobs): add mon-connector* CI scripts.

---
+++ jobs/connector/18.9/mon-connector-package.opensuse-leap.sh
@@ -0,0 +1,15 @@
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Retrieve source tarball, spectemplate and additional source files.
+mv "$PROJECT-$VERSION.tar.gz" input/
+cp `dirname $0`/../../../packaging/connector/centreon-connector-18.9.spectemplate input/
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key ci.int.centreon.com:5000/mon-build-dependencies-18.9:opensuse-leap input output
+
+# Publish RPMs.
+put_internal_rpms "18.9" "leap" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
