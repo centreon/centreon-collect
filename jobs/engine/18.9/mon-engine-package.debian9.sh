8dc137c90acc438380340a438d8ff216e7e72aad
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-21 09:30:04 +0200

    fix(jobs): properly use relative path in mon-engine-package (debian9 and debian9-armhf).

--- jobs/engine/18.9/mon-engine-package.debian9.sh
+++ jobs/engine/18.9/mon-engine-package.debian9.sh
@@ -4,9 +4,9 @@ mkdir debuildir
 cp "${PROJECT}-${VERSION}.tar.gz" "debuildir/${PROJECT}_${VERSION}.orig.tar.gz"
 cd debuildir
 tar xzf "${PROJECT}_${VERSION}.orig.tar.gz"
-cp -r `dirname $0`/../../../packaging/engine/debian "${PROJECT}-${VERSION}/"
-sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "${PROJECT}-${VERSION}/debian/changelog.in" > "${PROJECT}-${VERSION}/debian/changelog"
 cd ..
+cp -r `dirname $0`/../../../packaging/engine/debian "debuildir/${PROJECT}-${VERSION}/"
+sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "debuildir/${PROJECT}-${VERSION}/debian/changelog.in" > "debuildir/${PROJECT}-${VERSION}/debian/changelog"
 
 # Launch debuild.
 containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies-18.9:debian9 sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && debuild -us -uc -i"`
