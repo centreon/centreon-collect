f2aa6c7bb6c415e25d1d96d85a412b2e1f127854
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-21 09:22:48 +0200

    fix(jobs): clean centreon-engine release jobs.

--- centreon-build/jobs/engine/centreon-engine-3.4-testing.sh
+++ centreon-build/jobs/engine/centreon-engine-3.4-testing.sh
@@ -13,8 +13,8 @@ if [ -z "$COMMIT" -o -z "$RELEASE" ] ; then
 fi
 
 # Pull mon-build-dependencies containers.
-docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos6
-docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos7
+docker pull ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos6
+docker pull ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos7
 
 # Create input and output directories for docker-rpm-builder.
 rm -rf input
@@ -38,11 +38,12 @@ git archive --prefix="$PROJECT-$VERSION/" HEAD | gzip > "../input/$PROJECT-$VERS
 cd ..
 
 # Retrieve spectemplate and additional source files.
-cp -r `dirname $0`/../../packaging/engine/* input/
+cp `dirname $0`/../../packaging/engine/centreon-engine-3.4.spectemplate input/
+cp `dirname $0`/../../packaging/engine/centreonengine_integrate_centreon_engine2centreon.sh input/
 
 # Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos6 input output-centos6
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos7 input output-centos7
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos6 input output-centos6
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos7 input output-centos7
 
 # Copy sources to server.
 SSH_REPO="ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com"
