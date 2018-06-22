f2ca06e9a47e0f89282e441d4f2ba3c96a506eda
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-22 13:22:43 +0200

    feat(jobs): update centreon-widget release jobs to handle 3.4 and 18.9 versions.

---
+++ centreon-build/jobs/widgets/centreon-widget-18.9-testing.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Check arguments.
+if [ -z "$WIDGET" -o -z "$COMMIT" -o -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify WIDGET, COMMIT, VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Pull mon-build-dependencies containers.
+docker pull ci.int.centreon.com:5000/mon-build-dependencies-18.9:centos7
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output-centos7
+mkdir output-centos7
+
+# Create source tarball.
+export PROJECT="centreon-widget-$WIDGET"
+rm -rf "$PROJECT"
+git clone "https://github.com/centreon/$PROJECT"
+cd "$PROJECT"
+git checkout --detach "$COMMIT"
+export SUMMARY="`sed -n 's|\s*<description>\(.*\)</description>|\1|p' $WIDGET/configs.xml 2>/dev/null`"
+rm -rf "../$PROJECT-$VERSION"
+mkdir "../$PROJECT-$VERSION"
+git archive HEAD | tar -C "../$PROJECT-$VERSION" -x
+cd ..
+tar czf "input/$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+
+# Retrieve spec file.
+if [ \! -d packaging-centreon-web ] ; then
+  git clone http://gitbot:gitbot@git.int.centreon.com/packaging-centreon packaging-centreon-web
+else
+  cd packaging-centreon-web
+  git pull
+  cd ..
+fi
+cp packaging-centreon-web/rpm/widgets/centreon-widget-18.9.spectemplate input/
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies-18.9:centos7 input output-centos7
+
+# Copy files to server.
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mkdir -p "/srv/sources/standard/testing/$PROJECT-$VERSION-$RELEASE"
+scp -o StrictHostKeyChecking=no "input/$PROJECT-$VERSION.tar.gz" "ubuntu@srvi-repo.int.centreon.com:/srv/sources/standard/testing/$PROJECT-$VERSION-$RELEASE/"
+FILES_CENTOS7='output-centos7/noarch/*.rpm'
+scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/standard/18.9/el7/testing/noarch/RPMS"
+ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/standard/18.9/el7/testing/noarch
--- centreon-build/jobs/widgets/centreon-widget-3.4-testing.sh
+++ centreon-build/jobs/widgets/centreon-widget-3.4-testing.sh
@@ -10,8 +10,8 @@ if [ -z "$WIDGET" -o -z "$COMMIT" -o -z "$VERSION" -o -z "$RELEASE" ] ; then
 fi
 
 # Pull mon-build-dependencies containers.
-docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos6
-docker pull ci.int.centreon.com:5000/mon-build-dependencies:centos7
+docker pull ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos6
+docker pull ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos7
 
 # Create input and output directories for docker-rpm-builder.
 rm -rf input
@@ -42,11 +42,11 @@ else
   git pull
   cd ..
 fi
-cp packaging-centreon-web/rpm/widgets/centreon-widget.spectemplate input/
+cp packaging-centreon-web/rpm/widgets/centreon-widget-3.4.spectemplate input/
 
 # Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos6 input output-centos6
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies:centos7 input output-centos7
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos6 input output-centos6
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies-3.4:centos7 input output-centos7
 
 # Copy files to server.
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mkdir -p "/srv/sources/standard/testing/$PROJECT-$VERSION-$RELEASE"
