8fea8434e9a69f2536515c12fe995579f718408e
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-22 14:36:51 +0200

    fix(jobs): update des-map* jobs to use 18.9 version instead of 3.5.

--- centreon-build/jobs/map/18.9/des-map-bundle.sh
+++ centreon-build/jobs/map/18.9/des-map-bundle.sh
@@ -16,12 +16,12 @@ DISTRIB="$1"
 
 # Pull base image.
 REGISTRY="ci.int.centreon.com:5000"
-DEP_IMAGE="$REGISTRY/mon-dependencies-3.5:$DISTRIB"
-BASE_IMAGE="$REGISTRY/mon-web-3.5:$DISTRIB"
+DEP_IMAGE="$REGISTRY/mon-dependencies-18.9:$DISTRIB"
+BASE_IMAGE="$REGISTRY/mon-web-18.9:$DISTRIB"
 SERVER_IMAGE="$REGISTRY/des-map-server-$VERSION-$RELEASE:$DISTRIB"
-SERVER_WIP_IMAGE="$REGISTRY/des-map-server-3.5-wip:$DISTRIB"
+SERVER_WIP_IMAGE="$REGISTRY/des-map-server-18.9-wip:$DISTRIB"
 WEB_IMAGE="$REGISTRY/des-map-web-$VERSION-$RELEASE:$DISTRIB"
-WEB_WIP_IMAGE="$REGISTRY/des-map-web-3.5-wip:$DISTRIB"
+WEB_WIP_IMAGE="$REGISTRY/des-map-web-18.9-wip:$DISTRIB"
 docker pull $DEP_IMAGE
 docker pull $BASE_IMAGE
 
@@ -30,8 +30,8 @@ rm -rf centreon-build-containers
 cp -r `dirname $0`/../../../containers centreon-build-containers
 cd centreon-build-containers
 sed "s#@BASE_IMAGE@#$DEP_IMAGE#g" < map/server.Dockerfile.in > map/server.$DISTRIB.Dockerfile
-sed "s#@BASE_IMAGE@#$BASE_IMAGE#g" < map/3.5/web.Dockerfile.in > map/web.$DISTRIB.Dockerfile
-sed "s#@VERSION@#3.5#g;s#@DISTRIB@#el7#g" < repo/centreon-internal.repo.in > repo/centreon-internal.repo
+sed "s#@BASE_IMAGE@#$BASE_IMAGE#g" < map/18.9/web.Dockerfile.in > map/web.$DISTRIB.Dockerfile
+sed "s#@VERSION@#18.9#g;s#@DISTRIB@#el7#g" < repo/centreon-internal.repo.in > repo/centreon-internal.repo
 
 # Server image.
 docker build --no-cache -t "$SERVER_IMAGE" -f map/server.$DISTRIB.Dockerfile .
--- centreon-build/jobs/map/18.9/des-map-server.sh
+++ centreon-build/jobs/map/18.9/des-map-server.sh
@@ -30,4 +30,4 @@ mvn -f com.centreon.studio.server.parent/pom.xml clean install
 # This is run once the Maven build terminated.
 cd ..
 FILES_TOMCAT7="$PROJECT-server-$VERSION/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/target/rpm/centreon-map4-server/RPMS/noarch/"'*.rpm'
-put_internal_rpms "3.5" "el7" "noarch" "map" "" $FILES_TOMCAT7
+put_internal_rpms "18.9" "el7" "noarch" "map" "" $FILES_TOMCAT7
