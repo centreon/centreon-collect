b93a29d49564774a1ad9364729c9aa9effaf25c9
Kevin Duret <kduret@centreon.com>
2018-06-28 16:34:13 +0200

    chore(build): fix mysql start in mediawiki container

--- centreon-build/containers/mediawiki/run.sh
+++ centreon-build/containers/mediawiki/run.sh
@@ -4,7 +4,7 @@ set -x
 
 sed -i -e 's|http://mediawiki|http://'`hostname --ip-address`'|g' /var/www/html/LocalSettings.php
 chmod a+w /var/www/html/config
-service mysqld start
+/etc/init.d/mysqld start
 echo "CREATE USER 'centreon'@'%' IDENTIFIED BY 'centreon'; GRANT ALL PRIVILEGES ON *.* TO 'centreon'@'%' WITH GRANT OPTION;CREATE USER 'centreon'@'localhost' IDENTIFIED BY 'centreon'; GRANT ALL PRIVILEGES ON *.* TO 'centreon'@'localhost' WITH GRANT OPTION;" | mysql --user=root
 cat /usr/share/wikidb.db | mysql --user=root
 httpd -k start
