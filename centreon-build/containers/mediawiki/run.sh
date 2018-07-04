a5654a622b8946af5f611595e4566d23ebf4af39
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-07-04 11:09:20 +0200

    Revert "fix(containers): allow only one IP address in mon-mediawiki LocalSettings.php."
    This reverts commit 3a8f504aeb59b4fa39cc24ea4e3682eb5dfbc062.

--- centreon-build/containers/mediawiki/run.sh
+++ centreon-build/containers/mediawiki/run.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-sed -i -e 's|http://mediawiki|http://'`hostname --ip-address | cut -d ' ' -f 1`'|g' /var/www/html/LocalSettings.php
+sed -i -e 's|http://mediawiki|http://'`hostname --ip-address`'|g' /var/www/html/LocalSettings.php
 chmod a+w /var/www/html/config
 service mysql start
 echo "CREATE USER 'centreon'@'%' IDENTIFIED BY 'centreon'; GRANT ALL PRIVILEGES ON *.* TO 'centreon'@'%' WITH GRANT OPTION;CREATE USER 'centreon'@'localhost' IDENTIFIED BY 'centreon'; GRANT ALL PRIVILEGES ON *.* TO 'centreon'@'localhost' WITH GRANT OPTION;" | mysql --user=root
