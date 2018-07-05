aa5de6f7860a6475def5c6609547628c7d20ac27
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-07-05 08:54:21 +0200

    chore(containers): remove default LDAP configuration from mon-web image.

--- containers/web/18.9/standard.sh
+++ containers/web/18.9/standard.sh
@@ -7,7 +7,6 @@ service mysql start
 httpd -k start
 # Temporary fix to provide default setup while we're working on free plugin packs.
 mysql centreon < /tmp/standard/sql/standard.sql
-mysql centreon < /tmp/standard/sql/openldap.sql
 mysql centreon < /tmp/standard/sql/media.sql
 centreon -d -u admin -p centreon -a POLLERGENERATE -v 1
 centreon -d -u admin -p centreon -a CFGMOVE -v 1
