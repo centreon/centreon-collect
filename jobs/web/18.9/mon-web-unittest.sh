20701e708f9f90544dc5fb5a505818ba6c6e604d
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-07-19 10:11:55 +0200

    Revert "chore(jobs): temporarily disable front-end unit tests in 18.9 mon-web-unittest."
    This reverts commit 9c2fc3c0dcb7b9c2d9b7766435afa296b211cc58.

--- jobs/web/18.9/mon-web-unittest.sh
+++ jobs/web/18.9/mon-web-unittest.sh
@@ -29,24 +29,24 @@ tar xzf "$PROJECT-$VERSION.tar.gz"
 ##### FRONTEND #####
 ####################
 
-# # Prepare NodeJS container.
-# NODEJS_IMAGE="node:6"
-# docker pull "$NODEJS_IMAGE"
-# containerid=`docker create $NODEJS_IMAGE sh /tmp/unittest.sh`
+# Prepare NodeJS container.
+NODEJS_IMAGE="node:6"
+docker pull "$NODEJS_IMAGE"
+containerid=`docker create $NODEJS_IMAGE sh /tmp/unittest.sh`
 
-# # Copy files to container.
-# docker cp "$PROJECT-$VERSION" "$containerid:/usr/local/src/$PROJECT"
-# docker cp `dirname $0`/unittest.sh "$containerid:/tmp/unittest.sh"
+# Copy files to container.
+docker cp "$PROJECT-$VERSION" "$containerid:/usr/local/src/$PROJECT"
+docker cp `dirname $0`/unittest.sh "$containerid:/tmp/unittest.sh"
 
-# # Run unit tests and build release.
-# rm -rf "coverage" "centreon-release-$VERSION"
-# docker start -a "$containerid"
-# docker cp "$containerid:/usr/local/src/centreon/coverage" "coverage"
-# docker cp "$containerid:/usr/local/src/centreon/jest-test-results.xml" "jest-test-results.xml"
+# Run unit tests and build release.
+rm -rf "coverage" "centreon-release-$VERSION"
+docker start -a "$containerid"
+docker cp "$containerid:/usr/local/src/centreon/coverage" "coverage"
+docker cp "$containerid:/usr/local/src/centreon/jest-test-results.xml" "jest-test-results.xml"
 
-# # Stop container.
-# docker stop "$containerid"
-# docker rm "$containerid"
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
 
 
 ###################
