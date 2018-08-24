b047f1a637e1ec84aeff7bd7978a0a0b9d53da3b
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-08-24 15:12:09 +0200

    fix(jobs): update .deb upload routine in mon-clib-package.

--- jobs/clib/18.9/mon-clib-package.debian10.sh
+++ jobs/clib/18.9/mon-clib-package.debian10.sh
@@ -14,8 +14,9 @@ docker cp debuildir "$containerid:/usr/local/src/debuildir"
 docker start -a "$containerid"
 
 # Send package to repository.
-docker cp "$containerid:/usr/local/src/debuildir/${PROJECT}_${VERSION}-${RELEASE}_amd64.deb" .
-put_internal_debs "18.9" "buster" *.deb
+rm -rf debuildir
+docker cp "$containerid:/usr/local/src/debuildir" .
+put_internal_debs "18.9" "buster" debuildir/*.deb
 
 # Stop container.
 docker stop "$containerid"
