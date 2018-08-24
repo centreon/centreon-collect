fa75927c1a38dcb05edd9a91ea8d9fa69e08c6f1
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-08-24 15:08:15 +0200

    fix(jobs): provide different names for Debian 9 and Debian 10 packages in mon-clib-package.

--- jobs/clib/18.9/mon-clib-package.debian10.sh
+++ jobs/clib/18.9/mon-clib-package.debian10.sh
@@ -6,7 +6,7 @@ cd debuildir
 tar xzf "${PROJECT}_${VERSION}.orig.tar.gz"
 cd ..
 cp -r `dirname $0`/../../../packaging/clib/debian "debuildir/${PROJECT}-${VERSION}/"
-sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "debuildir/${PROJECT}-${VERSION}/debian/changelog.in" > "debuildir/${PROJECT}-${VERSION}/debian/changelog"
+sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}.debian10/g" < "debuildir/${PROJECT}-${VERSION}/debian/changelog.in" > "debuildir/${PROJECT}-${VERSION}/debian/changelog"
 
 # Launch debuild.
 containerid=`docker create ci.int.centreon.com:5000/mon-build-dependencies-18.9:debian10 sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && debuild -us -uc -i"`
