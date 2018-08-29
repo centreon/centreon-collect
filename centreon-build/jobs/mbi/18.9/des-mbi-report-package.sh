4dbb290d01eaf13ee9fe12a57e1faaa676137202
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-08-29 09:23:14 +0200

    feat(jobs): update des-mbi-report-package for pipeline compatibility.

--- centreon-build/jobs/mbi/18.9/des-mbi-report-package.sh
+++ centreon-build/jobs/mbi/18.9/des-mbi-report-package.sh
@@ -5,39 +5,40 @@ set -x
 
 . `dirname $0`/../../common.sh
 
+# Project.
+PROJECT=centreon-bi-report
+
 # Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
 if [ "$#" -lt 1 ] ; then
   echo "USAGE: $0 <centos7|...>"
   exit 1
 fi
 DISTRIB="$1"
-PROJECT=centreon-bi-report
+
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+get_internal_source "mbi-report/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+
 # Create input and output directories.
 rm -rf input
 mkdir input
 rm -rf output
 mkdir output
 
-# Get version.
-cd centreon-bi-report
-VERSION=`cat packaging/$PROJECT.spectemplate | grep Version: | cut -d ' ' -f 9`
-export VERSION="$VERSION"
-
-# Get release.
-commit=`git log -1 "$GIT_COMMIT" --pretty=format:%h`
-now=`date +%s`
-export RELEASE="$now.$commit"
-
-# Generate archive of Centreon MBI report.
-git archive --prefix="$PROJECT-$VERSION/" "$GIT_BRANCH" | gzip > "../input/$PROJECT-$VERSION.tar.gz"
-cd ..
+# Retrieve sources.
+cp "$PROJECT-$VERSION.tar.gz" input/
+cp "$PROJECT-$VERSION/packaging/$PROJECT.spectemplate" input/
 
 # Pull latest build dependencies.
 BUILD_IMG="ci.int.centreon.com:5000/mon-build-dependencies-18.9:$DISTRIB"
 docker pull "$BUILD_IMG"
 
 # Build RPMs.
-cp centreon-bi-report/packaging/centreon-bi-report.spectemplate input/
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
 
 # Copy files to server.
