639e3bef1ef65eb17cfe4d79f214f9af04e9f42a
Kevin Duret <kduret@centreon.com>
2018-08-31 10:40:32 +0200

    test(ut): remove frontend unit tests from centreon web

--- jobs/web/18.10/mon-web-unittest.sh
+++ jobs/web/18.10/mon-web-unittest.sh
@@ -30,23 +30,23 @@ tar xzf "$PROJECT-$VERSION.tar.gz"
 ####################
 
 # Prepare NodeJS container.
-NODEJS_IMAGE="node:6"
-docker pull "$NODEJS_IMAGE"
-containerid=`docker create $NODEJS_IMAGE sh /tmp/unittest.sh`
+#NODEJS_IMAGE="node:6"
+#docker pull "$NODEJS_IMAGE"
+#containerid=`docker create $NODEJS_IMAGE sh /tmp/unittest.sh`
 
 # Copy files to container.
-docker cp "$PROJECT-$VERSION" "$containerid:/usr/local/src/$PROJECT"
-docker cp `dirname $0`/unittest.sh "$containerid:/tmp/unittest.sh"
+#docker cp "$PROJECT-$VERSION" "$containerid:/usr/local/src/$PROJECT"
+#docker cp `dirname $0`/unittest.sh "$containerid:/tmp/unittest.sh"
 
 # Run unit tests and build release.
-rm -rf "coverage" "centreon-release-$VERSION"
-docker start -a "$containerid"
-docker cp "$containerid:/usr/local/src/$PROJECT/coverage" "coverage"
-docker cp "$containerid:/usr/local/src/$PROJECT/jest-test-results.xml" "jest-test-results.xml"
+#rm -rf "coverage" "centreon-release-$VERSION"
+#docker start -a "$containerid"
+#docker cp "$containerid:/usr/local/src/$PROJECT/coverage" "coverage"
+#docker cp "$containerid:/usr/local/src/$PROJECT/jest-test-results.xml" "jest-test-results.xml"
 
 # Stop container.
-docker stop "$containerid"
-docker rm "$containerid"
+#docker stop "$containerid"
+#docker rm "$containerid"
 
 
 ###################
