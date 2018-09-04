b80e75de9e327a4ad021f45d4ae887040080bb73
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-09-04 10:18:28 +0200

    feat(jobs): publish tested RPMs to unstable in most delivery jobs.

--- centreon-build/jobs/awie/18.10/mon-awie-delivery.sh
+++ centreon-build/jobs/awie/18.10/mon-awie-delivery.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=centreon-awie
+
 # Check arguments.
 if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   echo "You need to specify VERSION and RELEASE environment variables."
@@ -16,3 +21,6 @@ for distrib in centos7 ; do
   docker tag "$REGISTRY/mon-awie-$VERSION-$RELEASE:$distrib" "$REGISTRY/mon-awie-18.10:$distrib"
   docker push "$REGISTRY/mon-awie-18.10:$distrib"
 done
+
+# Move RPMs to unstable.
+move_internal_rpms_to_unstable "standard" "18.10" "el7" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
