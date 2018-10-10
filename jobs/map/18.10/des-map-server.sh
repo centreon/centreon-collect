6f2151ac1d68432ed9be1311899819fd5279a4f5
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-10-10 14:01:32 +0200

    fix(jobs): sign (unsigned) RPMs in des-map-server.

--- jobs/map/18.10/des-map-server.sh
+++ jobs/map/18.10/des-map-server.sh
@@ -31,6 +31,9 @@ mvn -f com.centreon.studio.server.parent/pom.xml clean install
 cd ..
 FILES_TOMCAT7="$PROJECT-server-$VERSION/com.centreon.studio.server.parent/com.centreon.studio.map.server/com.centreon.studio.map.server.packaging/com.centreon.studio.map.server.packaging.tomcat7/target/rpm/centreon-map4-server/RPMS/noarch/"'*.rpm'
 put_internal_rpms "18.10" "el7" "noarch" "map-server" "$PROJECT-$VERSION-$RELEASE" $FILES_TOMCAT7
+SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
+$SSH_REPO rpm --resign "/srv/yum/internal/18.10/el7/noarch/map-server/$PROJECT-$VERSION-$RELEASE/*.rpm"
+$SSH_REPO createrepo "/srv/yum/internal/18.10/el7/noarch/map-server/$PROJECT-$VERSION-$RELEASE"
 if [ "$BRANCH_NAME" '=' 'master' ] ; then
   copy_internal_rpms_to_canary "map" "18.10" "el7" "noarch" "map-server" "$PROJECT-$VERSION-$RELEASE"
 fi
