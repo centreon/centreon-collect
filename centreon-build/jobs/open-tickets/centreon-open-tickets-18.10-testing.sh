6f90c006835860e61109c68434c3de701f38c038
Laurent Pinsivy <lpinsivy@centreon.com>
2018-10-23 13:40:07 +0200

    fix(open-ticket): add script for 18.10 testing

---
+++ centreon-build/jobs/open-tickets/centreon-open-tickets-18.10-testing.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-open-tickets
+
+# Check arguments.
+if [ -z "$COMMIT" -o -z "$RELEASE" ] ; then
+  echo "You need to specify COMMIT and RELEASE environment variables."
+  exit 1
+fi
+
+# Pull mon-build-dependencies containers.
+docker pull ci.int.centreon.com:5000/mon-build-dependencies-18.10:centos7
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output-centos7
+mkdir output-centos7
+
+# Get version.
+cd "$PROJECT"
+git checkout --detach "$COMMIT"
+export VERSION=`grep mod_release www/modules/centreon-open-tickets/conf.php | cut -d '"' -f 4`
+
+# Create source tarball.
+git archive "--prefix=$PROJECT-$VERSION/" HEAD | gzip > "../input/$PROJECT-$VERSION.tar.gz"
+cd ..
+
+# Build RPMs.
+cp `dirname $0`/../../packaging/centreon-open-tickets/centreon-open-tickets-3.4.spectemplate input/
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies-18.10:centos7 input output-centos7
+
+# Copy files to server.
+put_testing_source "standard" "open-tickets" "$PROJECT-$VERSION-$RELEASE" "input/$PROJECT-$VERSION.tar.gz"
+put_testing_rpms "standard" "18.10" "el7" "noarch" "open-tickets" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+
+# Generate testing documentation.
+DOC="root@doc-dev.int.centreon.com"
+ssh "$DOC" bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-open-tickets -V latest -p'"
+ssh "$DOC" bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-open-tickets -V latest -p'"
