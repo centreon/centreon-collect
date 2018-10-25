c2ef66ad01b02052e3a411641ae0d23663018a06
Jeremy Delpierre <jdelpierre@centreon.com>
2018-10-25 10:00:19 +0200

    fix(job/iso): name of ISO

--- jobs/iso/centreon-iso-18.10-stable.sh
+++ jobs/iso/centreon-iso-18.10-stable.sh
@@ -18,11 +18,11 @@ export VERSION=18.10
 
 # Release ISO.
 for distrib in el7 ; do
-  SRCHASH=`$SSH_REPO "md5sum /srv/iso/centreon-$VERSION.$RELEASE.$distrib.x86_64.iso | cut -d ' ' -f 1"`
-  $SSH_REPO aws s3 cp --acl public-read "/srv/iso/centreon-$VERSION.$RELEASE.$distrib.x86_64.iso" "s3://centreon-iso/stable/centreon-$VERSION.$RELEASE.$distrib.x86_64.iso"
+  SRCHASH=`$SSH_REPO "md5sum /srv/iso/centreon-$VERSION-$RELEASE.$distrib.x86_64.iso | cut -d ' ' -f 1"`
+  $SSH_REPO aws s3 cp --acl public-read "/srv/iso/centreon-$VERSION-$RELEASE.$distrib.x86_64.iso" "s3://centreon-iso/stable/centreon-$VERSION-$RELEASE.$distrib.x86_64.iso"
 
   # Sync ISO in database (dryrun=1 does not show ISO on website)
-  OUTPUT=`curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=centreon&version=$VERSION.$RELEASE.$distrib.x86_64&extension=iso&md5=$SRCHASH&ddos=1&dryrun=1"`
+  OUTPUT=`curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=centreon&version=$VERSION-$RELEASE.$distrib.x86_64&extension=iso&md5=$SRCHASH&ddos=1&dryrun=1"`
   SUCCESS=`echo $OUTPUT | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["status"]'`
   if [ \( "$SUCCESS" -ne "success" \) ] ; then
     echo "ISO synchronization failed."
