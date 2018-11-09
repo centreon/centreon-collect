d5c8ce7bb4dc0a1dc8d57bb3388bb76cb0cb299d
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-11-09 13:32:42 +0100

    feat(jobs): add packs-stable.

---
+++ jobs/packs/packs-stable.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+export PROJECT=centreon-packs
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Move sources to the stable directory.
+SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
+$SSH_REPO mv "/srv/sources/plugin-packs/testing/packs/$PROJECT-$VERSION-$RELEASE" "/srv/sources/plugin-packs/stable/"
+
+# Move RPMs to the stable repository.
+promote_testing_rpms_to_stable "plugin-packs" "3.4" "el6" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "plugin-packs" "3.4" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "plugin-packs" "18.10" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
+
+# Move cache files to the stable directory.
+TESTINGCACHE="/srv/cache/packs/testing/cache-$VERSION-$RELEASE"
+STABLECACHE="/srv/cache/packs/stable"
+$SSH_REPO 'for i in `ls '$TESTINGCACHE'` ; do rm -rf '$STABLECACHE'/$i ; mv '$TESTINGCACHE'/$i '$STABLECACHE'/ ; rm -rf '$TESTINGCACHE' ; done'
+
+# Generate online documentation.
+SSH_DOC="$SSH_REPO ssh -o StrictHostKeyChecking=no ubuntu@10.24.1.54"
+$SSH_DOC "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos plugins-packs -V latest -p'"
+$SSH_DOC "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos plugins-packs -V latest -p'"
