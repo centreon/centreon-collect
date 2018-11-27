d63d5c45042497790c70f34c3bc38320f97d49f1
Matthieu Kermagoret <ganoze@gmail.com>
2018-11-27 10:13:31 +0100

    feat: rename package to centreon-map instead of centreon-map4

--- jobs/map/18.10/des-map-bundle.sh
+++ jobs/map/18.10/des-map-bundle.sh
@@ -29,7 +29,7 @@ docker pull $BASE_IMAGE
 rm -rf centreon-build-containers
 cp -r `dirname $0`/../../../containers centreon-build-containers
 cd centreon-build-containers
-sed "s#@BASE_IMAGE@#$DEP_IMAGE#g" < map/server.Dockerfile.in > map/server.$DISTRIB.Dockerfile
+sed "s#@BASE_IMAGE@#$DEP_IMAGE#g" < map/18.10/server.Dockerfile.in > map/server.$DISTRIB.Dockerfile
 sed "s#@BASE_IMAGE@#$BASE_IMAGE#g" < map/18.10/web.Dockerfile.in > map/web.$DISTRIB.Dockerfile
 sed "s#@PROJECT@#map-web#g;s#@SUBDIR@#18.10/el7/noarch/map-web/centreon-map-$VERSION-$RELEASE#g" < repo/centreon-internal.repo.in > repo/centreon-internal.repo
 sed "s#@PROJECT@#map-server#g;s#@SUBDIR@#18.10/el7/noarch/map-server/centreon-map-$VERSION-$RELEASE#g" < repo/centreon-internal.repo.in >> repo/centreon-internal.repo
