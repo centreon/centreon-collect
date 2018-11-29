5b8bb47b0d6a31629fe1ac4d769e6181593aa7b1
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-11-29 16:56:05 +0100

    fix(jobs): use new package names in centreon-map-web-18.10-{testing,stable} jobs.

--- jobs/map/centreon-map-web-18.10-testing.sh
+++ jobs/map/centreon-map-web-18.10-testing.sh
@@ -6,7 +6,7 @@ set -x
 . `dirname $0`/../common.sh
 
 # Project.
-PROJECT=centreon-map4-web-client
+PROJECT=centreon-map-web-client
 
 # Check arguments.
 if [ -z "$COMMIT" -o -z "$RELEASE" ] ; then
@@ -32,24 +32,24 @@ node ./node_modules/gulp/bin/gulp.js build-module
 node ./node_modules/gulp/bin/gulp.js build-widget
 
 # Generate source tarball used for packaging.
-rm -rf ../centreon-map4-web-client-$VERSION
-mkdir ../centreon-map4-web-client-$VERSION
-cp -a build/module ../centreon-map4-web-client-$VERSION
-cp -a build/widget ../centreon-map4-web-client-$VERSION
-cp -a build/install.sh ../centreon-map4-web-client-$VERSION
-cp -a build/libinstall ../centreon-map4-web-client-$VERSION
-cp -a build/examples ../centreon-map4-web-client-$VERSION
+rm -rf ../centreon-map-web-client-$VERSION
+mkdir ../centreon-map-web-client-$VERSION
+cp -a build/module ../centreon-map-web-client-$VERSION
+cp -a build/widget ../centreon-map-web-client-$VERSION
+cp -a build/install.sh ../centreon-map-web-client-$VERSION
+cp -a build/libinstall ../centreon-map-web-client-$VERSION
+cp -a build/examples ../centreon-map-web-client-$VERSION
 cd ..
-tar czf centreon-map4-web-client-$VERSION.tar.gz centreon-map4-web-client-$VERSION
+tar czf centreon-map-web-client-$VERSION.tar.gz centreon-map-web-client-$VERSION
 
 # Encode source tarball.
-curl -F "file=@centreon-map4-web-client-$VERSION.tar.gz" -F "version=71" 'http://encode.int.centreon.com/api/index.php' -o "input/centreon-map4-web-client-$VERSION-php71.tar.gz"
+curl -F "file=@centreon-map-web-client-$VERSION.tar.gz" -F "version=71" 'http://encode.int.centreon.com/api/index.php' -o "input/centreon-map-web-client-$VERSION-php71.tar.gz"
 
 # Pull mon-build-dependencies containers.
 docker pull ci.int.centreon.com:5000/mon-build-dependencies-18.10:centos7
 
 # Build RPMs.
-cp web/packaging/centreon-map4-web-client.spectemplate input
+cp web/packaging/centreon-map-web-client.spectemplate input
 docker-rpm-builder dir --sign-with `dirname $0`/../ces.key ci.int.centreon.com:5000/mon-build-dependencies-18.10:centos7 input output-centos7
 
 # Copy files to server.
