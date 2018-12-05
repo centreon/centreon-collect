76a30a2e16a39bfa62e4b926d2dc71c731d27e73
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-12-05 16:05:25 +0100

    fix(jobs): update mon-web-analysis so that it can run standalone.

--- jobs/web/18.10/mon-web-analysis.sh
+++ jobs/web/18.10/mon-web-analysis.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
 # This job is run directly after mon-web-unittest on centos7.
 
+# Project.
+PROJECT=centreon-web
+
+# Retrieve copy of git repository.
 curl -o 'centreon-web-git.tar.gz' "http://srvi-repo.int.centreon.com/sources/internal/web/$PROJECT-$VERSION-$RELEASE/centreon-web-git.tar.gz"
 rm -rf centreon-web
 tar xzf centreon-web-git.tar.gz
+
+# Copy reports and run analysis.
 cd centreon-web
 cp ../ut.xml .
 cp ../coverage.xml .
