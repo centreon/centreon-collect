5c155a26da30ba96c7ea8084382419698c2d7ac0
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-12-07 13:30:41 +0100

    feat(jobs): add mon-clib-analysis.

--- jobs/clib/19.4/mon-clib-source.sh
+++ jobs/clib/19.4/mon-clib-source.sh
@@ -15,6 +15,7 @@ set -x
 
 # Project.
 PROJECT=centreon-clib
+tar czf "$PROJECT-git.tar.gz" "$PROJECT"
 
 # Get version.
 cd $PROJECT
@@ -38,6 +39,7 @@ cd ..
 
 # Send it to srvi-repo.
 put_internal_source "clib" "$PROJECT-$VERSION-$RELEASE" "$PROJECT-$VERSION.tar.gz"
+put_internal_source "clib" "$PROJECT-$VERSION-$RELEASE" "$PROJECT-git.tar.gz"
 
 # Generate properties files for downstream jobs.
 cat > source.properties << EOF
