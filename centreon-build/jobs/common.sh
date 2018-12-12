f519623cb3f6e9050c1c8dbdb79440a95871a1ef
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-12-12 10:37:30 +0100

    fix(jobs): fix clean_directory() common function.

--- centreon-build/jobs/common.sh
+++ centreon-build/jobs/common.sh
@@ -12,7 +12,7 @@ export COMPOSE_HTTP_TIMEOUT=180
 # Cleanup routine.
 
 clean_directory () {
-  CMD='ls -rc '"$1"' | head -n -10 | xargs rm -rf'
+  CMD='ls -drc '"$1/*"' | head -n -10 | xargs rm -rf'
   ssh "$REPO_CREDS" "$CMD"
 }
 
