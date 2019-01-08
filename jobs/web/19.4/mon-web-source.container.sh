1b5529384acf5d53a84ef3f45bf58102cd3f6595
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-01-08 08:56:40 +0100

    fix(jobs): fix variable use in mon-web-source.

--- jobs/web/19.4/mon-web-source.container.sh
+++ jobs/web/19.4/mon-web-source.container.sh
@@ -33,11 +33,12 @@ cd ../..
 mkdir -p www/locale/en_US.UTF-8/LC_MESSAGES
 php bin/centreon-translations.php en lang/fr_FR.UTF-8/LC_MESSAGES/messages.po www/locale/en_US.UTF-8/LC_MESSAGES/messages.ser
 for i in lang/* ; do
-  langcode=`basename $i | cut -d _ -f 1`
+  localefull=`basename $i`
+  langcode=`echo $localefull | cut -d _ -f 1`
   mkdir -p "www/locale/$i/LC_MESSAGES"
-  msgfmt "lang/$i/LC_MESSAGES/messages.po" -o "www/locale/$i/LC_MESSAGES/messages.mo"
-  msgfmt "lang/$i/LC_MESSAGES/help.po" -o "www/locale/$i/LC_MESSAGES/help.mo"
-  php bin/centreon-translations.php "$langcode" "lang/$i/LC_MESSAGES/messages.po" "www/locale/$i/LC_MESSAGES/messages.ser"
+  msgfmt "lang/$localefull/LC_MESSAGES/messages.po" -o "www/locale/$localefull/LC_MESSAGES/messages.mo"
+  msgfmt "lang/$localefull/LC_MESSAGES/help.po" -o "www/locale/$localefull/LC_MESSAGES/help.mo"
+  php bin/centreon-translations.php "$langcode" "lang/$localefull/LC_MESSAGES/messages.po" "www/locale/$localefull/LC_MESSAGES/messages.ser"
 done
 rm -rf lang
 
