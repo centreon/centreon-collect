99ac48d35ee04997d2cf399a3125bac35e3f74d0
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-01-11 09:49:01 +0100

    feat(jobs): improve testing-cleanup to clean only a project-related artifacts.

--- jobs/testing-cleanup.sh
+++ jobs/testing-cleanup.sh
@@ -7,16 +7,48 @@ set -x
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
 
 # Check arguments.
-if [ -z "$REPO" ] ; then
-  echo "You need to specify REPO environment variable."
+if [ -z "$VERSION" -o -z "$PROJECT" ] ; then
+  echo "You need to specify VERSION and PROJECT environment variables."
   exit 1
 fi
 
+# Get repo and architecture based on project.
+case "$PROJECT" in
+  awie|dsm|lm|open-tickets|plugins|ppm|web|widgets)
+    arch=noarch
+    repo=standard
+    ;;
+  autodisco|broker|clib|connector|engine|nrpe)
+    arch=x86_64
+    repo=standard
+    ;;
+  bam)
+    arch=noarch
+    repo=bam
+    ;;
+  map-server|map-web)
+    arch=noarch
+    repo=map
+    ;;
+  bi-engine|bi-etl|bi-report|bi-reporting-server|bi-server)
+    arch=noarch
+    repo=mbi
+    ;;
+  packs)
+    arch=noarch
+    repo=plugin-packs
+    ;;
+  *)
+    echo "Unsupported project $PROJECT."
+    exit 1
+    ;;
+esac
+
 # Clean REPO.
-for distrib in el6 el7 ; do
-  for arch in noarch x86_64 ; do
-    $SSH_REPO rm -f "/srv/yum/$REPO/3.4/$distrib/testing/$arch/RPMS/*.rpm"
-    $SSH_REPO rm -rf "/srv/sources/$REPO/testing/*"
-    $SSH_REPO createrepo "/srv/yum/$REPO/3.4/$distrib/testing/$arch"
-  done
-done
+$SSH_REPO rm -rf "/srv/sources/$repo/testing/$PROJECT/*"
+$SSH_REPO rm -rf "/srv/yum/$repo/$VERSION/el7/testing/$arch/$PROJECT/*"
+$SSH_REPO createrepo "/srv/yum/$repo/$VERSION/el7/testing/$arch"
+if [ "$VERSION" = '3.4' ] ; then
+  $SSH_REPO rm -rf "/srv/yum/$repo/$VERSION/el6/testing/$arch/$PROJECT/*"
+  $SSH_REPO createrepo "/srv/yum/$repo/$VERSION/el6/testing/$arch"
+fi
