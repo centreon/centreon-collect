c386553978d83edff7b2d7cd157769fd6748199d
Kevin Duret <duret.kevin@gmail.com>
2019-01-15 15:22:17 +0100

    feat(job): add job to push react-components on npm registry (#76)

---
+++ centreon-build/jobs/react-components/react-components-18.10-stable.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-react-components
+
+# Check arguments.
+if [ -z "$COMMIT" ]; then
+  echo "You need to specify COMMIT environment variables."
+  exit 1
+fi
+
+# Get version.
+cd $PROJECT
+git checkout --detach "$COMMIT"
+export VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+cat > .npmrc << EOF
+//registry.npmjs.org/:_authToken=2a5c102e-2d32-449a-9a5f-03da082f123b
+EOF
+
+# release on npm registry
+npm publish --access=public --tag=latest ./
+
+# push tag on github if $COMMIT is not a tag
+if ! git tag --list | egrep -q "^$COMMIT$" ; then
+  git tag $VERSION
+  git push origin $VERSION
+fi
+
