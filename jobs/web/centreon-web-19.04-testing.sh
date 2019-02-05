d5ff9d2b2bea52c8b59f6f38f3c60d6ae69cd499
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-02-05 16:05:14 +0100

    feat(jobs): add centreon-web-19.04-testing.

---
+++ jobs/web/centreon-web-19.04-testing.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-web
+
+# Check arguments.
+if [ -z "$COMMIT" -o -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify COMMIT, VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Pull mon-build-dependencies containers.
+BUILD_CENTOS7=registry.centreon.com/mon-build-dependencies-19.04:centos7
+docker pull "$BUILD_CENTOS7"
+
+# Prepare base source tarball.
+cd "$PROJECT"
+git checkout --detach "$COMMIT"
+git archive --prefix="$PROJECT-$VERSION/" HEAD | gzip > "../$PROJECT-$VERSION.tar.gz"
+SRCCOMMIT=`git log -1 HEAD --pretty=format:%h`
+cd ..
+
+# Create and populate container.
+containerid=`docker create -e "PROJECT=$PROJECT" -e "VERSION=$VERSION" -e "COMMIT=$SRCCOMMIT" $BUILD_CENTOS7 /usr/local/bin/source.sh`
+docker cp `dirname $0`/19.04/mon-web-source.container.sh "$containerid:/usr/local/bin/source.sh"
+docker cp "$PROJECT-$VERSION.tar.gz" "$containerid:/usr/local/src/"
+
+# Run container that will generate complete tarball.
+docker start -a "$containerid"
+rm -f "$PROJECT-$VERSION.tar.gz"
+docker cp "$containerid:/usr/local/src/$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION.tar.gz"
+rm -rf "$PROJECT-$VERSION" "centreon-$VERSION" "centreon-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+mv "$PROJECT-$VERSION" "centreon-$VERSION"
+tar czf "centreon-$VERSION.tar.gz" "centreon-$VERSION"
+
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output-centos7
+mkdir output-centos7
+
+# Retrieve spec file.
+rm -rf packaging-centreon-web
+git clone http://gitbot:gitbot@git.int.centreon.com/packaging-centreon packaging-centreon-web
+cp packaging-centreon-web/rpm/centreon-19.04.spectemplate input/
+
+# Retrieve additional sources.
+cp packaging-centreon-web/src/19.04/* input/
+cp "centreon-$VERSION.tar.gz" input/
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_CENTOS7" input output-centos7
+
+# Copy files to server.
+put_testing_source "standard" "web" "$PROJECT-$VERSION-$RELEASE" "centreon-$VERSION.tar.gz" "$PROJECT-$VERSION.tar.gz"
+put_testing_rpms "standard" "19.04" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+
+# Generate doc.
+SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
+$SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon -V latest -p'"
+$SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon -V latest -p'"
+$SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon -V 19.04 -p'"
+$SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon -V 19.04 -p'"
