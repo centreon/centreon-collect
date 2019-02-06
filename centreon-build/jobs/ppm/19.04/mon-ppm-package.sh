f07b9ba24467967953afb54e07190bccfc00f55b
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-02-06 13:42:47 +0100

    fix(jobs): change branch names that copy packages to canary repository in mon-ppm-package.

--- centreon-build/jobs/ppm/19.04/mon-ppm-package.sh
+++ centreon-build/jobs/ppm/19.04/mon-ppm-package.sh
@@ -49,6 +49,6 @@ else
   exit 1
 fi
 put_internal_rpms "19.04" "$DISTRIB" "noarch" "ppm" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
-if [ "$BRANCH_NAME" '=' 'master' ] ; then
+if [ "$BRANCH_NAME" '=' 'master' -o "$BRANCH_NAME" '=' '19.04.x' ] ; then
   copy_internal_rpms_to_canary "standard" "19.04" "el7" "noarch" "ppm" "$PROJECT-$VERSION-$RELEASE"
 fi
