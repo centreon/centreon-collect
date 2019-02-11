87db63e1432fb340acb107bf8af4c604ecebb542
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-02-11 09:30:20 +0100

    feat(jobs): add Centreon VMWare CI scripts.

---
+++ jobs/vmware/vmware-package.centos7.sh
@@ -0,0 +1,20 @@
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Retrieve sources.
+mv "$PKGNAME-$VERSION.tar.gz" input/
+cp `dirname $0`/../../packaging/vmware/$PROJECT.spectemplate input/
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key registry.centreon.com/mon-build-dependencies-18.10:centos7 input output
+
+# Publish RPMs.
+put_internal_rpms "18.10" "el7" "noarch" "vmware" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
+put_internal_rpms "19.04" "el7" "noarch" "vmware" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
+if [ "$BRANCH_NAME" '=' 'master' ] ; then
+  copy_internal_rpms_to_canary "standard" "18.10" "el7" "noarch" "vmware" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_canary "standard" "19.04" "el7" "noarch" "vmware" "$PROJECT-$VERSION-$RELEASE"
+fi
