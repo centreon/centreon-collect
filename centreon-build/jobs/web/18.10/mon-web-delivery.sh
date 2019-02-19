69597a476c8a9b58b338e758e49a07f4256a0801
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-02-19 11:43:44 +0100

    feat(jobs): handle release branches in 18.10 Centreon Web jobs.

--- centreon-build/jobs/web/18.10/mon-web-delivery.sh
+++ centreon-build/jobs/web/18.10/mon-web-delivery.sh
@@ -14,15 +14,32 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 
-# Set Docker images as latest.
-REGISTRY='registry.centreon.com'
-for image in mon-web-fresh mon-web mon-web-widgets ; do
-  for distrib in centos7 ; do
-    docker pull "$REGISTRY/$image-$VERSION-$RELEASE:$distrib"
-    docker tag "$REGISTRY/$image-$VERSION-$RELEASE:$distrib" "$REGISTRY/$image-18.10:$distrib"
-    docker push "$REGISTRY/$image-18.10:$distrib"
-  done
-done
+#
+# Release delivery.
+#
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  copy_internal_source_to_testing "standard" "web" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_testing "standard" "18.10" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
+  SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon -V latest -p'"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon -V latest -p'"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon -V 18.10 -p'"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon -V 18.10 -p'"
 
-# Move RPMs to unstable.
-promote_canary_rpms_to_unstable "standard" "18.10" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
+#
+# CI delivery.
+#
+else
+  # Set Docker images as latest.
+  REGISTRY='registry.centreon.com'
+  for image in mon-web-fresh mon-web mon-web-widgets ; do
+    for distrib in centos7 ; do
+      docker pull "$REGISTRY/$image-$VERSION-$RELEASE:$distrib"
+      docker tag "$REGISTRY/$image-$VERSION-$RELEASE:$distrib" "$REGISTRY/$image-18.10:$distrib"
+      docker push "$REGISTRY/$image-18.10:$distrib"
+    done
+  done
+
+  # Move RPMs to unstable.
+  promote_canary_rpms_to_unstable "standard" "18.10" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
+fi
