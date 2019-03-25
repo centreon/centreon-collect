a97a59acbba44c27620236f173a511ef7f187f84
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-03-25 09:53:47 +0100

    feat(jobs): handle release, reference and ci build types in mon-clib-*.

--- centreon-build/jobs/clib/19.04/mon-clib-analysis.sh
+++ centreon-build/jobs/clib/19.04/mon-clib-analysis.sh
@@ -17,4 +17,9 @@ tar xzf "$PROJECT-git.tar.gz"
 
 # Copy reports and run analysis.
 cd "$PROJECT"
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  sed -i -e 's/centreon-clib-19.04/centreon-clib-19.04-release/g' sonar-project.properties
+  sed -i -e 's/Centreon Clib 19.04/Centreon Clib 19.04 (release)/g' sonar-project.properties
+fi
+echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
 sonar-scanner
