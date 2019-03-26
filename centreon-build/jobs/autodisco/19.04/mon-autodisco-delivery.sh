1d21ff7642809fc98bf38e1124771120cbc105b0
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-03-26 09:32:52 +0100

    feat(jobs): handle release, reference and ci build types in mon-autodisco-*.

--- centreon-build/jobs/autodisco/19.04/mon-autodisco-delivery.sh
+++ centreon-build/jobs/autodisco/19.04/mon-autodisco-delivery.sh
@@ -14,13 +14,30 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 
-# Set Docker images as latest.
-REGISTRY='registry.centreon.com'
-for distrib in centos7 ; do
-  docker pull "$REGISTRY/mon-autodisco-$VERSION-$RELEASE:$distrib"
-  docker tag "$REGISTRY/mon-autodisco-$VERSION-$RELEASE:$distrib" "$REGISTRY/mon-autodisco-19.04:$distrib"
-  docker push "$REGISTRY/mon-autodisco-19.04:$distrib"
-done
+#
+# Release delivery.
+#
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  copy_internal_source_to_testing "standard" "autodisco" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_testing "standard" "19.04" "el7" "noarch" "autodisco" "$PROJECT-$VERSION-$RELEASE"
+  SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-auto-discovery -V latest -p'"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-auto-discovery -V latest -p'"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-auto-discovery -V 19.04 -p'"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-auto-discovery -V 19.04 -p'"
 
-# Move RPMs to unstable.
-promote_canary_rpms_to_unstable "standard" "19.04" "el7" "x86_64" "autodisco" "$PROJECT-$VERSION-$RELEASE"
+#
+# CI delivery.
+#
+else
+  # Set Docker images as latest.
+  REGISTRY='registry.centreon.com'
+  for distrib in centos7 ; do
+    docker pull "$REGISTRY/mon-autodisco-$VERSION-$RELEASE:$distrib"
+    docker tag "$REGISTRY/mon-autodisco-$VERSION-$RELEASE:$distrib" "$REGISTRY/mon-autodisco-19.04:$distrib"
+    docker push "$REGISTRY/mon-autodisco-19.04:$distrib"
+  done
+
+  # Move RPMs to unstable.
+  promote_canary_rpms_to_unstable "standard" "19.04" "el7" "x86_64" "autodisco" "$PROJECT-$VERSION-$RELEASE"
+fi
