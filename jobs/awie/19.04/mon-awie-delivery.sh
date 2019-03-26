ac6a8d253487b4b1717f44705c8b7d98643da3b6
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-03-26 09:45:46 +0100

    feat(jobs): handle release, reference and ci build types in mon-awie-*.

--- jobs/awie/19.04/mon-awie-delivery.sh
+++ jobs/awie/19.04/mon-awie-delivery.sh
@@ -14,13 +14,28 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 
-# Set Docker images as latest.
-REGISTRY='registry.centreon.com'
-for distrib in centos7 ; do
-  docker pull "$REGISTRY/mon-awie-$VERSION-$RELEASE:$distrib"
-  docker tag "$REGISTRY/mon-awie-$VERSION-$RELEASE:$distrib" "$REGISTRY/mon-awie-19.04:$distrib"
-  docker push "$REGISTRY/mon-awie-19.04:$distrib"
-done
+#
+# Release delivery.
+#
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  copy_internal_source_to_testing "standard" "awie" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_testing "standard" "19.04" "el7" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
+  SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-awie -V latest -p'"
+  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-awie -V latest -p'"
 
-# Move RPMs to unstable.
-promote_canary_rpms_to_unstable "standard" "19.04" "el7" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
+#
+# CI delivery.
+#
+else
+  # Set Docker images as latest.
+  REGISTRY='registry.centreon.com'
+  for distrib in centos7 ; do
+    docker pull "$REGISTRY/mon-awie-$VERSION-$RELEASE:$distrib"
+    docker tag "$REGISTRY/mon-awie-$VERSION-$RELEASE:$distrib" "$REGISTRY/mon-awie-19.04:$distrib"
+    docker push "$REGISTRY/mon-awie-19.04:$distrib"
+  done
+
+  # Move RPMs to unstable.
+  promote_canary_rpms_to_unstable "standard" "19.04" "el7" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
+fi
