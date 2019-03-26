9a43063d2eee054d8ad2b022bd3c9a2de798d8cc
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-03-26 09:40:34 +0100

    fix(jobs): do not generate non-existing documentation tags in mon-autodisco-delivery.

--- centreon-build/jobs/autodisco/19.04/mon-autodisco-delivery.sh
+++ centreon-build/jobs/autodisco/19.04/mon-autodisco-delivery.sh
@@ -23,8 +23,6 @@ if [ "$BUILD" '=' 'RELEASE' ] ; then
   SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
   $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-auto-discovery -V latest -p'"
   $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-auto-discovery -V latest -p'"
-  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-auto-discovery -V 19.04 -p'"
-  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-auto-discovery -V 19.04 -p'"
 
 #
 # CI delivery.
