f15ac6387c771a82b137894269551682a0daac87
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-03-27 08:55:00 +0100

    feat(jobs): handle release, reference and ci build types in mon-open-tickets-*.

--- jobs/open-tickets/19.04/mon-open-tickets-analysis.sh
+++ jobs/open-tickets/19.04/mon-open-tickets-analysis.sh
@@ -17,4 +17,9 @@ tar xzf "$PROJECT-git.tar.gz"
 
 # Copy reports and run analysis.
 cd "$PROJECT"
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  sed -i -e 's/centreon-open-tickets-19.04/centreon-open-tickets-19.04-release/g' sonar-project.properties
+  sed -i -e 's/Centreon Open Tickets 19.04/Centreon Open Tickets 19.04 (release)/g' sonar-project.properties
+fi
+echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
 sonar-scanner
