ba050210cf4a1aeeaaf128add5221fbb251abe35
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-04-19 13:43:41 +0200

    feat(jobs): update des-mbi-engine-* jobs to the state of the art.

--- jobs/mbi/19.04/des-mbi-engine-package.sh
+++ jobs/mbi/19.04/des-mbi-engine-package.sh
@@ -49,6 +49,6 @@ else
   exit 1
 fi
 put_internal_rpms "19.04" "$DISTRIB" "noarch" "mbi-engine" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
-if [ "$BRANCH_NAME" '=' 'master' ] ; then
+if [ "$BUILD" '=' 'REFERENCE' ] ; then
   copy_internal_rpms_to_canary "mbi" "19.04" "el7" "noarch" "mbi-engine" "$PROJECT-$VERSION-$RELEASE"
 fi
