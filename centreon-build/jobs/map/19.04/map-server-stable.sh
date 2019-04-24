0463abe65a6b1e2eb37340731d9336f1117f753e
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-04-24 16:01:22 +0200

    fix(jobs): fix map-server-stable job.

--- centreon-build/jobs/map/19.04/map-server-stable.sh
+++ centreon-build/jobs/map/19.04/map-server-stable.sh
@@ -6,7 +6,7 @@ set -x
 . `dirname $0`/../../common.sh
 
 # Project.
-PROJECT=centreon-map-server
+PROJECT=centreon-map
 
 # Move RPMs to the stable repository.
 promote_testing_rpms_to_stable "map" "19.04" "el7" "noarch" "map-server" "$PROJECT-19.04-$RELEASE"
