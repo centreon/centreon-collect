3c4ac4ca19f6dada9d377d5d70a27f7b3e4f7778
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-05-02 13:22:03 +0200

    fix(jobs): path fix in des-map-server-analysis.

--- jobs/map/19.04/des-map-server-analysis.sh
+++ jobs/map/19.04/des-map-server-analysis.sh
@@ -9,7 +9,7 @@ set -x
 PROJECT=centreon-map
 
 # Copy reports and run analysis.
-cd "$PROJECT-server-$VERSION"
+cd "$PROJECT-server-$VERSIONSERVER"
 if [ "$BUILD" '=' 'RELEASE' ] ; then
   sed -i -e 's/centreon-map-server-19.04/centreon-map-server-19.04-release/g' sonar-project.properties
   sed -i -e 's/Centreon Map Server 19.04/Centreon Map Server 19.04 (release)/g' sonar-project.properties
