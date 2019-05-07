09c1c1d3d4c99a419333e76702cfa5b956d6b6a9
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-05-07 10:35:38 +0200

    feat(packaging): add 19.10 spectemplate for Centreon Broker.

--- centreon-build/jobs/broker/19.10/mon-broker-package.opensuse-leap.sh
+++ centreon-build/jobs/broker/19.10/mon-broker-package.opensuse-leap.sh
@@ -6,7 +6,7 @@ mkdir output
 
 # Retrieve sources.
 mv "$PROJECT-$VERSION.tar.gz" input/
-cp `dirname $0`/../../../packaging/broker/rpm/centreon-broker-19.10.spectemplate input/
+cp `dirname $0`/../../../packaging/broker/rpm/19.10/centreon-broker.spectemplate input/
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key registry.centreon.com/mon-build-dependencies-19.10:opensuse-leap input output
