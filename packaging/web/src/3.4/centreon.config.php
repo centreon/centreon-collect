2b3c6d261b71c58fd12a739a576eddfee7689915
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-05-09 10:23:03 +0200

    feat(packaging): add Centreon Web packaging files to this repo.

---
+++ packaging/web/src/3.4/centreon.config.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Global include for all files
+ */
+/* MySQL configuration file */
+define('_CENTREON_ETC_', '/etc/centreon');
+if (file_exists(_CENTREON_ETC_ . '/centreon.conf.php')) {
+    include_once _CENTREON_ETC_ . '/centreon.conf.php';
+    define('_CENTREON_PATH_', $centreon_path);
+    define('_CENTREON_LOG_', '/var/log/centreon');
+    define('_CENTREON_VARLIB_', '/var/lib/centreon');
+    define('hostCentreon', $conf_centreon['hostCentreon']);
+    define('hostCentstorage', $conf_centreon['hostCentstorage']);
+    define('user', $conf_centreon['user']);
+    define('password', $conf_centreon['password']);
+    define('db', $conf_centreon['db']);
+    define('dbcstg', $conf_centreon['dbcstg']);
+    define('port', $conf_centreon['port']);
+}
+/* Enable PHP error */
+ini_set('display_errors', 'Off');
