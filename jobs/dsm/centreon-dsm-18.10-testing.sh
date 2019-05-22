49556d312d496a23738cf0c67de43f760df4ae0d
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-05-22 10:02:34 +0200

    feat(jobs): populate download platforms in dsm release jobs.

--- jobs/dsm/centreon-dsm-18.10-testing.sh
+++ jobs/dsm/centreon-dsm-18.10-testing.sh
@@ -42,6 +42,10 @@ docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_CENTOS7" inpu
 put_testing_source "standard" "dsm" "$PROJECT-$VERSION-$RELEASE" "input/$PROJECT-$VERSION.tar.gz"
 put_testing_rpms "standard" "18.10" "el7" "noarch" "dsm" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
 
+# Create entry in download-dev.
+SRCHASH=00112233445566778899aabbccddeeff
+curl "$DLDEV_URL/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
+
 # Generate testing documentation.
 DOC="root@doc-dev.int.centreon.com"
 ssh "$DOC" bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-dsm -V latest -p'"
