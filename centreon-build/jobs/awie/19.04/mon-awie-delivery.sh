22e3f5c5d40c1e377d14c3ce6d30e25bcb037246
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-05-22 09:46:34 +0200

    feat(jobs): populate download platforms in awie release jobs.

--- centreon-build/jobs/awie/19.04/mon-awie-delivery.sh
+++ centreon-build/jobs/awie/19.04/mon-awie-delivery.sh
@@ -18,11 +18,18 @@ fi
 # Release delivery.
 #
 if [ "$BUILD" '=' 'RELEASE' ] ; then
+  # Copy build artifacts.
   copy_internal_source_to_testing "standard" "awie" "$PROJECT-$VERSION-$RELEASE"
   copy_internal_rpms_to_testing "standard" "19.04" "el7" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
+
+  # Generate documentation.
   SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
   $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-awie -V latest -p'"
   $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-awie -V latest -p'"
+
+  # Create entry in download-dev.
+  SRCHASH=00112233445566778899aabbccddeeff
+  curl "$DLDEV_URL/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
 
 #
 # CI delivery.
