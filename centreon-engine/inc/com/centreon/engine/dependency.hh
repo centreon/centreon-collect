centreon/centreon-engine

enh(dependency): rework host and service dependency to derivate from a dependency class

inc/com/centreon/engine/dependency.hh
@@ -1,0 +1,88 @@
+/*
+** Copyright 2011-2019 Centreon
+**
+** This file is part of Centreon Engine.
+**
+** Centreon Engine is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Engine is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Engine. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CENTREON_ENGINE_DEPENDENCY_HH
+#define CENTREON_ENGINE_DEPENDENCY_HH
+
+#  include <ostream>
+#  include <string>
+#  include <com/centreon/engine/namespace.hh>
+#  include <com/centreon/engine/timeperiod.hh>
+
+CCE_BEGIN();
+
+class                         dependency {
+
+ public:
+  enum                        types {
+    notification = 1,
+    execution
+  };
+
+                                dependency(std::string const& dependent_host_name,
+                                           std::string const& host_name,
+                                           types dependency_type,
+                                           bool inherits_parent,
+                                           bool fail_on_pending,
+                                           std::string const& dependency_period);
+  virtual                       ~dependency();
+
+  types                         get_dependency_type() const;
+  void                          set_dependency_type(types dependency_type);
+  std::string const&            get_dependent_host_name() const;
+  void                          set_dependent_host_name(
+    std::string const& dependent_host_name);
+  std::string const&            get_host_name() const;
+  void                          set_host_name(std::string const& host_name);
+  std::string const&            get_dependency_period() const;
+  void                          set_dependency_period(
+    std::string const& dependency_period);
+  bool                          get_inherits_parent() const;
+  void                          set_inherits_parent(bool inherits_parent);
+  bool                          get_fail_on_pending() const;
+  void                          set_fail_on_pending(bool fail_on_pending);
+  bool                          get_circular_path_checked() const;
+  void                          set_circular_path_checked(
+    bool circular_path_checked);
+  bool                          get_contains_circular_path() const;
+  void                          set_contains_circular_path(
+    bool contains_circular_path);
+
+  virtual bool                  operator==(dependency const& obj) throw ();
+  bool                          operator!=(dependency const& obj) throw ();
+  virtual bool                  operator<(dependency const& obj) throw ();
+
+  com::centreon::engine::timeperiod*
+                                dependency_period_ptr;
+
+ protected:
+  types                         _dependency_type;
+  std::string                   _dependent_host_name;
+  std::string                   _host_name;
+  std::string                   _dependency_period;
+  bool                          _inherits_parent;
+  bool                          _fail_on_pending;
+  bool                          _circular_path_checked;
+  bool                          _contains_circular_path;
+
+};
+
+CCE_END();
+
+#endif //CENTREON_ENGINE_DEPENDENCY_HH

src/configuration/applier/state.cc
@@ -745,7 +745,7 @@ std::unordered_map<std::pair<unsigned long, unsigned long>,
  *
  *  @return The current servicedependencies.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> > const& applier::state::servicedependencies() const throw () {
+servicedependency_mmap const& applier::state::servicedependencies() const throw () {
   return _servicedependencies;
 }
 
@@ -754,7 +754,7 @@ umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency
  *
  *  @return The current servicedependencies.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> >& applier::state::servicedependencies() throw () {
+servicedependency_mmap& applier::state::servicedependencies() throw () {
   return _servicedependencies;
 }
 
@@ -766,7 +766,7 @@ umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency
  *  @return Iterator to the element if found,
  *          servicedependencies().end() otherwise.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> >::const_iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) const {
+servicedependency_mmap ::const_iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) const {
   return const_cast<state*>(this)->servicedependencies_find(k);
 }
 
@@ -778,41 +778,39 @@ umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency
  *  @return Iterator to the element if found,
  *          servicedependencies().end() otherwise.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> >::iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) {
-  typedef umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> > collection;
+servicedependency_mmap ::iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) {
+  typedef umultimap<std::pair<std::string, std::string>, std::shared_ptr<engine::servicedependency> > collection;
   std::pair<collection::iterator, collection::iterator> p;
   p = _servicedependencies.equal_range(std::make_pair(k.dependent_hosts().front(), k.dependent_service_description().front()));
   while (p.first != p.second) {
     configuration::servicedependency current;
     current.configuration::object::operator=(k);
     current.dependent_hosts().push_back(
-                                p.first->second->dependent_host_name);
+                                p.first->second->get_dependent_host_name());
     current.dependent_service_description().push_back(
-              p.first->second->dependent_service_description);
-    current.hosts().push_back(p.first->second->host_name);
+              p.first->second->get_dependent_service_description());
+    current.hosts().push_back(p.first->second->get_host_name());
     current.service_description().push_back(
-              p.first->second->service_description);
-    current.dependency_period(p.first->second->dependency_period
-                              ? p.first->second->dependency_period
-                              : "");
-    current.inherits_parent(p.first->second->inherits_parent);
+              p.first->second->get_service_description());
+    current.dependency_period(p.first->second->get_dependency_period());
+    current.inherits_parent(p.first->second->get_inherits_parent());
     unsigned int options(
-                   (p.first->second->fail_on_ok
+                   (p.first->second->get_fail_on_ok()
                     ? configuration::servicedependency::ok
                     : 0)
-                   | (p.first->second->fail_on_warning
+                   | (p.first->second->get_fail_on_warning()
                       ? configuration::servicedependency::warning
                       : 0)
-                   | (p.first->second->fail_on_unknown
+                   | (p.first->second->get_fail_on_unknown()
                       ? configuration::servicedependency::unknown
                       : 0)
-                   | (p.first->second->fail_on_critical
+                   | (p.first->second->get_fail_on_critical()
                       ? configuration::servicedependency::critical
                       : 0)
-                   | (p.first->second->fail_on_pending
+                   | (p.first->second->get_fail_on_pending()
                       ? configuration::servicedependency::pending
                       : 0));
-    if (p.first->second->dependency_type == engine::hostdependency::notification) {
+    if (p.first->second->get_dependency_type() == engine::dependency::notification) {
       current.dependency_type(
         configuration::servicedependency::notification_dependency);
       current.notification_failure_options(options);

centreon/centreon-collect

centreon-engine/inc/com/centreon/engine/dependency.hh
@@ -1,0 +1,87 @@
+/*
+** Copyright 2011-2019 Centreon
+**
+** This file is part of Centreon Engine.
+**
+** Centreon Engine is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Engine is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Engine. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CENTREON_ENGINE_DEPENDENCY_HH
+#define CENTREON_ENGINE_DEPENDENCY_HH
+
+#  include <ostream>
+#  include <string>
+#  include <com/centreon/engine/namespace.hh>
+#  include <com/centreon/engine/timeperiod.hh>
+
+CCE_BEGIN();
+
+class                         dependency {
+
+ public:
+  enum                        types {
+    notification = 1,
+    execution
+  };
+
+                                dependency(std::string const& dependent_hostname,
+                                           std::string const& hostname,
+                                           types dependency_type,
+                                           bool inherits_parent,
+                                           bool fail_on_pending,
+                                           std::string const& dependency_period);
+
+  types                         get_dependency_type() const;
+  void                          set_dependency_type(types dependency_type);
+  std::string const&            get_dependent_hostname() const;
+  void                          set_dependent_hostname(
+    std::string const& dependent_host_name);
+  std::string const&            get_hostname() const;
+  void                          set_hostname(std::string const& hostname);
+  std::string const&            get_dependency_period() const;
+  void                          set_dependency_period(
+    std::string const& dependency_period);
+  bool                          get_inherits_parent() const;
+  void                          set_inherits_parent(bool inherits_parent);
+  bool                          get_fail_on_pending() const;
+  void                          set_fail_on_pending(bool fail_on_pending);
+  bool                          get_circular_path_checked() const;
+  void                          set_circular_path_checked(
+    bool circular_path_checked);
+  bool                          get_contains_circular_path() const;
+  void                          set_contains_circular_path(
+    bool contains_circular_path);
+
+  virtual bool                  operator==(dependency const& obj) throw ();
+  bool                          operator!=(dependency const& obj) throw ();
+  virtual bool                  operator<(dependency const& obj) throw ();
+
+  com::centreon::engine::timeperiod*
+                                dependency_period_ptr;
+
+ protected:
+  types                         _dependency_type;
+  std::string                   _dependent_hostname;
+  std::string                   _hostname;
+  std::string                   _dependency_period;
+  bool                          _inherits_parent;
+  bool                          _fail_on_pending;
+  bool                          _circular_path_checked;
+  bool                          _contains_circular_path;
+
+};
+
+CCE_END();
+
+#endif //CENTREON_ENGINE_DEPENDENCY_HH

centreon-engine/src/configuration/applier/state.cc
@@ -523,8 +523,8 @@ umultimap<std::string, std::shared_ptr<com::centreon::engine::hostdependency> >:
     configuration::hostdependency current;
     current.configuration::object::operator=(k);
     current.dependent_hosts().insert(
-                                p.first->second->get_dependent_host_name());
-    current.hosts().insert(p.first->second->get_host_name());
+                                p.first->second->get_dependent_hostname());
+    current.hosts().insert(p.first->second->get_hostname());
     current.dependency_period((!p.first->second->get_dependency_period().empty()
                               ? p.first->second->get_dependency_period().c_str()
                               : ""));
@@ -745,7 +745,7 @@ std::unordered_map<std::pair<unsigned long, unsigned long>,
  *
  *  @return The current servicedependencies.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> > const& applier::state::servicedependencies() const throw () {
+servicedependency_mmap const& applier::state::servicedependencies() const throw () {
   return _servicedependencies;
 }
 
@@ -754,7 +754,7 @@ umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency
  *
  *  @return The current servicedependencies.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> >& applier::state::servicedependencies() throw () {
+servicedependency_mmap& applier::state::servicedependencies() throw () {
   return _servicedependencies;
 }
 
@@ -766,7 +766,7 @@ umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency
  *  @return Iterator to the element if found,
  *          servicedependencies().end() otherwise.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> >::const_iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) const {
+servicedependency_mmap ::const_iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) const {
   return const_cast<state*>(this)->servicedependencies_find(k);
 }
 
@@ -778,41 +778,39 @@ umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency
  *  @return Iterator to the element if found,
  *          servicedependencies().end() otherwise.
  */
-umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> >::iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) {
-  typedef umultimap<std::pair<std::string, std::string>, std::shared_ptr<servicedependency_struct> > collection;
+servicedependency_mmap ::iterator applier::state::servicedependencies_find(configuration::servicedependency::key_type const& k) {
+  typedef umultimap<std::pair<std::string, std::string>, std::shared_ptr<engine::servicedependency> > collection;
   std::pair<collection::iterator, collection::iterator> p;
   p = _servicedependencies.equal_range(std::make_pair(k.dependent_hosts().front(), k.dependent_service_description().front()));
   while (p.first != p.second) {
     configuration::servicedependency current;
     current.configuration::object::operator=(k);
     current.dependent_hosts().push_back(
-                                p.first->second->dependent_host_name);
+                                p.first->second->get_dependent_hostname());
     current.dependent_service_description().push_back(
-              p.first->second->dependent_service_description);
-    current.hosts().push_back(p.first->second->host_name);
+              p.first->second->get_dependent_service_description());
+    current.hosts().push_back(p.first->second->get_hostname());
     current.service_description().push_back(
-              p.first->second->service_description);
-    current.dependency_period(p.first->second->dependency_period
-                              ? p.first->second->dependency_period
-                              : "");
-    current.inherits_parent(p.first->second->inherits_parent);
+              p.first->second->get_service_description());
+    current.dependency_period(p.first->second->get_dependency_period());
+    current.inherits_parent(p.first->second->get_inherits_parent());
     unsigned int options(
-                   (p.first->second->fail_on_ok
+                   (p.first->second->get_fail_on_ok()
                     ? configuration::servicedependency::ok
                     : 0)
-                   | (p.first->second->fail_on_warning
+                   | (p.first->second->get_fail_on_warning()
                       ? configuration::servicedependency::warning
                       : 0)
-                   | (p.first->second->fail_on_unknown
+                   | (p.first->second->get_fail_on_unknown()
                       ? configuration::servicedependency::unknown
                       : 0)
-                   | (p.first->second->fail_on_critical
+                   | (p.first->second->get_fail_on_critical()
                       ? configuration::servicedependency::critical
                       : 0)
-                   | (p.first->second->fail_on_pending
+                   | (p.first->second->get_fail_on_pending()
                       ? configuration::servicedependency::pending
                       : 0));
-    if (p.first->second->dependency_type == engine::hostdependency::notification) {
+    if (p.first->second->get_dependency_type() == engine::dependency::notification) {
       current.dependency_type(
         configuration::servicedependency::notification_dependency);
       current.notification_failure_options(options);