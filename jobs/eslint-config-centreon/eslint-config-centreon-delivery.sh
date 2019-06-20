8ba7c929d3ab1a31870b8105b5af394eb6f1b9d7
Kevin Duret <kduret@centreon.com>
2019-06-20 17:09:03 +0200

    feat(jobs): create job to publish eslint-config-centreon (#145)

---
+++ jobs/eslint-config-centreon/eslint-config-centreon-delivery.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=eslint-config-centreon
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+get_internal_source "eslint-config-centreon/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+
+cd "$PROJECT-$VERSION"
+
+cat > .npmrc << EOF
+//registry.npmjs.org/:_authToken=2a5c102e-2d32-449a-9a5f-03da082f123b
+EOF
+
+npm ci
+if [ "$BRANCH_NAME" == "master" ] ; then
+  # if job is run from master branch, publish the next release as an alpha
+  npm version $VERSION-alpha.$BUILD_NUMBER
+  npm publish --access=public --tag=next ./
+else
+  # if job is run from another branch, publish the branch as version 0.0.0 and with tag unstable
+  npm version 0.0.0-$BRANCH_NAME.$BUILD_NUMBER
+  if [ "$BUILD_NUMBER" -gt "1" ] ; then
+    npm deprecate @centreon/eslint-config-centreon@"0.0.0-$BRANCH_NAME.0 - 0.0.0-$BRANCH_NAME.$(($BUILD_NUMBER-1))" "deprecate previous versions of branch $BRANCH_NAME"
+  fi
+  npm publish --access=public --tag=unstable ./
+fi
---
+++ jobs/eslint-config-centreon/eslint-config-centreon-stable.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=eslint-config-centreon
+
+# Check arguments.
+if [ -z "$COMMIT" ]; then
+  echo "You need to specify COMMIT environment variables."
+  exit 1
+fi
+
+# Get version.
+cd $PROJECT
+git checkout --detach "$COMMIT"
+export VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+cat > .npmrc << EOF
+//registry.npmjs.org/:_authToken=2a5c102e-2d32-449a-9a5f-03da082f123b
+EOF
+
+# install dependencies
+npm ci
+
+# release on npm registry
+npm publish --access=public --tag=latest ./
+
+# push tag on github if $COMMIT is not a tag
+if ! git tag --list | egrep -q "^$COMMIT$" ; then
+  git config remote.origin.url ssh://git@github.com/centreon/eslint-config-centreon
+  git tag $VERSION
+  git push origin $VERSION
+fi
+
