94c620fc58bd14113d1ba8bbc4523a4b732b6b30
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-07-31 09:40:29 +0200

    fix(jobs): upport 19.04 fixes to 19.10 in map-desktop-stable.

--- centreon-build/jobs/map/19.10/map-desktop-stable.sh
+++ centreon-build/jobs/map/19.10/map-desktop-stable.sh
@@ -18,14 +18,14 @@ export BUGFIX=`echo $VERSION | cut -d . -f 3`
 # Move artifacts to the stable directory.
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
 $SSH_REPO rm -rf "/srv/sources/map/stable/$PROJECT-desktop-$VERSION"
-$SSH_REPO mv "/srv/sources/map/testing/map-desktop/$PROJECT-desktop-$MAJOR.$MINOR-$RELEASE" "/srv/sources/map/stable/"
+$SSH_REPO mv "/srv/sources/map/testing/map-desktop/$PROJECT-desktop-$MAJOR.$MINOR-$RELEASE/*" "/srv/sources/map/stable/"
 
 # Upload installers to centreon download website
 # The url is specific for this version. We have to change it for new minor versions
 
 BASE_INTERNAL_URL="/srv/sources/map/stable/$PROJECT-desktop-$MAJOR.$MINOR-$RELEASE/"
 PRIVATE_EXTERNAL_URL="s3://centreon-download/enterprises/centreon-map/centreon-map-$MAJOR.$MINOR/centreon-map-$MAJOR.$MINOR/9ae03a4457fa0ce578379a4e0c8b51f2/"
-PUBLIC_EXTERNAL_URL="s3://centreon-download/public/centreon-map/"
+PUBLIC_EXTERNAL_URL="s3://centreon-download/public/centreon-map"
 
 # Copy MacOS .tar.gz version
 PRODUCT_NAME='Centreon-Map4.product-macosx.cocoa.x86_64.tar.gz'
@@ -33,7 +33,7 @@ EXTERNAL_PRODUCT_NAME="centreon-map-$VERSION.product-macosx.cocoa.x86_64.tar.gz"
 $SSH_REPO aws s3 cp --acl public-read "$BASE_INTERNAL_URL/$PRODUCT_NAME" "$PRIVATE_EXTERNAL_URL/$EXTERNAL_PRODUCT_NAME"
 $SSH_REPO aws s3 cp --acl public-read "$BASE_INTERNAL_URL/$PRODUCT_NAME" "$PUBLIC_EXTERNAL_URL/$EXTERNAL_PRODUCT_NAME"
 SRCHASH=`$SSH_REPO "md5sum $BASE_INTERNAL_URL/$PRODUCT_NAME | cut -d ' ' -f 1"`
-curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION.product-macosx.cocoa.x86_64.tar.gz&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
+curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION.product-macosx.cocoa.x86_64&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
 
 # Copy Windows .exe
 PRODUCT_NAME="centreon-map4-desktop-client-$VERSION-SNAPSHOT-x86_64.exe"
