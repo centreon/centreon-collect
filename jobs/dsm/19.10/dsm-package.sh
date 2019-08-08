20cb9889aea515bc4d64f8ab9bde25b1632e009b
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-08-08 10:20:44 +0200

    feat(jobs): handle pre-release identifiers in dsm-package.

--- jobs/dsm/19.10/dsm-package.sh
+++ jobs/dsm/19.10/dsm-package.sh
@@ -19,16 +19,26 @@ if [ "$#" -lt 1 ] ; then
 fi
 DISTRIB="$1"
 
-# Fetch sources.
-rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
-get_internal_source "dsm/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
-tar xzf "$PROJECT-$VERSION.tar.gz"
-
 # Create input and output directories.
 rm -rf input
 mkdir input
 rm -rf output
 mkdir output
+
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+get_internal_source "dsm/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+OLDVERSION="$VERSION"
+OLDRELEASE="$RELEASE"
+PRERELEASE=`echo $VERSION | cut -d - -f 2-`
+if [ -n "$PRERELEASE" ] ; then
+  export VERSION=`echo $VERSION | cut -d - -f 1`
+  export RELEASE="$PRERELEASE.$RELEASE"
+  rm -rf "$PROJECT-$VERSION"
+  mv "$PROJECT-$OLDVERSION" "$PROJECT-$VERSION"
+  tar czf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+fi
 
 # Pull latest build dependencies.
 BUILD_IMG="registry.centreon.com/mon-build-dependencies-19.10:$DISTRIB"
@@ -39,6 +49,8 @@ cp "$PROJECT-$VERSION.tar.gz" input/
 cp `dirname $0`"/../../../packaging/dsm/19.10/$PROJECT.spectemplate" input/
 cp `dirname $0`/../../../packaging/dsm/src/* input/
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
+export VERSION="$OLDVERSION"
+export RELEASE="$OLDRELEASE"
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos7' ] ; then
