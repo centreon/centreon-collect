e724fb796c4f086d1a9a3cb5671dcf5ec005bc0a
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-08-08 09:15:50 +0200

    feat(jobs): handle pre-release identifiers in mon-ppm-package.

--- centreon-build/jobs/ppm/19.10/mon-ppm-package.sh
+++ centreon-build/jobs/ppm/19.10/mon-ppm-package.sh
@@ -19,16 +19,26 @@ if [ "$#" -lt 1 ] ; then
 fi
 DISTRIB="$1"
 
-# Fetch sources.
-rm -rf "$PROJECT-$VERSION-php72.tar.gz" "$PROJECT-$VERSION-php72"
-get_internal_source "ppm/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php72.tar.gz"
-tar xzf "$PROJECT-$VERSION-php72.tar.gz"
-
 # Create input and output directories.
 rm -rf input
 mkdir input
 rm -rf output
 mkdir output
+
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION-php72.tar.gz" "$PROJECT-$VERSION-php72"
+get_internal_source "ppm/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php72.tar.gz"
+tar xzf "$PROJECT-$VERSION-php72.tar.gz"
+OLDVERSION="$VERSION"
+OLDRELEASE="$RELEASE"
+PRERELEASE=`echo $VERSION | cut -d - -f 2-`
+if [ -n "$PRERELEASE" ] ; then
+  export VERSION=`echo $VERSION | cut -d - -f 1`
+  export RELEASE="$PRERELEASE.$RELEASE"
+  rm -rf "$PROJECT-$VERSION"
+  mv "$PROJECT-$OLDVERSION-php72" "$PROJECT-$VERSION-php72"
+  tar czf "$PROJECT-$VERSION-php72.tar.gz" "$PROJECT-$VERSION-php72"
+fi
 
 # Retrieve sources.
 cp "$PROJECT-$VERSION-php72.tar.gz" input/"$PROJECT-$VERSION-php72.tar.gz"
@@ -40,6 +50,8 @@ docker pull "$BUILD_IMG"
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
+export VERSION="$OLDVERSION"
+export RELEASE="$OLDRELEASE"
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos7' ] ; then
