44d4dc875dc34c300b55b5568b28ea68d3c1b8e1
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-08-08 09:06:38 +0200

    feat(jobs): handle pre-release identifiers in mon-lm-package.

--- centreon-build/jobs/lm/19.10/mon-lm-package.sh
+++ centreon-build/jobs/lm/19.10/mon-lm-package.sh
@@ -30,6 +30,21 @@ mkdir input
 rm -rf output
 mkdir output
 
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION-php72.tar.gz" "$PROJECT-$VERSION-php72"
+get_internal_source "lm/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php72.tar.gz"
+tar xzf "$PROJECT-$VERSION-php72.tar.gz"
+OLDVERSION="$VERSION"
+OLDRELEASE="$RELEASE"
+PRERELEASE=`echo $VERSION | cut -d - -f 2-`
+if [ -n "$PRERELEASE" ] ; then
+  export VERSION=`echo $VERSION | cut -d - -f 1`
+  export RELEASE="$PRERELEASE.$RELEASE"
+  rm -rf "$PROJECT-$VERSION"
+  mv "$PROJECT-$OLDVERSION-php72" "$PROJECT-$VERSION-php72"
+  tar czf "$PROJECT-$VERSION-php72.tar.gz" "$PROJECT-$VERSION-php72"
+fi
+
 # Retrieve sources.
 cp "$PROJECT-$VERSION-php72.tar.gz" input/"$PROJECT-$VERSION-php72.tar.gz"
 cp "$PROJECT-$VERSION-php72/packaging/$PROJECT.spectemplate" input/
@@ -40,6 +55,8 @@ docker pull "$BUILD_IMG"
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
+export VERSION="$OLDVERSION"
+export RELEASE="$OLDRELEASE"
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos7' ] ; then
