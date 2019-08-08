3c9d3dd0ba1cdd4ec00294e5943f3f7f73a0ec43
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-08-08 09:48:22 +0200

    feat(jobs): handle pre-release identifiers in mon-awie-package.

--- jobs/awie/19.10/mon-awie-package.sh
+++ jobs/awie/19.10/mon-awie-package.sh
@@ -19,16 +19,26 @@ if [ "$#" -lt 1 ] ; then
 fi
 DISTRIB="$1"
 
-# Fetch sources.
-rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
-get_internal_source "awie/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
-tar xzf "$PROJECT-$VERSION.tar.gz"
-
 # Create input and output directories.
 rm -rf input
 mkdir input
 rm -rf output
 mkdir output
+
+# Fetch sources.
+rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+get_internal_source "awie/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+tar xzf "$PROJECT-$VERSION.tar.gz"
+OLDVERSION="$VERSION"
+OLDRELEASE="$RELEASE"
+PRERELEASE=`echo $VERSION | cut -d - -f 2-`
+if [ -n "$PRERELEASE" ] ; then
+  export VERSION=`echo $VERSION | cut -d - -f 1`
+  export RELEASE="$PRERELEASE.$RELEASE"
+  rm -rf "$PROJECT-$VERSION"
+  mv "$PROJECT-$OLDVERSION" "$PROJECT-$VERSION"
+  tar czf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
+fi
 
 # Retrieve sources.
 cp "$PROJECT-$VERSION.tar.gz" input/"$PROJECT-$VERSION.tar.gz"
@@ -40,6 +50,8 @@ docker pull "$BUILD_IMG"
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_IMG" input output
+export VERSION="$OLDVERSION"
+export RELEASE="$OLDRELEASE"
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos7' ] ; then
