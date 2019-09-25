7c86c8ba64b6a4cb21e18658512cd114d395049f
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-09-25 11:52:01 +0200

    feat(jobs): add gorgone-delivery.

---
+++ jobs/gorgone/19.10/gorgone-delivery.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=centreon-gorgone
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+#
+# Release delivery.
+#
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  copy_internal_source_to_testing "standard" "gorgone" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_testing "standard" "19.10" "el7" "noarch" "gorgone" "$PROJECT-$VERSION-$RELEASE"
+
+#
+# CI delivery.
+#
+else
+  promote_canary_rpms_to_unstable "standard" "19.10" "el7" "noarch" "gorgone" "$PROJECT-$VERSION-$RELEASE"
+fi
