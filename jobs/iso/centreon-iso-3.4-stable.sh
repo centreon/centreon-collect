9f3ef19a591905ce900bc57706654c8e53482272
Laurent Pinsivy <lpinsivy@centreon.com>
2019-10-16 16:34:49 +0200

    enh(build): only generate Centreon 3.4 ISO for el7 (no more el6)

--- jobs/iso/centreon-iso-3.4-stable.sh
+++ jobs/iso/centreon-iso-3.4-stable.sh
@@ -14,28 +14,26 @@ if [ -z "$RELEASE" ] ; then
 fi
 export VERSION=3.4
 
-# Release ISO for el6 and el7
-for distrib in el6 el7 ; do
-  SRCHASH=`$SSH_REPO "md5sum /srv/iso/centreon-$VERSION.$RELEASE.$distrib.x86_64.iso | cut -d ' ' -f 1"`
-  $SSH_REPO aws s3 cp --acl public-read "/srv/iso/centreon-$VERSION.$RELEASE.$distrib.x86_64.iso" "s3://centreon-iso/stable/centreon-$VERSION.$RELEASE.$distrib.x86_64.iso"
+# Release ISO for el7
+SRCHASH=`$SSH_REPO "md5sum /srv/iso/centreon-$VERSION.$RELEASE.el7.x86_64.iso | cut -d ' ' -f 1"`
+$SSH_REPO aws s3 cp --acl public-read "/srv/iso/centreon-$VERSION.$RELEASE.el7.x86_64.iso" "s3://centreon-iso/stable/centreon-$VERSION.$RELEASE.el7.x86_64.iso"
 
-  # Sync ISO in database (dryrun=1 does not show ISO on website)
-  OUTPUT=`curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=centreon&version=$VERSION.$RELEASE.$distrib.x86_64&extension=iso&md5=$SRCHASH&ddos=1&dryrun=1"`
-  SUCCESS=`echo $OUTPUT | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["status"]'`
-  if [ \( "$SUCCESS" '!=' "success" \) ] ; then
-    echo "ISO synchronization failed."
-    exit 1
-  fi
+# Sync ISO in database (dryrun=1 does not show ISO on website)
+OUTPUT=`curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=centreon&version=$VERSION.$RELEASE.el7.x86_64&extension=iso&md5=$SRCHASH&ddos=1&dryrun=1"`
+SUCCESS=`echo $OUTPUT | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["status"]'`
+if [ \( "$SUCCESS" '!=' "success" \) ] ; then
+  echo "ISO synchronization failed."
+  exit 1
+fi
 
-  DOWNLOADID=`echo $OUTPUT | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"]'`
+DOWNLOADID=`echo $OUTPUT | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"]'`
 
-  # enable download link on website
-  curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&action=enable&release_id=$DOWNLOADID"
+# enable download link on website
+curl "https://download.centreon.com/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&action=enable&release_id=$DOWNLOADID"
 
-  # Check if download link is available
-  STATUSCODE=`curl -s -w "%{http_code}" "https://download.centreon.com/?action=download&id=$DOWNLOADID" -o /dev/null`
-  if [ \( "$STATUSCODE" -ne "200" \) ] ; then
-    echo "ISO cannot be downloaded."
-    exit 1
-  fi
-done
+# Check if download link is available
+STATUSCODE=`curl -s -w "%{http_code}" "https://download.centreon.com/?action=download&id=$DOWNLOADID" -o /dev/null`
+if [ \( "$STATUSCODE" -ne "200" \) ] ; then
+  echo "ISO cannot be downloaded."
+  exit 1
+fi
