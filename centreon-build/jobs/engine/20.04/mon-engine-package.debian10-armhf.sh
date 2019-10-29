0c95d52a817efc212712ad4e3d6e1999989a2680
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-10-29 10:24:29 +0100

    chore(jobs): only support Debian builds in 20.04 branch of Centreon Engine.

--- centreon-build/jobs/engine/20.04/mon-engine-package.debian10-armhf.sh
+++ centreon-build/jobs/engine/20.04/mon-engine-package.debian10-armhf.sh
@@ -9,14 +9,14 @@ cp -r `dirname $0`/../../../packaging/engine/debian "debuildir/${PROJECT}-${VERS
 sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}.debian10/g" < "debuildir/${PROJECT}-${VERSION}/debian/changelog.in" > "debuildir/${PROJECT}-${VERSION}/debian/changelog"
 
 # Launch debuild.
-containerid=`docker create registry.centreon.com/mon-build-dependencies-19.04:debian10 sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && debuild -us -uc -i"`
+containerid=`docker create registry.centreon.com/mon-build-dependencies-20.04:debian10-armhf sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && export CC=arm-linux-gnueabihf-gcc && export CXX=arm-linux-gnueabihf-g++ && dpkg-buildpackage -us -uc -d -aarmhf"`
 docker cp debuildir "$containerid:/usr/local/src/debuildir"
 docker start -a "$containerid"
 
 # Send package to repository.
 rm -rf debuildir
 docker cp "$containerid:/usr/local/src/debuildir" .
-put_internal_debs "19.04" "buster" debuildir/*.deb
+put_internal_debs "20.04" "buster" debuildir/*.deb
 
 # Stop container.
 docker stop "$containerid"
