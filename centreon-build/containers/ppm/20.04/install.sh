ddac12a009b098566b50912295d6371811d575c2
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-10-30 16:13:44 +0100

    feat(containers): base 20.04 mon-ppm image on mon-web instead of mon-lm.

--- centreon-build/containers/ppm/20.04/install.sh
+++ centreon-build/containers/ppm/20.04/install.sh
@@ -4,5 +4,6 @@ set -e
 set -x
 
 service mysql start
+/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-license-manager
 /tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-pp-manager
 service mysql stop
