e7c2bd727c20d8af9b358165bcb7e427bcedef36
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-11-05 11:17:18 +0100

    fix(containers): use base yum configuration for dnf.

--- centreon-build/containers/build-dependencies/20.04/install.centos8.sh
+++ centreon-build/containers/build-dependencies/20.04/install.centos8.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+# Base dnf configuration.
+echo 'http_caching=none' >> /etc/yum.conf
+echo 'assumeyes=1' >> /etc/yum.conf
+
 # Install development repository.
 curl -o centreon-release.rpm "http://srvi-repo.int.centreon.com/yum/standard/20.04/el8/stable/noarch/RPMS/centreon-release-20.04-1.el8.noarch.rpm"
 dnf install --nogpgcheck centreon-release.rpm
