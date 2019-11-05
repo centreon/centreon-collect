796ff8ee1d1e6924d547764b213a2a54df4b78b1
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-11-05 10:38:21 +0100

    feat(containers): first version of mon-build-dependencies:centos8.

---
+++ centreon-build/containers/build-dependencies/20.04/install.centos8.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Install development repository.
+curl -o centreon-release.rpm "http://srvi-repo.int.centreon.com/yum/standard/20.04/el7/stable/noarch/RPMS/centreon-release-20.04-2.el8.centos.noarch.rpm"
+dnf install --nogpgcheck centreon-release.rpm
+sed -i -e 's#yum.centreon.com#srvi-repo.int.centreon.com/yum#g' /etc/yum.repos.d/centreon.repo
+yum-config-manager --enable 'centreon-testing*'
+
+# Install required build dependencies for all Centreon projects.
+xargs dnf install < /tmp/build-dependencies.txt
+
+# Install Node.js and related elements.
+curl --silent --location https://rpm.nodesource.com/setup_12.x | bash -
+dnf install --nogpgcheck -y nodejs
+npm install -g gulp
+
+# Install Composer.
+yum install -y php php-cli php-dom php-mbstring
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
+
+# Enable unstable repositories.
+yum-config-manager --enable 'centreon-unstable*'
