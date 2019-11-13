3903bfddeb6ee8c029811a34ae494d14f7dc97d1
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-11-13 11:48:22 +0100

    feat(jobs): run frontend unit tests in des-bam-unittest

--- jobs/bam/20.04/des-bam-unittest.sh
+++ jobs/bam/20.04/des-bam-unittest.sh
@@ -27,16 +27,19 @@ tar xzf "$PROJECT-$VERSION-full.tar.gz"
 # Launch mon-unittest container.
 UT_IMAGE=registry.centreon.com/mon-unittest-20.04:$DISTRIB
 docker pull $UT_IMAGE
-containerid=`docker create $UT_IMAGE /usr/local/bin/unittest-phing $PROJECT`
+containerid=`docker create $UT_IMAGE /usr/local/bin/unittest.sh $PROJECT`
 
 # Copy sources to container.
+docker cp `dirname $0`/des-bam-unittest.container.sh "$containerid:/usr/local/bin/unittest.sh"
 docker cp "$PROJECT-$VERSION-full" "$containerid:/usr/local/src/$PROJECT"
 
 # Run unit tests.
 docker start -a "$containerid"
-docker cp "$containerid:/tmp/ut.xml" ut.xml
-docker cp "$containerid:/tmp/coverage.xml" coverage.xml
-docker cp "$containerid:/tmp/codestyle.xml" codestyle.xml
+docker cp "$containerid:/tmp/ut-be.xml" ut-be.xml
+docker cp "$containerid:/tmp/ut-fe.xml" ut-fe.xml
+docker cp "$containerid:/tmp/coverage-be.xml" coverage-be.xml
+docker cp "$containerid:/tmp/codestyle-be.xml" codestyle-be.xml
+docker cp "$containerid:/tmp/codestyle-fe.xml" codestyle-fe.xml
 
 # Stop container.
 docker stop "$containerid"
