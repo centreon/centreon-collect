b895c8cd6b9eef2eb1c803f0847c9f9d685740be
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-11-29 10:18:18 +0100

    feat(jobs): update des-map-server to use specific build image.

--- centreon-build/jobs/map/20.04/des-map-server.sh
+++ centreon-build/jobs/map/20.04/des-map-server.sh
@@ -17,16 +17,28 @@ fi
 # Fetch sources.
 rm -rf "$PROJECT-server-$VERSION.tar.gz" "$PROJECT-server-$VERSION"
 get_internal_source "map/$PROJECT-server-$VERSIONSERVER-$RELEASE/$PROJECT-server-$VERSIONSERVER.tar.gz"
-tar xzf "$PROJECT-server-$VERSIONSERVER.tar.gz"
 
-# Build with Maven.
-cd "$PROJECT-server-$VERSIONSERVER"
-mvn -f map-server-parent/pom.xml clean install
+# Create and populate container.
+BUILD_IMAGE="registry.centreon.com/map-build-dependencies-20.04:centos7"
+docker pull "$BUILD_IMAGE"
+containerid=`docker create -e "VERSION=$VERSIONSERVER" $BUILD_IMAGE /usr/local/bin/server.sh`
+docker cp `dirname $0`/des-map-server.container.sh "$containerid:/usr/local/bin/server.sh"
+docker cp "$PROJECT-server-$VERSIONSERVER.tar.gz" "$containerid:/usr/local/src/"
 
-# This is run once the Maven build terminated.
-cd ..
-FILES_TOMCAT7="$PROJECT-server-$VERSIONSERVER/map-server-parent/map-server-packaging/map-server-packaging-tomcat7/target/rpm/centreon-map-server/RPMS/noarch/"'*.rpm'
-put_internal_rpms "20.04" "el7" "noarch" "map-server" "$PROJECT-server-$VERSIONSERVER-$RELEASE" $FILES_TOMCAT7
+# Run container.
+docker start -a "$containerid"
+
+# Copy artifacts.
+rm -rf noarch
+docker cp "$containerid:/usr/local/src/$PROJECT-server-$VERSIONSERVER/map-server-parent/map-server-packaging/map-server-packaging-tomcat7/target/rpm/centreon-map-server/RPMS/noarch" noarch
+
+# Stop container.
+docker stop "$containerid"
+docker rm "$containerid"
+
+# Upload artifacts.
+FILES_TOMCAT7="noarch/"'*.rpm'
+put_internal_rpms "20.04" "el7" "noarch" "map-server" "$PROJECT-server-$VERSIONSERVER-$RELEASE" noarch/*.rpm
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
 $SSH_REPO rpm --resign "/srv/yum/internal/20.04/el7/noarch/map-server/$PROJECT-server-$VERSIONSERVER-$RELEASE/*.rpm"
 $SSH_REPO createrepo "/srv/yum/internal/20.04/el7/noarch/map-server/$PROJECT-server-$VERSIONSERVER-$RELEASE"
