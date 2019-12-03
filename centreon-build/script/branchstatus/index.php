6cb7ad779a8c02f5f9d34417531350ae999d0ff6
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-12-03 11:47:57 +0100

    feat(script): update branch status script.

--- centreon-build/script/branchstatus/index.php
+++ centreon-build/script/branchstatus/index.php
@@ -14,240 +14,218 @@
     <div class="container container-fluid">
       <h1>Branch Status</h1>
 
-      <h2>20.04</h2>
-
       <table class="table table-hover table-sm" style="margin-top:50px">
         <thead class="thead-dark">
           <th scope="col">Project</th>
-          <th scope="col">Branch</th>
-          <th scope="col">Status</th>
-        </thead>
-        <tbody>
 <?php
 
-function printProjectStatus($projects)
-{
-  foreach ($projects as $project) {
-    // Retrieve project status from Jenkins.
-    $ch = curl_init();
-    curl_setopt(
-      $ch,
-      CURLOPT_URL,
-      'https://jenkins.centreon.com/job/' . $project['name'] . '/job/' .
-      $project['branch'] . '/lastBuild/api/json'
-    );
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERPWD, 'mkermagoret:1137c11b1b163d762610e4250e078582d7');
-    $job = json_decode(curl_exec($ch), true);
-
-    // Print project line.
-    $lineStyle = ($job['result'] === 'SUCCESS') ? 'alert-success' : 'alert-danger';
-    $jobLink = 'https://jenkins.centreon.com/job/' . $project['name'] . '/job/' . $project['branch'];
-    echo '<tr class="' . $lineStyle . '">' .
-         '  <td><a href="' . $jobLink . '">' . $project['name'] . '</a></td>' .
-         '  <td>' . $project['branch'] . '</td>' .
-         '  <td>' . $job['result'] . '</td>' .
-         '</tr>';
-
-    // Clone connection.
-    curl_close($ch);
-  }
+$versions = array('20.04.x', '19.10.x', '19.04.x', '18.10.x', '3.4.x');
+foreach ($versions as $version) {
+  echo '<th scope="col">' . $version . '</th>';
 }
 
-$projects2004 = array(
-  array('name' => 'centreon-autodiscovery', 'branch' => 'master'),
-  array('name' => 'centreon-awie', 'branch' => 'master'),
-  array('name' => 'centreon-broker', 'branch' => 'master'),
-  array('name' => 'centreon-clib', 'branch' => 'master'),
-  array('name' => 'centreon-connector', 'branch' => 'master'),
-  array('name' => 'centreon-dsm', 'branch' => 'master'),
-  array('name' => 'centreon-engine', 'branch' => 'master'),
-  array('name' => 'centreon-export', 'branch' => 'master'),
-  array('name' => 'centreon-license-manager', 'branch' => 'master'),
-  array('name' => 'centreon-open-tickets', 'branch' => 'master'),
-  array('name' => 'centreon-pp-manager', 'branch' => 'master'),
-  array('name' => 'centreon-react-components', 'branch' => 'master'),
-  array('name' => 'centreon-web', 'branch' => 'master'),
-  array('name' => 'centreon-bam', 'branch' => 'master'),
-  array('name' => 'centreon-map', 'branch' => 'master'),
-  array('name' => 'centreon-bi-engine', 'branch' => 'master'),
-  array('name' => 'centreon-bi-etl', 'branch' => 'master'),
-  array('name' => 'centreon-bi-report', 'branch' => 'master'),
-  array('name' => 'centreon-bi-reporting-server', 'branch' => 'master'),
-  array('name' => 'centreon-bi-server', 'branch' => 'master')
-);
-printProjectStatus($projects2004);
-
 ?>
-        </tbody>
-      </table>
-
-      <h2>19.10</h2>
-
-      <table class="table table-hover table-sm" style="margin-top:50px">
-        <thead class="thead-dark">
-          <th scope="col">Project</th>
-          <th scope="col">Branch</th>
-          <th scope="col">Status</th>
         </thead>
         <tbody>
 <?php
 
-$projects1910 = array(
-  array('name' => 'centreon-autodiscovery', 'branch' => '19.10.x'),
-  array('name' => 'centreon-awie', 'branch' => '19.10.x'),
-  array('name' => 'centreon-broker', 'branch' => '19.10.x'),
-  array('name' => 'centreon-clib', 'branch' => '19.10.x'),
-  array('name' => 'centreon-connector', 'branch' => '19.10.x'),
-  array('name' => 'centreon-dsm', 'branch' => '19.10.x'),
-  array('name' => 'centreon-engine', 'branch' => '19.10.x'),
-  array('name' => 'centreon-export', 'branch' => '19.10.x'),
-  array('name' => 'centreon-license-manager', 'branch' => '19.10.x'),
-  array('name' => 'centreon-open-tickets', 'branch' => '19.10.x'),
-  array('name' => 'centreon-pp-manager', 'branch' => '19.10.x'),
-  array('name' => 'centreon-react-components', 'branch' => '19.10.x'),
-  array('name' => 'centreon-web', 'branch' => '19.10.x'),
-  array('name' => 'centreon-bam', 'branch' => '19.10.x'),
-  array('name' => 'centreon-map', 'branch' => '19.10.x'),
-  array('name' => 'centreon-bi-engine', 'branch' => '19.10.x'),
-  array('name' => 'centreon-bi-etl', 'branch' => '19.10.x'),
-  array('name' => 'centreon-bi-report', 'branch' => '19.10.x'),
-  array('name' => 'centreon-bi-reporting-server', 'branch' => '19.10.x'),
-  array('name' => 'centreon-bi-server', 'branch' => '19.10.x')
+$projects = array(
+  'centreon-autodiscovery' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '2.4.x'
+  ),
+  'centreon-awie' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '1.0.x'
+  ),
+  'centreon-bam' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '3.6.x'
+  ),
+  'centreon-bi-engine' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x'
+  ),
+  'centreon-bi-etl' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x'
+  ),
+  'centreon-bi-report' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x'
+  ),
+  'centreon-bi-reporting-server' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x'
+  ),
+  'centreon-bi-server' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x'
+  ),
+  'centreon-broker' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '3.0.x'
+  ),
+  'centreon-clib' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04',
+    '18.10.x' => '18.10',
+    '3.4.x' => '1.4'
+  ),
+  'centreon-connector' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x'
+  ),
+  'centreon-dsm' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x'
+  ),
+  'centreon-engine' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '1.8.x'
+  ),
+  'centreon-export' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '2.3.x'
+  ),
+  'centreon-gorgone' => array(
+    '20.04.x' => 'master'
+  ),
+  'centreon-hub' => array(
+    '20.04.x' => 'master'
+  ),
+  'centreon-imp-portal-api' => array(
+    '20.04.x' => 'master'
+  ),
+  'centreon-license-manager' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '1.2.x'
+  ),
+  'centreon-map' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '4.4.x'
+  ),
+  'centreon-open-tickets' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '1.2.x'
+  ),
+  'centreon-plugins' => array(
+    '20.04.x' => 'master'
+  ),
+  'centreon-plugin-packs' => array(
+    '20.04.x' => 'master'
+  ),
+  'centreon-poller-display' => array(
+    '3.4.x' => 'master'
+  ),
+  'centreon-pp-manager' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '2.4.x'
+  ),
+  'centreon-react-components' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x'
+  ),
+  'centreon-vmware' => array(
+    '20.04.x' => 'master'
+  ),
+  'centreon-web' => array(
+    '20.04.x' => 'master',
+    '19.10.x' => '19.10.x',
+    '19.04.x' => '19.04.x',
+    '18.10.x' => '18.10.x',
+    '3.4.x' => '2.8.x'
+  )
 );
-printProjectStatus($projects1910);
 
-?>
-        </tbody>
-      </table>
+foreach ($projects as $project => $branches) {
+  // Line start.
+  echo '<tr><td>' . $project . '</td>';
 
-      <h2>19.04</h2>
+  foreach ($versions as $version) {
+    // Check if project has this version.
+    if (array_key_exists($version, $branches)) {
+      // Retrieve project status from Jenkins.
+      $ch = curl_init();
+      curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        'https://jenkins.centreon.com/job/' . $project . '/job/' .
+        $branches[$version] . '/lastBuild/api/json'
+      );
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_USERPWD, 'mkermagoret:1137c11b1b163d762610e4250e078582d7');
+      $job = json_decode(curl_exec($ch), true);
 
-      <table class="table table-hover table-sm" style="margin-top:50px">
-        <thead class="thead-dark">
-          <th scope="col">Project</th>
-          <th scope="col">Branch</th>
-          <th scope="col">Status</th>
-        </thead>
-        <tbody>
-<?php
+      // Print project/version cell.
+      if (empty($job['result'])) {
+        $cellStyle = 'alert-warning';
+        $jobResult = 'UNKNOWN';
+      } else if ($job['result'] === 'SUCCESS') {
+        $cellStyle = 'alert-success';
+        $jobResult = 'SUCCESS';
+      } else {
+        $cellStyle = 'alert-danger';
+        $jobResult = $job['result'];
+      }
+      $jobLink = 'https://jenkins.centreon.com/job/' . $project . '/job/' . $branches[$version];
+      echo '<td class="' . $cellStyle . '"><a href="' . $jobLink . '">' . $jobResult . '</a></td>';
 
-$projects1904 = array(
-  array('name' => 'centreon-autodiscovery', 'branch' => '19.04.x'),
-  array('name' => 'centreon-awie', 'branch' => '19.04.x'),
-  array('name' => 'centreon-broker', 'branch' => '19.04.x'),
-  array('name' => 'centreon-clib', 'branch' => '19.04'),
-  array('name' => 'centreon-connector', 'branch' => '19.04.x'),
-  array('name' => 'centreon-dsm', 'branch' => '19.04.x'),
-  array('name' => 'centreon-engine', 'branch' => '19.04.x'),
-  array('name' => 'centreon-export', 'branch' => '19.04.x'),
-  array('name' => 'centreon-license-manager', 'branch' => '19.04.x'),
-  array('name' => 'centreon-open-tickets', 'branch' => '19.04.x'),
-  array('name' => 'centreon-pp-manager', 'branch' => '19.04.x'),
-  array('name' => 'centreon-react-components', 'branch' => '19.04.x'),
-  array('name' => 'centreon-web', 'branch' => '19.04.x'),
-  array('name' => 'centreon-bam', 'branch' => '19.04.x'),
-  array('name' => 'centreon-map', 'branch' => '19.04.x'),
-  array('name' => 'centreon-bi-engine', 'branch' => '19.04.x'),
-  array('name' => 'centreon-bi-etl', 'branch' => '19.04.x'),
-  array('name' => 'centreon-bi-report', 'branch' => '19.04.x'),
-  array('name' => 'centreon-bi-reporting-server', 'branch' => '19.04.x'),
-  array('name' => 'centreon-bi-server', 'branch' => '19.04.x')
-);
-printProjectStatus($projects1904);
+      // Clone connection.
+      curl_close($ch);
+    } else {
+      echo '<td>N/A</td>';
+    }
+  }
 
-?>
-        </tbody>
-      </table>
-
-      <h2>18.10</h2>
-
-      <table class="table table-hover table-sm" style="margin-top:50px">
-        <thead class="thead-dark">
-          <th scope="col">Project</th>
-          <th scope="col">Branch</th>
-          <th scope="col">Status</th>
-        </thead>
-        <tbody>
-<?php
-
-$projects1810 = array(
-  array('name' => 'centreon-autodiscovery', 'branch' => '18.10.x'),
-  array('name' => 'centreon-awie', 'branch' => '18.10.x'),
-  array('name' => 'centreon-broker', 'branch' => '18.10.x'),
-  array('name' => 'centreon-clib', 'branch' => '18.10'),
-  array('name' => 'centreon-connector', 'branch' => '18.10.x'),
-  array('name' => 'centreon-engine', 'branch' => '18.10.x'),
-  array('name' => 'centreon-export', 'branch' => '18.10.x'),
-  array('name' => 'centreon-license-manager', 'branch' => '18.10.x'),
-  array('name' => 'centreon-open-tickets', 'branch' => '18.10.x'),
-  array('name' => 'centreon-pp-manager', 'branch' => '18.10.x'),
-  array('name' => 'centreon-web', 'branch' => '18.10.x'),
-  array('name' => 'centreon-bam', 'branch' => '18.10.x'),
-  array('name' => 'centreon-map', 'branch' => '18.10.x'),
-  array('name' => 'centreon-bi-engine', 'branch' => '18.10.x'),
-  array('name' => 'centreon-bi-etl', 'branch' => '18.10.x'),
-  array('name' => 'centreon-bi-report', 'branch' => '18.10.x'),
-  array('name' => 'centreon-bi-reporting-server', 'branch' => '18.10.x'),
-  array('name' => 'centreon-bi-server', 'branch' => '18.10.x')
-);
-printProjectStatus($projects1810);
-
-?>
-        </tbody>
-      </table>
-
-      <h2>3.4</h2>
-
-      <table class="table table-hover table-sm" style="margin-top:50px">
-        <thead class="thead-dark">
-          <th scope="col">Project</th>
-          <th scope="col">Branch</th>
-          <th scope="col">Status</th>
-        </thead>
-        <tbody>
-<?php
-
-$projects34 = array(
-  array('name' => 'centreon-autodiscovery', 'branch' => '2.4.x'),
-  array('name' => 'centreon-awie', 'branch' => '1.0.x'),
-  array('name' => 'centreon-broker', 'branch' => '3.0.x'),
-  array('name' => 'centreon-clib', 'branch' => '1.4'),
-  array('name' => 'centreon-engine', 'branch' => '1.8.x'),
-  array('name' => 'centreon-export', 'branch' => '2.3.x'),
-  array('name' => 'centreon-license-manager', 'branch' => '1.2.x'),
-  array('name' => 'centreon-open-tickets', 'branch' => '1.2.x'),
-  array('name' => 'centreon-poller-display', 'branch' => 'master'),
-  array('name' => 'centreon-pp-manager', 'branch' => '2.4.x'),
-  array('name' => 'centreon-web', 'branch' => '2.8.x'),
-  array('name' => 'centreon-bam', 'branch' => '3.6.x'),
-  array('name' => 'centreon-map', 'branch' => '4.4.x')
-);
-printProjectStatus($projects34);
-
-?>
-        </tbody>
-      </table>
-
-      <h2>Common</h2>
-
-      <table class="table table-hover table-sm" style="margin-top:50px">
-        <thead class="thead-dark">
-          <th scope="col">Project</th>
-          <th scope="col">Branch</th>
-          <th scope="col">Status</th>
-        </thead>
-        <tbody>
-<?php
-
-$projectsCommon = array(
-  array('name' => 'centreon-hub', 'branch' => 'master'),
-  array('name' => 'centreon-imp-portal-api', 'branch' => 'master'),
-  array('name' => 'centreon-plugins', 'branch' => 'master'),
-  array('name' => 'centreon-plugin-packs', 'branch' => 'master'),
-  array('name' => 'centreon-vmware', 'branch' => 'master')
-);
-printProjectStatus($projectsCommon);
+  // Line end.
+  echo '</tr>';
+}
 
 ?>
     </div>
