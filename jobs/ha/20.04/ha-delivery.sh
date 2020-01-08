e4b346dfa24439f8315658d972c616dbe177f594
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-08 09:04:02 +0100

    feat(jobs): add jobs for Centreon HA.

---
+++ jobs/ha/20.04/ha-delivery.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=centreon-ha
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+#
+# Release delivery.
+#
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  # Copy build artifacts.
+  copy_internal_source_to_testing "standard" "ha" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_testing "standard" "20.04" "el7" "noarch" "ha" "$PROJECT-$VERSION-$RELEASE"
+
+  # Create entry in download-dev.
+  SRCHASH=00112233445566778899aabbccddeeff
+  curl "$DLDEV_URL/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
+
+#
+# CI delivery.
+#
+else
+  promote_canary_rpms_to_unstable "standard" "20.04" "el7" "noarch" "ha" "$PROJECT-$VERSION-$RELEASE"
+fi
