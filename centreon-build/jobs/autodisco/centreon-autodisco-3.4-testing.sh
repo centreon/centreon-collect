a39b84885d36c96e7c5203486753adaef3d518d3
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-13 14:27:17 +0100

    feat(jobs): add centreon-autodisco-3.4-testing.

---
+++ centreon-build/jobs/autodisco/centreon-autodisco-3.4-testing.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=centreon-autodiscovery
+
+# Check arguments.
+if [ -z "$COMMIT" -o -z "$RELEASE" ] ; then
+  echo "You need to specify COMMIT and RELEASE environment variables."
+  exit 1
+fi
+
+# Pull mon-build-dependencies containers.
+docker pull registry.centreon.com/mon-build-dependencies-3.4:centos6
+docker pull registry.centreon.com/mon-build-dependencies-3.4:centos7
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output-centos6
+mkdir output-centos6
+rm -rf output-centos7
+mkdir output-centos7
+
+# Get version.
+cd "$PROJECT"
+git checkout --detach "$COMMIT"
+export VERSION=`grep mod_release www/modules/centreon-autodiscovery-server/conf.php | cut -d '"' -f 4`
+
+# Create source tarball.
+git archive "--prefix=$PROJECT-$VERSION/" HEAD | gzip > "../$PROJECT-$VERSION.tar.gz"
+cd ..
+
+# Encrypt source tarballs.
+curl -F file=@$PROJECT-$VERSION.tar.gz -F 'version=53' -F "modulename=$PROJECT" 'http://encode.int.centreon.com/api/' -o "input/$PROJECT-$VERSION-php53.tar.gz"
+curl -F file=@$PROJECT-$VERSION.tar.gz -F 'version=54' -F "modulename=$PROJECT" 'http://encode.int.centreon.com/api/' -o "input/$PROJECT-$VERSION-php54.tar.gz"
+
+# Build RPMs.
+cp $PROJECT/packaging/* input/
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key registry.centreon.com/mon-build-dependencies-3.4:centos6 input output-centos6
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key registry.centreon.com/mon-build-dependencies-3.4:centos7 input output-centos7
+
+# Copy files to server.
+put_testing_rpms "plugin-packs" "3.4" "el6" "noarch" "$PROJECT-$VERSION-$RELEASE" output-centos6/noarch/*.rpm
+put_testing_rpms "plugin-packs" "3.4" "el7" "noarch" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
