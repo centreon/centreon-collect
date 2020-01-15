c99605956aa960d6b0d76dca7d8709b32e790e93
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-15 09:00:06 +0100

    fix(jobs): update source paths in centreon-autodisco-3.4-stable.

--- jobs/autodisco/centreon-autodisco-3.4-stable.sh
+++ jobs/autodisco/centreon-autodisco-3.4-stable.sh
@@ -16,14 +16,14 @@ fi
 
 # Move sources to the stable directory.
 SSH_REPO='ssh ubuntu@srvi-repo.int.centreon.com'
-$SSH_REPO mv "/srv/sources/standard/testing/autodisco/$PROJECT-$VERSION-$RELEASE" "/srv/sources/standard/stable/"
+$SSH_REPO mv "/srv/sources/plugin-packs/testing/autodisco/$PROJECT-$VERSION-$RELEASE" "/srv/sources/plugin-packs/stable/"
 
 # Put sources online.
-SRCHASH=`$SSH_REPO "md5sum /srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php53.tar.gz | cut -d ' ' -f 1"`
-$SSH_REPO aws s3 cp --acl public-read "/srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php53.tar.gz" "s3://centreon-download/public/$PROJECT/$PROJECT-$VERSION-php53.tar.gz"
+SRCHASH=`$SSH_REPO "md5sum /srv/sources/plugin-packs/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php53.tar.gz | cut -d ' ' -f 1"`
+$SSH_REPO aws s3 cp --acl public-read "/srv/sources/plugin-packs/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php53.tar.gz" "s3://centreon-download/public/$PROJECT/$PROJECT-$VERSION-php53.tar.gz"
 curl "$DL_URL/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION-php53&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
-SRCHASH=`$SSH_REPO "md5sum /srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php54.tar.gz | cut -d ' ' -f 1"`
-$SSH_REPO aws s3 cp --acl public-read "/srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php54.tar.gz" "s3://centreon-download/public/$PROJECT/$PROJECT-$VERSION-php54.tar.gz"
+SRCHASH=`$SSH_REPO "md5sum /srv/sources/plugin-packs/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php54.tar.gz | cut -d ' ' -f 1"`
+$SSH_REPO aws s3 cp --acl public-read "/srv/sources/plugin-packs/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION-php54.tar.gz" "s3://centreon-download/public/$PROJECT/$PROJECT-$VERSION-php54.tar.gz"
 curl "$DL_URL/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION-php54&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
 
 # Move RPMs to the stable repository.
