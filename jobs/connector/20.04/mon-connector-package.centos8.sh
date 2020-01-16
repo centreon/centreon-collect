04cbe0760299b99e6869d5751969e50bd97b416e
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-16 10:51:07 +0100

    feat(jobs): handle CentOS 8 build for Centreon Connector.

---
+++ jobs/connector/20.04/mon-connector-package.centos8.sh
@@ -0,0 +1,18 @@
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+mkdir input
+rm -rf output
+mkdir output
+
+# Retrieve source tarball, spectemplate and additional source files.
+mv "$PROJECT-$VERSION.tar.gz" input/
+cp `dirname $0`/../../../packaging/connector/20.04/centreon-connector.spectemplate input/
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key registry.centreon.com/mon-build-dependencies-20.04:centos8 input output
+
+# Publish RPMs.
+put_internal_rpms "20.04" "el8" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE" output/x86_64/*.rpm
+if [ "$BUILD" '=' 'REFERENCE' ] ; then
+  copy_internal_rpms_to_canary "standard" "20.04" "el8" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE"
+fi
