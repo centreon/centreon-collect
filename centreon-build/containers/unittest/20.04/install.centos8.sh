c521b4d45b24653f9538e19b4f8a14edc0d20b06
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-16 14:00:49 +0100

    fix(containers): remove non-existing package from CentOS 8 install of unit test image.

--- centreon-build/containers/unittest/20.04/install.centos8.sh
+++ centreon-build/containers/unittest/20.04/install.centos8.sh
@@ -11,7 +11,7 @@ curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-8.phar
 chmod +x /usr/local/bin/phpunit
 
 # Install Xdebug PHP extension (for PHPUnit code coverage).
-dnf install make php-devel php-pecl
+dnf install make php-devel
 pecl install debug
 echo 'zend_extension=/usr/lib64/php/modules/xdebug.so' >> /etc/php.d/10-xdebug.ini
 
