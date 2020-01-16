07052b94c9db8a8dc19e08fe675ad056749fe1b0
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-16 13:53:17 +0100

    feat(containers): install Xdebug in mon-unittest-20.04:centos8.

--- containers/unittest/20.04/install.centos8.sh
+++ containers/unittest/20.04/install.centos8.sh
@@ -10,6 +10,11 @@ xargs dnf install < /tmp/build-dependencies.txt
 curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-8.phar
 chmod +x /usr/local/bin/phpunit
 
+# Install Xdebug PHP extension (for PHPUnit code coverage).
+dnf install make php-devel php-pecl
+pecl install debug
+echo 'zend_extension=/usr/lib64/php/modules/xdebug.so' >> /etc/php.d/10-xdebug.ini
+
 # Install Composer.
 dnf install -y php php-cli php-dom php-json php-mbstring
 curl -sS https://getcomposer.org/installer | php
