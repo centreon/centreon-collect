a5203a07539de0637642892e302b2532392bbdf1
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-28 17:50:10 +0100

    fix(jobs): update ui-unittest with more npm commands.

--- centreon-build/jobs/ui/ui-unittest.container.sh
+++ centreon-build/jobs/ui/ui-unittest.container.sh
@@ -16,6 +16,8 @@ cd "/usr/local/src/$PROJECT"
 npm ci
 
 # Run frontend unit tests and code style.
+npm run bundle
+npm run build:storybook
 npm t -- --reporters=jest-junit
 npm run eslint -- -o checkstyle-fe.xml -t checkstyle
 
