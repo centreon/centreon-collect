82b6d273f81dd04378a707837220f620083239f7
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-29 13:58:40 +0100

    fix(jobs): update paths in doc-build.

--- jobs/doc/doc-build.sh
+++ jobs/doc/doc-build.sh
@@ -13,17 +13,17 @@ rm -rf build
 mkdir build
 BUILDIMG="$REGISTRY/node:lts"
 docker pull "$BUILDIMG"
-containerid=`docker create -w /doc/website "$BUILDIMG" sh -c 'yarn install && yarn run build'`
-docker cp "$PROJECT" "$containerid:/doc"
+containerid=`docker create -w /$PROJECT/website "$BUILDIMG" sh -c 'yarn install && yarn run build'`
+docker cp "$PROJECT" "$containerid:/"
 
 # Build documentation in all languages.
 cd "$PROJECT"
 for lang in en fr ; do
   sed -e 's/@ENVIRONMENT@/staging/g' -e "s/@VERSION@/$VERSION/g" -e "s/@LANGUAGE@/$lang/g" < website/siteConfig.js.in > website/siteConfig.js
-  docker cp website/siteConfig.js "$containerid:/doc/website/siteConfig.js"
-  docker cp "$lang/sidebars.json" "$containerid:/doc/website/sidebars.json"
+  docker cp website/siteConfig.js "$containerid:/$PROJECT/website/siteConfig.js"
+  docker cp "$lang/sidebars.json" "$containerid:/$PROJECT/website/sidebars.json"
   docker start -a "$containerid"
-  docker cp "$containerid:/doc/website/build/centreon-documentation" "../build/$lang"
+  docker cp "$containerid:/$PROJECT/website/build/centreon-documentation" "../build/$lang"
 done
 cd ..
 
