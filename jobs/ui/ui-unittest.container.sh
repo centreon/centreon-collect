c71b4e67570c137c71b38c2ceea9da35531fa1c8
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-29 09:51:06 +0100

    fix(jobs): fix checkstyle report file name.

--- jobs/ui/ui-unittest.container.sh
+++ jobs/ui/ui-unittest.container.sh
@@ -19,7 +19,7 @@ npm ci
 npm run bundle
 npm run build:storybook
 npm t -- --reporters=jest-junit
-npm run eslint -- -o checkstyle-fe.xml -f checkstyle
+npm run eslint -- -o checkstyle.xml -f checkstyle
 
 # Move reports to expected places.
 mv junit.xml /tmp/ut.xml
