73c20cee49d5948e4889997cf654767fcd3650d7
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-01-29 14:26:08 +0100

    feat(containers): add MySQL server startup detection routine to its init script.

--- containers/dependencies/20.04/mysql-init.sh
+++ containers/dependencies/20.04/mysql-init.sh
@@ -3,8 +3,23 @@
 pidfile='/var/run/mariadb/mariadb.pid'
 
 service_start() {
+    # Launch database.
     echo "mysqldevscript: starting mysql"
     su - mysql -s /bin/sh -c "/usr/libexec/mysqld --basedir=/usr" &
+
+    # Wait for the database to be up and running.
+    while true ; do
+      timeout 10 mysql -e 'SELECT 1'
+      retval=$?
+      if [ "$retval" = 0 ] ; then
+        break ;
+      else
+        echo 'DB server is not yet responding.'
+        sleep 1
+      fi
+    done
+
+    # Good to go.
     echo "mysqldevscript: mysql started"
 }
 
