804ec275e299d5cf303aef1133c1432a5ba09336
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-02-03 10:55:28 +0100

    fix(containers): install centreon-license-manager in des-bam image.

--- centreon-build/containers/bam/20.04/install.sh
+++ centreon-build/containers/bam/20.04/install.sh
@@ -4,5 +4,6 @@ set -e
 set -x
 
 service mysql start
+/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-license-manager
 /tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-bam-server
 service mysql stop
