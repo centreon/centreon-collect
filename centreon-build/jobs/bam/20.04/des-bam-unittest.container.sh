dcf7fe534ac537dcce974888979b6149aef7a240
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-02-03 09:37:13 +0100

    fix(jobs): use ESLint for Centreon BAM.

--- centreon-build/jobs/bam/20.04/des-bam-unittest.container.sh
+++ centreon-build/jobs/bam/20.04/des-bam-unittest.container.sh
@@ -29,7 +29,7 @@ cd ../../../..
 # Run frontend unit tests and code style.
 cd "$FEDIR"
 npm t -- --reporters=jest-junit
-npm run tslint -- -o checkstyle-fe.xml -t checkstyle
+npm run eslint -- -o checkstyle-fe.xml -f checkstyle
 cd ../../../..
 
 # Move reports to expected places.
