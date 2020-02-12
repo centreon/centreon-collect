b069cfcef363be5ff39321375540b1def03437f0
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-02-12 14:29:57 +0100

    fix(jobs): use common.sh in centreon-widget-3.4-stable.

--- jobs/widgets/centreon-widget-3.4-stable.sh
+++ jobs/widgets/centreon-widget-3.4-stable.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+. `dirname $0`/../common.sh
+
 # Check arguments.
 if [ -z "$WIDGET" -o -z "$VERSION" -o -z "$RELEASE" ] ; then
   echo "You need to specify WIDGET, VERSION and RELEASE environment variables."
