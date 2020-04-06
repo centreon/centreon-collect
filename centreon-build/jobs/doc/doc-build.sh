88a15a1d96a3d9733ac240aa3d868720124a7b47
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-06 14:33:44 +0200

    feat(jobs): update doc-build to use native Docusaurus translation.

--- centreon-build/jobs/doc/doc-build.sh
+++ centreon-build/jobs/doc/doc-build.sh
@@ -8,28 +8,35 @@ set -x
 # Project.
 PROJECT=centreon-documentation
 
-# Prepare for documentation build.
-rm -rf build
-mkdir -p build/vanilla build/testing build/unstable
+# Reorganize directory.
+rm -rf tmpsrc
+mkdir -p "tmpsrc/$PROJECT"
+cd "$PROJECT"
+git archive HEAD | tar -C "../tmpsrc/$PROJECT" -x
+cd "../tmpsrc/$PROJECT"
+sed 's/@LANGUAGE@/en/g' < website/siteConfig.js.in > website/siteConfig.js
+cp en/sidebars.json website/sidebars.json
+mkdir website/translated_docs
+mv fr website/translated_docs
+cd ../..
+
+# Create container.
 BUILDIMG="$REGISTRY/node:lts"
 docker pull "$BUILDIMG"
 containerid=`docker create -w /$PROJECT/website "$BUILDIMG" sh -c 'yarn install && yarn run build'`
-docker cp "$PROJECT" "$containerid:/"
+docker cp "tmpsrc/$PROJECT/." "$containerid:/$PROJECT"
+
+# Prepare for documentation build.
+rm -rf build
+mkdir build
 
 # Build documentation in all languages.
-cd "$PROJECT"
-for lang in en fr ; do
-  sed -e "s/@LANGUAGE@/$lang/g" < website/siteConfig.js.in > website/siteConfig.js
-  docker cp website/siteConfig.js "$containerid:/$PROJECT/website/siteConfig.js"
-  docker cp "$lang/sidebars.json" "$containerid:/$PROJECT/website/sidebars.json"
-  docker start -a "$containerid"
-  docker cp "$containerid:/$PROJECT/website/build/centreon-documentation" "../build/vanilla/$lang"
-  cp -r "../build/vanilla/$lang" "../build/testing/$lang"
-  find "../build/testing/$lang" -type f | xargs sed -i -e "s#@BASEURL@#$VERSION/$lang#g"
-  cp -r "../build/vanilla/$lang" "../build/unstable/$lang"
-  find "../build/unstable/$lang" -type f | xargs sed -i -e "s#@BASEURL@#job/centreon-documentation/job/$BRANCH_NAME/Centreon_20${lang}_20documentation_20preview#g"
-done
-cd ..
+docker start -a "$containerid"
+docker cp "$containerid:/$PROJECT/website/build/centreon-documentation" build/vanilla
+cp -r build/vanilla build/testing
+find build/testing -type f | xargs sed -i -e "s#@BASEURL@#$VERSION#g"
+cp -r build/vanilla build/unstable
+find build/unstable -type f | xargs sed -i -e "s#@BASEURL@#job/centreon-documentation/job/$BRANCH_NAME/Centreon_20documentation_20preview#g"
 
 # Upload documentation.
 put_internal_source "doc" "$PROJECT-$VERSION-$RELEASE" "build"
