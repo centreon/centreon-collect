f99a4b730b89cf4e120bbbc573cb0a8167a41f2e
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-14 11:08:20 +0200

    enh(build): use language-specific pages to build documentation.

--- centreon-build/jobs/doc/doc-build.sh
+++ centreon-build/jobs/doc/doc-build.sh
@@ -13,30 +13,44 @@ rm -rf build
 mkdir -p build/vanilla build/testing build/unstable
 BUILDIMG="$REGISTRY/node:lts"
 docker pull "$BUILDIMG"
-containerid=`docker create -w /$PROJECT/website "$BUILDIMG" sh -c 'npm ci && npm run build'`
-docker cp "$PROJECT" "$containerid:/"
 
 # Build documentation in all languages.
-cd "$PROJECT"
 for lang in en fr ; do
+  # Extract sources in lang-specific directory.
+  rm -rf "$lang"
+  mkdir "$lang"
+  cd "$PROJECT"
+  git archive HEAD | tar -C "../$lang" -x
+  cd ..
+
+  # Update sources for target language.
+  cd "$lang"
   sed -e "s/@LANGUAGE@/$lang/g" < website/siteConfig.js.in > website/siteConfig.js
-  docker cp website/siteConfig.js "$containerid:/$PROJECT/website/siteConfig.js"
-  docker cp "$lang/sidebars.json" "$containerid:/$PROJECT/website/sidebars.json"
+  mv "$lang/sidebars.json" "website/sidebars.json"
+  mv "website/pages/$lang" "pages-$lang"
+  rm -rf website/pages
+  mkdir website/pages
+  mv "pages-$lang" website/pages/en
+  mv "website/core/$lang" "core-$lang"
+  rm -rf website/core
+  mv "core-$lang" website/core
+  cd ..
+
+  # Build documentation.
+  containerid=`docker create -w /$PROJECT/website "$BUILDIMG" sh -c 'npm ci && npm run build'`
+  docker cp "$lang/." "$containerid:/$PROJECT"
   docker start -a "$containerid"
-  docker cp "$containerid:/$PROJECT/website/build/centreon-documentation" "../build/vanilla/$lang"
-  cp -r "../build/vanilla/$lang" "../build/testing/$lang"
-  find "../build/testing/$lang" -type f | xargs -d '\n' sed -i -e "s#@BASEURL@#$VERSION/$lang#g"
-  cp -r "../build/vanilla/$lang" "../build/unstable/$lang"
-  find "../build/unstable/$lang" -type f | xargs -d '\n' sed -i -e "s#@BASEURL@#job/centreon-documentation/job/$BRANCH_NAME/Centreon_20documentation_20preview/${lang}#g"
+  docker cp "$containerid:/$PROJECT/website/build/centreon-documentation" "build/vanilla/$lang"
+  cp -r "build/vanilla/$lang" "build/testing/$lang"
+  find "build/testing/$lang" -type f | xargs -d '\n' sed -i -e "s#@BASEURL@#$VERSION/$lang#g"
+  cp -r "build/vanilla/$lang" "build/unstable/$lang"
+  find "build/unstable/$lang" -type f | xargs -d '\n' sed -i -e "s#@BASEURL@#job/centreon-documentation/job/$BRANCH_NAME/Centreon_20documentation_20preview/${lang}#g"
+  docker stop "$containerid"
+  docker rm "$containerid"
 done
-cd ..
 
 # Upload documentation.
 cp `dirname $0`/redirect.html build/vanilla/index.html
 cp `dirname $0`/redirect.html build/unstable/index.html
 cp `dirname $0`/redirect.html build/testing/index.html
 put_internal_source "doc" "$PROJECT-$VERSION-$RELEASE" "build"
-
-# Cleanup.
-docker stop "$containerid"
-docker rm "$containerid"
