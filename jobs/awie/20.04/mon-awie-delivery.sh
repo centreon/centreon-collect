9a34cc3af2f41922eb3297a5817a1b2d7e8d2296
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-16 08:47:44 +0200

    fix(jobs): remove documentation generation from mon-awie-delivery.

--- jobs/awie/20.04/mon-awie-delivery.sh
+++ jobs/awie/20.04/mon-awie-delivery.sh
@@ -22,15 +22,6 @@ if [ "$BUILD" '=' 'RELEASE' ] ; then
   copy_internal_source_to_testing "standard" "awie" "$PROJECT-$VERSION-$RELEASE"
   copy_internal_rpms_to_testing "standard" "20.04" "el7" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
 
-  # Generate documentation.
-  SSH_DOC="ssh -o StrictHostKeyChecking=no root@doc-dev.int.centreon.com"
-  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos centreon-awie -V latest -p'"
-  $SSH_DOC bash -c "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos centreon-awie -V latest -p'"
-
-  # Create entry in download-dev.
-  SRCHASH=00112233445566778899aabbccddeeff
-  curl "$DLDEV_URL/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&version=$VERSION&extension=tar.gz&md5=$SRCHASH&ddos=0&dryrun=0"
-
   # Docker image target version.
   TARGETVERSION="$VERSION"
 
