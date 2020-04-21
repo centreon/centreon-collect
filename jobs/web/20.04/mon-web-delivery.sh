231a741e6c6048fd6d725d0fb40c5f3d4274edab
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-21 18:16:32 +0200

    fix(jobs): upload API documentation to new bucket (centreon-documentation-prod).

--- jobs/web/20.04/mon-web-delivery.sh
+++ jobs/web/20.04/mon-web-delivery.sh
@@ -26,8 +26,8 @@ else
   promote_canary_rpms_to_unstable "standard" "20.04" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
   SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
   put_internal_source "web" "$PROJECT-$VERSION-$RELEASE" centreon-api-v2.html
-  $SSH_REPO aws s3 cp --acl public-read "/srv/sources/internal/web/$PROJECT-$VERSION-$RELEASE/centreon-api-v2.html" s3://centreon-documentation/centreon-web/centreon-api-v2.html
-  $SSH_REPO aws cloudfront create-invalidation --distribution-id E1W5L9V83QVVLX --paths /centreon-web/centreon-api-v2.html
+  $SSH_REPO aws s3 cp --acl public-read "/srv/sources/internal/web/$PROJECT-$VERSION-$RELEASE/centreon-api-v2.html" s3://centreon-documentation-prod/api/centreon-web/index.html
+  $SSH_REPO aws cloudfront create-invalidation --distribution-id E3KVGH6VYVX7DP --paths /api/centreon-web/index.html
   TARGETVERSION='20.04'
 fi
 
