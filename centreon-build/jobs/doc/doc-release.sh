929bcd5add8a0f6dc6374ad335faa9d477026833
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-21 09:06:21 +0200

    fix(jobs): remove invalid invalidations from doc-release.

--- centreon-build/jobs/doc/doc-release.sh
+++ centreon-build/jobs/doc/doc-release.sh
@@ -11,5 +11,3 @@ TARGETDIR="s3://centreon-documentation/$VERSION"
 ssh $REPO_CREDS aws s3 sync --acl public-read --delete "/srv/sources/internal/doc/$PROJECT-$VERSION-$RELEASE/build/version" "$TARGETDIR"
 TARGETDIR="s3://centreon-documentation/current"
 ssh $REPO_CREDS aws s3 sync --acl public-read --delete "/srv/sources/internal/doc/$PROJECT-$VERSION-$RELEASE/build/current" "$TARGETDIR"
-ssh $REPO_CREDS aws cloudfront create-invalidation --distribution-id E1DRHP505UQYSQ --paths "/$VERSION/"'*'
-ssh $REPO_CREDS aws cloudfront create-invalidation --distribution-id E1DRHP505UQYSQ --paths '/current/*'
