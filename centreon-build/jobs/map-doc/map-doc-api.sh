9ea4cf423cbd1ad920c0336ca13473037197329c
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-21 18:16:32 +0200

    fix(jobs): upload API documentation to new bucket (centreon-documentation-prod).

--- centreon-build/jobs/map-doc/map-doc-api.sh
+++ centreon-build/jobs/map-doc/map-doc-api.sh
@@ -13,5 +13,5 @@ npm run redoc-bundle
 # Publish documentation to S3.
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
 put_internal_source "map-doc" "temp" docs/index.html
-$SSH_REPO aws s3 cp --acl public-read "/srv/sources/internal/map-doc/temp/index.html" s3://centreon-documentation/centreon-map/index.html
-$SSH_REPO aws cloudfront create-invalidation --distribution-id E1W5L9V83QVVLX --paths /centreon-map/index.html
+$SSH_REPO aws s3 cp --acl public-read "/srv/sources/internal/map-doc/temp/index.html" s3://centreon-documentation-prod/api/centreon-map/index.html
+$SSH_REPO aws cloudfront create-invalidation --distribution-id E3KVGH6VYVX7DP --paths /api/centreon-map/index.html
