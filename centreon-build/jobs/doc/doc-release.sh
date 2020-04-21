25f26972692a054628bad7007c2813c3956df761
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-21 09:22:23 +0200

    enh(jobs): update invalidations in doc-staging and doc-release.

--- centreon-build/jobs/doc/doc-release.sh
+++ centreon-build/jobs/doc/doc-release.sh
@@ -9,5 +9,7 @@ set -x
 PROJECT=centreon-documentation
 TARGETDIR="s3://centreon-documentation/$VERSION"
 ssh $REPO_CREDS aws s3 sync --acl public-read --delete "/srv/sources/internal/doc/$PROJECT-$VERSION-$RELEASE/build/version" "$TARGETDIR"
+ssh $REPO_CREDS aws cloudfront create-invalidation --distribution-id E3KVGH6VYVX7DP --paths "/$VERSION/"'*'
 TARGETDIR="s3://centreon-documentation/current"
 ssh $REPO_CREDS aws s3 sync --acl public-read --delete "/srv/sources/internal/doc/$PROJECT-$VERSION-$RELEASE/build/current" "$TARGETDIR"
+ssh $REPO_CREDS aws cloudfront create-invalidation --distribution-id E3KVGH6VYVX7DP --paths '/current/*'
