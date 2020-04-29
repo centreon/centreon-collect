2272bd26be325ed643edeaa46233c850f92fe904
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-29 17:24:47 +0200

    feat(jobs): analyze assets use in doc-source job.

--- jobs/doc/doc-source.sh
+++ jobs/doc/doc-source.sh
@@ -16,6 +16,15 @@ export RELEASE="$now.$COMMIT"
 
 # Get committer.
 COMMITTER=`git show --format='%cN <%cE>' HEAD | head -n 1`
+
+# Analyze assets.
+for lang in en fr ; do
+  cd "$lang"
+  find assets -type f | sort -u > "../../existing_assets_$lang.txt"
+  find . -name '*.md' | xargs -d '\n' cat | grep -o -e 'assets/[0-9a-zA-Z./_\\-]*' | sort -u > "../../used_assets_$lang.txt"
+  diff "../../existing_assets_$lang.txt" "../../used_assets_$lang.txt" > "../../assets_diff_$lang.txt"
+  cd ..
+done
 cd ..
 
 # Generate properties files for downstream jobs.
