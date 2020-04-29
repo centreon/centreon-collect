1fc1ae59e02440397e3bd36395437c80e9b291f0
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-29 13:09:08 +0200

    feat(jobs): support CentOS 8 in mon-autodisco-* jobs.

--- centreon-build/jobs/autodisco/20.04/autodisco-stable.sh
+++ centreon-build/jobs/autodisco/20.04/autodisco-stable.sh
@@ -25,3 +25,4 @@ curl "$DL_URL/api/?token=ML2OA4P43FDF456FG3EREYUIBAHT521&product=$PROJECT&versio
 
 # Move RPMs to the stable repository.
 promote_testing_rpms_to_stable "standard" "20.04" "el7" "noarch" "autodisco" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "20.04" "el8" "noarch" "autodisco" "$PROJECT-$VERSION-$RELEASE"
