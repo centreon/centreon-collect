3ea74c59f4a9a99e6aa26775e437ec6a88fc9be5
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-02 14:47:02 +0200

    fix(containers): change the user running install-centreon-module (should be apache).

--- containers/ppm/20.10/install-autodisco.sh
+++ containers/ppm/20.10/install-autodisco.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 service mysql start
-/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-autodiscovery-server
+su -c '/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-autodiscovery-server' -s /bin/bash apache
 service mysql stop
