0719938deaaae9692effbb25ab39edcbe0d8db12
jeanbaptiste rouze <jbrouze@centreon.com>
2020-06-08 15:49:47 +0200

    wip

--- src/cbwd/main.cc
+++ src/cbwd/main.cc
@@ -20,19 +20,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
 #include <csignal>
 #include <cstring>
 #include <iostream>
 #include <set>
 #include <unordered_map>
+
 #include "com/centreon/broker/exceptions/msg.hh"
 #include "com/centreon/broker/watchdog/configuration.hh"
 #include "com/centreon/broker/watchdog/configuration_parser.hh"
 #include "com/centreon/broker/watchdog/instance.hh"
-
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 
 using namespace com::centreon::broker;
 using namespace com::centreon::broker::watchdog;
@@ -58,18 +59,17 @@ static void print_help() {
  * @param cfg The new configuration to set.
  */
 static void apply_new_configuration(configuration const& cfg) {
-    
-    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    console_sink->set_level(spdlog::level::warn);
-    console_sink->set_pattern("[cbwd] [%^%l%$] %v");
-  
-    if (config.get_log_filename() != cfg.get_log_filename()) {
-      auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("cfg.get_log_filename()", true);
-      file_sink->set_level(spdlog::level::trace);
-      logger.reset(new spdlog::logger("cbwd", {console_sink, file_sink}));
-    }
-    else
-      logger.reset(new spdlog::logger("cbwd", {console_sink}));
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  console_sink->set_level(spdlog::level::warn);
+  console_sink->set_pattern("[cbwd] [%^%l%$] %v");
+
+  if (config.get_log_filename() != cfg.get_log_filename()) {
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+        cfg.get_log_filename(), true);
+    file_sink->set_level(spdlog::level::trace);
+    logger.reset(new spdlog::logger("cbwd", {console_sink, file_sink}));
+  } else
+    logger.reset(new spdlog::logger("cbwd", {console_sink}));
 
   std::set<std::string> to_update;
   std::set<std::string> to_delete;
@@ -196,7 +196,8 @@ int main(int argc, char** argv) {
     configuration_parser parser;
     config = parser.parse(config_filename);
   } catch (std::exception const& e) {
-    logger->error("watchdog: Could not parse the configuration file '{}': {}",config_filename, e.what());
+    logger->error("watchdog: Could not parse the configuration file '{}': {}",
+                  config_filename, e.what());
     return 2;
   }
 
@@ -229,7 +230,9 @@ int main(int argc, char** argv) {
          */
         freq++;
         if (freq / instances.size() > 5) {
-          logger->error("watchdog: cbd seems to stop too many times, you must look at its configuration. The watchdog loop is slow down");
+          logger->error(
+              "watchdog: cbd seems to stop too many times, you must look at "
+              "its configuration. The watchdog loop is slow down");
         } else
           timeout = 0;
       } else
@@ -243,7 +246,8 @@ int main(int argc, char** argv) {
           config = parser.parse(config_filename);
           apply_new_configuration(config);
         } catch (std::exception const& e) {
-          logger->error("watchdog: Could not parse the new configuration: {}", e.what());
+          logger->error("watchdog: Could not parse the new configuration: {}",
+                        e.what());
         }
         sighup = false;
         continue;
