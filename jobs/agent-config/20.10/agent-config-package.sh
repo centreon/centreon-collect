c2388505e9e3923ca2dbff2bf717639758563dd1
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-09 13:44:35 +0200

    feat(jobs): support CentOS 8 in agent-config-package.

--- jobs/agent-config/20.10/agent-config-package.sh
+++ jobs/agent-config/20.10/agent-config-package.sh
@@ -51,12 +51,14 @@ export RELEASE="$OLDRELEASE"
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos7' ] ; then
-  DISTRIB='el7'
+  DISTRIB=el7
+elif [ "$DISTRIB" = 'centos8' ] ; then
+  DISTRIB=el8
 else
   echo "Unsupported distribution $DISTRIB."
   exit 1
 fi
 put_internal_rpms "20.10" "$DISTRIB" "noarch" "agent-config" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
 if [ "$BUILD" '=' 'REFERENCE' ] ; then
-  copy_internal_rpms_to_canary "standard" "20.10" "el7" "noarch" "agent-config" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_canary "standard" "20.10" "$DISTRIB" "noarch" "agent-config" "$PROJECT-$VERSION-$RELEASE"
 fi
