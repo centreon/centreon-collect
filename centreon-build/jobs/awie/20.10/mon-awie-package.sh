c2a6bf60564085b8050c1c558df8514ff43df9e5
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-09 14:01:52 +0200

    feat(jobs): support CentOS 8 in all Centreon AWIE jobs.

--- centreon-build/jobs/awie/20.10/mon-awie-package.sh
+++ centreon-build/jobs/awie/20.10/mon-awie-package.sh
@@ -14,7 +14,7 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <centos7|...>"
+  echo "USAGE: $0 <centos7|centos8|...>"
   exit 1
 fi
 DISTRIB="$1"
@@ -55,12 +55,14 @@ export RELEASE="$OLDRELEASE"
 
 # Copy files to server.
 if [ "$DISTRIB" = 'centos7' ] ; then
-  DISTRIB='el7'
+  DISTRIB=el7
+elif [ "$DISTRIB" = 'centos8' ] ; then
+  DISTRIB=el8
 else
   echo "Unsupported distribution $DISTRIB."
   exit 1
 fi
 put_internal_rpms "20.10" "$DISTRIB" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE" output/noarch/*.rpm
 if [ "$BUILD" '=' 'REFERENCE' ] ; then
-  copy_internal_rpms_to_canary "standard" "20.10" "el7" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
+  copy_internal_rpms_to_canary "standard" "20.10" "$DISTRIB" "noarch" "awie" "$PROJECT-$VERSION-$RELEASE"
 fi
