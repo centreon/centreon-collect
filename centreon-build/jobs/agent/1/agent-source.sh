df536fc36b17582eebd6db14a6e3adf43f34a645
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-11 14:06:13 +0200

    feat(jobs): create directory for Centreon Agent version 1.

---
+++ centreon-build/jobs/agent/1/agent-source.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=centreon-agent
+
+# Get version.
+cd "$PROJECT"
+VERSION=`grep VERSION Makefile  | head -n 1 | cut -d '=' -f 2`
+export VERSION="$VERSION"
+
+# Get release.
+COMMIT=`git log -1 HEAD --pretty=format:%h`
+now=`date +%s`
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  export RELEASE="$BUILD_NUMBER"
+else
+  export RELEASE="$now.$COMMIT"
+fi
+
+# Get committer.
+COMMITTER=`git show --format='%cN <%cE>' HEAD | head -n 1`
+
+# Create source tarball.
+git archive --prefix="$PROJECT-$VERSION/" HEAD | gzip > "../$PROJECT-$VERSION.tar.gz"
+cd ..
+
+# Generate properties files for downstream jobs.
+cat > source.properties << EOF
+PROJECT=$PROJECT
+VERSION=$VERSION
+RELEASE=$RELEASE
+COMMIT=$COMMIT
+COMMITTER=$COMMITTER
+EOF
+
+# Generate summary report.
+rm -rf summary
+cp -r `dirname $0`/../../common/build-artifacts summary
+cp `dirname $0`/jobData.json summary/
+generate_summary
