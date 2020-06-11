ceb432deda6e85c5549a742dbe3e75215f8b1891
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-11 12:27:24 +0200

    feat(jobs): WIP on new job to generate old documentation.

--- centreon-build/jobs/doc/rest.sh
+++ centreon-build/jobs/doc/rest.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
 # Check arguments.
-if [ -z "$PROJECT" -o -z "$TAG" ] ; then
-  echo "You need to specify PROJECT and TAG environment variables."
+if [ -z "$PROJECT" -o -z "$COMMIT" ] ; then
+  echo "You need to specify PROJECT and COMMIT environment variables."
   exit 1
 fi
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
-SSH_DOC="$SSH_REPO ssh -o StrictHostKeyChecking=no ubuntu@10.24.1.54"
 
-# English doc.
-if [ "$ENGLISH" '=' true ] ; then
-  $SSH_DOC "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage.py update_repos $PROJECT -V $TAG -p'"
-fi
+# Pull build image.
+BUILD_IMAGE=registry.centreon.com/sphinx
+docker pull "$BUILD_IMAGE"
 
-# French doc.
-if [ "$FRENCH" '=' true ] ; then
-  $SSH_DOC "'source /srv/env/documentation/bin/activate ; /srv/prod/readthedocs.org/readthedocs/manage_fr.py update_repos $PROJECT -V $TAG -p'"
-fi
+# Checkout sources.
+rm -rf "$PROJECT"
+git clone "ssh://git@github.com/centreon/$PROJECT"
+cd "$PROJECT"
+git checkout "$COMMIT"
+cd doc
+
+# Languages
+for lang in en fr ; do
+  if [ -f "$lang/conf.py" ] ; then
+    containerid=`docker create $BUILD_IMAGE`
+    docker cp "$lang/." "$containerid:/docs"
+    docker start -a "$containerid"
+    docker stop "$containerid"
+    docker rm "$containerid"
+  fi
+done
