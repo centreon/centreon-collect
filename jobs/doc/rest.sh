083c7aaed4815802d9400a381fb761978d80aa85
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-12 09:31:02 +0200

    feat(jobs): improve reST documentation generation.

--- jobs/doc/rest.sh
+++ jobs/doc/rest.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
 # Check arguments.
-if [ -z "$PROJECT" -o -z "$COMMIT" ] ; then
-  echo "You need to specify PROJECT and COMMIT environment variables."
+if [ -z "$PROJECT" -o -z "$COMMIT" -o -z "$DOCTAG" ] ; then
+  echo "You need to specify PROJECT, COMMIT and DOCTAG environment variables."
   exit 1
 fi
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
@@ -21,10 +26,40 @@ cd doc
 # Languages
 for lang in en fr ; do
   if [ -f "$lang/conf.py" ] ; then
+    # Prepare for build.
+    mkdir "$lang/templates"
+    touch "$lang/templates/searchbox.html"
+    cat >> "$lang/conf.py" << EOF
+templates_path = ["templates"]
+html_context = {
+        'READTHEDOCS': True,
+        'current_version': version,
+        'versions': [
+                ['latest', '/docs/$PROJECT/en/latest/'],
+                ['19.10', '/docs/$PROJECT/en/19.10/'],
+                ['19.04', '/docs/$PROJECT/en/19.04/'],
+                ['18.10', '/docs/$PROJECT/en/18.10/'],
+                ['2.8', '/docs/$PROJECT/en/2.8/']
+        ],
+        'display_github': True,
+        'github_user': 'centreon',
+        'github_repo': '$PROJECT',
+        'github_version': '$COMMIT/',
+        'conf_py_path': 'doc/$lang/'
+}
+EOF
+
+    # Build documentation.
     containerid=`docker create $BUILD_IMAGE`
     docker cp "$lang/." "$containerid:/docs"
     docker start -a "$containerid"
+    rm -rf "html-$lang"
+    docker cp "$containerid:/docs/_build/html" "html-$lang"
     docker stop "$containerid"
     docker rm "$containerid"
+
+    # Upload documentation.
+    put_internal_source "docrest" "docrest-$BUILD_NUMBER" "html-$lang"
+    $SSH_REPO aws s3 sync --acl public-read "/srv/internal/docrest/docrest-$BUILD_NUMBER/html-$lang" "s3://centreon-documentation-preprod/docs/$PROJECT/$lang/$DOCTAG"
   fi
 done
