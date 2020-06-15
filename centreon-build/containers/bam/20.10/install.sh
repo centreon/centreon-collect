2beca8bf4a31ac960adee2948061de0341bb3280
Kevin Duret <kduret@centreon.com>
2020-06-15 14:54:07 +0200

    fix(containers): run module install with apache user

--- centreon-build/containers/bam/20.10/install.sh
+++ centreon-build/containers/bam/20.10/install.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 service mysql start
-/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-license-manager
-/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-bam-server
+su -s /bin/bash -c "/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-license-manager" apache
+su -s /bin/bash -c "/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-bam-server" apache
 service mysql stop
