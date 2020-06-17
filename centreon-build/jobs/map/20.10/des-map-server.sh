f8119baa41a3c104b073b92bd5d1cf1bfd902a1e
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-17 09:43:02 +0200

    feat(jobs): handle CentOS 8 in Centreon Map jobs.

--- centreon-build/jobs/map/20.10/des-map-server.sh
+++ centreon-build/jobs/map/20.10/des-map-server.sh
@@ -13,13 +13,24 @@ if [ -z "$VERSION" -o -z "$VERSIONSERVER" -o -z "$RELEASE" ] ; then
   echo "You need to specify VERSION, VERSIONSERVER and RELEASE environment variables."
   exit 1
 fi
+if [ "$#" -lt 1 ] ; then
+  echo "USAGE: $0 <centos7|centos8|...>"
+  exit 1
+fi
+DISTRIB="$1"
 
 # Fetch sources.
-rm -rf "$PROJECT-server-$VERSION.tar.gz" "$PROJECT-server-$VERSION"
+rm -rf "$PROJECT-server-$VERSIONSERVER.tar.gz" "$PROJECT-server-$VERSIONSERVER"
 get_internal_source "map/$PROJECT-server-$VERSIONSERVER-$RELEASE/$PROJECT-server-$VERSIONSERVER.tar.gz"
+tar xzf "$PROJECT-server-$VERSIONSERVER.tar.gz"
+if [ "$DISTRIB" = 'centos8' ] ; then
+  sed -i 's@el7</project.release>@el8</project.release>@g' "$PROJECT-server-$VERSIONSERVER/map-server-parent/map-server-packaging/pom.xml"
+  rm -rf "$PROJECT-server-$VERSIONSERVER.tar.gz"
+  tar czf "$PROJECT-server-$VERSIONSERVER.tar.gz" "$PROJECT-server-$VERSIONSERVER"
+fi
 
 # Create and populate container.
-BUILD_IMAGE="registry.centreon.com/map-build-dependencies-20.10:centos7"
+BUILD_IMAGE="registry.centreon.com/map-build-dependencies-20.10:$DISTRIB"
 docker pull "$BUILD_IMAGE"
 containerid=`docker create -e "VERSION=$VERSIONSERVER" $BUILD_IMAGE /usr/local/bin/server.sh`
 docker cp `dirname $0`/des-map-server.container.sh "$containerid:/usr/local/bin/server.sh"
@@ -37,11 +48,19 @@ docker stop "$containerid"
 docker rm "$containerid"
 
 # Upload artifacts.
+if [ "$DISTRIB" = 'centos7' ] ; then
+  DISTRIB=el7
+elif [ "$DISTRIB" = 'centos8' ] ; then
+  DISTRIB=el8
+else
+  echo "Unsupported distribution $DISTRIB."
+  exit 1
+fi
 FILES_MAP_SERVER="$PROJECT-server-$VERSIONSERVER/map-server-parent/map-server-packaging/target/rpm/centreon-map-server/RPMS/noarch/"'*.rpm'
-put_internal_rpms "20.10" "el7" "noarch" "map-server" "$PROJECT-server-$VERSIONSERVER-$RELEASE" $FILES_MAP_SERVER
+put_internal_rpms "20.10" "$DISTRIB" "noarch" "map-server" "$PROJECT-server-$VERSIONSERVER-$RELEASE" $FILES_MAP_SERVER
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
-$SSH_REPO rpm --resign "/srv/yum/internal/20.10/el7/noarch/map-server/$PROJECT-server-$VERSIONSERVER-$RELEASE/*.rpm"
-$SSH_REPO createrepo "/srv/yum/internal/20.10/el7/noarch/map-server/$PROJECT-server-$VERSIONSERVER-$RELEASE"
+$SSH_REPO rpm --resign "/srv/yum/internal/20.10/$DISTRIB/noarch/map-server/$PROJECT-server-$VERSIONSERVER-$RELEASE/*.rpm"
+$SSH_REPO createrepo "/srv/yum/internal/20.10/$DISTRIB/noarch/map-server/$PROJECT-server-$VERSIONSERVER-$RELEASE"
 if [ "$BUILD" '=' 'REFERENCE' ] ; then
-  copy_internal_rpms_to_canary "map" "20.10" "el7" "noarch" "map-server" "$PROJECT-server-$VERSIONSERVER-$RELEASE"
+  copy_internal_rpms_to_canary "map" "20.10" "$DISTRIB" "noarch" "map-server" "$PROJECT-server-$VERSIONSERVER-$RELEASE"
 fi
--- centreon-build/jobs/map/20.10/map-server-stable.sh
+++ centreon-build/jobs/map/20.10/map-server-stable.sh
@@ -10,3 +10,4 @@ PROJECT=centreon-map
 
 # Move RPMs to the stable repository.
 promote_testing_rpms_to_stable "map" "20.10" "el7" "noarch" "map-server" "$PROJECT-server-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "map" "20.10" "el8" "noarch" "map-server" "$PROJECT-server-$VERSION-$RELEASE"
