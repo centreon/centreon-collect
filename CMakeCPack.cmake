0a90cc1989fdf33a80f25d50b212d86c90f258a4
David Boucher <dboucher@centreon.com>
2020-06-21 00:19:48 +0200

    enh(cpack): rpm and dpkg detected automatically
    If rpmbuild is detected, rpm packages will be packaged.
    If dpkg is detected, deb packages will be packaged.

--- CMakeCPack.cmake
+++ CMakeCPack.cmake
@@ -1,4 +1,5 @@
 find_program(RPMBUILD rpmbuild)
+find_program(DEBBUILD dpkg-deb)
 
 set(CPACK_RPM_PACKAGE_DEBUG On)
 set(CPACK_PACKAGE_CONTACT "David Boucher <dboucher@centreon.com>")
@@ -10,78 +11,106 @@ set(CPACK_RPM_PACKAGE_SUMMARY "The Centreon collect softwares mainly composed of
 set(CPACK_RPM_PACKAGE_DESCRIPTION "Centreon Broker is a Centreon Engine/Nagios module that reports events in one or multiple databases.")
 set(CPACK_BINARY_RPM On)
 
-set(CPACK_COMPONENTS_ALL broker cbd core graphite influxdb clib connector-perl connector-ssh engine-daemon engine-extcommands) # storage cbmod devel)
+set(CPACK_COMPONENTS_ALL broker cbd core storage graphite influxdb clib connector-perl connector-ssh engine-daemon engine-extcommands) # cbmod)
 set(CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
 set(CPACK_PACKAGE_VERSION ${COLLECT_VERSION})
 set(CPACK_PACKAGE_RELEASE 1)
 set(CPACK_PACKAGE_NAME "centreon")
+set(PACK_VERSION "${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}")
 
 if (RPMBUILD)
-  set(CPACK_GENERATOR RPM)
+  set(CPACK_GENERATOR ${CPACK_GENERATOR} RPM)
   set(CPACK_RPM_COMPONENT_INSTALL ON)
-else ()
-  set(CPACK_GENERATOR DEB)
+endif ()
+if (DEBBUILD)
+  set(CPACK_GENERATOR ${CPACK_GENERATOR} DEB)
   set(CPACK_DEB_COMPONENT_INSTALL ON)
+  execute_process(COMMAND dpkg --print-architecture
+                  OUTPUT_VARIABLE ARCH
+                  OUTPUT_STRIP_TRAILING_WHITESPACE)
 endif ()
 
 #centreon-broker
-set(CPACK_PACKAGE_BROKER_PACKAGE_NAME "centreon-broker-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}")
+set(CPACK_PACKAGE_BROKER_PACKAGE_NAME "centreon-broker-${PACK_VERSION}")
 set(CPACK_RPM_BROKER_FILE_NAME "${CPACK_PACKAGE_BROKER_PACKAGE_NAME}.rpm")
-set(CPACK_DEBIAN_BROKER_FILE_NAME "${CPACK_PACKAGE_BROKER_PACKAGE_NAME}.deb")
+set(CPACK_DEBIAN_BROKER_FILE_NAME "centreon-broker_${PACK_VERSION}_${ARCH}.deb")
 set(CPACK_RPM_BROKER_DEFAULT_USER "centreon-broker")
 set(CPACK_RPM_BROKER_DEFAULT_GROUP "centreon-broker")
 
 #centreon-broker-cbd
-set(CPACK_PACKAGE_CBD_PACKAGE_NAME "centreon-broker-cbd-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}")
-set(CPACK_DEBIAN_CBD_FILE_NAME "${CPACK_PACKAGE_CBD_PACKAGE_NAME}.deb")
+set(CPACK_PACKAGE_CBD_PACKAGE_NAME "centreon-broker-cbd-${PACK_VERSION}")
+set(CPACK_DEBIAN_CBD_FILE_NAME "centreon-broker-cbd_${PACK_VERSION}_${ARCH}.deb")
 set(CPACK_RPM_CBD_FILE_NAME "${CPACK_PACKAGE_CBD_PACKAGE_NAME}.rpm")
 
 #centreon-broker-core
-set(CPACK_PACKAGE_CORE_PACKAGE_NAME "centreon-broker-core-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}")
-set(CPACK_DEBIAN_CORE_FILE_NAME "${CPACK_PACKAGE_CORE_PACKAGE_NAME}.deb")
+set(CPACK_PACKAGE_CORE_PACKAGE_NAME "centreon-broker-core-${PACK_VERSION}")
+set(CPACK_DEBIAN_CORE_FILE_NAME "centreon-broker-core_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_CORE_PACKAGE_DEPENDS "centreon-broker (= ${PACK_VERSION}), libgnutls-openssl27 (>= 2.0)")
 set(CPACK_RPM_CORE_FILE_NAME "${CPACK_PACKAGE_CORE_PACKAGE_NAME}.rpm")
-set(CPACK_RPM_CORE_PACKAGE_REQUIRES "centreon-broker = ${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}, centreon-broker-storage = ${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}, gnutls >= 2.0")
+set(CPACK_RPM_CORE_PACKAGE_REQUIRES "centreon-broker = ${PACK_VERSION}, centreon-broker-storage = ${PACK_VERSION}, gnutls >= 2.0")
+
+#centreon-broker-storage
+set(CPACK_PACKAGE_STORAGE_PACKAGE_NAME "centreon-broker-storage-${PACK_VERSION}")
+set(CPACK_DEBIAN_STORAGE_FILE_NAME "centreon-broker-storage_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_STORAGE_PACKAGE_DEPENDS "centreon-broker-core (= ${PACK_VERSION})")
+set(CPACK_RPM_STORAGE_FILE_NAME "${CPACK_PACKAGE_STORAGE_PACKAGE_NAME}.rpm")
+set(CPACK_RPM_STORAGE_SUMMARY "Write Centreon performance data to Mariadb database.")
+set(CPACK_RPM_STORAGE_GROUP "Application/Communications")
+set(CPACK_RPM_STORAGE_PACKAGE_REQUIRES "centreon-broker-core = ${PACK_VERSION}")
 
 #centreon-broker-graphite
-set(CPACK_PACKAGE_GRAPHITE_PACKAGE_NAME "centreon-broker-graphite-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}")
-set(CPACK_DEBIAN_GRAPHITE_FILE_NAME "${CPACK_PACKAGE_GRAPHITE_PACKAGE_NAME}.deb")
+set(CPACK_PACKAGE_GRAPHITE_PACKAGE_NAME "centreon-broker-graphite-${PACK_VERSION}")
+set(CPACK_DEBIAN_GRAPHITE_FILE_NAME "centreon-broker-graphite_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_GRAPHITE_PACKAGE_DEPENDS "centreon-broker-core (= ${PACK_VERSION})")
 set(CPACK_RPM_GRAPHITE_FILE_NAME "${CPACK_PACKAGE_GRAPHITE_PACKAGE_NAME}.rpm")
 set(CPACK_RPM_GRAPHITE_SUMMARY "Write Centreon performance data to Graphite.")
 set(CPACK_RPM_GRAPHITE_GROUP "Application/Communications")
-set(CPACK_RPM_CORE_PACKAGE_REQUIRES "centreon-broker-core = ${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}")
+set(CPACK_RPM_GRAPHITE_PACKAGE_REQUIRES "centreon-broker-core = ${PACK_VERSION}")
+
+#centreon-broker-influxdb
+set(CPACK_PACKAGE_INFLUXDB_PACKAGE_NAME "centreon-broker-influxdb-${PACK_VERSION}")
+set(CPACK_DEBIAN_INFLUXDB_FILE_NAME "centreon-broker-influxdb_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_INFLUXDB_PACKAGE_DEPENDS "centreon-broker-core (= ${PACK_VERSION})")
+set(CPACK_RPM_INFLUXDB_FILE_NAME "${CPACK_PACKAGE_INFLUXDB_PACKAGE_NAME}.rpm")
+set(CPACK_RPM_INFLUXDB_SUMMARY "Write Centreon performance data to InfluxDB.")
+set(CPACK_RPM_INFLUXDB_GROUP "Application/Communications")
+set(CPACK_RPM_INFLUXDB_PACKAGE_REQUIRES "centreon-broker-core = ${PACK_VERSION}")
 
 #centreon-clib
-set(CPACK_PACKAGE_clib_PACKAGE_NAME "centreon-clib-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}")
-set(CPACK_DEBIAN_clib_FILE_NAME "${CPACK_PACKAGE_clib_PACKAGE_NAME}.deb")
-set(CPACK_RPM_clib_FILE_NAME "${CPACK_PACKAGE_clib_PACKAGE_NAME}.rpm")
-set(CPACK_RPM_clib_PACKAGE_NAME "centreon-clib")
-set(CPACK_RPM_clib_PACKAGE_GROUP "Development/Libraries")
-set(CPACK_RPM_clib_PACKAGE_LICENSE "ASL 2.0")
-set(CPACK_RPM_clib_PACKAGE_SUMMARY "Centreon core library.")
-set(CPACK_RPM_clib_PACKAGE_URL "https://github.com/centreon/centreon-clib")
-set(CPACK_RPM_clib_PACKAGE_PACKAGER "David BOUCHER <dboucher@centreon.com>")
-set(CPACK_RPM_clib_PACKAGE_VENDOR "Centreon")
-set(CPACK_RPM_clib_PACKAGE_DESCRIPTION "Centreon Clib is a common library for all Centreon products written in C/C++.")
+set(CPACK_PACKAGE_CLIB_PACKAGE_NAME "centreon-clib-${PACK_VERSION}")
+set(CPACK_DEBIAN_CLIB_FILE_NAME "centreon-clib_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_RPM_CLIB_FILE_NAME "${CPACK_PACKAGE_CLIB_PACKAGE_NAME}.rpm")
+set(CPACK_RPM_CLIB_PACKAGE_NAME "centreon-clib")
+set(CPACK_RPM_CLIB_PACKAGE_GROUP "Development/Libraries")
+set(CPACK_RPM_CLIB_PACKAGE_LICENSE "ASL 2.0")
+set(CPACK_RPM_CLIB_PACKAGE_SUMMARY "Centreon core library.")
+set(CPACK_RPM_CLIB_PACKAGE_URL "https://github.com/centreon/centreon-collect")
+set(CPACK_RPM_CLIB_PACKAGE_PACKAGER "David BOUCHER <dboucher@centreon.com>")
+set(CPACK_RPM_CLIB_PACKAGE_VENDOR "Centreon")
+set(CPACK_RPM_CLIB_PACKAGE_DESCRIPTION "Centreon Clib is a common library for all Centreon products written in C/C++.")
 
 #centreon-connector-perl
-set(CPACK_PACKAGE_connector-perl_PACKAGE_NAME "centreon-connector-perl")
-set(CPACK_DEBIAN_connector-perl_FILE_NAME "${CPACK_PACKAGE_connector-perl_PACKAGE_NAME}.deb")
-set(CPACK_RPM_connector-perl_FILE_NAME "${CPACK_PACKAGE_connector-perl_PACKAGE_NAME}.rpm")
+set(CPACK_PACKAGE_CONNECTOR-PERL_PACKAGE_NAME "centreon-connector-perl-${PACK_VERSION}")
+set(CPACK_DEBIAN_CONNECTOR-PERL_FILE_NAME "centreon-connector-perl_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_RPM_CONNECTOR-PERL_FILE_NAME "${CPACK_PACKAGE_CONNECTOR-PERL_PACKAGE_NAME}.rpm")
 
 #centreon-connector-ssh
-set(CPACK_PACKAGE_connector-ssh_PACKAGE_NAME "centreon-connector-ssh")
-set(CPACK_DEBIAN_connector-ssh_FILE_NAME "${CPACK_PACKAGE_connector-ssh_PACKAGE_NAME}.deb")
-set(CPACK_RPM_connector-ssh_FILE_NAME "${CPACK_PACKAGE_connector-ssh_PACKAGE_NAME}.rpm")
+set(CPACK_PACKAGE_CONNECTOR-SSH_PACKAGE_NAME "centreon-connector-ssh-${PACK_VERSION}")
+set(CPACK_DEBIAN_CONNECTOR-SSH_FILE_NAME "centreon-connector-ssh_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_RPM_CONNECTOR-SSH_FILE_NAME "${CPACK_PACKAGE_CONNECTOR-SSH_PACKAGE_NAME}.rpm")
 
 #centreon-engine-daemon
-set(CPACK_PACKAGE_engine-daemon_PACKAGE_NAME "centreon-engine-daemon")
-set(CPACK_DEBIAN_engine-daemon_FILE_NAME "${CPACK_PACKAGE_engine-daemon_PACKAGE_NAME}.deb")
-set(CPACK_RPM_engine-daemon_FILE_NAME "${CPACK_PACKAGE_engine-daemon_PACKAGE_NAME}.rpm")
-#set(CPACK_RPM_engine-daemon_REQUIRES centreon-clib)
+set(CPACK_PACKAGE_ENGINE-DAEMON_PACKAGE_NAME "centreon-engine-daemon-${PACK_VERSION}")
+set(CPACK_DEBIAN_ENGINE-DAEMON_FILE_NAME "centreon-engine-daemon_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_ENGINE-DAEMON_DEPENDS "centreon-clib (= ${PACK_VERSION})")
+set(CPACK_RPM_ENGINE-DAEMON_FILE_NAME "${CPACK_PACKAGE_ENGINE-DAEMON_PACKAGE_NAME}.rpm")
+set(CPACK_RPM_ENGINE-DAEMON_REQUIRES "centreon-clib = ${PACK_VERSION}")
 
 #centreon-engine-extcommands
-set(CPACK_PACKAGE_engine-extcommands_PACKAGE_NAME "centreon-engine-extcommands")
-set(CPACK_DEBIAN_engine-extcommands_FILE_NAME ${CPACK_PACKAGE_engine-extcommands_PACKAGE_NAME}.deb)
-set(CPACK_RPM_engine-extcommands_FILE_NAME ${CPACK_PACKAGE_engine-extcommands_PACKAGE_NAME}.rpm)
+set(CPACK_PACKAGE_ENGINE-EXTCOMMANDS_PACKAGE_NAME "centreon-engine-extcommands-${PACK_VERSION}")
+set(CPACK_DEBIAN_ENGINE-EXTCOMMANDS_FILE_NAME "centreon-engine-extcommands_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_ENGINE-EXTCOMMANDS_DEPENDS "centreon-engine-daemon (= ${PACK_VERSION})")
+set(CPACK_RPM_ENGINE-EXTCOMMANDS_FILE_NAME ${CPACK_PACKAGE_ENGINE-EXTCOMMANDS_PACKAGE_NAME}.rpm)
+set(CPACK_RPM_ENGINE-EXTCOMMANDS_DEPENDS "centreon-engine-daemon = ${PACK_VERSION}")
 
 include(CPack)
