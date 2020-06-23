726047757a240d89b3620349524cfdbe21e0d0a5
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-23 11:45:45 +0200

    fix(jobs): install missing dependency in agent-unittest.

--- jobs/agent/2/agent-unittest.container.sh
+++ jobs/agent/2/agent-unittest.container.sh
@@ -11,6 +11,7 @@ rm -f /tmp/ut.xml
 # Install dependencies.
 cd /root/go/src/github.com/centreon/$PROJECT/
 go get github.com/shirou/w32
+go get -u github.com/jstemmer/go-junit-report
 
 # Run unit tests.
 go test ./... -v 2>&1 | go-junit-report > /tmp/ut.xml
