b578d0717b71c5d0b6f3edc8e1740a9799aea73a
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-24 08:57:53 +0200

    fix(containers): install AutoDiscovery as apache user.

--- centreon-build/containers/autodisco/20.10/install.sh
+++ centreon-build/containers/autodisco/20.10/install.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 service mysql start
-/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-pp-manager
-/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-autodiscovery-server
+su -c "/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-pp-manager" -s /bin/bash apache
+su -c "/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-autodiscovery-server" -s /bin/bash apache
 service mysql stop
