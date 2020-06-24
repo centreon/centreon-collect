e4b8c59768a1b3268b8038eaf729885ed81237c8
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-06-24 15:55:52 +0200

    fix(jobs): add go mod vendor to agent-unittest.

--- jobs/agent/2/agent-unittest.container.sh
+++ jobs/agent/2/agent-unittest.container.sh
@@ -12,6 +12,7 @@ rm -f /tmp/ut.xml
 cd /root/go/src/github.com/centreon/$PROJECT/
 go get github.com/shirou/w32
 go get -u github.com/jstemmer/go-junit-report
+go mod vendor
 
 # Run unit tests.
 go test ./... -v 2>&1 | go-junit-report > /tmp/ut.xml
