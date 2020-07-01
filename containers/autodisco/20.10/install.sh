32e8e38f181ae1f1625341fbb597acee38effec6
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-07-01 16:00:07 +0200

    feat(containers): install EPP license in mon-autodisco image.

--- containers/autodisco/20.10/install.sh
+++ containers/autodisco/20.10/install.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 service mysql start
+su -c "/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-license-manager" -s /bin/bash apache
 su -c "/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-pp-manager" -s /bin/bash apache
 su -c "/tmp/install-centreon-module.php -b /usr/share/centreon/bootstrap.php -m centreon-autodiscovery-server" -s /bin/bash apache
 service mysql stop
