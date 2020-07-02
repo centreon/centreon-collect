5cc1028514d108c74a08e70bdf2564920255357b
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2020-07-02 15:56:11 +0100

    make improvements to the dependencies

--- CMakeCPack.cmake
+++ CMakeCPack.cmake
@@ -40,14 +40,16 @@ set(CPACK_RPM_BROKER_DEFAULT_GROUP "centreon-broker")
 #centreon-broker-cbd
 set(CPACK_PACKAGE_CBD_PACKAGE_NAME "centreon-broker-cbd-${PACK_VERSION}")
 set(CPACK_DEBIAN_CBD_FILE_NAME "centreon-broker-cbd_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_CBD_PACKAGE_DEPENDS "centreon-broker (= ${PACK_VERSION}), libmariadb3")
 set(CPACK_RPM_CBD_FILE_NAME "${CPACK_PACKAGE_CBD_PACKAGE_NAME}.rpm")
+set(CPACK_RPM_CBD_PACKAGE_REQUIRES "centreon-broker = ${PACK_VERSION}")
 
 #centreon-broker-core
 set(CPACK_PACKAGE_CORE_PACKAGE_NAME "centreon-broker-core-${PACK_VERSION}")
 set(CPACK_DEBIAN_CORE_FILE_NAME "centreon-broker-core_${PACK_VERSION}_${ARCH}.deb")
 set(CPACK_DEBIAN_CORE_PACKAGE_DEPENDS "centreon-broker (= ${PACK_VERSION}), libgnutls-openssl27 (>= 2.0)")
 set(CPACK_RPM_CORE_FILE_NAME "${CPACK_PACKAGE_CORE_PACKAGE_NAME}.rpm")
-set(CPACK_RPM_CORE_PACKAGE_REQUIRES "centreon-broker = ${PACK_VERSION}, centreon-broker-storage = ${PACK_VERSION}, gnutls >= 2.0")
+set(CPACK_RPM_CORE_PACKAGE_REQUIRES "centreon-broker = ${PACK_VERSION}, centreon-broker-storage = ${PACK_VERSION}, gnutls >= 2.0, lua >= 5.1")
 
 #centreon-broker-storage
 set(CPACK_PACKAGE_STORAGE_PACKAGE_NAME "centreon-broker-storage-${PACK_VERSION}")
@@ -79,6 +81,7 @@ set(CPACK_RPM_INFLUXDB_PACKAGE_REQUIRES "centreon-broker-core = ${PACK_VERSION}"
 #centreon-clib
 set(CPACK_PACKAGE_CLIB_PACKAGE_NAME "centreon-clib-${PACK_VERSION}")
 set(CPACK_DEBIAN_CLIB_FILE_NAME "centreon-clib_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_CLIB_PACKAGE_DEPENDS "libstdc++6 (>= 8.3.0-6)")
 set(CPACK_RPM_CLIB_FILE_NAME "${CPACK_PACKAGE_CLIB_PACKAGE_NAME}.rpm")
 set(CPACK_RPM_CLIB_PACKAGE_NAME "centreon-clib")
 set(CPACK_RPM_CLIB_PACKAGE_GROUP "Development/Libraries")
@@ -88,28 +91,33 @@ set(CPACK_RPM_CLIB_PACKAGE_URL "https://github.com/centreon/centreon-collect")
 set(CPACK_RPM_CLIB_PACKAGE_PACKAGER "David BOUCHER <dboucher@centreon.com>")
 set(CPACK_RPM_CLIB_PACKAGE_VENDOR "Centreon")
 set(CPACK_RPM_CLIB_PACKAGE_DESCRIPTION "Centreon Clib is a common library for all Centreon products written in C/C++.")
+set(CPACK_RPM_CLIB_PACKAGE_REQUIRES "libstdc++ >= 4.8.5-39")
 
 #centreon-connector-perl
 set(CPACK_PACKAGE_CONNECTOR-PERL_PACKAGE_NAME "centreon-connector-perl-${PACK_VERSION}")
 set(CPACK_DEBIAN_CONNECTOR-PERL_FILE_NAME "centreon-connector-perl_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_CONNECTOR-PERL_PACKAGE_DEPENDS "centreon-clib (= ${PACK_VERSION}), perl")
 set(CPACK_RPM_CONNECTOR-PERL_FILE_NAME "${CPACK_PACKAGE_CONNECTOR-PERL_PACKAGE_NAME}.rpm")
+set(CPACK_RPM_CONNECTOR-PERL_PACKAGE_REQUIRES "centreon-clib = ${PACK_VERSION}, perl")
 
 #centreon-connector-ssh
 set(CPACK_PACKAGE_CONNECTOR-SSH_PACKAGE_NAME "centreon-connector-ssh-${PACK_VERSION}")
 set(CPACK_DEBIAN_CONNECTOR-SSH_FILE_NAME "centreon-connector-ssh_${PACK_VERSION}_${ARCH}.deb")
+set(CPACK_DEBIAN_CONNECTOR-SSH_PACKAGE_DEPENDS "centreon-clib (= ${PACK_VERSION}), libssl")
 set(CPACK_RPM_CONNECTOR-SSH_FILE_NAME "${CPACK_PACKAGE_CONNECTOR-SSH_PACKAGE_NAME}.rpm")
+set(CPACK_RPM_CONNECTOR-SSH_PACKAGE_REQUIRES "centreon-clib = ${PACK_VERSION}, libgcrypt, libssh2 >= 1.4")
 
 #centreon-engine-daemon
 set(CPACK_PACKAGE_ENGINE-DAEMON_PACKAGE_NAME "centreon-engine-daemon-${PACK_VERSION}")
 set(CPACK_DEBIAN_ENGINE-DAEMON_FILE_NAME "centreon-engine-daemon_${PACK_VERSION}_${ARCH}.deb")
-set(CPACK_DEBIAN_ENGINE-DAEMON_DEPENDS "centreon-clib (= ${PACK_VERSION})")
+set(CPACK_DEBIAN_ENGINE-DAEMON_PACKAGE_DEPENDS "centreon-clib (= ${PACK_VERSION})")
 set(CPACK_RPM_ENGINE-DAEMON_FILE_NAME "${CPACK_PACKAGE_ENGINE-DAEMON_PACKAGE_NAME}.rpm")
 set(CPACK_RPM_ENGINE-DAEMON_REQUIRES "centreon-clib = ${PACK_VERSION}")
 
 #centreon-engine-extcommands
 set(CPACK_PACKAGE_ENGINE-EXTCOMMANDS_PACKAGE_NAME "centreon-engine-extcommands-${PACK_VERSION}")
 set(CPACK_DEBIAN_ENGINE-EXTCOMMANDS_FILE_NAME "centreon-engine-extcommands_${PACK_VERSION}_${ARCH}.deb")
-set(CPACK_DEBIAN_ENGINE-EXTCOMMANDS_DEPENDS "centreon-engine-daemon (= ${PACK_VERSION})")
+set(CPACK_DEBIAN_ENGINE-EXTCOMMANDS_PACKAGE_DEPENDS "centreon-engine-daemon (= ${PACK_VERSION})")
 set(CPACK_RPM_ENGINE-EXTCOMMANDS_FILE_NAME ${CPACK_PACKAGE_ENGINE-EXTCOMMANDS_PACKAGE_NAME}.rpm)
 set(CPACK_RPM_ENGINE-EXTCOMMANDS_DEPENDS "centreon-engine-daemon = ${PACK_VERSION}")
 
