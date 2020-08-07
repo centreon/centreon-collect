132a315b34bc673ed0b5bf052b7035c14f0c0787
jbrouze <jbrouze@centreon.com>
2020-08-07 14:40:36 +0200

    enh(group_member) : added ut for host and service group_member

---
+++ tests/broker/neb/service_group_member.cc
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+#include "com/centreon/broker/neb/service_group_member.hh"
+#include <gtest/gtest.h>
+
+using namespace com::centreon::broker;
+
+TEST(ServiceGroupMember, Constructor) {
+  neb::service_group_member sgm("service_group_member",
+                    10,
+                    5,
+                    2,
+                    true,
+                    20);
+  
+  ASSERT_EQ(sgm.group_name, "service_group_member");
+  ASSERT_EQ(sgm.group_id, 10);
+  ASSERT_EQ(sgm.poller_id, 5);
+  ASSERT_EQ(sgm.host_id, 2);
+  ASSERT_EQ(sgm.enabled, true);
+  ASSERT_EQ(sgm.service_id, 20);
+}
