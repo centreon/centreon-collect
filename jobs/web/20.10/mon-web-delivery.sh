4e8a5e5925b7aa4dde4ec451878199d6575930f2
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-08-18 11:43:16 +0200

    chore(jobs): "temporarily" disable CentOS 8 from mon-web-delivery.

--- jobs/web/20.10/mon-web-delivery.sh
+++ jobs/web/20.10/mon-web-delivery.sh
@@ -17,7 +17,7 @@ fi
 if [ "$BUILD" '=' 'RELEASE' ] ; then
   put_testing_source "standard" "web" "$PROJECT-$VERSION-$RELEASE" "$PROJECT-$VERSION.tar.gz" centreon-api-v2.html
   copy_internal_rpms_to_testing "standard" "20.10" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
-  copy_internal_rpms_to_testing "standard" "20.10" "el8" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
+#  copy_internal_rpms_to_testing "standard" "20.10" "el8" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
   TARGETVERSION="$VERSION"
 
 #
@@ -25,7 +25,7 @@ if [ "$BUILD" '=' 'RELEASE' ] ; then
 #
 else
   promote_canary_rpms_to_unstable "standard" "20.10" "el7" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
-  promote_canary_rpms_to_unstable "standard" "20.10" "el8" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
+#  promote_canary_rpms_to_unstable "standard" "20.10" "el8" "noarch" "web" "$PROJECT-$VERSION-$RELEASE"
   SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
   put_internal_source "web" "$PROJECT-$VERSION-$RELEASE" centreon-api-v2.html
   $SSH_REPO aws s3 cp --acl public-read "/srv/sources/internal/web/$PROJECT-$VERSION-$RELEASE/centreon-api-v2.html" s3://centreon-documentation-prod/api/centreon-web/index.html
@@ -36,7 +36,7 @@ fi
 # Set Docker images as latest.
 REGISTRY='registry.centreon.com'
 for image in mon-web-fresh mon-web mon-web-widgets ; do
-  for distrib in centos7 centos8 ; do
+  for distrib in centos7 ; do
     docker pull "$REGISTRY/$image-$VERSION-$RELEASE:$distrib"
     docker tag "$REGISTRY/$image-$VERSION-$RELEASE:$distrib" "$REGISTRY/$image-$TARGETVERSION:$distrib"
     docker push "$REGISTRY/$image-$TARGETVERSION:$distrib"
