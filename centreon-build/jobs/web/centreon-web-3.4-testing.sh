a182ad6359a1d802afb4bc1d056fc444d25efad4
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-09-07 11:12:13 +0200

    fix(jobs): remove CentOS 6 build from Centreon Web 3.4.

--- centreon-build/jobs/web/centreon-web-3.4-testing.sh
+++ centreon-build/jobs/web/centreon-web-3.4-testing.sh
@@ -10,14 +10,11 @@ if [ -z "$COMMIT" -o -z "$VERSION" -o -z "$RELEASE" ] ; then
 fi
 
 # Pull mon-build-dependencies containers.
-docker pull registry.centreon.com/mon-build-dependencies-3.4:centos6
 docker pull registry.centreon.com/mon-build-dependencies-3.4:centos7
 
 # Create input and output directories for docker-rpm-builder.
 rm -rf input
 mkdir input
-rm -rf output-centos6
-mkdir output-centos6
 rm -rf output-centos7
 mkdir output-centos7
 
@@ -61,16 +58,12 @@ cp `dirname $0`/../../packaging/web/rpm/3.4/centreon.spectemplate input/
 cp `dirname $0`/../../packaging/web/src/3.4/* input
 
 # Build RPMs.
-docker-rpm-builder dir --sign-with `dirname $0`/../ces.key registry.centreon.com/mon-build-dependencies-3.4:centos6 input output-centos6
 docker-rpm-builder dir --sign-with `dirname $0`/../ces.key registry.centreon.com/mon-build-dependencies-3.4:centos7 input output-centos7
 
 # Copy files to server.
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" mkdir -p "/srv/sources/lts/testing/centreon-web-$VERSION-$RELEASE"
 scp -o StrictHostKeyChecking=no "input/centreon-$VERSION.tar.gz" "ubuntu@srvi-repo.int.centreon.com:/srv/sources/lts/testing/centreon-web-$VERSION-$RELEASE/"
 scp -o StrictHostKeyChecking=no "input/centreon-web-$VERSION.tar.gz" "ubuntu@srvi-repo.int.centreon.com:/srv/sources/lts/testing/centreon-web-$VERSION-$RELEASE/"
-FILES_CENTOS6='output-centos6/noarch/*.rpm'
 FILES_CENTOS7='output-centos7/noarch/*.rpm'
-scp -o StrictHostKeyChecking=no $FILES_CENTOS6 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/lts/3.4/el6/testing/noarch/RPMS"
 scp -o StrictHostKeyChecking=no $FILES_CENTOS7 "ubuntu@srvi-repo.int.centreon.com:/srv/yum/lts/3.4/el7/testing/noarch/RPMS"
-ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/lts/3.4/el6/testing/noarch
 ssh -o StrictHostKeyChecking=no "ubuntu@srvi-repo.int.centreon.com" createrepo /srv/yum/lts/3.4/el7/testing/noarch
