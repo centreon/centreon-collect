ffda9266c866f21bc97ec321d99d579aaa22992c
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-09-23 13:54:24 +0200

    enh(jobs): use CMake 3 when running Centreon Broker unit tests.

--- jobs/broker/20.10/unittest.sh
+++ jobs/broker/20.10/unittest.sh
@@ -10,7 +10,7 @@ cd /tmp/build
 
 # Configure project.
 conan install /usr/local/src/centreon-broker
-CXXFLAGS="-O0 -g3 -std=c++11 -Wall -Wno-long-long" cmake -DWITH_TESTING=1 /usr/local/src/centreon-broker
+CXXFLAGS="-O0 -g3 -std=c++11 -Wall -Wno-long-long" cmake3 -DWITH_TESTING=1 /usr/local/src/centreon-broker
 
 # Build project.
 make -j 4
