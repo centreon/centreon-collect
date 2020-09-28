1a7d0b6414d60cf664da9a41a0b95f40fb9cd479
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-09-28 09:45:09 +0200

    fix: use CMake 3 to build 20.10 Centreon Engine on CentOS 7.

--- centreon-build/jobs/engine/20.10/unittest.sh
+++ centreon-build/jobs/engine/20.10/unittest.sh
@@ -9,8 +9,13 @@ mkdir /tmp/build
 cd /tmp/build
 
 # Configure project.
+if [ -e /usr/bin/cmake3 ] ; then
+  mycmake=cmake3
+else
+  mycmake=cmake
+fi
 conan install /usr/local/src/centreon-engine
-CXXFLAGS="-std=c++11" cmake -DWITH_TESTING=1 /usr/local/src/centreon-engine
+CXXFLAGS="-std=c++11" $mycmake -DWITH_TESTING=1 /usr/local/src/centreon-engine
 
 # Build project.
 make -j 4
