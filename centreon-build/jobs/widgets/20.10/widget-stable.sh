9ed796434549945cbd1c9f04474bf533c5804add
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-10-21 11:52:54 +0200

    fix(jobs): fix widget-stable.

--- centreon-build/jobs/widgets/20.10/widget-stable.sh
+++ centreon-build/jobs/widgets/20.10/widget-stable.sh
@@ -16,11 +16,11 @@ export PROJECT="centreon-widget-$WIDGET"
 
 # Move sources to the stable directory.
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
-$SSH_REPO mv "/srv/sources/standard/testing/widget/$PROJECT-$VERSION-$RELEASE" "/srv/sources/standard/stable/"
+$SSH_REPO mv "/srv/sources/standard/testing/widget-$WIDGET/$PROJECT-$VERSION-$RELEASE" "/srv/sources/standard/stable/"
 
 # Put sources online.
 upload_tarball_for_download "$PROJECT" "$VERSION" "/srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz" "s3://centreon-download/public/centreon-widgets/$PROJECT/$PROJECT-$VERSION.tar.gz"
 
 # Move RPMs to the stable repository.
-promote_testing_rpms_to_stable "standard" "20.10" "el7" "noarch" "widget" "$PROJECT-$VERSION-$RELEASE"
-promote_testing_rpms_to_stable "standard" "20.10" "el8" "noarch" "widget" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "20.10" "el7" "noarch" "widget-$WIDGET" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "20.10" "el8" "noarch" "widget-$WIDGET" "$PROJECT-$VERSION-$RELEASE"
