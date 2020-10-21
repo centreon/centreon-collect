c6cf8a9de67624a8806c2bb2d4ac771ed80b52c6
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-10-21 12:34:41 +0200

    enh(jobs): set 20.10 as /current/ version for documentation.

--- jobs/doc/20.04/doc-staging.sh
+++ jobs/doc/20.04/doc-staging.sh
@@ -8,16 +8,14 @@ set -x
 # Project.
 PROJECT=centreon-documentation
 
-# We will prepare two source directories ("$VERSION/" and "current/").
+# We will prepare the source directory ("$VERSION/").
 rm -rf prod vanilla
 mkdir prod
 tar xzf vanilla.tar.gz
-cp -r vanilla prod/version
-mv vanilla prod/current
+mv vanilla prod/version
 
 # Let's replace their respective base URL.
 find "prod/version" -type f | xargs -d '\n' sed -i -e "s#@BASEURL@#$VERSION#g"
-find "prod/current" -type f | xargs -d '\n' sed -i -e "s#@BASEURL@#current#g"
 tar czf prod.tar.gz prod
 
 # We cannot upload sources to S3 directly. We need to get
@@ -29,7 +27,5 @@ ssh $REPO_CREDS sh -c "'cd $tmpdir && tar xzf prod.tar.gz'"
 # Upload to S3.
 TARGETDIR="s3://centreon-documentation-preprod/$VERSION"
 ssh $REPO_CREDS aws s3 sync --acl public-read --delete "$tmpdir/prod/version" "$TARGETDIR"
-TARGETDIR="s3://centreon-documentation-preprod/current"
-ssh $REPO_CREDS aws s3 sync --acl public-read --delete "$tmpdir/prod/current" "$TARGETDIR"
-ssh $REPO_CREDS aws cloudfront create-invalidation --distribution-id E1DRHP505UQYSQ --paths "/$VERSION/"'* /current/*'
+ssh $REPO_CREDS aws cloudfront create-invalidation --distribution-id E1DRHP505UQYSQ --paths "/$VERSION/"'*'
 ssh $REPO_CREDS rm -rf "$tmpdir"
