8f1d8150fd484084803a34b27179bb54be147e5f
Kevin Duret <kduret@centreon.com>
2020-11-03 11:04:08 +0100

    jobs(plugin-packs): manage el8 and 21.04

--- centreon-build/jobs/packs/packs-package.sh
+++ centreon-build/jobs/packs/packs-package.sh
@@ -10,10 +10,8 @@ PROJECT=centreon-packs
 
 # Pull build dependencies images.
 REGISTRY="registry.centreon.com"
-BUILD_IMG_CENTOS6="$REGISTRY/mon-build-dependencies-3.4:centos6"
 BUILD_IMG_CENTOS7="$REGISTRY/mon-build-dependencies-19.04:centos7"
 BUILD_IMG_CENTOS8="$REGISTRY/mon-build-dependencies-20.10:centos8"
-docker pull "$BUILD_IMG_CENTOS6"
 docker pull "$BUILD_IMG_CENTOS7"
 docker pull "$BUILD_IMG_CENTOS8"
 
@@ -28,8 +26,6 @@ mkdir "cache-$VERSION-$RELEASE"
 # Create input and output directories for docker-rpm-builder.
 rm -rf input
 mkdir input
-rm -rf output-centos6
-mkdir output-centos6
 rm -rf output-centos7
 mkdir output-centos7
 rm -rf output-centos8
@@ -64,31 +60,28 @@ if [ "$packagecount" -gt 0 ] ; then
   put_internal_source "packs" "$PROJECT-$VERSION-$RELEASE" "$PROJECT-$VERSION.tar.gz"
 
   # Build RPMs.
-  docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG_CENTOS6" input output-centos6
+
   docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG_CENTOS7" input output-centos7
   docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_IMG_CENTOS8" input output-centos8
 
   # Remove 'fake' package.
-  rm -f output-centos6/noarch/centreon-pack-1.0.0*.rpm
   rm -f output-centos7/noarch/centreon-pack-1.0.0*.rpm
   rm -f output-centos8/noarch/centreon-pack-1.0.0*.rpm
 
   # Publish RPMs.
-  put_internal_rpms "3.4" "el6" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos6/noarch/*.rpm
-  put_internal_rpms "3.4" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
-  put_internal_rpms "19.04" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
   put_internal_rpms "19.10" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
   put_internal_rpms "20.04" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
   put_internal_rpms "20.10" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
   put_internal_rpms "20.10" "el8" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos8/noarch/*.rpm
+  put_internal_rpms "21.04" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+  put_internal_rpms "21.04" "el8" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE" output-centos8/noarch/*.rpm
   if [ "$BRANCH_NAME" '=' 'master' ] ; then
-    copy_internal_rpms_to_unstable "plugin-packs" "3.4" "el6" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
-    copy_internal_rpms_to_unstable "plugin-packs" "3.4" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
-    copy_internal_rpms_to_unstable "plugin-packs" "19.04" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
     copy_internal_rpms_to_unstable "plugin-packs" "19.10" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
     copy_internal_rpms_to_unstable "plugin-packs" "20.04" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
     copy_internal_rpms_to_unstable "plugin-packs" "20.10" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
     copy_internal_rpms_to_unstable "plugin-packs" "20.10" "el8" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
+    copy_internal_rpms_to_unstable "plugin-packs" "21.04" "el7" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
+    copy_internal_rpms_to_unstable "plugin-packs" "21.04" "el8" "noarch" "packs" "$PROJECT-$VERSION-$RELEASE"
   fi
 
   # Populate cache.
