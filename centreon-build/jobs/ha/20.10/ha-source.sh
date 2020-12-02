0bdcb5bf39c951818538876fa778201508c8af74
Kevin Duret <kduret@centreon.com>
2020-12-02 17:11:00 +0100

    fix(ha): properly export default version

--- centreon-build/jobs/ha/20.10/ha-source.sh
+++ centreon-build/jobs/ha/20.10/ha-source.sh
@@ -20,7 +20,7 @@ tar czf "$PROJECT-git.tar.gz" "$PROJECT"
 cd "$PROJECT"
 export VERSION=`grep -E "20.10.[0-9]+" CHANGELOG.md | cut -d "[" -f2 | cut -d "]" -f1 | head -n 1`
 if [ "$VERSION" '=' '' ] ; then
-  echo VERSION='20.10.0'
+  export VERSION='20.10.0'
 fi
 
 # Get release.
