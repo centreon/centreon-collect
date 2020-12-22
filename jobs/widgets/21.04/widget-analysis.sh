9fc8daac86c3b80a8d53c35ed801ffbae8be960b
sc979 <34628915+sc979@users.noreply.github.com>
2020-12-22 09:32:09 +0100

    enh(chore): add requirements on widgets jobs (#384)

---
+++ jobs/widgets/21.04/widget-analysis.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#
+# Copyright 2005 - 2020 Centreon (https://www.centreon.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For more information : contact@centreon.com
+#
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# This job is run directly after des-bam-unittest on centos7.
+
+# Project.
+PROJECT=centreon-widget-$WIDGET
+
+# Retrieve copy of git repository.
+curl -o "$PROJECT-git.tar.gz" "http://srvi-repo.int.centreon.com/sources/internal/widget-engine-status/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+rm -rf "$PROJECT"
+tar xzf "$PROJECT-git.tar.gz"
+
+# Copy reports and run analysis.
+cd "$PROJECT"
+cp ../ut-be.xml .
+cp ../coverage-be.xml .
+sed -i -e 's#/usr/local/src/centreon-widget-engine-status/##g' coverage-be.xml
+if [ "$BUILD" '=' 'RELEASE' ] ; then
+  sed -i -e 's/centreon-widget-engine-status-21.04/centreon-widget-engine-status-21.04-release/g' sonar-project.properties
+  sed -i -e 's/Centreon Widget Engine Status 21.04/Centreon Widget Engine Status 21.04 (release)/g' sonar-project.properties
+fi
+echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
+sonar-scanner
