825e36b201f06c7fffb5d9833ed85b4c4b67e5e0
Kevin Duret <kduret@centreon.com>
2020-12-23 15:51:57 +0100

    enh(ppm): check coding style & phpstan (#388)
    Refs: MON-6428

--- jobs/ppm/21.04/mon-ppm-unittest.sh
+++ jobs/ppm/21.04/mon-ppm-unittest.sh
@@ -27,16 +27,19 @@ tar xzf "$PROJECT-$VERSION.tar.gz"
 # Launch mon-unittest container.
 UT_IMAGE=registry.centreon.com/mon-unittest-21.04:$DISTRIB
 docker pull $UT_IMAGE
-containerid=`docker create $UT_IMAGE /usr/local/bin/unittest-phing $PROJECT`
+containerid=`docker create -e "GITHUB_TOKEN=$GITHUB_TOKEN" $UT_IMAGE /usr/local/bin/unittest.sh`
 
 # Copy sources to container.
+docker cp `dirname $0`/mon-ppm-unittest.container.sh "$containerid:/usr/local/bin/unittest.sh"
 docker cp "$PROJECT-$VERSION" "$containerid:/usr/local/src/$PROJECT"
 
 # Run unit tests.
 docker start -a "$containerid"
-docker cp "$containerid:/tmp/ut.xml" ut.xml
-docker cp "$containerid:/tmp/coverage.xml" coverage.xml
-docker cp "$containerid:/tmp/codestyle.xml" codestyle.xml
+
+docker cp "$containerid:/tmp/ut-be.xml" ut-be.xml
+docker cp "$containerid:/tmp/coverage-be.xml" coverage-be.xml
+docker cp "$containerid:/tmp/codestyle-be.xml" codestyle-be.xml
+docker cp "$containerid:/tmp/phpstan.xml" phpstan.xml
 
 # Stop container.
 docker stop "$containerid"
