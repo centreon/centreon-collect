a12d39501743ea92e6f3ca66bd4bf67164fedc05
sc979 <34628915+sc979@users.noreply.github.com>
2021-01-07 15:08:46 +0100

    enh(chore): add requirements on awie jobs (#398)

--- jobs/awie/21.04/mon-awie-unittest.sh
+++ jobs/awie/21.04/mon-awie-unittest.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+#
+#
+# Copyright 2005 - 2020 Centreon (https://www.centreon.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For more information : contact@centreon.com
+#
+#
+
 set -e
 set -x
 
@@ -14,7 +34,7 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 if [ "$#" -lt 1 ] ; then
-  echo "USAGE: $0 <centos7|...>"
+  echo "USAGE: $0 <centos7|centos8|...>"
   exit 1
 fi
 DISTRIB="$1"
@@ -24,19 +44,18 @@ rm -rf "$PROJECT-$VERSION.tar.gz" "$PROJECT-$VERSION"
 get_internal_source "awie/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
 tar xzf "$PROJECT-$VERSION.tar.gz"
 
-# Launch mon-unitttest container.
+# Launch mon-unittest container.
 UT_IMAGE=registry.centreon.com/mon-unittest-21.04:$DISTRIB
 docker pull $UT_IMAGE
-containerid=`docker create $UT_IMAGE /usr/local/bin/unittest-phing $PROJECT`
+containerid=`docker create $UT_IMAGE /usr/local/bin/unittest.sh $PROJECT`
 
 # Copy sources to container.
+docker cp `dirname $0`/mon-awie-unittest.container.sh "$containerid:/usr/local/bin/unittest.sh"
 docker cp "$PROJECT-$VERSION" "$containerid:/usr/local/src/$PROJECT"
 
 # Run unit tests.
 docker start -a "$containerid"
-docker cp "$containerid:/tmp/ut.xml" ut.xml
-docker cp "$containerid:/tmp/coverage.xml" coverage.xml
-docker cp "$containerid:/tmp/codestyle.xml" codestyle.xml
+docker cp "$containerid:/tmp/codestyle-be.xml" codestyle.xml
 
 # Stop container.
 docker stop "$containerid"
