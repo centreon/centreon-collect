48d11ceeadc8731c33576d3d132472f413c5013f
Kevin Duret <kduret@centreon.com>
2021-03-05 13:05:50 +0100

    build business rpm release

--- jobs/repo/21.04/repo-testing.sh
+++ jobs/repo/21.04/repo-testing.sh
@@ -11,7 +11,7 @@ php `dirname $0`/../../../packaging/repo/21.04/genrepo.php
 
 # Build all release RPMs.
 for distrib in el7 el8 ; do
-  for project in centreon centreon-bam centreon-map centreon-mbi centreon-plugin-packs ; do
+  for project in centreon-business ; do
     # Create temporary directories.
     rm -rf input output
     mkdir input
@@ -38,14 +38,8 @@ for distrib in el7 el8 ; do
     # Push RPM.
     if [ "$project" = centreon ] ; then
       REPO='standard'
-    elif [ "$project" = 'centreon-bam' ] ; then
-      REPO='bam'
-    elif [ "$project" = 'centreon-map' ] ; then
-      REPO='map'
-    elif [ "$project" = 'centreon-mbi' ] ; then
-      REPO='mbi'
-    elif [ "$project" = 'centreon-plugin-packs' ] ; then
-      REPO='plugin-packs'
+    elif [ "$project" = 'centreon-business' ] ; then
+      REPO='business'
     fi
     put_testing_rpms "$REPO" 21.04 "$distrib" noarch repo '' output/noarch/*.rpm
   done
