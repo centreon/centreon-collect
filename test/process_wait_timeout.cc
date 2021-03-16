1a94ca182237cea641e106676aae100669babae4
David Boucher <dboucher@centreon.com>
2021-03-16 14:41:16 +0100

    enh(process): debug informations added.

--- test/process_wait_timeout.cc
+++ test/process_wait_timeout.cc
@@ -34,12 +34,12 @@ int main() {
   try {
     process p;
     p.exec("./bin_test_process_output check_sleep 1");
-    if (p.wait(500) == true)
-      throw(basic_error() << "wait timeout failed: "
-                             "waiting more than necessary");
-    if (p.wait(1500) == false)
-      throw(basic_error() << "wait timeout failed: "
-                             "waiting less than necessary");
+    if (p.wait(500))
+      throw basic_error() << "wait timeout failed: "
+                             "waiting more than necessary";
+    if (!p.wait(1500))
+      throw basic_error() << "wait timeout failed: "
+                             "waiting less than necessary";
   }
   catch (std::exception const& e) {
     ret = EXIT_FAILURE;
