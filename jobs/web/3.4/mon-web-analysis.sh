ed3628a4de7c6eef9af859db55b954b19441a8e4
sc979 <34628915+sc979@users.noreply.github.com>
2021-03-19 09:02:41 +0100

    enh(chore): integrate sonarQube dev edition on centreon-web (#445)

--- jobs/web/3.4/mon-web-analysis.sh
+++ jobs/web/3.4/mon-web-analysis.sh
@@ -9,6 +9,7 @@ set -x
 
 # Project.
 PROJECT=centreon-web
+PROJECT_NAME="Centreon Web"
 
 # Retrieve copy of git repository.
 curl -o 'centreon-web-git.tar.gz' "http://srvi-repo.int.centreon.com/sources/internal/web/$PROJECT-$VERSION-$RELEASE/centreon-web-git.tar.gz"
@@ -17,4 +18,17 @@ tar xzf centreon-web-git.tar.gz
 
 # Copy reports and run analysis.
 cd centreon-web
+
+# environment values required to replace sonarQube project versioning and binding
+#   sonar.projectKey="{PROJECT_TITLE}"
+#   sonar.projectName="{PROJECT_NAME}"
+#   sonar.projectKey="{PROJECT_VERSION}"
+echo "BRANCH_NAME      -> $BRANCH_NAME"
+echo "PROJECT_TITLE    -> $PROJECT"
+echo "PROJECT_NAME     -> $PROJECT_NAME"
+echo "PROJECT_VERSION  -> $VERSION"
+sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
+sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
+sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
+
 sonar-scanner
