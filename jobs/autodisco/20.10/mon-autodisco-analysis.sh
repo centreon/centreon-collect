e1763ef7bb97fe3f632f48550483524f2d8586b4
sc979 <34628915+sc979@users.noreply.github.com>
2021-03-24 10:30:53 +0100

    enh(chore): integrate sonarQube dev edition on autodisco (#453)

--- jobs/autodisco/20.10/mon-autodisco-analysis.sh
+++ jobs/autodisco/20.10/mon-autodisco-analysis.sh
@@ -7,12 +7,21 @@ set -x
 
 # Project.
 PROJECT=centreon-autodiscovery
+PROJECT_NAME="Centreon Autodiscovery"
 
 # Copy reports and run analysis.
 cd "$PROJECT"
-if [ "$BUILD" '=' 'RELEASE' ] ; then
-  sed -i -e 's/centreon-autodiscovery-20.10/centreon-autodiscovery-20.10-release/g' sonar-project.properties
-  sed -i -e 's/Centreon Autodiscovery 20.10/Centreon Autodiscovery 20.10 (release)/g' sonar-project.properties
-fi
-echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
+
+# environment values required to replace sonarQube project versioning and binding
+#   sonar.projectKey="{PROJECT_TITLE}"
+#   sonar.projectName="{PROJECT_NAME}"
+#   sonar.projectKey="{PROJECT_VERSION}"
+echo "BRANCH_NAME      -> $BRANCH_NAME"
+echo "PROJECT_TITLE    -> $PROJECT"
+echo "PROJECT_NAME     -> $PROJECT_NAME"
+echo "PROJECT_VERSION  -> $VERSION"
+sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
+sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
+sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
+
 sonar-scanner
