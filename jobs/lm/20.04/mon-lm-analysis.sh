f59b75c61a21e38a4c27a200b263a18a51262c3e
sc979 <34628915+sc979@users.noreply.github.com>
2021-03-24 14:41:31 +0100

    enh(chore): integrate sonarQube dev edition on LM (#458)

--- jobs/lm/20.04/mon-lm-analysis.sh
+++ jobs/lm/20.04/mon-lm-analysis.sh
@@ -9,6 +9,7 @@ set -x
 
 # Project.
 PROJECT=centreon-license-manager
+PROJECT_NAME="Centreon License Manager"
 
 # Retrieve copy of git repository.
 curl -o "$PROJECT-git.tar.gz" "http://srvi-repo.int.centreon.com/sources/internal/lm/$PROJECT-$VERSION-$RELEASE/$PROJECT-git.tar.gz"
@@ -20,9 +21,17 @@ cd "$PROJECT"
 cp ../ut-be.xml .
 cp ../coverage.xml .
 sed -i -e 's#/usr/local/src/centreon-license-manager/##g' coverage.xml
-if [ "$BUILD" '=' 'RELEASE' ] ; then
-  sed -i -e 's/centreon-license-manager-20.04/centreon-license-manager-20.04-release/g' sonar-project.properties
-  sed -i -e 's/Centreon License Manager 20.04/Centreon License Manager 20.04 (release)/g' sonar-project.properties
-fi
-echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
+
+# environment values required to replace sonarQube project versioning and binding
+#   sonar.projectKey="{PROJECT_TITLE}"
+#   sonar.projectName="{PROJECT_NAME}"
+#   sonar.projectKey="{PROJECT_VERSION}"
+echo "BRANCH_NAME      -> $BRANCH_NAME"
+echo "PROJECT_TITLE    -> $PROJECT"
+echo "PROJECT_NAME     -> $PROJECT_NAME"
+echo "PROJECT_VERSION  -> $VERSION"
+sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
+sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
+sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
+
 sonar-scanner
