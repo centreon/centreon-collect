ea5128552d220fd39569789659b9ed1e69525814
sc979 <34628915+sc979@users.noreply.github.com>
2021-03-25 15:25:09 +0100

    enh(chore): integrate sonarQube dev edition on BAM (#456)

--- jobs/bam/19.10/des-bam-analysis.sh
+++ jobs/bam/19.10/des-bam-analysis.sh
@@ -9,6 +9,7 @@ set -x
 
 # Project.
 PROJECT=centreon-bam-server
+PROJECT_NAME="Centreon Bam Server"
 
 # Retrieve copy of git repository.
 curl -o "$PROJECT-git.tar.gz" "http://srvi-repo.int.centreon.com/sources/internal/bam/$PROJECT-$VERSION-$RELEASE/$PROJECT-git.tar.gz"
@@ -20,9 +21,17 @@ cd "$PROJECT"
 cp ../ut-be.xml .
 cp ../coverage-be.xml .
 sed -i -e 's#/usr/local/src/centreon-bam-server/##g' coverage-be.xml
-if [ "$BUILD" '=' 'RELEASE' ] ; then
-  sed -i -e 's/centreon-bam-19.10/centreon-bam-19.10-release/g' sonar-project.properties
-  sed -i -e 's/Centreon BAM 19.10/Centreon BAM 19.10 (release)/g' sonar-project.properties
-fi
-echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
+
+# environment values required to replace sonarQube project versioning and binding
+#   sonar.projectKey="{PROJECT_TITLE}"
+#   sonar.projectName="{PROJECT_NAME}"
+#   sonar.projectKey="{PROJECT_VERSION}"
+echo "BRANCH_NAME      -> $BRANCH_NAME"
+echo "PROJECT_TITLE    -> $PROJECT"
+echo "PROJECT_NAME     -> $PROJECT_NAME"
+echo "PROJECT_VERSION  -> $VERSION"
+sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
+sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
+sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
+
 sonar-scanner
