7d6d0b048dc3535784c63e46bc27bf740ad15d31
Kevin Duret <kduret@centreon.com>
2021-03-29 13:14:10 +0200

    chore(packaging): build centreon-plugins-sudoers rpm (#466)
    Refs: MON-4647

---
+++ centreon-build/jobs/plugins-sudoers/plugins-sudoers-stable.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=plugins-sudoers
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Move RPMs to the stable repository.
+promote_testing_rpms_to_stable "standard" "19.10" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "20.04" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "20.10" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "20.10" "el8" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "21.04" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "21.04" "el8" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE"
---
+++ centreon-build/jobs/plugins-sudoers/plugins-sudoers-testing.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../common.sh
+
+# Project.
+PROJECT=plugins-sudoers
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Pull mon-build-dependencies containers.
+BUILD_CENTOS7=registry.centreon.com/mon-build-dependencies-21.04:centos7
+docker pull "$BUILD_CENTOS7"
+BUILD_CENTOS8=registry.centreon.com/mon-build-dependencies-21.04:centos8
+docker pull "$BUILD_CENTOS8"
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+cp -r `dirname $0`/../../packaging/plugins-sudoers input
+rm -rf output-centos7
+mkdir output-centos7
+rm -rf output-centos8
+mkdir output-centos8
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_CENTOS7" input output-centos7
+docker-rpm-builder dir --sign-with `dirname $0`/../ces.key "$BUILD_CENTOS8" input output-centos8
+
+# Copy files to server.
+put_testing_rpms "standard" "19.10" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+put_testing_rpms "standard" "20.04" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+put_testing_rpms "standard" "20.10" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+put_testing_rpms "standard" "20.10" "el8" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE" output-centos8/noarch/*.rpm
+put_testing_rpms "standard" "21.04" "el7" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+put_testing_rpms "standard" "21.04" "el8" "noarch" "plugins-sudoers" "$PROJECT-$VERSION-$RELEASE" output-centos8/noarch/*.rpm
