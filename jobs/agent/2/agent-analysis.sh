b23661a78e61013ab978aacdb561e77c02b37a31
sc979 <34628915+sc979@users.noreply.github.com>
2021-03-30 11:36:01 +0200

    enh(chore): integrate sonarQubeDev on agent (#469)

--- jobs/agent/2/agent-analysis.sh
+++ jobs/agent/2/agent-analysis.sh
@@ -9,12 +9,21 @@ set -x
 
 # Project.
 PROJECT=centreon-agent
+PROJECT_NAME="Centreon Agent"
 
 # Copy reports and run analysis.
 cd "$PROJECT"
-if [ "$BUILD" '=' 'RELEASE' ] ; then
-  sed -i -e 's/centreon-agent/centreon-agent-release/g' sonar-project.properties
-  sed -i -e 's/Centreon Agent/Centreon Agent (release)/g' sonar-project.properties
-fi
-echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
+
+# environment values required to replace sonarQube project versioning and binding
+#   sonar.projectKey="{PROJECT_TITLE}"
+#   sonar.projectName="{PROJECT_NAME}"
+#   sonar.projectKey="{PROJECT_VERSION}"
+echo "BRANCH_NAME      -> $BRANCH_NAME"
+echo "PROJECT_TITLE    -> $PROJECT"
+echo "PROJECT_NAME     -> $PROJECT_NAME"
+echo "PROJECT_VERSION  -> $VERSION"
+sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
+sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
+sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
+
 sonar-scanner
