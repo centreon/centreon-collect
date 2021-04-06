ca05030a220afde1f98052d31eeb8aef0a2abafc
Kevin Duret <kduret@centreon.com>
2021-04-06 14:49:24 +0200

    chore(packaging): build perl-statistics-regression RPM (#483)
    Refs: DEVOPS-264

---
+++ jobs/dependencies/perl-statistics-regression/perl-statistics-regression-stable.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=perl-Statistics-Regression
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Move RPMs to the stable repository.
+promote_testing_rpms_to_stable "standard" "20.10" "el7" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "20.10" "el8" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "21.04" "el7" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "21.04" "el8" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE"
---
+++ jobs/dependencies/perl-statistics-regression/perl-statistics-regression-testing.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. `dirname $0`/../../common.sh
+
+# Project.
+PROJECT=perl-Statistics-Regression
+
+# Check arguments.
+if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
+  echo "You need to specify VERSION and RELEASE environment variables."
+  exit 1
+fi
+
+# Pull mon-build-dependencies containers.
+BUILD_CENTOS7=registry.centreon.com/mon-build-dependencies-21.04:centos7
+docker pull "$BUILD_CENTOS7"
+BUILD_CENTOS8=registry.centreon.com/mon-build-dependencies-21.04:centos8
+docker pull "$BUILD_CENTOS8"
+
+# Create input and output directories for docker-rpm-builder.
+rm -rf input
+cp -r `dirname $0`/../../../packaging/dependencies/perl-Statistics-Regression input
+rm -rf output-centos7
+mkdir output-centos7
+rm -rf output-centos8
+mkdir output-centos8
+
+# Get source.
+curl -o "input/Statistics-Regression-$VERSION.tar.gz" "https://cpan.metacpan.org/authors/id/I/IA/IAWELCH/Statistics-Regression-$VERSION.tar.gz"
+
+# Build RPMs.
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_CENTOS7" input output-centos7
+docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key "$BUILD_CENTOS8" input output-centos8
+
+# Copy files to server.
+put_testing_rpms "standard" "20.10" "el7" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+put_testing_rpms "standard" "20.10" "el8" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE" output-centos8/noarch/*.rpm
+put_testing_rpms "standard" "21.04" "el7" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE" output-centos7/noarch/*.rpm
+put_testing_rpms "standard" "21.04" "el8" "noarch" "$PROJECT" "$PROJECT-$VERSION-$RELEASE" output-centos8/noarch/*.rpm
