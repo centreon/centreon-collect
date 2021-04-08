af16ff8632ac251b5b0de6f088fa53c3182b0d28
Thomas Arnaud <38663853+Nohzoh@users.noreply.github.com>
2021-04-08 09:05:43 +0200

    Devops 261: publish and release pulgin packs to ppd mw (#486)
    Publish PP versions in the preprod middleware and set them to released.

--- centreon-build/jobs/packs/packs-testing.sh
+++ centreon-build/jobs/packs/packs-testing.sh
@@ -19,13 +19,16 @@ SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
 $SSH_REPO mv "/srv/sources/internal/packs/$PROJECT-$VERSION-$RELEASE" "/srv/sources/plugin-packs/testing/packs/"
 
 # Retrieve sources.
-curl -o "$PROJECT-$VERSION.tar.gz" "http://srvi-repo.int.centreon.com/sources/plugin-packs/testing/packs/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
+curl -s -o "$PROJECT-$VERSION.tar.gz" "http://srvi-repo.int.centreon.com/sources/plugin-packs/testing/packs/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz"
 rm -rf "$PROJECT-$VERSION"
 tar xzf "$PROJECT-$VERSION.tar.gz"
 
 # Upload packs to the middleware.
 MIDDLEWARE='https://api.imp.centreon.com/api'
-TOKEN=`curl -H "Content-Type: application/json" -X POST -d '{ "name": "batchimport", "token": "b46567488e4140d921b76cc063678af3dfeace77" }' "$MIDDLEWARE/auth/application" | python -c "import sys, json; print json.load(sys.stdin)['token']"`
+TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d '{ "name": "batchimport", "token": "b46567488e4140d921b76cc063678af3dfeace77" }' "$MIDDLEWARE/auth/application" | python -c "import sys, json; print json.load(sys.stdin)['token']"`
+PPDMIDDLEWARE='https://ppd-api.imp.centreon.com/api'
+PPDTOKEN=`curl -s -H "Content-Type: application/json" -X POST -d '{ "name": "batchimport", "token": "3d3e1700e611b1c7113694c14c51126bcb1f2604" }' "$PPDMIDDLEWARE/auth/application" | python -c "import sys, json; print json.load(sys.stdin)['token']"`
+PPOUTPUT=$(mktemp)
 cd "$PROJECT-$VERSION"
 for json in *.json ; do
   echo -n '{"data": {"type": "pluginpack", "attributes": { "slug": "' > ../query.json
@@ -34,7 +37,28 @@ for json in *.json ; do
   echo '", "information": ' >> ../query.json
   cat < "$json" >> ../query.json
   echo '}}}' >> ../query.json
-  curl -H "Content-Type: application/json" -H "centreon-imp-token: $TOKEN" -X POST -d '@-' "$MIDDLEWARE/pluginpack/pluginpack" < ../query.json
+  curl -s -H "Content-Type: application/json" -H "centreon-imp-token: $TOKEN" -X POST -d '@-' "$MIDDLEWARE/pluginpack/pluginpack" < ../query.json > ${PPOUTPUT}
+  ppId=$(cat ${PPOUTPUT} | python -c "import sys, json; print json.load(sys.stdin)['data']['id']")
+  curl -s -X PATCH "$MIDDLEWARE/pluginpack/pluginpack/$ppId/catalog" \
+    -d "{\"data\":{\"id\":$ppId,\"type\":\"pluginpack\",\"attributes\":{\"catalog_level\":4}}}" \
+    -H "centreon-imp-token: $TOKEN" \
+    -H "content-type: application/json"
+
+  #ppd
+  curl -s -H "Content-Type: application/json" -H "centreon-imp-token: $PPDTOKEN" -X POST -d '@-' "$PPDMIDDLEWARE/pluginpack/pluginpack" < ../query.json > ${PPOUTPUT}
+  ppId=$(cat ${PPOUTPUT} | python -c "import sys, json; print json.load(sys.stdin)['data']['id']")
+  ppVersion=$(cat ${PPOUTPUT} | python -c "import sys, json; print json.load(sys.stdin)['data']['attributes']['version']")
+  echo "Plugin pack id:${ppId} version:${ppVersion}"
+
+  curl -s -X PATCH "$PPDMIDDLEWARE/pluginpack/pluginpack/$ppId/catalog" \
+    -d "{\"data\":{\"id\":$ppId,\"type\":\"pluginpack\",\"attributes\":{\"catalog_level\":4}}}" \
+    -H "centreon-imp-token: $PPDTOKEN" \
+    -H "content-type: application/json"
+
+  curl -s -X PATCH "$PPDMIDDLEWARE/pluginpack/pluginpack/$ppId/version/$ppVersion" \
+    -d "{\"data\":{\"id\":$ppId,\"type\":\"pluginpack\",\"attributes\":{\"released\":1}}}" \
+    -H "centreon-imp-token: $PPDTOKEN" \
+    -H "content-type: application/json"
 done
 cd ..
 
