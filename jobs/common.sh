c7fa4e5ed0e85ada9d5cced6edbed938449a949c
Adrien Morais <amorais@centreon.com>
2021-04-13 11:17:40 +0200

    fix(import): try to fix parsing of repositories

--- jobs/common.sh
+++ jobs/common.sh
@@ -111,6 +111,8 @@ put_testing_rpms () {
   while [ \! -f "$UPDATEREPODIR/updaterepo.sh" ] ; do
     UPDATEREPODIR="$UPDATEREPODIR/.."
   done
+  oIFS=$IFS
+  IFS=" "
   for TARGETPROJECT in $TARGETPROJECTS ; do
     DIR="/srv/yum/$TARGETPROJECT/$SERIE/$OS/testing/$ARCH/$PRODUCT"
     NEWDIR="$GROUP"
@@ -123,6 +125,7 @@ put_testing_rpms () {
     ssh "$REPO_CREDS" sh $DESTFILE $REPO
     $UPDATEREPODIR/sync-repo.sh --project $REPOROOT --path "$REPOSUBDIR" --confirm
   done
+  IFS=$oIFS
 }
 
 put_internal_debs () {
