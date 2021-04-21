9c88f6e1ecf6b2d3ebb564614a5264e88edbab9c
Kevin Duret <kduret@centreon.com>
2021-04-21 09:24:39 +0200

    Revert "chore(release): temporarily disable connector el7 stable release"
    This reverts commit ca74d6ec89b86e7c5e374088a7a27694afa5dfb2.

--- jobs/connector/21.04/connector-stable.sh
+++ jobs/connector/21.04/connector-stable.sh
@@ -16,11 +16,11 @@ fi
 
 # Move sources to the stable directory.
 SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
-#$SSH_REPO mv "/srv/sources/standard/testing/connector/$PROJECT-$VERSION-$RELEASE" "/srv/sources/standard/stable/"
+$SSH_REPO mv "/srv/sources/standard/testing/connector/$PROJECT-$VERSION-$RELEASE" "/srv/sources/standard/stable/"
 
 # Put sources online.
 upload_tarball_for_download centreon-connectors "$VERSION" "/srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/centreon-connectors-$VERSION.tar.gz" "s3://centreon-download/public/centreon-connectors/centreon-connectors-$VERSION.tar.gz"
 
 # Move RPMs to the stable repository.
-#promote_testing_rpms_to_stable "standard" "21.04" "el7" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE"
+promote_testing_rpms_to_stable "standard" "21.04" "el7" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE"
 promote_testing_rpms_to_stable "standard" "21.04" "el8" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE"
