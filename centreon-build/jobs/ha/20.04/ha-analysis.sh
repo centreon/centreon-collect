19db69bbe7090dac9b88fa9c2ba2438b98d4a869
sc979 <34628915+sc979@users.noreply.github.com>
2021-04-23 10:29:07 +0200

    enh(chore): integrate sonarQube dev edition on HA (#496)

--- centreon-build/jobs/ha/20.04/ha-analysis.sh
+++ centreon-build/jobs/ha/20.04/ha-analysis.sh
@@ -9,6 +9,7 @@ set -x
 
 # Project.
 PROJECT=centreon-ha
+PROJECT_NAME="Centreon High Availability"
 
 # Retrieve copy of git repository.
 curl -o "$PROJECT-git.tar.gz" "http://srvi-repo.int.centreon.com/sources/internal/ha/$PROJECT-$VERSION-$RELEASE/$PROJECT-git.tar.gz"
@@ -17,9 +18,17 @@ tar xzf "$PROJECT-git.tar.gz"
 
 # Copy reports and run analysis.
 cd "$PROJECT"
-if [ "$BUILD" '=' 'RELEASE' ] ; then
-  sed -i -e 's/centreon-ha-20.04/centreon-ha-20.04-release/g' sonar-project.properties
-  sed -i -e 's/Centreon HA 20.04/Centreon HA 20.04 (release)/g' sonar-project.properties
-fi
-echo "sonar.projectVersion=$VERSION" >> sonar-project.properties
+
+# environment values required to replace sonarQube project versioning and binding
+#   sonar.projectKey="{PROJECT_TITLE}"
+#   sonar.projectName="{PROJECT_NAME}"
+#   sonar.projectKey="{PROJECT_VERSION}"
+echo "BRANCH_NAME      -> $BRANCH_NAME"
+echo "PROJECT_TITLE    -> $PROJECT"
+echo "PROJECT_NAME     -> $PROJECT_NAME"
+echo "PROJECT_VERSION  -> $VERSION"
+sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
+sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
+sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
+
 sonar-scanner
