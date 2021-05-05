000ca6549b0e5bc8e1e698cd620a0a9711b4b949
David Boucher <dboucher@centreon.com>
2021-05-05 14:41:08 +0200

    fix(conan): builds for 21.04 should be functional again.

--- containers/unittest/21.04/install.debian10.sh
+++ containers/unittest/21.04/install.debian10.sh
@@ -20,5 +20,25 @@ chmod +x /usr/local/bin/composer
 
 # Install Conan, a C++ package manager.
 pip3 install conan
-conan remote add centreon-center https://api.bintray.com/conan/centreon/centreon
-conan remote remove conan-center
+
+mkdir /tmp/conan-pkgs
+cat <<EOF >/tmp/conan-pkgs/conanfile.txt
+[requires]
+gtest/cci.20210126
+asio/1.18.1
+fmt/7.1.3
+spdlog/1.8.5
+nlohmann_json/3.9.1
+openssl/1.1.1k
+protobuf/3.15.5
+grpc/1.37.0
+mariadb-connector-c/3.1.12
+zlib/1.2.11
+
+[generators]
+cmake_paths
+cmake_find_package
+EOF
+
+conan install /tmp/conan-pkgs -s compiler.libcxx=libstdc++11 --build=missing
+rm -rf /tmp/conan-pkgs
