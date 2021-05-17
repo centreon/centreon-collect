2010d0f1eca67952c4daa69c94c639c61e8c944a
Squalli Karim <karim.squalli@outlook.fr>
2021-05-17 14:12:33 +0000

    add test start many instance broker and stop them

--- centreon-tests/src/centreon-broker/start-stop.test.ts
+++ centreon-tests/src/centreon-broker/start-stop.test.ts
@@ -1,6 +1,6 @@
 import sleep from 'await-sleep';
 import shell from 'shelljs';
-
+import { once } from 'events'
 shell.config.silent = true;
 
 const broker = {
@@ -78,7 +78,35 @@ describe('broker testing', () => {
 
     const isBrokerRunning = await broker.isRunning();
     expect(isBrokerRunning).toBeFalsy();
-  }, 50000);
+  }, 30000);
 
+  it('start and stop many instances broker with .3 seconds interval', async () => {
 
+    for(let i = 0; i < 10; ++i) {
+      const process = shell.exec(`su centreon-broker -c '/usr/sbin/cbd /etc/centreon-broker/central-broker.json'`, {async: true})
+      await sleep(300)
+      process.kill('SIGINT')
+      await once(process, 'exit');  
+    }
+
+    const coreDumpResult = shell.exec('coredumpctl');
+    expect(coreDumpResult.code).toBe(1);
+    expect(coreDumpResult.stderr?.toLocaleLowerCase()).toContain('no coredumps found')
+    expect(0).toBe(0)
+  }, 60000)
+
+  it('start and stop many instances broker with 1 second interval', async () => {
+
+    for(let i = 0; i < 10; ++i) {
+      const process = shell.exec(`su centreon-broker -c '/usr/sbin/cbd /etc/centreon-broker/central-broker.json'`, {async: true})
+      await sleep(1000)
+      process.kill('SIGINT')
+      await once(process, 'exit');  
+    }
+
+    const coreDumpResult = shell.exec('coredumpctl');
+    expect(coreDumpResult.code).toBe(1);
+    expect(coreDumpResult.stderr?.toLocaleLowerCase()).toContain('no coredumps found')
+    expect(0).toBe(0)
+  }, 60000)
 });
