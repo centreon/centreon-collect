77f8d4426e7407740fa494e8be038206f0556494
Squalli Karim <karim.squalli@outlook.fr>
2021-05-20 21:39:36 +0000

    add centreon rdd to broker

--- centreon-tests/src/core/broker.ts
+++ centreon-tests/src/core/broker.ts
@@ -7,50 +7,49 @@ import  { once } from 'process'
 
 export class Broker {
     private process: any
+    private rddProcess: any
     private config: JSON;
 
+    static CENTREON_BROKER_UID = parseInt(shell.exec('id -u centreon-broker'))
     constructor() {
 
     }
 
     async start() {
-        this.process = shell.exec(`sh -c '/usr/sbin/cbd /etc/centreon-broker/central-broker.json'`, {async: true})
+        this.process = shell.exec(`usr/sbin/cbd /etc/centreon-broker/central-broker.json`, {async: true, uid: Broker.CENTREON_BROKER_UID})
+        this.rddProcess = shell.exec(`/usr/sbin/cbd /etc/centreon-broker/central-rrd.json`, {async: true, uid: Broker.CENTREON_BROKER_UID})
+
     }
 
 
     async stop() {
-        
         if(await this.isRunning(true, 5)) {
             this.process.kill()
-            await once('exit', this.process);
         }
     }
 
 
     async  isRunning(expected: boolean = true, seconds: number = 15) : Promise<boolean> {
         let centreonBrokerProcess;
-        let centreonBrokerRddProcesss;
-  
+        let centreonRddProcess;
+
         for(let i = 0; i < seconds * 2; ++i) {
     
           const processList = await psList();
     
-          centreonBrokerProcess = processList.find((process) => process.name == `cbd` 
-            && process.ppid == this.process.pid 
-            && process.cmd === `/usr/sbin/cbd /etc/centreon-broker/central-broker.json`);
-
-          centreonBrokerRddProcesss = processList.find((process) => process.name == `cbd` && process.cmd === `/usr/sbin/cbd /etc/centreon-broker/central-rrd.json`);
-    
-          if((centreonBrokerProcess && centreonBrokerRddProcesss) && expected)
+          centreonBrokerProcess = processList.find((process) => process.pid == this.process.pid);
+          centreonRddProcess = processList.find((process) => process.pid == this.rddProcess.pid);
+          
+          if(centreonBrokerProcess  && expected)
             return true;
     
-          if(expected == false && !centreonBrokerProcess && !centreonBrokerRddProcesss)
+          if(expected == false && !centreonBrokerProcess )
             return false;
 
           
           await sleep(500)
         }
-        return !!centreonBrokerProcess && !!centreonBrokerRddProcesss;
+        return !!centreonBrokerProcess;
     }
 
     static clearLogs() {
