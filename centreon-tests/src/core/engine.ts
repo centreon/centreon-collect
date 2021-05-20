304236a4334fe3e2b763678a03dcee430206a2db
Squalli Karim <karim.squalli@outlook.fr>
2021-05-20 22:48:49 +0000

    add more tests

--- centreon-tests/src/core/engine.ts
+++ centreon-tests/src/core/engine.ts
@@ -7,43 +7,42 @@ import  { once } from 'process'
 export class Engine {
     private process: any
     private config: JSON;
+    static CENTREON_ENGINE_UID = parseInt(shell.exec('id -u centreon-engine'))
 
     constructor() {
 
     }
 
     async start() {
-        this.process = shell.exec(`su centreon-broker -c '/usr/sbin/centengine /etc/centreon-engine/centegine.cfg'`, {async: true})
+        this.process = shell.exec(`/usr/sbin/centengine /etc/centreon-engine/centengine.cfg`, {async: true, uid: Engine.CENTREON_ENGINE_UID})  
     }
 
 
     async stop() {
-        if(await this.isRunning(5, true)) {
+        if(await this.isRunning()) {
             this.process.kill()
-            await once('exit', this.process);
         }
     }
 
 
-    async isRunning(repeat: number = 15, expected: boolean = true) : Promise<boolean> {
-        let centreonBrokerProcess;
-        let centreonBrokerRddProcesss;
+    async  isRunning(expected: boolean = true, seconds: number = 15) : Promise<boolean> {
+      let centreonEngineProcess;   
+
+      for(let i = 0; i < seconds * 2; ++i) {
+  
+
+        const processList = await psList();
+          centreonEngineProcess = processList.find((process) => process.pid == this.process.pid);
     
-        for(let i = 0; i < repeat; ++i) {
-    
-          const processList = await psList();
-    
-          centreonBrokerProcess = processList.find((process) => process.name == `cbd` && process.cmd === `/usr/sbin/cbd /etc/centreon-broker/central-broker.json`);
-          centreonBrokerRddProcesss = processList.find((process) => process.name == `cbd` && process.cmd === `/usr/sbin/cbd /etc/centreon-broker/central-rrd.json`);
-    
-          if((centreonBrokerProcess && centreonBrokerRddProcesss) && expected)
+          if(centreonEngineProcess && expected)
             return true;
     
-          if(expected == false && !centreonBrokerProcess && !centreonBrokerRddProcesss)
+          else if(!centreonEngineProcess && !expect)
             return false;
 
-          await sleep(1000)
-        }
-        return !!centreonBrokerProcess && !!centreonBrokerRddProcesss;
-    }
+          await sleep(500)
+      }
+
+      return !!centreonEngineProcess;
+  }
 }
