b0ec9d4751224269e7876040d58e2f70e5269b38
Squalli Karim <karim.squalli@outlook.fr>
2021-05-20 21:39:36 +0000

    add centreon rdd to broker

--- src/centreon-broker/sql-config.test.ts
+++ src/centreon-broker/sql-config.test.ts
@@ -1,134 +1,169 @@
 import sleep from 'await-sleep';
 import shell from 'shelljs';
 import { once } from 'events'
-import { broker } from '../shared';
-import { getConfig, writeConfig } from '../config';
+import { Broker } from '../core/broker';
+import { getConfig, writeConfig } from '../core/config';
 import fs from 'fs/promises'
 shell.config.silent = true;
 
-
-const repeat = async (times: number, interval: number, cond: () => boolean) => {
-
-  let condResult;
-
-
-  return false;
-}
-
 beforeEach(async () => {
-  await broker.stop()
-  shell.rm('/var/log/centreon-broker/central-broker-master.log')
+  Broker.clearLogs()
 }, 30000)
 
+it('should deny access when database user password is wrong', async () => {
 
-afterAll(async () => {
+  const config = await getConfig()
+  const centrealBorkerMasterSql = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-sql'))
+  const centrealBorkerMasterperfData = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-perfdata'))
+  centrealBorkerMasterSql['db_password'] = "centreon1"
+  centrealBorkerMasterperfData['db_password'] = "centreon1"
+  await writeConfig(config)
+
+  const broker = new Broker();
+  await broker.start();
+
+  expect(await broker.isRunning()).toBeTruthy()
+
+  let logFile;
+  let logFileString;
+
+  for (let i = 0; i < 10; ++i) {
+    logFile = await fs.readFile('/var/log/centreon-broker/central-broker-master.log')
+    logFileString = logFile.toString()
+
+    if (logFileString.includes("[core] [error] failover: global error: mysql_connection: error while starting connection") && 
+    logFileString.includes("[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection") )
+      break;
+
+    await sleep(1000)
+  }
+
+  expect(logFileString).toContain(`[core] [error] failover: global error: mysql_connection: error while starting connection`);
+  expect(logFileString).toContain(`[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection`);
+
   await broker.stop()
-  shell.rm('/var/log/centreon-broker/central-broker-master.log')
-}, 30000)
+  expect(await broker.isRunning(false)).toBeFalsy();
 
-describe('Bad credentials', () => {
-  it('should deny access when database user password is wrong', async () => {
+  // reset config to default for vm user
+  centrealBorkerMasterSql['db_password'] = "centreon"
+  centrealBorkerMasterperfData['db_password'] = "centreon"
+  
+  await writeConfig(config)
 
+}, 60000);
 
-    const config = await getConfig()
+it('should deny access when database user password is wrong for storage', async () => {
 
-    const centrealBorkerMasterSql = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-sql'))
-    const centrealBorkerMasterperfData = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-perfdata'))
-
-    centrealBorkerMasterSql['db_password'] = "centreon1"
-    centrealBorkerMasterperfData['db_password'] = "centreon1"
-
-    await writeConfig(config)
-
-
-    await broker.start();
-
-    let logFile;
-    let logFileString;
-
-    for (let i = 0; i < 10; ++i) {
-      logFile = await fs.readFile('/var/log/centreon-broker/central-broker-master.log')
-      logFileString = logFile.toString()
-
-      if (logFileString.includes("[core] [error] failover: global error: mysql_connection: error while starting connection") && 
-      logFileString.includes("[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection") )
-        break;
-
-      await sleep(1000)
-    }
-
-    expect(logFileString).toContain(`[core] [error] failover: global error: mysql_connection: error while starting connection`);
-    expect(logFileString).toContain(`[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection`);
-
-    // reset config to default for vm user
-    centrealBorkerMasterSql['db_password'] = "centreon"
-    centrealBorkerMasterperfData['db_password'] = "centreon"
-
-    await writeConfig(config)
-
-  }, 30000);
-
-  it('should deny access when database user password is wrong for storage', async () => {
-
-    const config = await getConfig()
-    const centrealBorkerMasterperfData = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-perfdata'))
-    centrealBorkerMasterperfData['db_password'] = "centreon1"
-    await writeConfig(config)
+  const config = await getConfig()
+  const centrealBorkerMasterperfData = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-perfdata'))
+  centrealBorkerMasterperfData['db_password'] = "centreon1"
+  await writeConfig(config)
 
 
-    await broker.start();
+  const broker = new Broker();
+  await broker.start();
 
-    let logFile;
-    let logFileString;
+  expect(await broker.isRunning()).toBeTruthy()
 
-    for (let i = 0; i < 10; ++i) {
-      logFile = await fs.readFile('/var/log/centreon-broker/central-broker-master.log')
-      logFileString = logFile.toString()
+  let logFile;
+  let logFileString;
 
-      if (logFileString.includes("[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection"))
-        break;
+  for (let i = 0; i < 10; ++i) {
+    logFile = await fs.readFile('/var/log/centreon-broker/central-broker-master.log')
+    logFileString = logFile.toString()
 
-      await sleep(1000)
-    }
+    if (logFileString.includes("[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection"))
+      break;
 
-    expect(logFileString).toContain(`[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection`);
+    await sleep(1000)
+  }
 
-    // reset config to default for vm user
-    centrealBorkerMasterperfData['db_password'] = "centreon"
-    await writeConfig(config)
-
-  }, 30000);
+  expect(logFileString).toContain(`[sql] [error] storage: rebuilder: Unable to connect to the database: mysql_connection: error while starting connection`);
 
 
-  it('should deny access when database user password is wrong for sql', async () => {
-
-    const config = await getConfig()
-    const centrealBorkerMasterSql = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-sql'))
-    centrealBorkerMasterSql['db_password'] = "centreon1"
-    await writeConfig(config)
+  await broker.stop()
+  expect(await broker.isRunning(false)).toBeFalsy();
 
 
-    await broker.start();
+  // reset config to default for vm user
+  centrealBorkerMasterperfData['db_password'] = "centreon"
+  await writeConfig(config)
 
-    let logFile;
-    let logFileString;
+}, 30000);
 
-    for (let i = 0; i < 10; ++i) {
-      logFile = await fs.readFile('/var/log/centreon-broker/central-broker-master.log')
-      logFileString = logFile.toString()
+it('should deny access when database user password is wrong for sql', async () => {
 
-      if (logFileString.includes("[core] [error] failover: global error: mysql_connection: error while starting connection"))
-        break;
+  const config = await getConfig()
+  const centrealBorkerMasterSql = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-sql'))
+  centrealBorkerMasterSql['db_password'] = "centreon1"
+  await writeConfig(config)
 
-      await sleep(1000)
-    }
 
-    expect(logFileString).toContain(`[core] [error] failover: global error: mysql_connection: error while starting connection`);
+  const broker = new Broker();
+  await broker.start();
 
-    // reset config to default for vm user
-    centrealBorkerMasterSql['db_password'] = "centreon"
-    await writeConfig(config)
+  expect(await broker.isRunning()).toBeTruthy()
 
-  }, 30000);
-});
+  let logFile;
+  let logFileString;
+
+  for (let i = 0; i < 10; ++i) {
+    logFile = await fs.readFile('/var/log/centreon-broker/central-broker-master.log')
+    logFileString = logFile.toString()
+
+    if (logFileString.includes("[core] [error] failover: global error: mysql_connection: error while starting connection"))
+      break;
+
+    await sleep(1000)
+  }
+
+  expect(logFileString).toContain(`[core] [error] failover: global error: mysql_connection: error while starting connection`);
+
+  await broker.stop()
+  expect(await broker.isRunning(false)).toBeFalsy();
+
+  // reset config to default for vm user
+  centrealBorkerMasterSql['db_password'] = "centreon"
+  await writeConfig(config)
+
+}, 30000);
+
+
+it('should log error when databse name is not correct', async () => {
+  const config = await getConfig()
+  const centrealBorkerMasterSql = config['centreonBroker']['output'].find((output => output.name === 'central-broker-master-sql'))
+  centrealBorkerMasterSql['db_name'] = "centreon1"
+  await writeConfig(config)
+
+  const broker = new Broker();
+  await broker.start();
+
+  expect(await broker.isRunning()).toBeTruthy()
+
+  let logFile;
+  let logFileString;
+
+  for (let i = 0; i < 10; ++i) {
+    logFile = await fs.readFile('/var/log/centreon-broker/central-broker-master.log')
+    logFileString = logFile.toString()
+
+    if (logFileString.includes("[core] [error] failover: global error: mysql_connection: error while starting connection"))
+      break;
+
+    await sleep(1000)
+  }
+
+  expect(logFileString).toContain(`[core] [error] failover: global error: mysql_connection: error while starting connection`);
+
+  await broker.stop()
+  expect(await broker.isRunning(false)).toBeFalsy();
+
+  centrealBorkerMasterSql['db_name'] = "centreon"
+  await writeConfig(config)
+})
+
+
+it("should handle database service stop and start", () => {
+  shell.exec('service mysql stop')
+})
 
--- src/centreon-broker/start-stop.test.ts
+++ src/centreon-broker/start-stop.test.ts
@@ -7,14 +7,11 @@ shell.config.silent = true;
 
 describe('broker testing', () => {
 
-
-  beforeEach(async () => {
-  
-  }, 60000)
-
-  
-  it('start/stop centreon broker', async () => {
+  beforeEach(() => {
     Broker.clearLogs()
+  })
+
+  it('start/stop centreon broker', async () => {
     const broker = new Broker();
 
     await broker.start();
@@ -23,36 +20,22 @@ describe('broker testing', () => {
 
     await broker.stop()
 
-    expect(await broker.isRunning()).toBeTruthy();
+    expect(await broker.isRunning(false)).toBeFalsy();
   }, 60000);
 
-  it('start and stop many instances broker with .3 seconds interval', async () => {
+  it('start and stop many instances broker', async () => {
 
     for(let i = 0; i < 5; ++i) {
-      const process = shell.exec(`su centreon-broker -c '/usr/sbin/cbd /etc/centreon-broker/central-broker.json'`, {async: true})
-      await sleep(300)
-      process.kill()
-      await once(process, 'exit');  
+      const broker = new Broker();
+      await broker.start();
+      expect(await broker.isRunning()).toBeTruthy()
+      await broker.stop()
+      expect(await broker.isRunning(false)).toBeFalsy();
     }
 
     const coreDumpResult = shell.exec('coredumpctl');
     expect(coreDumpResult.code).toBe(1);
     expect(coreDumpResult.stderr?.toLocaleLowerCase()).toContain('no coredumps found')
     expect(0).toBe(0)
-  }, 60000)
-
-  it('start and stop many instances broker with 1 second interval', async () => {
-
-    for(let i = 0; i < 5; ++i) {
-      const process = shell.exec(`su  centreon-broker -c '/usr/sbin/cbd /etc/centreon-broker/central-broker.json'`, {async: true})
-      await sleep(1000)
-      process.kill()
-      await once(process, 'exit');  
-    }
-
-    const coreDumpResult = shell.exec('coredumpctl');
-    expect(coreDumpResult.code).toBe(1);
-    expect(coreDumpResult.stderr?.toLocaleLowerCase()).toContain('no coredumps found')
-    expect(0).toBe(0)
-  }, 60000)
+  }, 120000)
 });
