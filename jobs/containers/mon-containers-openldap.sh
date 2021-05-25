1769488c51c829d884c543c37beb32c63b1325af
Kevin Duret <kduret@centreon.com>
2021-05-25 10:17:16 +0200

    chore(container): build openldap container with lot of users (#526)
    Refs: MON-6260

--- jobs/containers/mon-containers-openldap.sh
+++ jobs/containers/mon-containers-openldap.sh
@@ -6,9 +6,23 @@ set -x
 # Pull base image.
 docker pull osixia/openldap:latest
 
-# Build image.
+# Build light image
 cd centreon-build/containers
 docker build -t registry.centreon.com/mon-openldap:latest -f openldap/openldap.Dockerfile .
 
-# Push image
+# Push light image
 docker push registry.centreon.com/mon-openldap:latest
+
+# Build image with dataset (lot of users)
+cd openldap/bootstrap/ldif
+max=1000
+for i in `seq 6 $max`
+do
+    cat 07-user.ldif.in | sed "s/@NAME@/centreon-ldap$i/g" | sed "s/@UID@/$i/g" >> 07-user.ldif
+done
+
+cd ../../..
+docker build -t registry.centreon.com/mon-openldap-dataset:latest -f openldap/openldap.Dockerfile .
+
+# Push image with dataset
+docker push registry.centreon.com/mon-openldap-dataset:latest
