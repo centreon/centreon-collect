ac417d551c7251ea5b8a7cc6e623d474d6fa3ee3
David Boucher <dboucher@centreon.com>
2021-06-01 20:57:55 +0200

    fix(sql): sql tests improved
    Be careful, two of them still fail.

--- centreon-tests/src/centreon-broker/start-stop.test.ts
+++ centreon-tests/src/centreon-broker/start-stop.test.ts
@@ -24,9 +24,8 @@ describe('broker testing', () => {
     }, 60000);
 
     it('repeat 10 times start/stop broker with .3sec interval => no coredump', async () => {
+        const broker = new Broker();
         for (let i = 0; i < 10; ++i) {
-            const broker = new Broker();
-
             const isStarted = await broker.start();
             expect(isStarted).toBeTruthy()
 
@@ -35,22 +34,15 @@ describe('broker testing', () => {
             const isStopped = await broker.stop()
             expect(isStopped).toBeTruthy();
 
-            //const coreDumpResult = shell.exec('coredumpctl');
-            //expect(coreDumpResult.code).toBe(1);
-            //expect(coreDumpResult.stderr?.toLocaleLowerCase()).toContain('no coredumps found')
-            //expect(0).toBe(0)
-
-            // TODO: should not contain any error
-            // expect(await Broker.getLogs()).not.toContain("error")
+            expect(await broker.checkCoredump()).toBeFalsy()
         }
-        expect(await broker.checkCoredump()).toBeFalsy()
     }, 240000)
 
 
     it('repeat 10 times start/stop broker with 1sec interval => no coredump', async () => {
 
+        const broker = new Broker();
         for (let i = 0; i < 10; ++i) {
-            const broker = new Broker();
 
             const isStarted = await broker.start();
             expect(isStarted).toBeTruthy()
@@ -59,7 +51,8 @@ describe('broker testing', () => {
 
             const isStopped = await broker.stop()
             expect(isStopped).toBeTruthy();
+
+            expect(await broker.checkCoredump()).toBeFalsy()
         }
-        expect(await broker.checkCoredump()).toBeFalsy()
     }, 240000)
 });
