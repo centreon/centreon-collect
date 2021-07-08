951c54ec86d199ef156dd0626debef8fc6c02627
centreonpm <pmargale@centreon.com>
2021-07-08 16:06:40 +0100

    enh(tests) : fix bug on tls test

--- src/centreon-broker-engine/communication-with-compression.test.ts
+++ src/centreon-broker-engine/communication-with-compression.test.ts
@@ -30,53 +30,53 @@ describe('engine and broker testing in same time for compression', () => {
         const broker = new Broker()
         const engine = new Engine()
 
-	const compression = ["yes", "no", "auto"]
+        const compression = ["yes", "no", "auto"]
         const errors = [
-          // yes, yes
-	  [
-           "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-          ], 
-          // yes, no 
-          [
-           "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS compression\'" // central side
-          ], 
-          // yes, auto 
-	  [
-           "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-          ], 
-          // no, yes
-          [
-           "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'compression\'", 
-           "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS\'"
-          ],
-          // no, no
-          [
-           "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS\'" // central side
-          ], 
-          // no, auto
-	  [
-           "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'compression\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS\'" // central side
-          ],
-          // auto, yes
-          [
-           "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-          ],
-          // auto, no
-          [
-           "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS compression\'" // central side
-          ], 
-          // auto, auto
-          [
-           "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-           "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-          ],
+            // yes, yes
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
+            ],
+            // yes, no 
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS compression\'" // central side
+            ],
+            // yes, auto 
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
+            ],
+            // no, yes
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'compression\'",
+                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS\'"
+            ],
+            // no, no
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS\'" // central side
+            ],
+            // no, auto
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'compression\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS\'" // central side
+            ],
+            // auto, yes
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
+            ],
+            // auto, no
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS compression\'" // central side
+            ],
+            // auto, auto
+            [
+                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
+                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
+            ],
         ]
 
         const config_broker = await Broker.getConfig()
@@ -92,36 +92,36 @@ describe('engine and broker testing in same time for compression', () => {
         centralBrokerLoggers['core'] = "trace"
 
         const centralModuleMaster = config_module['centreonBroker']['output'].find((
-              output => output.name === 'central-module-master-output'))
-const centralBrokerMaster = config_broker['centreonBroker']['input'].find((
-              input => input.name === 'central-broker-master-input'))
+            output => output.name === 'central-module-master-output'))
+        const centralBrokerMaster = config_broker['centreonBroker']['input'].find((
+            input => input.name === 'central-broker-master-input'))
 
-	
-        for (let i = 0, k = 0; i < 3; ++i, k = i*3) {
-          for (let j = 0; j < 3; ++j) { 
-            Broker.clearLogs()
-            Broker.clearLogsCentralModule()
-            centralBrokerMaster['compression'] = compression[i] 
-            centralModuleMaster['compression'] = compression[j] 
 
-            await Broker.writeConfigCentralModule(config_module)
-            await Broker.writeConfig(config_broker)
+        for (let i = 0, k = 0; i < 3; ++i, k = i * 3) {
+            for (let j = 0; j < 3; ++j) {
+                Broker.clearLogs()
+                Broker.clearLogsCentralModule()
+                centralBrokerMaster['compression'] = compression[i]
+                centralModuleMaster['compression'] = compression[j]
 
-            console.log(centralBrokerMaster)
-            console.log(centralModuleMaster)
+                await Broker.writeConfigCentralModule(config_module)
+                await Broker.writeConfig(config_broker)
 
-            expect(await broker.start()).toBeTruthy()
-            expect(await engine.start()).toBeTruthy()
+                console.log(centralBrokerMaster)
+                console.log(centralModuleMaster)
 
-            expect(await isBrokerAndEngineConnected()).toBeTruthy()
+                expect(await broker.start()).toBeTruthy()
+                expect(await engine.start()).toBeTruthy()
 
-            console.log("index = ", k+j)
-            expect(await Broker.checkLogFileContains([errors[k+j][0]])).toBeTruthy()
-            expect(await Broker.checkLogFileCentralModuleContains([errors[k+j][1]])).toBeTruthy()
+                expect(await isBrokerAndEngineConnected()).toBeTruthy()
 
-            expect(await broker.stop()).toBeTruthy();
-            expect(await engine.stop()).toBeTruthy();
-          }
+                console.log("index = ", k + j)
+                expect(await Broker.checkLogFileContains([errors[k + j][0]])).toBeTruthy()
+                expect(await Broker.checkLogFileCentralModuleContains([errors[k + j][1]])).toBeTruthy()
+
+                expect(await broker.stop()).toBeTruthy();
+                expect(await engine.stop()).toBeTruthy();
+            }
         }
     }, 400000);
 });
--- src/centreon-broker-engine/communication-with-tls.test.ts
+++ src/centreon-broker-engine/communication-with-tls.test.ts
@@ -6,6 +6,7 @@ import { Engine } from '../core/engine';
 import { isBrokerAndEngineConnected } from '../core/brokerEngine';
 import { broker } from 'shared';
 
+const util = require('util')
 const process = require('process')
 
 // shell.config.silent = true;
@@ -29,165 +30,180 @@ describe('engine and broker testing in same time for compression', () => {
     })
 
     it('tls without keys checks between broker - engine', async () => {
-      const broker = new Broker()
-      const engine = new Engine()
+        const broker = new Broker()
+        const engine = new Engine()
 
-      const tls = ["yes", "no", "auto"]
-      const errors = [
-        // yes, yes
-        [
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
-        ],
-        // yes, no 
-        [
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''",
-          "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'"
-        ],
-        // yes, auto
-        [
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
-        ],
-        // no, yes
-        [
-          "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'",
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''"
-        ],
-        // no, no
-        [
-          "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'\'",
-          "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'\'"
-        ],
+        const tls = ["yes", "no", "auto"]
+        const errors = [
+            // yes, yes
+            [
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
+            ],
+            // yes, no 
+            [
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''",
+                "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'"
+            ],
+            // yes, auto
+            [
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
+            ],
+            // no, yes
+            [
+                "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'",
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''"
+            ],
+            // no, no
+            [
+                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'\'",
+                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'\'"
+            ],
 
-        // no, auto 
-        [
-          "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'",
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''"
-        ],
-        // auto, yes 
-        [
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
-        ],
-        // auto, no 
-        [
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''",
-          "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'"
-        ],
-        // auto, auto 
-        [
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
-          "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
+            // no, auto 
+            [
+                "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'",
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''"
+            ],
+            // auto, yes 
+            [
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
+            ],
+            // auto, no 
+            [
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has ''",
+                "[bbdo] [info] BBDO: we have extensions '' and peer has 'TLS'"
+            ],
+            // auto, auto 
+            [
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'",
+                "[bbdo] [info] BBDO: we have extensions 'TLS' and peer has 'TLS'"
+            ]
         ]
-      ]
 
-      const config_broker = await Broker.getConfig()
-      const config_module = await Broker.getConfigCentralModule()
+        const config_broker = await Broker.getConfig()
+        const config_module = await Broker.getConfigCentralModule()
 
-      const centralModuleLoggers = config_module['centreonBroker']['log']['loggers']
-      const centralBrokerLoggers = config_broker['centreonBroker']['log']['loggers']
+        const centralModuleLoggers = config_module['centreonBroker']['log']['loggers']
+        const centralBrokerLoggers = config_broker['centreonBroker']['log']['loggers']
 
-      centralModuleLoggers['bbdo'] = "info"
-      centralModuleLoggers['core'] = "trace"
+        centralModuleLoggers['bbdo'] = "info"
+        centralModuleLoggers['core'] = "trace"
 
-      centralBrokerLoggers['bbdo'] = "info"
-      centralBrokerLoggers['core'] = "trace"
+        centralBrokerLoggers['bbdo'] = "info"
+        centralBrokerLoggers['core'] = "trace"
 
-      const centralModuleMaster = config_module['centreonBroker']['output'].find((
-              output => output.name === 'central-module-master-output'))
-      const centralBrokerMaster = config_broker['centreonBroker']['input'].find((
-              input => input.name === 'central-broker-master-input'))
+        const centralModuleMaster = config_module['centreonBroker']['output'].find((
+            output => output.name === 'central-module-master-output'))
+        const centralBrokerMaster = config_broker['centreonBroker']['input'].find((
+            input => input.name === 'central-broker-master-input'))
 
-      for (let i = 0, k = 0; i < 3; ++i, k = i*3) {
-          for (let j = 0; j < 3; ++j) { 
-            Broker.clearLogs()
-            Broker.clearLogsCentralModule()
-            centralBrokerMaster['tls'] = tls[i] 
-            centralModuleMaster['tls'] = tls[j] 
+        for (let i = 0, k = 0; i < 3; ++i, k = i * 3) {
+            for (let j = 0; j < 3; ++j) {
+                Broker.clearLogs()
+                Broker.clearLogsCentralModule()
+                centralBrokerMaster['tls'] = tls[i]
+                centralModuleMaster['tls'] = tls[j]
 
-            await Broker.writeConfigCentralModule(config_module)
-            await Broker.writeConfig(config_broker)
+                await Broker.writeConfigCentralModule(config_module)
+                await Broker.writeConfig(config_broker)
 
-            console.log(centralBrokerMaster)
-            console.log(centralModuleMaster)
+                console.log(centralBrokerMaster)
+                console.log(centralModuleMaster)
 
-            expect(await broker.start()).toBeTruthy()
-            expect(await engine.start()).toBeTruthy()
+                expect(await broker.start()).toBeTruthy()
+                expect(await engine.start()).toBeTruthy()
 
-            expect(await isBrokerAndEngineConnected()).toBeTruthy()
-            
-            console.log("index = ", k+j)
-            expect(await Broker.checkLogFileContains([errors[k+j][0]])).toBeTruthy()
-            expect(await Broker.checkLogFileCentralModuleContains([errors[k+j][1]])).toBeTruthy()
+                expect(await isBrokerAndEngineConnected()).toBeTruthy()
+                await shell.exec("cat /var/log/centreon-broker/central-broker-master.log")
 
-            expect(await broker.stop()).toBeTruthy();
-            expect(await engine.stop()).toBeTruthy();
-          }
+                console.log("index = ", k + j)
+                expect(await Broker.checkLogFileContains([errors[k + j][0]])).toBeTruthy()
+                expect(await Broker.checkLogFileCentralModuleContains([errors[k + j][1]])).toBeTruthy()
+
+                expect(await broker.stop()).toBeTruthy();
+                expect(await engine.stop()).toBeTruthy();
+            }
         }
     }, 400000);
 
-    it.only('tls with keys checks between broker - engine', async () => {
-      const broker = new Broker()
-      const engine = new Engine()
+    it('tls with keys checks between broker - engine', async () => {
+        const broker = new Broker()
+        const engine = new Engine()
 
-      const config_broker = await Broker.getConfig()
-      const config_rrd = await Broker.getConfigCentralRrd()
+        const config_broker = await Broker.getConfig()
+        const config_rrd = await Broker.getConfigCentralRrd()
 
-      const centralBrokerLoggers = config_broker['centreonBroker']['log']['loggers']
+        const centralBrokerLoggers = config_broker['centreonBroker']['log']['loggers']
 
-      centralBrokerLoggers['bbdo'] = "info"
-      centralBrokerLoggers['tcp']  = "info"
-      centralBrokerLoggers['tls']  = "trace"
+        centralBrokerLoggers['bbdo'] = "info"
+        centralBrokerLoggers['tcp'] = "info"
+        centralBrokerLoggers['tls'] = "trace"
 
-      var centralBrokerMaster = config_broker['centreonBroker']['output'].find((
-              output => output.name === 'centreon-broker-master-rrd'))
-      const centralRrdMaster = config_rrd['centreonBroker']['input'].find((
-              input => input.name === 'central-rrd-master-input'))
-      
-      // get hostname
-      const output = await shell.exec("hostname --fqdn")
-      const hostname = output.stdout.replace(/\n/g, '')
-      
-      // generates keys
-      await shell.exec("openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/centreon-broker/ssl/server.key -out /etc/centreon-broker/ssl/server.crt -subj '/CN='" + hostname)
-      await shell.exec("openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/centreon-broker/ssl/client.key -out /etc/centreon-broker/ssl/client.crt -subj '/CN='" + hostname)
-      
-      // update configuration file
-      centralBrokerMaster["tls"] = "yes"
-      centralBrokerMaster["private_key"] = "/etc/centreon-broker/ssl/client.key"
-      centralBrokerMaster["public_cert"] = "/etc/centreon-broker/ssl/client.crt"
-      centralBrokerMaster["ca_certificate"] = "/etc/centreon-broker/ssl/server.crt"
+        var centralBrokerMaster = config_broker['centreonBroker']['output'].find((
+            output => output.name === 'centreon-broker-master-rrd'))
+        const centralRrdMaster = config_rrd['centreonBroker']['input'].find((
+            input => input.name === 'central-rrd-master-input'))
 
-      centralRrdMaster["tls"] = "yes"
-      centralRrdMaster["private_key"] = "/etc/centreon-broker/ssl/server.key"
-      centralRrdMaster["public_cert"] = "/etc/centreon-broker/ssl/server.crt"
-      centralRrdMaster["ca_certificate"] = "/etc/centreon-broker/ssl/client.crt"
-      
-      // write changes in config_broker
-      await Broker.writeConfig(config_broker)
-      await Broker.writeConfigCentralRrd(config_rrd)
-      
-      console.log(centralBrokerMaster)
-      console.log(centralRrdMaster)
-      
-      // starts centreon
-      expect(await broker.start()).toBeTruthy()
-      expect(await engine.start()).toBeTruthy()
+        // get hostname
+        const output = await shell.exec("hostname --fqdn")
+        const hostname = output.stdout.replace(/\n/g, '')
 
-      expect(await isBrokerAndEngineConnected()).toBeTruthy()
+        // generates keys
+        await shell.exec("openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/centreon-broker/ssl/server.key -out /etc/centreon-broker/ssl/server.crt -subj '/CN='" + hostname)
+        await shell.exec("openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout /etc/centreon-broker/ssl/client.key -out /etc/centreon-broker/ssl/client.crt -subj '/CN='" + hostname)
 
-      // checking logs 
-      expect(await Broker.checkLogFileContains(["[tls] [info] TLS: using certificates as credentials"])).toBeTruthy()
-      expect(await Broker.checkLogFileContains(["[tls] [debug] TLS: performing handshake"])).toBeTruthy()
-      expect(await Broker.checkLogFileContains(["[tls] [debug] TLS: successful handshake"])).toBeTruthy()
+        // update configuration file
+        centralBrokerMaster["tls"] = "yes"
+        centralBrokerMaster["private_key"] = "/etc/centreon-broker/ssl/client.key"
+        centralBrokerMaster["public_cert"] = "/etc/centreon-broker/ssl/client.crt"
+        centralBrokerMaster["ca_certificate"] = "/etc/centreon-broker/ssl/server.crt"
 
-      expect(await broker.stop()).toBeTruthy();
-      expect(await engine.stop()).toBeTruthy();
+        centralRrdMaster["tls"] = "yes"
+        centralRrdMaster["private_key"] = "/etc/centreon-broker/ssl/server.key"
+        centralRrdMaster["public_cert"] = "/etc/centreon-broker/ssl/server.crt"
+        centralRrdMaster["ca_certificate"] = "/etc/centreon-broker/ssl/client.crt"
 
-      await shell.rm("/etc/centreon-broker/ssl/client.key")
-      await shell.rm("/etc/centreon-broker/ssl/client.crt")
-      await shell.rm("/etc/centreon-broker/ssl/server.key")
-      await shell.rm("/etc/centreon-broker/ssl/server.crt")
+        // write changes in config_broker
+        await Broker.writeConfig(config_broker)
+        await Broker.writeConfigCentralRrd(config_rrd)
+
+        console.log(centralBrokerMaster)
+        console.log(centralRrdMaster)
+
+        // starts centreon
+        expect(await broker.start()).toBeTruthy()
+        expect(await engine.start()).toBeTruthy()
+
+        expect(await isBrokerAndEngineConnected()).toBeTruthy()
+
+        // checking logs 
+        expect(await Broker.checkLogFileContains(["[tls] [info] TLS: using certificates as credentials"])).toBeTruthy()
+        expect(await Broker.checkLogFileContains(["[tls] [debug] TLS: performing handshake"])).toBeTruthy()
+        expect(await Broker.checkLogFileContains(["[tls] [debug] TLS: successful handshake"])).toBeTruthy()
+
+        expect(await broker.stop()).toBeTruthy();
+        expect(await engine.stop()).toBeTruthy();
+
+        await shell.rm("/etc/centreon-broker/ssl/client.key")
+        await shell.rm("/etc/centreon-broker/ssl/client.crt")
+        await shell.rm("/etc/centreon-broker/ssl/server.key")
+        await shell.rm("/etc/centreon-broker/ssl/server.crt")
     }, 90000);
+
+    /*
+    it('test a enlever', async () => {
+        let retval;
+        const engine = new Engine()
+
+        expect(await engine.start()).toBeTruthy()
+        engine.stop();
+
+        const cdList = shell.exec('ps ax').stdout.split('\n')
+        retval = cdList.find(line => line.includes('/usr/lib/systemd/systemd-coredump'))
+        console.log(retval)
+    }, 90000);
+    */
 });
