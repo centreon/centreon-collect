f644bccdcfbcd9945ebae84c3e782dc3b79ff090
centreonpm <pmargale@centreon.com>
2021-07-11 19:02:22 +0100

    enh(tests) update tls & compression tests

--- src/centreon-broker-engine/communication-with-compression.test.ts
+++ src/centreon-broker-engine/communication-with-compression.test.ts
@@ -26,58 +26,16 @@ describe('engine and broker testing in same time for compression', () => {
         })
     })
 
+        
     it('compression checks between broker - engine', async () => {
         const broker = new Broker()
         const engine = new Engine()
-
-        const compression = ["yes", "no", "auto"]
-        const errors = [
-            // yes, yes
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-            ],
-            // yes, no 
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS compression\'" // central side
-            ],
-            // yes, auto 
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-            ],
-            // no, yes
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'compression\'",
-                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS\'"
-            ],
-            // no, no
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS\'" // central side
-            ],
-            // no, auto
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS\' and peer has \'compression\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS\'" // central side
-            ],
-            // auto, yes
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-            ],
-            // auto, no
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'\' and peer has \'TLS compression\'" // central side
-            ],
-            // auto, auto
-            [
-                "[bbdo] [info] BBDO: we have extensions \'TLS compression\' and peer has \'compression\'", // broker side
-                "[bbdo] [info] BBDO: we have extensions \'compression\' and peer has \'TLS compression\'" // central side
-            ],
-        ]
+        
+        let compression = {
+           yes: 'compression',
+           no: '',
+           auto: 'compression'
+        }
 
         const config_broker = await Broker.getConfig()
         const config_module = await Broker.getConfigCentralModule()
@@ -96,32 +54,36 @@ describe('engine and broker testing in same time for compression', () => {
         const centralBrokerMaster = config_broker['centreonBroker']['input'].find((
             input => input.name === 'central-broker-master-input'))
 
-
-        for (let i = 0, k = 0; i < 3; ++i, k = i * 3) {
-            for (let j = 0; j < 3; ++j) {
+        for (let c1 in compression) {
+            for (let c2 in compression) {
                 Broker.clearLogs()
                 Broker.clearLogsCentralModule()
-                centralBrokerMaster['compression'] = compression[i]
-                centralModuleMaster['compression'] = compression[j]
 
-                await Broker.writeConfigCentralModule(config_module)
-                await Broker.writeConfig(config_broker)
+                centralBrokerMaster['compression'] = c1
+                centralModuleMaster['compression'] = c2
+
+                let peer1 = `[bbdo] [info] BBDO: we have extensions '${compression[c1]}' and peer has '${compression[c2]}'` 
+                let peer2 = `[bbdo] [info] BBDO: we have extensions '${compression[c2]}' and peer has '${compression[c1]}'` 
 
                 console.log(centralBrokerMaster)
                 console.log(centralModuleMaster)
+
+                await Broker.writeConfigCentralModule(config_module)
+                await Broker.writeConfig(config_broker)
 
                 expect(await broker.start()).toBeTruthy()
                 expect(await engine.start()).toBeTruthy()
 
                 expect(await isBrokerAndEngineConnected()).toBeTruthy()
 
-                console.log("index = ", k + j)
-                expect(await Broker.checkLogFileContains([errors[k + j][0]])).toBeTruthy()
-                expect(await Broker.checkLogFileCentralModuleContains([errors[k + j][1]])).toBeTruthy()
+                expect(await Broker.checkLogFileContains([peer1])).toBeTruthy()
+                expect(await Broker.checkLogFileCentralModuleContains([peer2])).toBeTruthy()
 
                 expect(await broker.stop()).toBeTruthy();
                 expect(await engine.stop()).toBeTruthy();
             }
         }
+
     }, 400000);
+ 
 });
