2d2e856e4db158fe269df6522ebd29fa08b42639
sc979 <34628915+sc979@users.noreply.github.com>
2021-07-26 15:26:51 +0200

    enh(chore): refacto plugin-packs SQ's versioning (#582)

--- jobs/packs/packs-analysis.sh
+++ jobs/packs/packs-analysis.sh
@@ -7,9 +7,26 @@ set -x
 
 # Project.
 PROJECT=centreon-plugin-packs
+PROJECT_NAME="Centreon Plugin Packs"
+if [ -z "$VERSION" ]; then
+  VERSION="latest"
+fi
 
 # Run analysis.
 cd "$PROJECT"
+
+# environment values required to replace sonarQube project versioning and binding
+#   sonar.projectKey="{PROJECT_TITLE}"
+#   sonar.projectName="{PROJECT_NAME}"
+#   sonar.projectKey="{PROJECT_VERSION}"
+echo "BRANCH_NAME      -> $BRANCH_NAME"
+echo "PROJECT_TITLE    -> $PROJECT"
+echo "PROJECT_NAME     -> $PROJECT_NAME"
+echo "PROJECT_VERSION  -> $VERSION"
+sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
+sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
+sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
+
 # override missing AMI requirement
 sudo apt-get install shellcheck
 sonar-scanner
