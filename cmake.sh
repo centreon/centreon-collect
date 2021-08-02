074366e29d91bb56333571dd272eca20bd9d3ef6
rem31 <73845199+rem31@users.noreply.github.com>
2021-08-02 14:33:19 +0200

    enh(test): Migrate centreon-clib tests to google-test (#51)
    enh(test): Migrate centreon-clib tests to google-test
    REFS : MON-10725

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Usage info
+
 show_help() {
 cat << EOF
 Usage: ${0##*/} -n=[yes|no] -v
@@ -8,11 +8,22 @@ This program build Centreon-clib
 
     -f|--force    : force rebuild
     -r|--release  : Build on release mode
+    -fcr|--force-conan-rebuild : rebuild conan data
     -h|--help     : help
 EOF
 }
-force=0
 BUILD_TYPE="Debug"
+CONAN_REBUILD="0"
+for i in $(cat conanfile.txt) ; do
+  if [[ $i =~ / ]] ; then
+    if [ ! -d ~/.conan/data/$i ] ; then
+      echo "The package '$i' is missing"
+      CONAN_REBUILD="1"
+      break
+    fi
+  fi
+done
+
 for i in "$@"
 do
   case $i in
@@ -22,6 +33,10 @@ do
       ;;
     -r|--release)
       BUILD_TYPE="Release"
+      shift
+      ;;
+    -fcr|--force-conan-rebuild)
+      CONAN_REBUILD="1"
       ;;
     -h|--help)
       show_help
@@ -43,19 +58,38 @@ if [ -r /etc/centos-release ] ; then
     cmake='cmake'
   else
     if rpm -q cmake3 ; then
-      cmake='cmake3'
+      rm -f /usr/bin/cmake
+      ln -s /usr/bin/cmake3 /usr/bin/cmake
+      cmake='cmake'
     elif [ $maj = "centos7" ] ; then
-      yum -y install epel-release
-      yum -y install cmake3
-      cmake='cmake3'
+      yum -y install epel-release cmake3
+      mv /usr/bin/cmake /usr/bin/cmake2
+      ln -s /usr/bin/cmake3 /usr/bin/cmake
+      cmake='cmake'
     else
       dnf -y install cmake
       cmake='cmake'
     fi
   fi
+  if [[ ! -x /usr/bin/python3 ]] ; then
+    yum -y install python3
+  else
+    echo "python3 already installed"
+  fi
+  if ! rpm -q python3-pip ; then
+    yum -y install python3-pip
+  else
+    echo "pip3 already installed"
+  fi
 
-  if ! rpm -q gcc-c++ ; then
-    yum -y install gcc-c++
+  good=$(gcc --version | awk '/gcc/ && ($3+0)>5.0{print 1}')
+
+  if [ ! $good ] ; then
+    echo "Your compiler is too old. Trying to used devtoolset-9."
+    yum -y install centos-release-scl
+    yum-config-manager --enable rhel-server-rhscl-7-rpms
+    yum -y install devtoolset-9
+    source /opt/rh/devtoolset-9/enable
   fi
 
   pkgs=(
@@ -73,16 +107,16 @@ if [ -r /etc/centos-release ] ; then
 elif [ -r /etc/issue ] ; then
   maj=$(cat /etc/issue | awk '{print $1}')
   version=$(cat /etc/issue | awk '{print $3}')
+  if [ $version = "9" ] ; then
+    dpkg='dpkg'
+  else
+    dpkg='dpkg --no-pager'
+  fi
   v=$(cmake --version)
   if [[ $v =~ "version 3" ]] ; then
     cmake='cmake'
   elif [ $maj = "Debian" ] ; then
-    if [ $version = "9" ] ; then
-      dpkg="dpkg"
-    else
-      dpkg="dpkg --no-pager"
-    fi
-    if $dpkg -l --no-pager cmake ; then
+    if $dpkg -l cmake ; then
       echo "Bad version of cmake..."
       exit 1
     else
@@ -94,29 +128,8 @@ elif [ -r /etc/issue ] ; then
         exit 1
       fi
     fi
-    pkgs=(
-      gcc
-      g++
-      ninja-build
-      pkg-config
-    )
-    for i in "${pkgs[@]}"; do
-      if ! $dpkg -l --no-pager $i | grep "^ii" ; then
-        if [ $my_id -eq 0 ] ; then
-          apt install -y $i
-        else
-          echo -e "The package \"$i\" is not installed, you can install it, as root, with the command:\n\tapt install -y $i\n\n"
-          exit 1
-        fi
-      fi
-    done
   elif [ $maj = "Raspbian" ] ; then
-    if [ $version = "9" ] ; then
-      dpkg="dpkg"
-    else
-      dpkg="dpkg --no-pager"
-    fi
-    if $dpkg -l --no-pager cmake ; then
+    if $dpkg -l cmake ; then
       echo "Bad version of cmake..."
       exit 1
     else
@@ -128,30 +141,66 @@ elif [ -r /etc/issue ] ; then
         exit 1
       fi
     fi
-    pkgs=(
-      gcc
-      g++
-      ninja-build
-      pkg-config
-    )
-    for i in "${pkgs[@]}"; do
-      if ! $dpkg -l --no-pager $i | grep "^ii" ; then
-        if [ $my_id -eq 0 ] ; then
-          apt install -y $i
-        else
-          echo -e "The package \"$i\" is not installed, you can install it, as root, with the command:\n\tapt install -y $i\n\n"
-          exit 1
-        fi
-      fi
-    done
   else
     echo "Bad version of cmake..."
     exit 1
   fi
+  if [ $maj = "Debian" ] ; then
+    pkgs=(
+      gcc
+      g++
+      pkg-config
+      ninja-build
+      python3
+      python3-pip
+    )
+    for i in "${pkgs[@]}"; do
+      if ! $dpkg -l $i | grep "^ii" ; then
+        if [ $my_id -eq 0 ] ; then
+          apt install -y $i
+        else
+          echo -e "The package \"$i\" is not installed, you can install it, as root, with the command:\n\tapt install -y $i\n\n"
+          exit 1
+        fi
+      fi
+    done
+  fi
+  if [ $maj = "Raspbian" ] ; then
+    pkgs=(
+      gcc
+      g++
+      pkg-config
+      ninja-build
+      python3
+      python3-pip
+    )
+    for i in "${pkgs[@]}"; do
+      if ! $dpkg -l $i | grep "^ii" ; then
+        if [ $my_id -eq 0 ] ; then
+          apt install -y $i
+        else
+          echo -e "The package \"$i\" is not installed, you can install it, as root, with the command:\n\tapt install -y $i\n\n"
+          exit 1
+        fi
+      fi
+    done
+  fi
+fi
+
+pip3 install conan --upgrade
+
+if [ $my_id -eq 0 ] ; then
+  conan='/usr/local/bin/conan'
+elif which conan ; then
+  conan=$(which conan)
+else
+  conan="$HOME/.local/bin/conan"
 fi
 
 if [ ! -d build ] ; then
   mkdir build
+else
+  echo "'build' directory already there"
 fi
 
 if [ "$force" = "1" ] ; then
@@ -159,12 +208,24 @@ if [ "$force" = "1" ] ; then
   mkdir build
 fi
 cd build
+
+if [ $maj = "centos7" ] ; then
+  rm -rf ~/.conan/profiles/default
+  if [ "$CONAN_REBUILD" = "1" ] ; then
+    $conan install .. -s compiler.libcxx=libstdc++11 --build="*"
+  else
+    $conan install .. -s compiler.libcxx=libstdc++11 --build=missing
+  fi
+else
+    $conan install .. -s compiler.libcxx=libstdc++11 --build=missing
+fi
+
 if [ $maj = "Raspbian" ] ; then
   CXXFLAGS="-Wall -Wextra" $cmake -DWITH_PREFIX=/usr -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_PREFIX_LIB=/usr/lib -DWITH_TESTING=On -DUSE_CXX11_ABI=1 $* ..
 elif [ $maj = "Debian" ] ; then
   CXXFLAGS="-Wall -Wextra" $cmake -DWITH_PREFIX=/usr -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_PREFIX_LIB=/usr/lib64 -DWITH_TESTING=On -DUSE_CXX11_ABI=1 $* ..
 else
-  CXXFLAGS="-Wall -Wextra" $cmake -DWITH_PREFIX=/usr -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_PREFIX_LIB=/usr/lib64 -DWITH_TESTING=On -DUSE_CXX11_ABI=0 $* ..
+  CXXFLAGS="-Wall -Wextra" $cmake -DWITH_PREFIX=/usr -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_PREFIX_LIB=/usr/lib64 -DWITH_TESTING=On -DUSE_CXX11_ABI=1 $* ..
 fi
 
 #CXX=/usr/bin/clang++ CC=/usr/bin/clang CXXFLAGS="-Wall -Wextra" cmake -DWITH_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DWITH_PREFIX_LIB=/usr/lib64 -DWITH_TESTING=On $* ..
---
+++ test/timestamp.cc
@@ -0,0 +1,299 @@
+/*
+ * Copyright 2020 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+#include <gtest/gtest.h>
+#include <iostream>
+#include "com/centreon/timestamp.hh"
+
+using namespace com::centreon;
+
+TEST(CLibTimestamp, Add) {
+  timestamp t1(1, 42);
+  timestamp t2(2, 24);
+
+  timestamp t3(t1 + t2);
+  ASSERT_EQ(t3.to_useconds(), 3000066);
+
+  timestamp t4(-1, -24);
+  timestamp t5(t2 + t4);
+  ASSERT_EQ(t5.to_useconds(), 1000000);
+
+  timestamp t6(1, 42);
+  t6 += t2;
+  ASSERT_EQ(t6.to_useconds(), 3000066);
+
+  timestamp t7(2, 24);
+  t7 += t4;
+  ASSERT_EQ(t7.to_useconds(), 1000000);
+}
+
+TEST(CLibTimestamp, AddMs) {
+  timestamp t1(1, 42);
+  t1.add_mseconds(2000);
+  ASSERT_EQ(t1.to_mseconds(), 3000);
+
+  timestamp t2(1, 42);
+  t2.add_mseconds(-1000);
+  ASSERT_EQ(t2.to_mseconds(), 0);
+}
+
+TEST(CLibTimestamp, AddS) {
+  timestamp t1(1, 42);
+  t1.add_seconds(2);
+  ASSERT_EQ(t1.to_seconds(), 3);
+
+  timestamp t2(1, 42);
+  t2.add_seconds(-1);
+  ASSERT_EQ(t2.to_seconds(), 0);
+}
+
+TEST(CLibTimestamp, AddUs) {
+  timestamp t1(1, 42);
+  t1.add_useconds(2000000);
+  ASSERT_EQ(t1.to_useconds(), 3000042);
+
+  timestamp t2(1, 42);
+  t2.add_useconds(-1000000);
+  ASSERT_EQ(t2.to_useconds(), 42);
+}
+
+TEST(CLibTimestamp, Clear) {
+  timestamp t(42, 24);
+  t.clear();
+  ASSERT_EQ(t, timestamp());
+}
+
+TEST(CLibTimestamp, Ctor) {
+  timestamp t1;
+  ASSERT_FALSE(t1.to_mseconds());
+  ASSERT_FALSE(t1.to_seconds());
+  ASSERT_FALSE(t1.to_useconds());
+
+  timestamp t2(42);
+  ASSERT_EQ(t2.to_seconds(), 42);
+
+  timestamp t3(42, 24);
+  ASSERT_EQ(t3.to_useconds(), 42000024);
+}
+
+TEST(CLibTimestamp, Copy) {
+  timestamp t1(42, 24);
+
+  timestamp t2(t1);
+  ASSERT_TRUE(t1 == t2);
+
+  timestamp t3 = t1;
+  ASSERT_TRUE(t1 == t3);
+}
+
+TEST(CLibTimestamp, Equal) {
+  timestamp t1(42, 24);
+
+  timestamp t2(42, 24);
+  ASSERT_TRUE(t1 == t2);
+
+  timestamp t3(41, 1000024);
+  ASSERT_TRUE(t3 == t2);
+
+  timestamp t4(43, -1000000 + 24);
+  ASSERT_TRUE(t4 == t2);
+}
+
+TEST(CLibTimestamp, GT) {
+  timestamp t1(2, 0);
+
+  timestamp t2(1, 0);
+  ASSERT_TRUE(t1 > t2);
+
+  timestamp t3(3, -1000);
+  ASSERT_TRUE(t3 > t2);
+
+  timestamp t4(1, -1);
+  ASSERT_TRUE(t2 > t4);
+
+  timestamp t5(-1, 0);
+  ASSERT_TRUE(t1 > t5);
+}
+
+TEST(CLibTimestamp, GE) {
+  timestamp t1(2, 0);
+
+  timestamp t2(1, 0);
+  ASSERT_TRUE(t1 >= t2);
+
+  timestamp t3(3, -1000);
+  ASSERT_TRUE(t3 >= t2);
+
+  timestamp t4(1, -1);
+  ASSERT_TRUE(t2 >= t4);
+
+  timestamp t5(-1, 0);
+  ASSERT_TRUE(t1 >= t5);
+
+  timestamp t6(0, 1000000);
+  ASSERT_TRUE(t6 >= t2);
+
+  timestamp t7(1, 0);
+  ASSERT_TRUE(t7 >= t2);
+}
+
+TEST(CLibTimestamp, LT) {
+  timestamp t1(2, 0);
+
+  timestamp t2(1, 0);
+  ASSERT_FALSE(t1 < t2);
+
+  timestamp t3(3, -1000);
+  ASSERT_FALSE(t3 < t2);
+
+  timestamp t4(1, -1);
+  ASSERT_FALSE(t2 < t4);
+
+  timestamp t5(-1, 0);
+  ASSERT_FALSE(t1 < t5);
+}
+
+TEST(CLibTimestamp, LE) {
+  timestamp t1(2, 0);
+
+  timestamp t2(1, 0);
+  ASSERT_FALSE(t1 <= t2);
+
+  timestamp t3(3, -1000);
+  ASSERT_FALSE(t3 <= t2);
+
+  timestamp t4(1, -1);
+  ASSERT_FALSE(t2 <= t4);
+
+  timestamp t5(-1, 0);
+  ASSERT_FALSE(t1 <= t5);
+
+  timestamp t6(0, 1000000);
+  ASSERT_TRUE(t6 <= t2);
+
+  timestamp t7(1, 0);
+  ASSERT_TRUE(t7 <= t2);
+}
+
+TEST(CLibTimestamp, NE) {
+  timestamp t1(42, 24);
+  timestamp t2(42, 42);
+
+  ASSERT_FALSE(t1 == t2);
+}
+
+TEST(CLibTimestamp, Sub) {
+  timestamp t1(1, 42);
+  timestamp t2(2, 24);
+
+  timestamp t3(t2 - t1);
+  ASSERT_EQ(t3.to_useconds(), 999982);
+
+  timestamp t4(-1, -24);
+  timestamp t5(t2 - t4);
+  ASSERT_EQ(t5.to_useconds(), 3000048);
+
+  timestamp t6(2, 24);
+  t6 -= t1;
+  ASSERT_EQ(t6.to_useconds(), 999982);
+
+  timestamp t7(2, 24);
+  t7 -= t4;
+  ASSERT_EQ(t7.to_useconds(), 3000048);
+}
+
+TEST(CLibTimestamp, SubMs) {
+  timestamp t1(2, 42);
+  t1.sub_mseconds(1000);
+  ASSERT_EQ(t1.to_mseconds(), 1000);
+
+  timestamp t2(1, 42);
+  t2.sub_mseconds(-1000);
+  ASSERT_EQ(t2.to_mseconds(), 2000);
+}
+
+TEST(CLibTimestamp, SubS) {
+  timestamp t1(2, 42);
+  t1.sub_seconds(1);
+  ASSERT_EQ(t1.to_seconds(), 1);
+
+  timestamp t2(1, 42);
+  t2.sub_seconds(-1);
+  ASSERT_EQ(t2.to_seconds(), 2);
+}
+
+TEST(CLibTimestamp, SubUs) {
+  timestamp t1(2, 42);
+  t1.sub_useconds(1000000);
+  ASSERT_EQ(t1.to_useconds(), 1000042);
+
+  timestamp t2(1, 42);
+  t2.sub_useconds(-1000000);
+  ASSERT_EQ(t2.to_useconds(), 2000042);
+}
+
+TEST(CLibTimestamp, ToMs) {
+  timestamp t1(1, 42);
+  ASSERT_EQ(t1.to_mseconds(), 1000);
+
+  timestamp t2(-1, 0);
+  ASSERT_EQ(t2.to_mseconds(), -1000);
+
+  timestamp t3(0, -42);
+  ASSERT_EQ(t3.to_mseconds(), -1);
+
+  timestamp t4(-1, -42);
+  ASSERT_EQ(t4.to_mseconds(), -1001);
+
+  timestamp t5(1, -42);
+  ASSERT_EQ(t5.to_mseconds(), 999);
+}
+
+TEST(CLibTimestamp, ToS) {
+  timestamp t1(1, 42);
+  ASSERT_EQ(t1.to_seconds(), 1);
+
+  timestamp t2(-1, 0);
+  ASSERT_EQ(t2.to_seconds(), -1);
+
+  timestamp t3(0, -42);
+  ASSERT_EQ(t3.to_seconds(), -1);
+
+  timestamp t4(-1, -42);
+  ASSERT_EQ(t4.to_seconds(), -2);
+
+  timestamp t5(1, -42);
+  ASSERT_EQ(t5.to_seconds(), 0);
+}
+
+TEST(CLibTimestamp, ToUs) {
+  timestamp t1(1, 42);
+  ASSERT_EQ(t1.to_useconds(), 1000042);
+
+  timestamp t2(-1, 0);
+  ASSERT_EQ(t2.to_useconds(), -1000000);
+
+  timestamp t3(0, -42);
+  ASSERT_EQ(t3.to_useconds(), -42);
+
+  timestamp t4(-1, -42);
+  ASSERT_EQ(t4.to_useconds(), -1000042);
+
+  timestamp t5(1, -42);
+  ASSERT_EQ(t5.to_useconds(), 999958);
+}
