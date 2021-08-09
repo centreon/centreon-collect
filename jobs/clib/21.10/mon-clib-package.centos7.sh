f685d3808ab90d327172754555342b1add8a7092
David Boucher <dboucher@centreon.com>
2021-08-09 11:31:50 +0200

    fix(clib): for the 21.10, clib works with conan, needed by gtest

--- jobs/clib/21.10/mon-clib-package.centos7.sh
+++ jobs/clib/21.10/mon-clib-package.centos7.sh
@@ -6,7 +6,7 @@ mkdir output
 
 # Retrieve sources.
 mv "$PROJECT-$VERSION.tar.gz" input/
-cp `dirname $0`/../../../packaging/clib/centreon-clib.spectemplate input/
+cp `dirname $0`/../../../packaging/clib/21.10/centreon-clib.spectemplate input/
 
 # Build RPMs.
 docker-rpm-builder dir --sign-with `dirname $0`/../../ces.key registry.centreon.com/mon-build-dependencies-21.10:centos7 input output
