cd29626046291b734133a9bc2b75aba99f107e4e
David Boucher <dboucher@centreon.com>
2021-08-09 17:17:07 +0200

    fix(clib): ARCH not defined

--- centreon-build/jobs/clib/21.10/mon-clib-package.debian10-armhf.sh
+++ centreon-build/jobs/clib/21.10/mon-clib-package.debian10-armhf.sh
@@ -9,7 +9,7 @@ cp -r `dirname $0`/../../../packaging/clib/21.10/debian "debuildir/${PROJECT}-${
 sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" < "debuildir/${PROJECT}-${VERSION}/debian/changelog.in" > "debuildir/${PROJECT}-${VERSION}/debian/changelog"
 
 # Launch debuild.
-containerid=`docker create registry.centreon.com/mon-build-dependencies-21.10:debian10-armhf sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && export CC=arm-linux-gnueabihf-gcc && export CXX=arm-linux-gnueabihf-g++ && dpkg-buildpackage -us -uc -d -aarmhf"`
+containerid=`docker create registry.centreon.com/mon-build-dependencies-21.10:debian10-armhf sh -c "cd /usr/local/src/debuildir/${PROJECT}-${VERSION} && export CC=arm-linux-gnueabihf-gcc && export CXX=arm-linux-gnueabihf-g++ && export ARCH=armv7hf && dpkg-buildpackage -us -uc -d -aarmhf"`
 docker cp debuildir "$containerid:/usr/local/src/debuildir"
 docker start -a "$containerid"
 
