302933729d77cada57ca18e91a687208ea441d34
Zakaria Guennoune <83596451+zguennoune02@users.noreply.github.com>
2021-09-02 16:22:05 +0200

    fix(ci): Composer lol (#619)
    * remove compose creds
    
    * remove useless shellcheck install
    
    * remove shellcheck install
    
    * remove shellcheck install
    
    * remove shellcheck install

--- centreon-build/jobs/web/21.04/mon-web-analysis.sh
+++ centreon-build/jobs/web/21.04/mon-web-analysis.sh
@@ -29,6 +29,4 @@ sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
 sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
 sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
 
-# override missing AMI requirement
-sudo apt-get install shellcheck
 sonar-scanner
