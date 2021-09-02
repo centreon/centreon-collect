116406b7a0060be42abebf1f5251eb70d6ca6416
Zakaria Guennoune <83596451+zguennoune02@users.noreply.github.com>
2021-09-03 00:07:44 +0200

    refacto rpm pushing 20.04 connector

--- jobs/connector/20.04/mon-connector-delivery.sh
+++ jobs/connector/20.04/mon-connector-delivery.sh
@@ -13,12 +13,18 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 
+MAJOR=`echo $VERSION | cut -d . -f 1,2`
+EL7RPMS=`echo output/x86_64/*.el7.*.rpm`
+EL8RPMS=`echo output/x86_64/*.el8.*.rpm`
+
 # Publish RPMs.
 if [ "$BUILD" '=' 'QA' ]
 then
   put_rpms "standard" "$MAJOR" "el7" "unstable" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE" $EL7RPMS
+  put_rpms "standard" "$MAJOR" "el8" "unstable" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE" $EL8RPMS
 elif [ "$BUILD" '=' 'RELEASE' ]
 then
   copy_internal_source_to_testing "standard" "connector" "$PROJECT-$VERSION-$RELEASE"
   put_rpms "standard" "$MAJOR" "el7" "testing" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE" $EL7RPMS
+  put_rpms "standard" "$MAJOR" "el8" "testing" "x86_64" "connector" "$PROJECT-$VERSION-$RELEASE" $EL8RPMS
 fi
