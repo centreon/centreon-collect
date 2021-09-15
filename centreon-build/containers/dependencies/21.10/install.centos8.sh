15123f3c8917b39d41008958cbab746b5fd5c090
Zakaria Guennoune <zguennoune@centreon.com>
2021-09-15 11:12:32 +0000

    adding business repo to mon dependencies centos8

--- centreon-build/containers/dependencies/21.10/install.centos8.sh
+++ centreon-build/containers/dependencies/21.10/install.centos8.sh
@@ -14,10 +14,15 @@ dnf config-manager --set-enabled 'PowerTools'
 # Install base tools.
 yum install curl nc
 
-# Install Centreon repository.
+# Install Centreon repositories.
 curl -o centreon-release.rpm "http://yum-1.centreon.com/standard/21.10/el8/stable/noarch/RPMS/centreon-release-21.10-1.el8.noarch.rpm"
 dnf install --nogpgcheck centreon-release.rpm
+curl -o centreon-release-business.rpm http://yum-1.centreon.com/centreon-business/1a97ff9985262bf3daf7a0919f9c59a6/21.10/el8/stable/noarch/RPMS/centreon-business-release-21.10-1.el8.noarch.rpm
+dnf install --nogpgcheck centreon-release-business.rpm
 dnf config-manager --set-enabled 'centreon-testing*'
+dnf config-manager --set-enabled 'centreon-unstable*'
+dnf config-manager --set-enabled 'centreon-business-testing'
+dnf config-manager --set-enabled 'centreon-business-unstable'
 
 # Switch AppStream to install php73
 dnf module enable php:7.3
@@ -32,5 +37,4 @@ xargs yum install < /tmp/dependencies.txt
 echo 'date.timezone = Europe/Paris' > /etc/php.d/centreon.ini
 
 # Clean packages
-dnf config-manager --set-enabled 'centreon-unstable*'
 dnf clean all
