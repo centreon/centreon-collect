20324accf4ee435dcc2351ea3ae2793766f50513
Zakaria Guennoune <zguennoune@centreon.com>
2021-09-15 10:36:53 +0000

    adapt docker build and push for bam

--- jobs/bam/21.10/des-bam-delivery.sh
+++ jobs/bam/21.10/des-bam-delivery.sh
@@ -26,12 +26,13 @@ then
   put_rpms "standard" "$MAJOR" "el7" "testing" "noarch" "bam" "$PROJECT-$VERSION-$RELEASE" $EL7RPMS
   put_rpms "standard" "$MAJOR" "el8" "testing" "noarch" "bam" "$PROJECT-$VERSION-$RELEASE" $EL8RPMS
   TARGETVERSION="$VERSION"
+  # Set Docker images as latest.
+  REGISTRY='registry.centreon.com'
+  for distrib in centos7 centos8 ; do
+    docker pull "$REGISTRY/des-bam-$VERSION-$RELEASE:$distrib"
+    docker tag "$REGISTRY/des-bam-$VERSION-$RELEASE:$distrib" "$REGISTRY/des-bam-$TARGETVERSION:$distrib"
+    docker push "$REGISTRY/des-bam-$TARGETVERSION:$distrib"
+done
 fi
 
-# Set Docker images as latest.
-REGISTRY='registry.centreon.com'
-for distrib in centos7 centos8 ; do
-  docker pull "$REGISTRY/des-bam-$VERSION-$RELEASE:$distrib"
-  docker tag "$REGISTRY/des-bam-$VERSION-$RELEASE:$distrib" "$REGISTRY/des-bam-$TARGETVERSION:$distrib"
-  docker push "$REGISTRY/des-bam-$TARGETVERSION:$distrib"
-done
+
