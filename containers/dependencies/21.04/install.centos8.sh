6b6d973c16cd2ca7a3d970609eb52af46714abfa
Zakaria Guennoune <zguennoune@centreon.com>
2021-09-16 14:22:56 +0000

    update 21.04 dependencies container

--- containers/dependencies/21.04/install.centos8.sh
+++ containers/dependencies/21.04/install.centos8.sh
@@ -7,16 +7,23 @@ set -x
 echo 'http_caching=none' >> /etc/yum.conf
 echo 'assumeyes=1' >> /etc/yum.conf
 dnf install dnf-plugins-core
+dnf install langpacks-en glibc-all-langpacks -y
 dnf config-manager --set-enabled 'PowerTools'
 
 # Install base tools.
 yum install curl nc
 
-# Install Centreon repository.
-curl -o centreon-release.rpm "http://srvi-repo.int.centreon.com/yum/standard/21.04/el8/stable/noarch/RPMS/centreon-release-21.04-2.el8.noarch.rpm"
-dnf install --nogpgcheck centreon-release.rpm
-sed -i -e 's#yum.centreon.com#srvi-repo.int.centreon.com/yum#g' /etc/yum.repos.d/centreon.repo
-dnf config-manager --set-enabled 'centreon-testing*'
+# Install Centreon repositories.
+curl -o centreon-release.rpm "http://yum-1.centreon.com/standard/21.04/el7/stable/noarch/RPMS/centreon-release-21.04-4.el7.centos.noarch.rpm"
+yum install --nogpgcheck centreon-release.rpm
+curl -o centreon-release-business.rpm "http://yum-1.centreon.com/centreon-business/1a97ff9985262bf3daf7a0919f9c59a6/21.04/el8/stable/noarch/RPMS/centreon-business-release-21.04-4.el8.noarch.rpm"
+yum install --nogpgcheck centreon-release-business.rpm
+yum-config-manager --enable 'centreon-testing*'
+yum-config-manager --enable 'centreon-unstable*'
+yum-config-manager --enable 'centreon-business-testing*'
+yum-config-manager --enable 'centreon-business-unstable*'
+yum-config-manager --enable 'centreon-business-testing-noarch'
+yum-config-manager --enable 'centreon-business-unstable-noarch'
 
 # Switch AppStream to install php73
 dnf module enable php:7.3
