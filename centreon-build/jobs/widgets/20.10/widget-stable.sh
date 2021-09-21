3547250c0cc0a8a82046788ccb65c27715b0a54e
Zakaria Guennoune <zguennoune@centreon.com>
2021-09-21 10:53:17 +0000

    widgets : new stable jobs

--- centreon-build/jobs/widgets/20.10/widget-stable.sh
+++ centreon-build/jobs/widgets/20.10/widget-stable.sh
@@ -22,5 +22,5 @@ $SSH_REPO mv "/srv/sources/standard/testing/widget-$WIDGET/$PROJECT-$VERSION-$RE
 upload_tarball_for_download "$PROJECT" "$VERSION" "/srv/sources/standard/stable/$PROJECT-$VERSION-$RELEASE/$PROJECT-$VERSION.tar.gz" "s3://centreon-download/public/centreon-widgets/$PROJECT/$PROJECT-$VERSION.tar.gz"
 
 # Move RPMs to the stable repository.
-promote_testing_rpms_to_stable "standard" "20.10" "el7" "noarch" "widget-$WIDGET" "$PROJECT-$VERSION-$RELEASE"
-promote_testing_rpms_to_stable "standard" "20.10" "el8" "noarch" "widget-$WIDGET" "$PROJECT-$VERSION-$RELEASE"
+promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el7" "noarch" "widget-$WIDGET" "centreon-$PROJECT-$VERSION-$RELEASE"
+promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el8" "noarch" "widget-$WIDGET" "centreon-$PROJECT-$VERSION-$RELEASE"
