652da2e528735847fb0bf4f530e002dd7cb19e96
Zakaria Guennoune <zguennoune@centreon.com>
2021-09-22 10:21:19 +0000

    map: new delivery

--- centreon-build/jobs/map/21.10/des-map-delivery.sh
+++ centreon-build/jobs/map/21.10/des-map-delivery.sh
@@ -14,28 +14,30 @@ if [ -z "$VERSION" -o -z "$RELEASE" ] ; then
   exit 1
 fi
 
-ls -lart
-
 MAJOR=`echo $VERSION | cut -d . -f 1,2`
-ls -lart output/noarch/
-SERVEREL7RPMS=`echo output/noarch/*.el7.*.rpm`
-SERVEREL8RPMS=`echo output/noarch/*.el8.*.rpm`
 
-# # Publish RPMs.
-# if [ "$BUILD" '=' 'QA' -o "$BUILD" '=' 'CI' ] ; then
-#     put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-server" "centreon-map-$VERSIONSERVER-$RELEASE" $EL7RPMS
-#     put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-server" "centreon-map-$VERSIONSERVER-$RELEASE" $EL8RPMS
-#     put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-server-ng" "centreon-map-$VERSIONSERVER-$RELEASE" $EL7RPMS
-#     put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-server-ng" "centreon-map-$VERSIONSERVER-$RELEASE" $EL8RPMS
-#     put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $EL7RPMS
-#     put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $EL8RPMS
-# fi
-# elif [ "$BUILD" '=' 'RELEASE' ]
-# then
-#   put_rpms "business" "$MAJOR" "el7" "testing" "noarch" "map-server" "centreon-map-$VERSIONSERVER-$RELEASE" $EL7RPMS
-#   put_rpms "business" "$MAJOR" "el8" "testing" "noarch" "map-server" "centreon-map-$VERSIONSERVER-$RELEASE" $EL8RPMS
-#   put_rpms "business" "$MAJOR" "el7" "testing" "noarch" "map-server-ng" "centreon-map-$VERSIONSERVER-$RELEASE" $EL7RPMS
-#   put_rpms "business" "$MAJOR" "el8" "testing" "noarch" "map-server-ng" "centreon-map-$VERSIONSERVER-$RELEASE" $EL8RPMS
-#   put_rpms "business" "$MAJOR" "el7" "testing" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $EL7RPMS
-#   put_rpms "business" "$MAJOR" "el8" "testing" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $EL8RPMS
-# fi
+SERVEREL7RPMS=`echo output/noarch/centreon-map-server-*.el7.*.rpm`
+SERVEREL8RPMS=`echo output/noarch/centreon-map-server-*.el8.*.rpm`
+NGEL7RPMS=`echo output/noarch/centreon-map-server-ng*.el7.*.rpm`
+NGEL8RPMS=`echo output/noarch/centreon-map-server-ng*.el8.*.rpm`
+WEBEL7RPMS=`echo output/noarch/centreon-map-web*.el7.*.rpm`
+WEBEL8RPMS=`echo output/noarch/centreon-map-web*.el8.*.rpm`
+
+# Publish RPMs.
+if [ "$BUILD" '=' 'QA' -o "$BUILD" '=' 'CI' ]
+then
+  put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-server" "centreon-map-server-$VERSIONSERVER-$RELEASE" $SERVEREL7RPMS
+  put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-server" "centreon-map-server-$VERSIONSERVER-$RELEASE" $SERVEREL8RPMS
+  put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-server-ng" "centreon-map-server-$VERSIONSERVER-$RELEASE" $NGEL7RPMS
+  put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-server-ng" "centreon-map-server-$VERSIONSERVER-$RELEASE" $NGEL8RPMS
+  put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $WEBEL7RPMS
+  put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $WEBEL8RPMS
+elif [ "$BUILD" '=' 'RELEASE' ]
+then
+  put_rpms "business" "$MAJOR" "el7" "testing" "noarch" "map-server" "centreon-map-server-$VERSIONSERVER-$RELEASE" $SERVEREL7RPMS
+  put_rpms "business" "$MAJOR" "el8" "testing" "noarch" "map-server" "centreon-map-server-$VERSIONSERVER-$RELEASE" $SERVEREL8RPMS
+  put_rpms "business" "$MAJOR" "el7" "testing" "noarch" "map-server-ng" "centreon-map-server-$VERSIONSERVER-$RELEASE" $NGEL7RPMS
+  put_rpms "business" "$MAJOR" "el8" "testing" "noarch" "map-server-ng" "centreon-map-server-$VERSIONSERVER-$RELEASE" $NGEL8RPMS
+  put_rpms "business" "$MAJOR" "el7" "testing" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $WEBEL7RPMS
+  put_rpms "business" "$MAJOR" "el8" "testing" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $WEBEL8RPMS
+fi
