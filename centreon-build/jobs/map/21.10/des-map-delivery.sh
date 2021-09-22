7c56fb50fdf173f47e0501883c9ec75f966e93f3
Zakaria Guennoune <zguennoune@centreon.com>
2021-09-22 08:11:45 +0000

    map: fix delivery 21.10

--- centreon-build/jobs/map/21.10/des-map-delivery.sh
+++ centreon-build/jobs/map/21.10/des-map-delivery.sh
@@ -19,20 +19,14 @@ EL7RPMS=`echo output/noarch/*.el7.*.rpm`
 EL8RPMS=`echo output/noarch/*.el8.*.rpm`
 
 # Publish RPMs.
-if [ "$BUILD" '=' 'QA' -o "$BUILD" '=' 'CI' ]
-then 
-  if [ "$PRODUCT" '=' 'server' -o "$PRODUCT" '=' 'all' ] 
-  then
+if [ "$BUILD" '=' 'QA' -o "$BUILD" '=' 'CI' ] ; then
     put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-server" "centreon-map-$VERSIONSERVER-$RELEASE" $EL7RPMS
     put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-server" "centreon-map-$VERSIONSERVER-$RELEASE" $EL8RPMS
     put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-server-ng" "centreon-map-$VERSIONSERVER-$RELEASE" $EL7RPMS
     put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-server-ng" "centreon-map-$VERSIONSERVER-$RELEASE" $EL8RPMS
-  fi
-  if [ "$PRODUCT" '=' 'web' -o "$PRODUCT" '=' 'all' ] 
-  then
     put_rpms "business" "$MAJOR" "el7" "unstable" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $EL7RPMS
     put_rpms "business" "$MAJOR" "el8" "unstable" "noarch" "map-web" "centreon-map-web-$VERSIONWEB-$RELEASE" $EL8RPMS
-  fi
+fi
 elif [ "$BUILD" '=' 'RELEASE' ]
 then
   put_rpms "business" "$MAJOR" "el7" "testing" "noarch" "map-server" "centreon-map-$VERSIONSERVER-$RELEASE" $EL7RPMS
