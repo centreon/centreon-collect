cf247f8556c19fb219495f2aef932f273a9ca96d
Zakaria Guennoune <zguennoune@centreon.com>
2021-10-05 09:22:16 +0000

    web: api doc on reference build instead of release

--- centreon-build/jobs/web/21.04/mon-web-delivery.sh
+++ centreon-build/jobs/web/21.04/mon-web-delivery.sh
@@ -27,11 +27,13 @@ then
   put_testing_source "standard" "web" "$PROJECT-$VERSION-$RELEASE" "centreon-web-$VERSION.tar.gz" centreon-api-v2.html
   put_rpms "standard" "$MAJOR" "el7" "testing" "noarch" "web" "centreon-web-$VERSION-$RELEASE" $EL7RPMS
   put_rpms "standard" "$MAJOR" "el8" "testing" "noarch" "web" "centreon-web-$VERSION-$RELEASE" $EL8RPMS
-  SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
-  $SSH_REPO aws s3 cp --acl public-read "/srv/sources/internal/web/centreon-web-$VERSION-$RELEASE/centreon-api-v2.html" s3://centreon-documentation-prod/api/centreon-web/index.html
-  $SSH_REPO aws cloudfront create-invalidation --distribution-id E3KVGH6VYVX7DP --paths /api/centreon-web/index.html
 elif  [ "$BUILD" '=' 'CI' ]
 then
   put_rpms "standard" "$MAJOR" "el7" "canary" "noarch" "web" "centreon-web-$VERSION-$RELEASE" $EL7RPMS
   put_rpms "standard" "$MAJOR" "el8" "canary" "noarch" "web" "centreon-web-$VERSION-$RELEASE" $EL8RPMS
+elif  [ "$BUILD" '=' 'REFERENCE' ]
+then
+  SSH_REPO='ssh -o StrictHostKeyChecking=no ubuntu@srvi-repo.int.centreon.com'
+  $SSH_REPO aws s3 cp --acl public-read "/srv/sources/internal/web/centreon-web-$VERSION-$RELEASE/centreon-api-v2.html" s3://centreon-documentation-prod/api/centreon-web/index.html
+  $SSH_REPO aws cloudfront create-invalidation --distribution-id E3KVGH6VYVX7DP --paths /api/centreon-web/index.html
 fi
