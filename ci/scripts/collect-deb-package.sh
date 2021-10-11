4b9d1554d50cfe11631c6dcbd4ea8660b608226f
rem31 <73845199+rem31@users.noreply.github.com>
2021-10-11 13:46:00 +0000

    enh(chore): updating packaging on ci for debian10 and debian11

---
+++ ci/scripts/collect-deb-package.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$DISTRIB" ] ; then
+  echo "You need to specify VERSION / RELEASE variables"
+  exit 1
+fi
+
+echo "################################################## PACKAGING COLLECT ##################################################"
+
+AUTHOR="Luiz Costa"
+AUTHOR_EMAIL="me@luizgustavo.pro.br"
+
+if ! [ -d ../repo ]; then
+    rm -rf ./repo
+fi
+
+mkdir -p centreon-collect
+cp -rv * centreon-collect
+tar czpvf centreon-collect-$VERSION.tar.gz centreon-collect
+cd centreon-collect/
+mv ci/debian .
+debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -r "$RELEASE"
+debuild-pbuilder
+
