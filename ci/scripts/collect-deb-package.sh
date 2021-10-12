20ce342543cbcc210eadd9de87d581b0f40b425f
rem31 <73845199+rem31@users.noreply.github.com>
2021-10-12 14:26:18 +0000

    enh(chore): updating packaging on ci for debian10 and debian11

--- ci/scripts/collect-deb-package.sh
+++ ci/scripts/collect-deb-package.sh
@@ -19,6 +19,7 @@ fi
 # cp -rv * centreon-collect
 tar czpvf centreon-collect-$VERSION.tar.gz centreon-collect
 cd centreon-collect/
+rm -rf debian
 mv ci/debian .
 debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -r "$RELEASE"
 debuild-pbuilder
