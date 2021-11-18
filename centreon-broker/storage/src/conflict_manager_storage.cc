567f752ae5f36f134d134c802c1dc00db3923811
Gres Remi <rgres@centreon.com>
2021-11-18 20:10:17 +0000

    fix(rrd): add query sql to check metrics to delete

--- centreon-broker/storage/src/conflict_manager_storage.cc
+++ centreon-broker/storage/src/conflict_manager_storage.cc
@@ -568,6 +568,15 @@ void conflict_manager::_check_deleted_index() {
         _metric_cache.erase({res.value_as_u64(0), res.value_as_str(2)});
         _index_cache.erase({res.value_as_u32(3), res.value_as_u32(4)});
       }
+      _mysql.run_query_and_get_result(
+          "SELECT metric_id, metric_name FROM metrics WHERE to_delete=1",
+          &promise, conn);
+      res = promise.get_future().get();
+
+      while (_mysql.fetch_row(res)) {
+        metrics_to_delete.push_back(res.value_as_u64(0));
+        _metric_cache.erase({res.value_as_u64(0), res.value_as_str(1)});
+      }
     } catch (const std::exception& e) {
       throw msg_fmt("could not query index table to get index to delete: {} ",
                     e.what());
