15974fc63e28ba82e1dd39618958a07979c4a724
rem31 <73845199+rem31@users.noreply.github.com>
2022-01-21 12:27:22 +0100

    fix(scheduler): now Host checks scheduling basis no longer than expected
    REFS: MON-10907

--- centreon-engine/src/host.cc
+++ centreon-engine/src/host.cc
@@ -22,6 +22,7 @@
 #include <cassert>
 #include <iomanip>
 
+#include <fmt/chrono.h>
 #include "com/centreon/engine/broker.hh"
 #include "com/centreon/engine/checks/checker.hh"
 #include "com/centreon/engine/configuration/applier/state.hh"
@@ -1603,11 +1604,6 @@ int host::run_scheduled_check(int check_options, double latency) {
       /* the host could not be rescheduled properly - set the next check time
        * for next week */
       if (!time_is_valid && next_valid_time == preferred_time) {
-        /*
-          get_next_check()=(time_t)(next_valid_time+(60*60*24*365));
-          get_should_be_scheduled()=false;
-        */
-
         set_next_check((time_t)(next_valid_time + (60 * 60 * 24 * 7)));
 
         engine_logger(log_runtime_warning, basic)
@@ -3021,7 +3017,8 @@ int host::process_check_result_3x(enum host::host_state new_state,
   std::list<host*> check_hostlist;
   host::host_state parent_state = host::state_up;
   time_t current_time = 0L;
-  time_t next_check = 0L;
+  time_t next_check{get_last_check() +
+                    get_check_interval() * config->interval_length()};
   time_t preferred_time = 0L;
   time_t next_valid_time = 0L;
   int run_async_check = true;
@@ -3043,13 +3040,6 @@ int host::process_check_result_3x(enum host::host_state new_state,
       get_check_type() == check_active ? "ACTIVE" : "PASSIVE",
       get_state_type() == hard ? "HARD" : "SOFT", get_current_state(),
       new_state);
-
-  /* get the current time */
-  time(&current_time);
-
-  /* default next check time */
-  next_check = (unsigned long)(current_time + (get_check_interval() *
-                                               config->interval_length()));
 
   /* we have to adjust current attempt # for passive checks, as it isn't done
    * elsewhere */
@@ -3097,8 +3087,6 @@ int host::process_check_result_3x(enum host::host_state new_state,
 
       /* reschedule the next check of the host at the normal interval */
       reschedule_check = true;
-      next_check = (unsigned long)(current_time + (get_check_interval() *
-                                                   config->interval_length()));
 
       /* propagate checks to immediate parents if they are not already UP */
       /* we do this because a parent host (or grandparent) may have recovered
@@ -3184,20 +3172,11 @@ int host::process_check_result_3x(enum host::host_state new_state,
       /* reschedule the next check if the host state changed */
       if (_last_state != _current_state || _last_hard_state != _current_state) {
         reschedule_check = true;
-
         /* schedule a re-check of the host at the retry interval because we
          * can't determine its final state yet... */
         if (get_state_type() == soft)
-          next_check =
-              (unsigned long)(current_time + (get_retry_interval() *
-                                              config->interval_length()));
-
-        /* host has maxed out on retries (or was previously in a hard problem
-         * state), so reschedule the next check at the normal interval */
-        else
-          next_check =
-              (unsigned long)(current_time + (get_check_interval() *
-                                              config->interval_length()));
+          next_check = get_last_check() +
+                       get_retry_interval() * config->interval_length();
       }
     }
   }
@@ -3219,11 +3198,6 @@ int host::process_check_result_3x(enum host::host_state new_state,
       /* set the state type */
       set_state_type(hard);
 
-      /* reschedule the next check at the normal interval */
-      if (reschedule_check)
-        next_check =
-            (unsigned long)(current_time +
-                            (get_check_interval() * config->interval_length()));
     }
     /***** HOST IS NOW DOWN/UNREACHABLE *****/
     else {
@@ -3241,9 +3215,6 @@ int host::process_check_result_3x(enum host::host_state new_state,
         /* host has maxed out on retries, so reschedule the next check at the
          * normal interval */
         reschedule_check = true;
-        next_check =
-            (unsigned long)(current_time +
-                            (get_check_interval() * config->interval_length()));
 
         /* we need to run SYNCHRONOUS checks of all parent hosts to accurately
          * determine the state of this host */
@@ -3377,16 +3348,8 @@ int host::process_check_result_3x(enum host::host_state new_state,
          * can't determine its final state yet... */
         if (get_check_type() == check_active ||
             config->passive_host_checks_are_soft())
-          next_check =
-              (unsigned long)(current_time + (get_retry_interval() *
-                                              config->interval_length()));
-
-        /* schedule a re-check of the host at the normal interval */
-        else
-          next_check =
-              (unsigned long)(current_time + (get_check_interval() *
-                                              config->interval_length()));
-
+          next_check = get_last_check() +
+                       get_retry_interval() * config->interval_length();
         /* propagate checks to immediate parents if they are UP */
         /* we do this because a parent host (or grandparent) may have gone down
          * and blocked our route */
@@ -3521,8 +3484,11 @@ int host::process_check_result_3x(enum host::host_state new_state,
   if (reschedule_check) {
     engine_logger(dbg_checks, more)
         << "Rescheduling next check of host at " << my_ctime(&next_check);
-    log_v2::checks()->debug("Rescheduling next check of host at {}",
-                            my_ctime(&next_check));
+    log_v2::checks()->debug(
+        "Rescheduling next check of host: {} of last check at "
+        "{:%Y-%m-%dT%H:%M:%S} and next "
+        "check at {:%Y-%m-%dT%H:%M:%S}",
+        _name, fmt::localtime(get_last_check()), fmt::localtime(next_check));
 
     /* default is to reschedule host check unless a test below fails... */
     set_should_be_scheduled(true);
--- centreon-tests/resources/Common.py
+++ centreon-tests/resources/Common.py
@@ -169,3 +169,40 @@ def engine_log_file_duplicate(log: str, date):
         logger.console("The file '{}' does not exist".format(log))
         return False
 
+def check_reschedule(log: str, date, content):
+    my_date = parser.parse(date)
+    try:
+        f = open(log, "r")
+        lines = f.readlines()
+        p = re.compile(r"\[([^\]]*)\]")
+
+        # Let's find my_date
+        start = 0
+        end = len(lines) - 1
+        idx = start
+        while end - start > 1:
+            idx = (start + end) // 2
+            m = p.match(lines[idx])
+            idx_d = get_date(m.group(1))
+            if my_date <= idx_d:
+                end = idx
+            elif my_date > idx_d:
+                start = idx
+        retry_check = False
+        normal_check = False
+        for c in content:
+            for i in range(idx, len(lines)):
+                line = lines[i]
+                if c in line:
+                    logger.console("\"{}\" found at line {} from {}".format(c, i, idx))
+                    row = line.split()
+                    delta = int(datetime.strptime(row[19], "%Y-%m-%dT%H:%M:%S").timestamp()) - int(datetime.strptime(row[14], "%Y-%m-%dT%H:%M:%S").timestamp())
+                    if delta == 60:
+                        retry_check = True
+                    elif delta == 300:
+                        normal_check = True
+        return retry_check , normal_check
+    except IOError:
+        logger.console("The file '{}' does not exist".format(log))
+        return False
+
