1a88c1757a00085150b071e25d2410f1eb75f81a
Gres Remi <rgres@centreon.com>
2022-02-07 12:30:53 +0000

    good bye el8

--- ci/release/collect-release.sh
+++ ci/release/collect-release.sh
@@ -20,10 +20,6 @@ upload_tarball_for_download "centreon-collect" "$VERSION" "/srv/sources/standard
 
 # Move RPMs to the stable repository.
 promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el7" "x86_64" "broker" "centreon-broker-$VERSION-$RELEASE"
-promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el8" "x86_64" "broker" "centreon-broker-$VERSION-$RELEASE"
 promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el7" "x86_64" "engine" "centreon-engine-$VERSION-$RELEASE"
-promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el8" "x86_64" "engine" "centreon-engine-$VERSION-$RELEASE"
 promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el7" "x86_64" "clib" "centreon-clib-$VERSION-$RELEASE"
-promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el8" "x86_64" "clib" "centreon-clib-$VERSION-$RELEASE"
 promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el7" "x86_64" "connector" "centreon-connector-$VERSION-$RELEASE"
-promote_rpms_from_testing_to_stable "standard" "$MAJOR" "el8" "x86_64" "connector" "centreon-connector-$VERSION-$RELEASE"
