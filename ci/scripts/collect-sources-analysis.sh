468cc7c7bd0da76278143896c9f324d8ded08ff6
Zakaria Guennoune <zguennoune@centreon.com>
2022-02-22 09:03:49 +0100

    analysis

--- ci/scripts/collect-sources-analysis.sh
+++ ci/scripts/collect-sources-analysis.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
+#!/bin/sh
+
 set -e
 
-echo "BRANCH_NAME      -> $BRANCH_NAME"
-echo "PROJECT_TITLE    -> $PROJECT"
-echo "PROJECT_NAME     -> $PROJECT_NAME"
-echo "PROJECT_VERSION  -> $VERSION"
-sed -i -e "s/{PROJECT_TITLE}/$PROJECT/g" sonar-project.properties
-sed -i -e "s/{PROJECT_NAME}/$PROJECT_NAME/g" sonar-project.properties
-sed -i -e "s/{PROJECT_VERSION}/$VERSION/g" sonar-project.properties
-sonar-scanner
+cmakelists=CMakeLists.txt
+
+major=`grep 'set(COLLECT_MAJOR' CMakeLists.txt | cut -d ' ' -f 2 | cut -d ')' -f 1`
+minor=`grep 'set(COLLECT_MINOR' CMakeLists.txt | cut -d ' ' -f 2 | cut -d ')' -f 1`
+patch=`grep 'set(COLLECT_PATCH' CMakeLists.txt | cut -d ' ' -f 2 | cut -d ')' -f 1`
+
+export VERSION="$major.$minor.$patch"
+
+if [ -z "$VERSION" ] ; then
+  echo "You need to specify the VERSION variable"
+  exit 1
+fi
+
+sonar-scanner -Dsonar.projectVersion="$VERSION" -Dsonar.login="$1" -Dsonar.host.url="$2"
