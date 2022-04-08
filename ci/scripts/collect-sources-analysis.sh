3952381abee4cdaa27f830b4f92da6914ab7e15a
schapron <schapron@centreon.com>
2022-04-08 16:14:56 +0200

    wip

--- ci/scripts/collect-sources-analysis.sh
+++ ci/scripts/collect-sources-analysis.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-ls -lart
-sudo find / -type d -name ".conan" 2>/dev/null
 echo "current location"
 pwd
 
 # Moving files in order to be able to use absolute path specified in compile_commands.json
 sudo mv build src
+ls -lart
 
 sonar-scanner -X -Dsonar.projectVersion="$VERSION"
