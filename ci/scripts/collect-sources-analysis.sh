c7d5de3bc4f2aea60476a721f602fc5fee452a05
schapron <schapron@centreon.com>
2022-04-08 14:54:13 +0200

    absolute files path

--- ci/scripts/collect-sources-analysis.sh
+++ ci/scripts/collect-sources-analysis.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+# Moving files in order to be able to use absolute path specified in compile_commands.json
+mkdir src
+mv build src
+
 sonar-scanner -Dsonar.projectVersion="$VERSION"
