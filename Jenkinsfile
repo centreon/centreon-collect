18a7db4a7940d7edc1a5108f852c145b46672fc1
schapron <schapron@centreon.com>
2022-04-14 16:40:02 +0200

    wip

--- Jenkinsfile
+++ Jenkinsfile
@@ -80,8 +80,11 @@ stage('Build / Unit tests // Packaging / Signing') {
         withSonarQubeEnv('SonarQubeDev') {
           echo "DEBUG 3"
           sh 'printenv'
-          sh 'docker run -i --entrypoint /src/ci/scripts/collect-sources-analysis.sh -v "$PWD:/src" registry.centreon.com/centreon-collect-centos7-dependencies:22.04-testdocker "$SONAR_AUTH_TOKEN" "$SONAR_HOST_URL"'
-          //sh 'docker run -i -v "$PWD:/src" -w="/src" --entrypoint ci/scripts/collect-sources-analysis.sh --rm -u $(id -u):$(id -g) registry.centreon.com/centreon-collect-centos7-dependencies:22.04-testdocker "$SONAR_AUTH_TOKEN" "$SONAR_HOST_URL"'
+          if (env.CHANGE_ID) {
+            sh 'docker run -i --entrypoint /src/ci/scripts/collect-sources-analysis.sh -v "$PWD:/src" registry.centreon.com/centreon-collect-centos7-dependencies:22.04-testdocker "PR" "$SONAR_AUTH_TOKEN" "$SONAR_HOST_URL" "$CHANGE_TARGET" "CHANGE_BRANCH" "$CHANGE_ID" '
+          } else {
+            sh 'docker run -i --entrypoint /src/ci/scripts/collect-sources-analysis.sh -v "$PWD:/src" registry.centreon.com/centreon-collect-centos7-dependencies:22.04-testdocker "NotPR" "$SONAR_AUTH_TOKEN" "$SONAR_HOST_URL" "$CHANGE_BRANCH"'
+          }
         }
       }
     }
--- ci/scripts/collect-sources-analysis.sh
+++ ci/scripts/collect-sources-analysis.sh
@@ -16,9 +16,19 @@ else
     CXXFLAGS="-Wall -Wextra" cmake3 -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DWITH_CENTREON_CLIB_INCLUDE_DIR=../clib/inc/ -DWITH_CENTREON_CLIB_LIBRARIES=centreon-clib/libcentreon_clib.so -DCMAKE_BUILD_TYPE=Debug -DWITH_PREFIX=/usr -DWITH_PREFIX_BIN=/usr/sbin -DWITH_USER_BROKER=centreon-broker -DWITH_USER_ENGINE=centreon-engine -DWITH_GROUP_BROKER=centreon-broker -DWITH_GROUP_ENGINE=centreon-engine -DWITH_TESTING=On -DWITH_PREFIX_MODULES=/usr/share/centreon/lib/centreon-broker -DWITH_PREFIX_CONF_BROKER=/etc/centreon-broker -DWITH_PREFIX_LIB_BROKER=/usr/lib64/nagios -DWITH_PREFIX_CONF_ENGINE=/etc/centreon-engine -DWITH_PREFIX_LIB_ENGINE=/usr/lib64/centreon-engine -DWITH_PREFIX_LIB_CLIB=/usr/lib64/ -DWITH_RW_DIR=/var/lib/centreon-engine/rw -DWITH_VAR_DIR=/var/log/centreon-engine -DWITH_MODULE_SIMU=On ..
 fi
 
-/src/tmp/sonar-scanner/bin/sonar-scanner -X -Dsonar.projectVersion="$VERSION" \
-  -Dsonar.pullrequest.key="42421" \
-  -Dsonar.pullrequest.branch="testBranch2" \
-  -Dsonar.pullrequest.base="develop" \
-  -Dsonar.login="$1" \
-  -Dsonar.host.url="$2"
+if [ "PR" == "$1" ] ; then
+  /src/tmp/sonar-scanner/bin/sonar-scanner -X -Dsonar.projectVersion="$VERSION" \
+    -Dsonar.login="$2" \
+    -Dsonar.host.url="$3" \
+    -Dsonar.pullrequest.branch="$4" \
+    -Dsonar.pullrequest.base="$5" \
+    -Dsonar.pullrequest.key="$6"
+else
+  /src/tmp/sonar-scanner/bin/sonar-scanner -X -Dsonar.projectVersion="$VERSION" \
+    -Dsonar.login="$2" \
+    -Dsonar.host.url="$3" \
+    -Dsonar.pullrequest.branch="$4"
+fi
+
+
+
