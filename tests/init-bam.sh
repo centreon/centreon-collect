7b64ae3817b2ec03e236b328de5351181bee28c3
David Boucher <dboucher@centreon.com>
2022-04-21 19:29:11 +0200

    (cleanup(tests): comments removed

--- tests/init-bam.sh
+++ tests/init-bam.sh
@@ -2,6 +2,3 @@
 
 mysql -u root -pcentreon -e "drop database centreon"
 mysql -u root -pcentreon < resources/centreon.sql
-#mysql -u root -pcentreon -e "SET GLOBAL FOREIGN_KEY_CHECKS=0"
-#mysql centreon -u root -pcentreon < ../broker/simu/docker/bam.sql
-#mysql -u root -pcentreon -e "SET GLOBAL FOREIGN_KEY_CHECKS=1"
