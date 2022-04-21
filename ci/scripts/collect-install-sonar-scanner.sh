4cc09cdc512cebb163697f9748402d60c1c48ffa
schapron <schapron@centreon.com>
2022-04-21 12:57:57 +0200

    comments

--- ci/scripts/collect-install-sonar-scanner.sh
+++ ci/scripts/collect-install-sonar-scanner.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
+# Installing missing requirements
 sudo apt-get install unzip || exit
 
+# Cleaning
 rm -rf tmp
 mkdir tmp
 cd tmp
-sudo rm -rf /root/.sonar
 
 curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip --output sonar-scanner-cli-4.7.0.2747-linux.zip
 
 unzip sonar-scanner-cli-4.7.0.2747-linux.zip
-rm sonar-scanner-cli-4.7.0.2747-linux.zip
+rm -f sonar-scanner-cli-4.7.0.2747-linux.zip
 sudo mv sonar-scanner-4.7.0.2747-linux sonar-scanner
