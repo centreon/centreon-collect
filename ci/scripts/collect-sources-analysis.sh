0b5266b14278399eab3c2f3c4f642bf563e331be
schapron <schapron@centreon.com>
2022-04-21 14:49:08 +0200

    test cache

--- ci/scripts/collect-sources-analysis.sh
+++ ci/scripts/collect-sources-analysis.sh
@@ -17,17 +17,25 @@ if [[ $(cat /etc/issue | awk '{print $1}') = "Debian" ]] ; then
 else
     CXXFLAGS="-Wall -Wextra" cmake3 -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DWITH_CENTREON_CLIB_INCLUDE_DIR=../clib/inc/ -DWITH_CENTREON_CLIB_LIBRARIES=centreon-clib/libcentreon_clib.so -DCMAKE_BUILD_TYPE=Debug -DWITH_PREFIX=/usr -DWITH_PREFIX_BIN=/usr/sbin -DWITH_USER_BROKER=centreon-broker -DWITH_USER_ENGINE=centreon-engine -DWITH_GROUP_BROKER=centreon-broker -DWITH_GROUP_ENGINE=centreon-engine -DWITH_TESTING=On -DWITH_PREFIX_MODULES=/usr/share/centreon/lib/centreon-broker -DWITH_PREFIX_CONF_BROKER=/etc/centreon-broker -DWITH_PREFIX_LIB_BROKER=/usr/lib64/nagios -DWITH_PREFIX_CONF_ENGINE=/etc/centreon-engine -DWITH_PREFIX_LIB_ENGINE=/usr/lib64/centreon-engine -DWITH_PREFIX_LIB_CLIB=/usr/lib64/ -DWITH_RW_DIR=/var/lib/centreon-engine/rw -DWITH_VAR_DIR=/var/log/centreon-engine -DWITH_MODULE_SIMU=On ..
 fi
-
 cd ..
+
+# Import script
+. `dirname $0`/collect-sonar-scanner-common.sh
 
 # Get thread number
 PROCNBR=$( nproc )
 
 # Run SQ with or without reference branch
 if [[ "PR" == "$1" ]] ; then
+  echo "Getting SQ cache"
+  get_cache
   echo "Running SQ in PR mode"
   /src/tmp/sonar-scanner/bin/sonar-scanner -X -Dsonar.scm.provider=git -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROCNBR" -Dsonar.scm.provider=git -Dsonar.login="$2" -Dsonar.host.url="$3" -Dsonar.projectVersion="$4" -Dsonar.pullrequest.branch="$5" -Dsonar.pullrequest.base="$6" -Dsonar.pullrequest.key="$7"
 else
+  echo "Cleaning previous cache"
+  clean_previous_cache
   echo "Running SQ in branch mode"
   /src/tmp/sonar-scanner/bin/sonar-scanner -X -Dsonar.scm.provider=git -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROCNBR" -Dsonar.scm.provider=git -Dsonar.login="$2" -Dsonar.host.url="$3" -Dsonar.projectVersion="$4" -Dsonar.branch.name="$5"
+  echo "Saving new cache"
+  set_cache
 fi
