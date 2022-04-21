198f50270c9bb18fb2c380d98f235fb85e99d57b
David Boucher <dboucher@centreon.com>
2022-04-21 19:19:47 +0200

    fix(tests:init-bam): the script is idempotent now

--- tests/init-bam.sh
+++ tests/init-bam.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mysql centreon -u centreon -p < ../broker/simu/docker/bam.sql
+mysql -u root -pcentreon -e "drop database centreon"
+mysql -u root -pcentreon < resources/centreon.sql
+#mysql -u root -pcentreon -e "SET GLOBAL FOREIGN_KEY_CHECKS=0"
+#mysql centreon -u root -pcentreon < ../broker/simu/docker/bam.sql
+#mysql -u root -pcentreon -e "SET GLOBAL FOREIGN_KEY_CHECKS=1"
