2690805f0260fd012c7fe4de48c93410083431e2
David Boucher <dboucher@centreon.com>
2022-04-22 16:50:18 +0200

    fix(regressions): missed changes from develop added

--- broker/bam/src/bool_not.cc
+++ broker/bam/src/bool_not.cc
@@ -91,7 +91,7 @@ void bool_not::set_value(std::shared_ptr<bool_value>& value) {
  *  @return Hard value.
  */
 double bool_not::value_hard() {
-  return _value->value_hard() == 0;
+  return std::abs(_value->value_hard()) < eps;
 }
 
 /**
@@ -100,7 +100,7 @@ double bool_not::value_hard() {
  *  @return Soft value.
  */
 double bool_not::value_soft() {
-  return _value->value_soft() == 0;
+  return std::abs(_value->value_soft()) < eps;
 }
 
 /**
