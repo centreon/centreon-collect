fedb2ceb5feee5ce601f08499687e0be2fb797e3
schapron <schapron@centreon.com>
2022-04-26 16:14:45 +0200

    fix cannot remove tmp

--- ci/scripts/collect-sonar-scanner-common.sh
+++ ci/scripts/collect-sonar-scanner-common.sh
@@ -21,14 +21,14 @@ install_scanner() {
   sudo apt-get install unzip || exit
 
   # Cleaning
-  rm -rf tmp
+  sudo rm -rf tmp
   mkdir tmp
   cd tmp
 
   # Getting latest archive
   curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip --output sonar-scanner-cli-4.7.0.2747-linux.zip
   unzip sonar-scanner-cli-4.7.0.2747-linux.zip
-  rm -rf sonar-scanner-cli-4.7.0.2747-linux.zip sonar-scanner
+  sudo rm -rf sonar-scanner-cli-4.7.0.2747-linux.zip sonar-scanner
   sudo mv sonar-scanner-4.7.0.2747-linux sonar-scanner
 }
 
