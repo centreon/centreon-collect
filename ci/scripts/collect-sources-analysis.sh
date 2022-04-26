7e81e5ce106450525cd5eb94f8c1c06ae06b4280
schapron <schapron@centreon.com>
2022-04-26 16:43:39 +0200

    remove debug

--- ci/scripts/collect-sources-analysis.sh
+++ ci/scripts/collect-sources-analysis.sh
@@ -10,6 +10,7 @@ DISTRIB=$( lsb_release -rs | cut -f1 -d. )
 if [[ "$DISTRIB" = "7" ]] ; then
     source /opt/rh/devtoolset-9/enable
 fi
+
 conan install .. -s compiler.cppstd=14 -s compiler.libcxx=libstdc++11 --build=missing
 if [[ $(cat /etc/issue | awk '{print $1}') = "Debian" ]] ; then
     CXXFLAGS="-Wall -Wextra" cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DWITH_CENTREON_CLIB_INCLUDE_DIR=../clib/inc/ -DWITH_CENTREON_CLIB_LIBRARIES=centreon-clib/libcentreon_clib.so -DCMAKE_BUILD_TYPE=Debug -DWITH_PREFIX=/usr -DWITH_PREFIX_BIN=/usr/sbin -DWITH_USER_BROKER=centreon-broker -DWITH_USER_ENGINE=centreon-engine -DWITH_GROUP_BROKER=centreon-broker -DWITH_GROUP_ENGINE=centreon-engine -DWITH_TESTING=On -DWITH_PREFIX_MODULES=/usr/share/centreon/lib/centreon-broker -DWITH_PREFIX_CONF_BROKER=/etc/centreon-broker -DWITH_PREFIX_LIB_BROKER=/usr/lib64/nagios -DWITH_PREFIX_CONF_ENGINE=/etc/centreon-engine -DWITH_PREFIX_LIB_ENGINE=/usr/lib64/centreon-engine -DWITH_PREFIX_LIB_CLIB=/usr/lib64/ -DWITH_RW_DIR=/var/lib/centreon-engine/rw -DWITH_VAR_DIR=/var/log/centreon-engine -DWITH_MODULE_SIMU=On ..
@@ -41,27 +42,9 @@ if [[ "PR" == "$1" ]] ; then
     SET_CACHE=1
   fi
 
-echo "DEBUG tmp state"
-pwd
-ls -la
-
-
-echo "DEBUG src state"
-cd /src
-pwd
-ls -la
-
-
-echo "DEBUG build state"
-cd /src/build
-pwd
-ls -la
-
-
-
   echo "INFO: Running SQ in PR mode ..."
   cd /src
-  /src/tmp/sonar-scanner/bin/sonar-scanner -X -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROCNBR" -Dsonar.scm.provider=git -Dsonar.login="$2" -Dsonar.host.url="$3" -Dsonar.projectVersion="$VERSION" -Dsonar.pullrequest.branch="$5" -Dsonar.pullrequest.base="$6" -Dsonar.pullrequest.key="$7"
+  /src/tmp/sonar-scanner/bin/sonar-scanner -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROCNBR" -Dsonar.scm.provider=git -Dsonar.login="$2" -Dsonar.host.url="$3" -Dsonar.projectVersion="$VERSION" -Dsonar.pullrequest.branch="$5" -Dsonar.pullrequest.base="$6" -Dsonar.pullrequest.key="$7"
 else
   echo "INFO: Cleaning previous run files ..."
   if [[ -d "/src/.scannerwork" ]]; then
@@ -69,7 +52,7 @@ else
   fi
 
   echo "INFO: Running SQ in branch mode ..."
-  /src/tmp/sonar-scanner/bin/sonar-scanner -X -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROCNBR" -Dsonar.scm.provider=git -Dsonar.login="$2" -Dsonar.host.url="$3" -Dsonar.projectVersion="$VERSION" -Dsonar.branch.name="$5"
+  /src/tmp/sonar-scanner/bin/sonar-scanner -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROCNBR" -Dsonar.scm.provider=git -Dsonar.login="$2" -Dsonar.host.url="$3" -Dsonar.projectVersion="$VERSION" -Dsonar.branch.name="$5"
 
   # Ask for cache saving
   SET_CACHE=1
@@ -79,8 +62,6 @@ fi
 if [[ 1 -eq "$SET_CACHE" ]]; then
   echo "INFO: Cleaning tmp folder ..."
   cd /src/tmp
-pwd
-ls -la
   rm -f "$PROJECT-SQ-cache-$VERSION.tar.gz"
 
   echo "INFO: Moving cache as tarball ..."
