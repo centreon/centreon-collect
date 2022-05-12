2a9c5b38f7af8c109a54b6d11dcb57d7166ba94b
rem31 <73845199+rem31@users.noreply.github.com>
2022-05-12 14:50:33 +0000

    embed-sonar-in-dockerfile

--- ci/scripts/collect-sources-analysis.sh
+++ ci/scripts/collect-sources-analysis.sh
@@ -59,6 +59,7 @@ if [[ "PR" == "$MODE" ]] ; then
 
   echo "INFO: Running SQ in PR mode ..."
   cd /src
+  echo "$SONAR_USER_HOME"
   /sonar-scanner/bin/sonar-scanner -X -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROC_NBR" -Dsonar.scm.provider=git -Dsonar.login="$AUTH_TOKEN" -Dsonar.host.url="$URL" -Dsonar.projectVersion="$VERSION" -Dsonar.pullrequest.base="$TARGET" -Dsonar.pullrequest.branch="$PR_BRANCH" -Dsonar.pullrequest.key="$PR_KEY"
 
   mkdir tmp
@@ -75,7 +76,8 @@ else
   # # fi
 
   echo "INFO: Running SQ in branch mode ..."
-
+  cd /src
+  echo "$SONAR_USER_HOME"
   /sonar-scanner/bin/sonar-scanner -X -Dsonar.scm.forceReloadAll=true -Dsonar.cfamily.threads="$PROC_NBR" -Dsonar.scm.provider=git -Dsonar.login="$AUTH_TOKEN" -Dsonar.host.url="$URL" -Dsonar.projectVersion="$VERSION" -Dsonar.branch.name="$TARGET"
 
   echo "INFO: Cleaning tmp folder ..."
