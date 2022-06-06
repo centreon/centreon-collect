3d7e95526a8478037a6e559dfaf0736d8ed09aff
rem31 <73845199+rem31@users.noreply.github.com>
2022-06-06 13:07:08 +0000

    enh(jenkins/robot): add robot in nightly job ci

--- ci/scripts/collect-test-robot.sh
+++ ci/scripts/collect-test-robot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-USERNAME = $1
+export RUN_ENV=docker
+
+USERTOKEN = $1
 TOKEN = $2
 BRANCH = $3
-
-export RUN_ENV=docker
 echo "########################### configure and start sshd ############################"
 ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
 ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
