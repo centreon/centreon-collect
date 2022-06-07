dd052eb5547cc127e3ba440fc38366dbb054a887
rem31 <73845199+rem31@users.noreply.github.com>
2022-06-07 13:00:50 +0000

    enh(jenkins/robot): add robot in nightly job ci

--- ci/scripts/collect-test-robot.sh
+++ ci/scripts/collect-test-robot.sh
@@ -4,7 +4,7 @@ set -e
 export RUN_ENV=docker
 
 USERNAME="$1"
-TOKENJENKINS="$2"
+TOKENJENKINS=$(aws ssm get-parameter --name service-account-ci-token --output text --query Parameter.Value)
 BRANCH="$3"
 
 
