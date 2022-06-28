bf05c9bc6ca25628b3f8d053b7edacc2f3c5dee9
David Boucher <dboucher@centreon.com>
2022-06-28 07:00:48 -0400

    fix(grpc): python script initialization fixed

--- grpc/init-proto.sh
+++ grpc/init-proto.sh
@@ -1,4 +1,4 @@
-python3 -m grpc_tools.protoc -I../centreon-engine/enginerpc --python_out=. --grpc_python_out=. engine.proto
-python3 -m grpc_tools.protoc -I../centreon-broker/core/src --python_out=. --grpc_python_out=. broker.proto
+python3 -m grpc_tools.protoc -I../engine/enginerpc --python_out=. --grpc_python_out=. engine.proto
+python3 -m grpc_tools.protoc -I../broker/core/src --python_out=. --grpc_python_out=. broker.proto
 #python3 -m grpc_tools.protoc --proto_path=../centreon-broker/core/src/ --python_out=. --grpc_python_out=. ../centreon-broker/core/src/broker.proto
 
