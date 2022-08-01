e87cd8ae89a71401ba649a5b5e7ce3a585d1ba6a
David Boucher <dboucher@centreon.com>
2022-08-01 14:43:33 +0200

    fix(tests/python): dateutil missing

--- ci/scripts/collect-test-robot.sh
+++ ci/scripts/collect-test-robot.sh
@@ -45,7 +45,7 @@ rpm -i centreon-broker*.el7.x86_64.rpm \
 
 echo "########################### install robot framework ############################"
 cd /src/tests/
-pip3 install -U robotframework robotframework-databaselibrary pymysql
+pip3 install -U robotframework robotframework-databaselibrary pymysql dateutil
 
 yum install "Development Tools" python3-devel -y
 
