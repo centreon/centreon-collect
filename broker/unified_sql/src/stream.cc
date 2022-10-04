1746fe2d9f3316a738a95cb942f0bde222987f2c
Jean Christophe Roques <jcroques@centreon.com>
2022-10-04 09:59:14 +0200

    MON-15246 add protobuf custom variable

--- broker/unified_sql/src/stream.cc
+++ broker/unified_sql/src/stream.cc
@@ -53,7 +53,7 @@ void (stream::*const stream::_neb_processing_table[])(
     &stream::_process_acknowledgement,
     &stream::_process_comment,
     &stream::_process_custom_variable,
-    &stream::_process_custom_variable_status,
+    nullptr,
     &stream::_process_downtime,
     &stream::_process_event_handler,
     nullptr,
