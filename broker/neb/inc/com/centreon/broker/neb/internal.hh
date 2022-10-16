8c439964c9219968faf802870240ab70cfcb143a
David Boucher <dboucher@centreon.com>
2022-10-16 22:19:12 +0200

    enh(broker/neb): module converted to protobuf.
    REFS: MON-15410

--- broker/neb/inc/com/centreon/broker/neb/internal.hh
+++ broker/neb/inc/com/centreon/broker/neb/internal.hh
@@ -91,6 +91,7 @@ using pb_instance_status =
     io::protobuf<InstanceStatus,
                  make_type(io::neb, neb::de_pb_instance_status)>;
 using pb_module = io::protobuf<Module, make_type(io::neb, neb::de_pb_module)>;
+
 using pb_acknowledgement =
     io::protobuf<Acknowledgement,
                  make_type(io::neb, neb::de_pb_acknowledgement)>;
