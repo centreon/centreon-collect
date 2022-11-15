c422fdf65dbb07616e84c29dc55effe8a9958ca4
David Boucher <dboucher@centreon.com>
2022-11-15 17:44:38 +0100

    enh(engine/conf): inheritence almost works

--- engine/inc/com/centreon/engine/configuration/file_info.hh
+++ engine/inc/com/centreon/engine/configuration/file_info.hh
@@ -30,32 +30,27 @@ class file_info {
   std::string _path;
 
  public:
-  file_info(std::string const& path = "", unsigned int line = 0)
+  file_info(const std::string& path, uint32_t line)
       : _line(line), _path(path) {}
-  file_info(file_info const& right) { operator=(right); }
-  ~file_info() noexcept {}
-  file_info& operator=(file_info const& right) {
-    if (this != &right) {
-      _line = right._line;
-      _path = right._path;
+  file_info(file_info&& other) : _line{other._line}, _path{std::move(_path)} {}
+  ~file_info() noexcept = default;
+  file_info(const file_info&) = delete;
+  file_info& operator=(const file_info&) = delete;
+  file_info& operator=(file_info&& other) {
+    if (this != &other) {
+      _line = other._line;
+      _path = std::move(other._path);
     }
     return *this;
   }
-  bool operator==(file_info const& right) const noexcept {
-    return _line == right._line && _path == right._path;
-  }
-  bool operator!=(file_info const& right) const noexcept {
-    return !operator==(right);
-  }
+
   friend exceptions::error& operator<<(exceptions::error& err,
                                        file_info const& info) {
     err << "in file '" << info.path() << "' on line " << info.line();
     return err;
   }
   unsigned int line() const noexcept { return _line; }
-  void line(unsigned int line) noexcept { _line = line; }
-  std::string const& path() const noexcept { return _path; }
-  void path(std::string const& path) { _path = path; }
+  const std::string& path() const noexcept { return _path; }
 };
 }  // namespace configuration
 
