1f6cf2de8748ad4b72738edbe17e1c82941d6904
David Boucher <dboucher@centreon.com>
2022-11-17 10:59:47 +0100

    fix(ci): docker images has its own version based on conanfile

--- ci/scripts/collect-deb-package.sh
+++ ci/scripts/collect-deb-package.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
 
+if [ -z "$ROOT" ] ; then
+  ROOT=centreon-collect
+fi
+
 if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$DISTRIB" ] ; then
   echo "You need to specify VERSION / RELEASE variables"
   exit 1
 fi
 
-echo "################################################## PACKAGING COLLECT ##################################################"
+echo "############################# PACKAGING COLLECT ################################"
 
 AUTHOR="Luiz Costa"
 AUTHOR_EMAIL="me@luizgustavo.pro.br"
@@ -14,12 +18,12 @@ AUTHOR_EMAIL="me@luizgustavo.pro.br"
 # fix version to debian format accept
 VERSION="$(echo $VERSION | sed 's/-/./g')"
 
-if [ -d centreon-collect/build ] ; then
-    rm -rf centreon-collect/build
+if [ -d "$ROOT/build" ] ; then
+    rm -rf "$ROOT/build"
 fi
-rm -rf centreon-collect/gorgone
-tar czpf centreon-collect-$VERSION.tar.gz centreon-collect
-cd centreon-collect/
+rm -rf "$ROOT/gorgone"
+tar --exclude={".git","build"} -czpf centreon-collect-$VERSION.tar.gz "$ROOT"
+cd "$ROOT"
 cp -rf ci/debian-collect debian
 sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION-$RELEASE)/" debian/substvars
 #sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" debian/substvars
