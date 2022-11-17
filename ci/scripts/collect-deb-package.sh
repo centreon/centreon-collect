4f351a6546a25e567bb2c45e30a86bf49504dee5
David Boucher <dboucher@centreon.com>
2022-11-17 15:06:26 +0100

    fix(ci): backport of ci in develop to 22.04

---
+++ ci/scripts/collect-deb-package.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+
+if [ -z "$ROOT" ] ; then
+  ROOT=centreon-collect
+fi
+
+if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$DISTRIB" ] ; then
+  echo "You need to specify VERSION / RELEASE variables"
+  exit 1
+fi
+
+echo "############################# PACKAGING COLLECT ################################"
+
+AUTHOR="Luiz Costa"
+AUTHOR_EMAIL="me@luizgustavo.pro.br"
+
+# fix version to debian format accept
+VERSION="$(echo $VERSION | sed 's/-/./g')"
+
+if [ -d "$ROOT/build" ] ; then
+    rm -rf "$ROOT/build"
+fi
+rm -rf "$ROOT/gorgone"
+tar --exclude={".git","build"} -czpf centreon-collect-$VERSION.tar.gz "$ROOT"
+cd "$ROOT"
+cp -rf ci/debian-collect debian
+sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION-$RELEASE)/" debian/substvars
+#sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" debian/substvars
+debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -r "$DISTRIB"
+debuild-pbuilder
+cd ../
