2772751200b17455392d94b480aa719a60180848
Jean Christophe Roques <jcroques@centreon.com>
2023-01-04 09:47:30 +0100

    MON-16310 fix tests ESS3 ESS4 BEDB4 and BLDIS1

--- tests/init-sql.sh
+++ tests/init-sql.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 DBUserRoot=$(awk '($1=="${DBUserRoot}") {print $2}' resources/db_variables.robot)
 DBPassRoot=$(awk '($1=="${DBPassRoot}") {print $2}' resources/db_variables.robot)
+DBStorage=$(awk '($1=="${DBName}") {print $2}' resources/db_variables.robot)
+DBConf=$(awk '($1=="${DBNameConf}") {print $2}' resources/db_variables.robot)
 
 if [ -z $DBUserRoot ] ; then
     DBUserRoot="root"
@@ -11,6 +12,18 @@ if [ -z $DBPassRoot ] ; then
     DBPassRoot="centreon"
 fi
 
-mysql --user="$DBUserRoot" --password="$DBPassRoot" -e "drop database centreon"
-mysql --user="$DBUserRoot" --password="$DBPassRoot" < ../resources/centreon.sql
-mysql --user="$DBUserRoot" --password="$DBPassRoot" < ../resources/centreon_storage.sql
+
+
+mysql --user="$DBUserRoot" --password="$DBPassRoot" -e "drop database ${DBConf}"
+cat ../resources/centreon.sql | sed "s/DBNameConf/${DBConf}/g" > /tmp/centreon.sql
+
+mysql --user="$DBUserRoot" --password="$DBPassRoot" < /tmp/centreon.sql
+
+if [ $DBStorage != 'centreon_storage' ]
+then
+    \rm /tmp/centreon_storage.sql
+    cat ../resources/centreon_storage.sql | sed "s/centreon_storage/${DBStorage}/g" > /tmp/centreon_storage.sql
+    mysql --user="$DBUserRoot" --password="$DBPassRoot" < /tmp/centreon_storage.sql    
+else
+    mysql --user="$DBUserRoot" --password="$DBPassRoot" < ../resources/centreon_storage.sql
+fi
