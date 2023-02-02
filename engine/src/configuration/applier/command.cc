78b857296167d4b293800ce48d2258209b2553a8
David Boucher <dboucher@centreon.com>
2023-02-02 08:58:18 +0100

    enh(engine/configuration): timeperiod migration

--- engine/src/configuration/applier/command.cc
+++ engine/src/configuration/applier/command.cc
@@ -121,11 +121,74 @@ void applier::command::expand_objects(configuration::state& s
                                       [[maybe_unused]]) {}
 
 /**
+ * @brief Modify command.
+ *
+ * @param obj The new command protobuf configuration for the object to modify
+ * in the monitoring engine.
+ */
+void applier::command::modify_object(const configuration::Command& obj) {
+  // Logging.
+  log_v2::config()->debug("Modifying command '{}'.", obj.command_name());
+
+  assert(98717 == 1782);
+  //  // Find old configuration.
+  //  set_command::iterator it_cfg(config->commands_find(obj.key()));
+  //  if (it_cfg == config->commands().end())
+  //    throw(engine_error() << "Cannot modify non-existing "
+  //                         << "command '" << obj.command_name() << "'");
+  //
+  //  // Find command object.
+  //  command_map::iterator it_obj(commands::command::commands.find(obj.key()));
+  //  if (it_obj == commands::command::commands.end())
+  //    throw(engine_error() << "Could not modify non-existing "
+  //                         << "command object '" << obj.command_name() <<
+  //                         "'");
+  //  commands::command* c(it_obj->second.get());
+  //
+  //  // Update the global configuration set.
+  //  config->commands().erase(it_cfg);
+  //  config->commands().insert(obj);
+  //
+  //  // Modify command.
+  //  if (c->get_command_line() != obj.command_line())
+  //    c->set_command_line(obj.command_line());
+  //
+  //  // Command will be temporarily removed from the command set but
+  //  // will be added back right after with _create_command. This does
+  //  // not create dangling pointers since commands::command object are
+  //  // not referenced anywhere, only ::command objects are.
+  //  commands::command::commands.erase(obj.command_name());
+  //  if (obj.connector().empty()) {
+  //    auto raw = std::make_shared<commands::raw>(
+  //        obj.command_name(), obj.command_line(),
+  //        &checks::checker::instance());
+  //    commands::command::commands[raw->get_name()] = raw;
+  //  } else {
+  //    connector_map::iterator found_con{
+  //        commands::connector::connectors.find(obj.connector())};
+  //    if (found_con != commands::connector::connectors.end() &&
+  //        found_con->second) {
+  //      std::shared_ptr<commands::forward> forward{
+  //          std::make_shared<commands::forward>(
+  //              obj.command_name(), obj.command_line(), found_con->second)};
+  //      commands::command::commands[forward->get_name()] = forward;
+  //    } else
+  //      throw engine_error() << "Could not register command '"
+  //                           << obj.command_name() << "': unable to find '"
+  //                           << obj.connector() << "'";
+  //  }
+  //  // Notify event broker.
+  //  timeval tv(get_broker_timestamp(NULL));
+  //  broker_command_data(NEBTYPE_COMMAND_UPDATE, NEBFLAG_NONE, NEBATTR_NONE, c,
+  //                      &tv);
+}
+
+/**
  *  Modified command.
  *
  *  @param[in] obj The new command to modify into the monitoring engine.
  */
-void applier::command::modify_object(configuration::command const& obj) {
+void applier::command::modify_object(const configuration::command& obj) {
   // Logging.
   engine_logger(logging::dbg_config, logging::more)
       << "Modifying command '" << obj.command_name() << "'.";
@@ -213,6 +276,22 @@ void applier::command::remove_object(configuration::command const& obj) {
   config->commands().erase(obj);
 }
 
+/**
+ *  @brief Resolve command.
+ *
+ *  This method will check for its connector's existence, if command is
+ *  configured to use one.
+ *
+ *  @param[in] obj  Command object.
+ */
+void applier::command::resolve_object(const configuration::Command& obj) {
+  if (!obj.connector().empty()) {
+    connector_map::iterator found =
+        commands::connector::connectors.find(obj.connector());
+    if (found == commands::connector::connectors.end() || !found->second)
+      throw engine_error() << "unknow command " << obj.connector();
+  }
+}
 /**
  *  @brief Resolve command.
  *
