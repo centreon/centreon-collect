6858e02382cfb56e5f6849c33269e6287bd5cc4e
David Boucher <dboucher@centreon.com>
2023-02-06 16:01:42 +0100

    fix(engine/configuration): severity is good with protobuf

--- engine/src/configuration/applier/command.cc
+++ engine/src/configuration/applier/command.cc
@@ -126,61 +126,52 @@ void applier::command::expand_objects(configuration::state& s
  * @param obj The new command protobuf configuration for the object to modify
  * in the monitoring engine.
  */
-void applier::command::modify_object(const configuration::Command& obj) {
+void applier::command::modify_object(configuration::Command* to_modify,
+                                     const configuration::Command& new_obj) {
   // Logging.
-  log_v2::config()->debug("Modifying command '{}'.", obj.command_name());
+  log_v2::config()->debug("Modifying command '{}'.", new_obj.command_name());
 
-  assert(98717 == 1782);
-  //  // Find old configuration.
-  //  set_command::iterator it_cfg(config->commands_find(obj.key()));
-  //  if (it_cfg == config->commands().end())
-  //    throw(engine_error() << "Cannot modify non-existing "
-  //                         << "command '" << obj.command_name() << "'");
-  //
-  //  // Find command object.
-  //  command_map::iterator it_obj(commands::command::commands.find(obj.key()));
-  //  if (it_obj == commands::command::commands.end())
-  //    throw(engine_error() << "Could not modify non-existing "
-  //                         << "command object '" << obj.command_name() <<
-  //                         "'");
-  //  commands::command* c(it_obj->second.get());
-  //
-  //  // Update the global configuration set.
-  //  config->commands().erase(it_cfg);
-  //  config->commands().insert(obj);
-  //
-  //  // Modify command.
-  //  if (c->get_command_line() != obj.command_line())
-  //    c->set_command_line(obj.command_line());
-  //
-  //  // Command will be temporarily removed from the command set but
-  //  // will be added back right after with _create_command. This does
-  //  // not create dangling pointers since commands::command object are
-  //  // not referenced anywhere, only ::command objects are.
+  // Find command object.
+  command_map::iterator it_obj =
+      commands::command::commands.find(new_obj.command_name());
+  if (it_obj == commands::command::commands.end())
+    throw engine_error() << fmt::format(
+        "Could not modify non-existing command object '{}'",
+        new_obj.command_name());
+
+  // Update the global configuration set.
+  to_modify->CopyFrom(new_obj);
+
+  // Command will be temporarily removed from the command set but
+  // will be added back right after with _create_command. This does
+  // not create dangling pointers since commands::command object are
+  // not referenced anywhere, only ::command objects are.
   //  commands::command::commands.erase(obj.command_name());
-  //  if (obj.connector().empty()) {
-  //    auto raw = std::make_shared<commands::raw>(
-  //        obj.command_name(), obj.command_line(),
-  //        &checks::checker::instance());
-  //    commands::command::commands[raw->get_name()] = raw;
-  //  } else {
-  //    connector_map::iterator found_con{
-  //        commands::connector::connectors.find(obj.connector())};
-  //    if (found_con != commands::connector::connectors.end() &&
-  //        found_con->second) {
-  //      std::shared_ptr<commands::forward> forward{
-  //          std::make_shared<commands::forward>(
-  //              obj.command_name(), obj.command_line(), found_con->second)};
-  //      commands::command::commands[forward->get_name()] = forward;
-  //    } else
-  //      throw engine_error() << "Could not register command '"
-  //                           << obj.command_name() << "': unable to find '"
-  //                           << obj.connector() << "'";
-  //  }
-  //  // Notify event broker.
-  //  timeval tv(get_broker_timestamp(NULL));
-  //  broker_command_data(NEBTYPE_COMMAND_UPDATE, NEBFLAG_NONE, NEBATTR_NONE, c,
-  //                      &tv);
+  if (new_obj.connector().empty()) {
+    auto raw = std::make_shared<commands::raw>(new_obj.command_name(),
+                                               new_obj.command_line(),
+                                               &checks::checker::instance());
+    it_obj->second = raw;
+  } else {
+    connector_map::iterator found_con{
+        commands::connector::connectors.find(new_obj.connector())};
+    if (found_con != commands::connector::connectors.end() &&
+        found_con->second) {
+      std::shared_ptr<commands::forward> forward{
+          std::make_shared<commands::forward>(new_obj.command_name(),
+                                              new_obj.command_line(),
+                                              found_con->second)};
+      it_obj->second = forward;
+    } else
+      throw engine_error() << fmt::format(
+          "Could not register command '{}': unable to find '{}'",
+          new_obj.command_name(), new_obj.connector());
+  }
+  // Notify event broker.
+  timeval tv(get_broker_timestamp(NULL));
+  commands::command* c = it_obj->second.get();
+  broker_command_data(NEBTYPE_COMMAND_UPDATE, NEBFLAG_NONE, NEBATTR_NONE, c,
+                      &tv);
 }
 
 /**
@@ -242,6 +233,37 @@ void applier::command::modify_object(const configuration::command& obj) {
   timeval tv(get_broker_timestamp(NULL));
   broker_command_data(NEBTYPE_COMMAND_UPDATE, NEBFLAG_NONE, NEBATTR_NONE, c,
                       &tv);
+}
+
+/**
+ * @brief Remove a protobuf command configuration at index idx
+ *
+ * @param idx The position in configuration of the configuration to remove.
+ */
+void applier::command::remove_object(ssize_t idx) {
+  const configuration::Command& obj = pb_config.commands()[idx];
+  // Logging.
+  log_v2::config()->debug("Removing command '{}'.", obj.command_name());
+
+  // Find command.
+  std::unordered_map<std::string, std::shared_ptr<commands::command> >::iterator
+      it = commands::command::commands.find(obj.command_name());
+  if (it != commands::command::commands.end()) {
+    commands::command* cmd(it->second.get());
+
+    // Notify event broker.
+    timeval tv(get_broker_timestamp(NULL));
+    broker_command_data(NEBTYPE_COMMAND_DELETE, NEBFLAG_NONE, NEBATTR_NONE, cmd,
+                        &tv);
+
+    // Erase command (will effectively delete the object).
+    commands::command::commands.erase(it);
+  } else
+    throw engine_error() << fmt::format(
+        "Could not remove command '{}': it does not exist", obj.command_name());
+
+  // Remove command from the global configuration set.
+  pb_config.mutable_commands()->DeleteSubrange(idx, 1);
 }
 
 /**
