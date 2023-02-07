bb49061be387b4be9ddd95290fbc1d3695a4a802
Kevin Duret <kduret@centreon.com>
2023-02-07 20:57:33 +0100

    optim

--- ci/scripts/collect-rpm-package.sh
+++ ci/scripts/collect-rpm-package.sh
@@ -16,22 +16,17 @@ echo "########################### BUILDING COLLECT ############################"
 
 
 # generate rpm broker
-if [ ! -d /root/rpmbuild/SOURCES ] ; then
-    mkdir -p /root/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+if [ ! -d ~/rpmbuild/SOURCES ] ; then
+    mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 fi
 
-tar czf /root/rpmbuild/SOURCES/centreon-collect-$VERSION.tar.gz \
+tar czf ~/rpmbuild/SOURCES/centreon-collect-$VERSION.tar.gz \
       --exclude './build' \
       --exclude './.git'  \
       --transform "s,^\.,centreon-collect-$VERSION," .
 
-cp packaging/rpm/centreonengine_integrate_centreon_engine2centreon.sh /root/rpmbuild/SOURCES/
-
-#echo -e "%_topdir      %(echo $HOME)/rpmbuild\n%_smp_mflags  -j9\n" > $HOME/rpmbuild/.rpmmacros
+cp packaging/rpm/centreonengine_integrate_centreon_engine2centreon.sh ~/rpmbuild/SOURCES/
 
 rpmbuild -ba packaging/rpm/centreon-collect.spec -D "VERSION $VERSION" -D "RELEASE $RELEASE"
 
-# cleaning and according permissions to slave to delivery rpms
-rm -rf *.rpm
-cp -r /root/rpmbuild/RPMS/x86_64/*.rpm .
-chmod 777 *.rpm
+cp -r ~/rpmbuild/RPMS/x86_64/*.rpm .
