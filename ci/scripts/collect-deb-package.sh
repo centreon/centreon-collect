3e769abee1ee8b9a583a100070eec4faec9c1c47
schapron <schapron@centreon.com>
2023-03-03 16:47:01 +0100

    chore(debian): manage version threshold

--- ci/scripts/collect-deb-package.sh
+++ ci/scripts/collect-deb-package.sh
@@ -6,7 +6,7 @@ if [ -z "$ROOT" ] ; then
 fi
 
 if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$DISTRIB" ] ; then
-  echo "You need to specify VERSION / RELEASE variables"
+  echo "You need to specify VERSION / RELEASE / DISTRIB variables"
   exit 1
 fi
 
@@ -18,6 +18,15 @@ AUTHOR_EMAIL="contact@centreon.com"
 # fix version to debian format accept
 VERSION="$(echo $VERSION | sed 's/-/./g')"
 
+MAJOR_LEFT_PART=$( echo $VERSION | cut -d "." -f1 )
+MAJOR_RIGHT_PART=$( echo $VERSION | cut -d "-" -f1 | cut -d "." -f2 )
+BUMP_MAJOR_RIGHT_PART=$(( MAJOR_RIGHT_PART+1 ))
+if [ ${#BUMP_MAJOR_RIGHT_PART} -eq 1 ]; then
+    # Add a zero before the new single numeric char
+    BUMP_MAJOR_RIGHT_PART="0$BUMP_MAJOR_RIGHT_PART"
+fi
+MAJOR_THRESHOLD="$MAJOR_LEFT_PART.$BUMP_MAJOR_RIGHT_PART"
+
 if [ -d "$ROOT/build" ] ; then
     rm -rf "$ROOT/build"
 fi
@@ -26,6 +35,8 @@ tar --exclude={".git","build"} -czpf centreon-collect-$VERSION.tar.gz "$ROOT"
 cd "$ROOT"
 cp -rf ci/debian-collect debian
 sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION-$RELEASE)/" debian/substvars
+sed -i "s/^centreon:versionThreshold=.*$/centreon:versionThreshold=$(echo $MAJOR_THRESHOLD | egrep -o '^[0-9][0-9].[0-9][0-9]')/" debian/substvars
+
 echo "debmake begin"
 debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -r "$RELEASE"
 echo "version de dwz"
