4aaba21abb6dcd048b9d758edffc61239f4186a9
David Boucher <dboucher@centreon.com>
2023-03-10 16:01:22 +0100

    fix(broker/muxer): reuse muxer in case of reload

--- broker/core/src/multiplexing/muxer.cc
+++ broker/core/src/multiplexing/muxer.cc
@@ -33,6 +33,9 @@ using namespace com::centreon::broker::multiplexing;
 
 uint32_t muxer::_event_queue_max_size = std::numeric_limits<uint32_t>::max();
 
+std::mutex muxer::_running_muxers_m;
+absl::flat_hash_map<std::string, std::weak_ptr<muxer>> muxer::_running_muxers;
+
 /**
  *  Constructor.
  *
@@ -120,9 +123,28 @@ std::shared_ptr<muxer> muxer::create(std::string name,
                                      muxer::filters r_filters,
                                      muxer::filters w_filters,
                                      bool persistent) {
-  std::shared_ptr<muxer> ret(new muxer(name, r_filters, w_filters, persistent));
-  engine::instance_ptr()->subscribe(ret);
-  return ret;
+  std::shared_ptr<muxer> retval;
+  {
+    std::lock_guard<std::mutex> lck(_running_muxers_m);
+    absl::erase_if(_running_muxers,
+                   [](const std::pair<std::string, std::weak_ptr<muxer>>& p) {
+                     return p.second.expired();
+                   });
+    retval = _running_muxers[name].lock();
+    if (retval) {
+      log_v2::config()->debug("muxer: muxer '{}' already exists, reusing it",
+                              name);
+      retval->set_filters(r_filters, w_filters);
+    } else {
+      log_v2::config()->debug("muxer: muxer '{}' unknown, creating it", name);
+      retval = std::shared_ptr<muxer>(
+          new muxer(name, r_filters, w_filters, persistent));
+      _running_muxers[name] = retval;
+    }
+  }
+
+  engine::instance_ptr()->subscribe(retval);
+  return retval;
 }
 
 /**
@@ -548,4 +570,12 @@ void muxer::remove_queue_files() {
 
 const std::string& muxer::name() const {
   return _name;
+}
+
+void muxer::set_filters(muxer::filters r_filters, muxer::filters w_filters) {
+  std::lock_guard<std::mutex> lck(_mutex);
+  _read_filters = std::move(r_filters);
+  _write_filters = std::move(w_filters);
+  _read_filters_str = misc::dump_filters(_read_filters);
+  _write_filters_str = misc::dump_filters(_write_filters);
 }
