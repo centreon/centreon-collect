efbc3a9960d3ed64e373a8a3213d18471b377f91
David Boucher <dboucher@centreon.com>
2023-05-30 09:34:53 +0200

    cleanup(broker/stats_exporter): dead code removed

--- broker/stats_exporter/src/main.cc
+++ broker/stats_exporter/src/main.cc
@@ -39,49 +39,6 @@ static uint32_t instances = 0;
 
 std::unique_ptr<stats_exporter::exporter> expt;
 
-// static void export_to_http(const std::string& url) {
-//  opentelemetry::exporter::otlp::OtlpHttpMetricExporterOptions otlpOptions;
-//  /* url should be of the form http://XX.XX.XX.XX:4318/v1/metrics */
-//  otlpOptions.url = url;
-//  otlpOptions.aggregation_temporality =
-//  opentelemetry::sdk::metrics::AggregationTemporality::kCumulative; auto
-//  exporter =
-//      opentelemetry::exporter::otlp::OtlpHttpMetricExporterFactory::Create(
-//          otlpOptions);
-//  // Initialize and set the periodic metrics reader
-//  metric_sdk::PeriodicExportingMetricReaderOptions options;
-//  options.export_interval_millis = std::chrono::milliseconds(1000);
-//  options.export_timeout_millis = std::chrono::milliseconds(500);
-//  std::unique_ptr<metric_sdk::MetricReader> reader{
-//        new metric_sdk::PeriodicExportingMetricReader(std::move(exporter),
-//                                                      options)};
-//
-//  // Initialize meter provider
-//  auto provider = std::shared_ptr<metrics_api::MeterProvider>(
-//      new metric_sdk::MeterProvider());
-//  auto p = std::static_pointer_cast<metric_sdk::MeterProvider>(provider);
-//  p->AddMetricReader(std::move(reader));
-//  metrics_api::Provider::SetMeterProvider(provider);
-//
-//  auto meter = provider->GetMeter("broker_stats_threadpool");
-//  s = meter->CreateInt64ObservableGauge("size", "Number of threads in the
-//  thread pool"); s->AddCallback(update_threadpool_size, nullptr);
-
-// auto s = meter->CreateUInt64Counter("size", "Number of threads in the thread
-// pool"); s->Add(tp.size());
-
-//    auto updown_counter = meter->CreateInt64UpDownCounter("toto_le_hero", "a
-//    well known hero"); updown_counter->Add(1); updown_counter->Add(-5);
-//      auto test_gauge = meter->CreateDoubleObservableGauge(
-//          "test_gauge", "a gauge to test", "p");
-//      test_gauge->AddCallback(my_cb, nullptr);
-//      // Create Gauge
-//      for (int i = 0; i < 100; i++) {
-//        log_v2::core()->error("1 second");
-//        sleep(1);
-//      }
-//}
-
 extern "C" {
 /**
  *  Module version symbol. Used to check for version mismatch.
