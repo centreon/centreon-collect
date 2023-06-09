9f23f87901b1b744aaf09d21adae6d98d9783258
David Boucher <dboucher@centreon.com>
2023-06-09 12:59:17 +0200

    enh(tests): new test to validate the patch

--- tests/resources/Common.py
+++ tests/resources/Common.py
@@ -920,3 +920,34 @@ def get_version():
         elif m3:
             patch = m3.group(1)
     return f"{maj}.{mini}.{patch}"
+
+
+def check_service_resource_status_with_timeout(hostname: str, service_desc: str, status: int, timeout: int, state_type: str = "SOFT"):
+    limit = time.time() + timeout
+    while time.time() < limit:
+        connection = pymysql.connect(host=DB_HOST,
+                                     user=DB_USER,
+                                     password=DB_PASS,
+                                     autocommit=True,
+                                     database=DB_NAME_STORAGE,
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    f"SELECT r.status,r.status_confirmed FROM resources r LEFT JOIN services s ON r.id=s.service_id AND r.parent_id=s.host_id LEFT JOIN hosts h ON s.host_id=h.host_id WHERE h.name='{hostname}' AND s.description='{service_desc}'")
+                result = cursor.fetchall()
+                logger.console(
+                    f"result: {int(result[0]['status'])} status: {int(status)}")
+                if len(result) > 0 and result[0]['status'] is not None and int(result[0]['status']) == int(status):
+                    logger.console(
+                        f"status={result[0]['status']} and status_confirmed={result[0]['status_confirmed']}")
+                    if state_type == 'HARD' and int(result[0]['status_confirmed']) == 1:
+                        return True
+                    else:
+                        return True
+        time.sleep(1)
+    return False
+
+
