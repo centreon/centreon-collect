1d966cad0f9d11a5a176642c1f8f903cce2cbba7
Kevin Duret <kduret@centreon.com>
2023-06-22 17:44:04 +0200

    trigger

--- packaging/scripts/centreon-broker-cbmod-postinstall.sh
+++ packaging/scripts/centreon-broker-cbmod-postinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-getent passwd centreon-engine &>/dev/null && usermod -a -G centreon-broker centreon-engine
-getent group centreon-engine &>/dev/null && usermod -a -G centreon-engine centreon-broker
+getent passwd centreon-engine > /dev/null 2>&1 && usermod -a -G centreon-broker centreon-engine
+getent group centreon-engine > /dev/null 2>&1 && usermod -a -G centreon-engine centreon-broker
--- packaging/scripts/centreon-broker-core-preinstall.sh
+++ packaging/scripts/centreon-broker-core-preinstall.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-getent group centreon-broker &>/dev/null || groupadd -r centreon-broker 2> /dev/null || :
-getent passwd centreon-broker &>/dev/null || useradd -m -g centreon-broker -d /var/lib/centreon-broker -r centreon-broker 2> /dev/null || :
+getent group centreon-broker > /dev/null 2>&1 || groupadd -r centreon-broker 2> /dev/null || :
+getent passwd centreon-broker > /dev/null 2>&1 || useradd -m -g centreon-broker -d /var/lib/centreon-broker -r centreon-broker 2> /dev/null || :
 
-if id centreon &>/dev/null; then
+if id centreon > /dev/null 2>&1; then
   usermod -a -G centreon-broker centreon
 fi
 
-if id centreon-gorgone &>/dev/null; then
+if id centreon-gorgone > /dev/null 2>&1; then
   usermod -a -G centreon-gorgone centreon-broker
 fi
 
-if id centreon-engine &>/dev/null; then
+if id centreon-engine > /dev/null 2>&1; then
   usermod -a -G centreon-broker centreon-engine
   usermod -a -G centreon-engine centreon-broker
 fi
 
-if id nagios &>/dev/null; then
+if id nagios > /dev/null 2>&1; then
   usermod -a -G centreon-broker nagios
 fi
