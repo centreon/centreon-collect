26e044e8bd4b7319bc5dc97e6162b383103eaae8
David Boucher <dboucher@centreon.com>
2023-06-30 11:41:51 +0200

    fix(resources): sql definitions updated to match centreon web

--- resources/centreon_storage.sql
+++ resources/centreon_storage.sql
@@ -871,7 +871,7 @@ DROP TABLE IF EXISTS `metrics`;
 CREATE TABLE `metrics` (
   `metric_id` int(11) NOT NULL AUTO_INCREMENT,
   `index_id` bigint unsigned DEFAULT NULL,
-  `metric_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `metric_name` varchar(21792) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `data_source_type` enum('0','1','2','3') DEFAULT NULL,
   `unit_name` varchar(32) DEFAULT NULL,
   `current_value` float DEFAULT NULL,
@@ -887,7 +887,7 @@ CREATE TABLE `metrics` (
   `locked` enum('0','1') DEFAULT NULL,
   `to_delete` int(1) DEFAULT '0',
   PRIMARY KEY (`metric_id`),
-  UNIQUE KEY `index_id` (`index_id`,`metric_id`),
+  UNIQUE KEY `index_id` (`index_id`,`metric_name`),
   KEY `index` (`index_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5426 DEFAULT CHARSET=utf8;
 
