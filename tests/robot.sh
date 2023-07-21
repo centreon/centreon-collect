80a4896a328b5042dcbe4a96cc44bade003f00c8
David Boucher <dboucher@centreon.com>
2023-07-21 17:23:57 +0200

    enh(robot): this script forces the use of protobuf configuration

--- tests/robot.sh
+++ tests/robot.sh
@@ -4,7 +4,10 @@ sed -i -r 's/(\$\{DBUserRoot\}\s*)root_centreon/\1root/g' resources/db_variables
 ulimit -c unlimited
 sysctl -w kernel.core_pattern=/tmp/core-%e.%p.%h.%t
 
-robot -L TRACE $*
+export CENTENGINE_LEGACY=0
+
+#robot -L TRACE $*
+robot $*
 rep=$(date +%s)
 mkdir $rep
 mv report.html log.html output.xml $rep
