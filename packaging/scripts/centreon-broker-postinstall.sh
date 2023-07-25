506610ff8d4c39e957ec4a787bc81441af8384a3
Kevin Duret <kduret@centreon.com>
2023-07-25 18:09:46 +0200

    fix(packaging): fix user/group on debian post update (#840)

--- packaging/scripts/centreon-broker-postinstall.sh
+++ packaging/scripts/centreon-broker-postinstall.sh
@@ -5,6 +5,24 @@ if [ "$1" = "configure" ] ; then
   if [ ! "$(getent passwd centreon-broker)" ]; then
     adduser --system --group --home /var/lib/centreon-broker --no-create-home centreon-broker
   fi
+
+  if [ "$(getent passwd www-data)" ]; then
+    usermod -a -G centreon-broker www-data
+  fi
+
+  if [ "$(getent passwd centreon-gorgone)" ]; then
+    usermod -a -G centreon-gorgone centreon-broker
+  fi
+
+  if [ "$(getent passwd centreon-engine)" ]; then
+    usermod -a -G centreon-broker centreon-engine
+    usermod -a -G centreon-engine centreon-broker
+  fi
+
+  if [ "$(getent passwd nagios)" ]; then
+    usermod -a -G centreon-broker nagios
+  fi
+
   chown -R centreon-broker:centreon-broker \
     /etc/centreon-broker \
     /var/lib/centreon-broker \
--- packaging/scripts/centreon-broker-preinstall.sh
+++ packaging/scripts/centreon-broker-preinstall.sh
@@ -3,15 +3,26 @@
 getent group centreon-broker > /dev/null 2>&1 || groupadd -r centreon-broker 2> /dev/null || :
 getent passwd centreon-broker > /dev/null 2>&1 || useradd -m -g centreon-broker -d /var/lib/centreon-broker -r centreon-broker 2> /dev/null || :
 
-if id centreon-gorgone > /dev/null 2>&1; then
+
+if  [ "$1" = "configure" ]; then # deb
+  if [ "$(getent passwd www-data)" ]; then
+    usermod -a -G centreon-broker www-data
+  fi
+else # rpm
+  if [ "$(getent passwd apache)" ]; then
+    usermod -a -G centreon-broker apache
+  fi
+fi
+
+if [ "$(getent passwd centreon-gorgone)" ]; then
   usermod -a -G centreon-gorgone centreon-broker
 fi
 
-if id centreon-engine > /dev/null 2>&1; then
+if [ "$(getent passwd centreon-engine)" ]; then
   usermod -a -G centreon-broker centreon-engine
   usermod -a -G centreon-engine centreon-broker
 fi
 
-if id nagios > /dev/null 2>&1; then
+if [ "$(getent passwd nagios)" ]; then
   usermod -a -G centreon-broker nagios
 fi
