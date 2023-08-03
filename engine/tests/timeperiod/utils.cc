5adadfd77220da35093dfcf07c717ab8d9102326
David Boucher <dboucher@centreon.com>
2023-08-03 11:01:56 +0200

    fix(engine): issues with clang and also with asio fixed

--- engine/tests/timeperiod/utils.cc
+++ engine/tests/timeperiod/utils.cc
@@ -314,7 +314,7 @@ extern "C" time_t time(time_t* t) __THROW {
   return (gl_now);
 }
 
-extern "C" int gettimeofday(struct timeval* tv, struct timezone*) __THROW {
+extern "C" int gettimeofday(struct timeval* tv, void*) __THROW {
   if (tv) {
     tv->tv_sec = gl_now;
     tv->tv_usec = 0;
