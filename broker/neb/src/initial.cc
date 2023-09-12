fe78cc919e4e0dae1769ec4859ca94686e404753
David Boucher <dboucher@centreon.com>
2023-09-12 17:03:27 +0200

    fix(broker/log_v2/neb): log_v3 ok on neb

--- broker/neb/src/initial.cc
+++ broker/neb/src/initial.cc
@@ -18,7 +18,6 @@
 
 #include "com/centreon/broker/neb/initial.hh"
 #include "com/centreon/broker/config/applier/state.hh"
-#include "com/centreon/broker/log_v2.hh"
 #include "com/centreon/broker/neb/callbacks.hh"
 #include "com/centreon/broker/neb/events.hh"
 #include "com/centreon/broker/neb/internal.hh"
@@ -58,7 +57,7 @@ typedef int (*neb_sender)(int, void*);
 static void send_custom_variables_list(
     neb_sender sender = neb::callback_custom_variable) {
   // Start log message.
-  log_v2::neb()->info("init: beginning custom variables dump");
+  neb_logger->info("init: beginning custom variables dump");
 
   // Iterate through all hosts.
   for (host_map::iterator it{com::centreon::engine::host::hosts.begin()},
@@ -112,7 +111,7 @@ static void send_custom_variables_list(
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of custom variables dump");
+  neb_logger->info("init: end of custom variables dump");
 }
 
 static void send_pb_custom_variables_list() {
@@ -124,7 +123,7 @@ static void send_pb_custom_variables_list() {
  */
 static void send_downtimes_list() {
   // Start log message.
-  log_v2::neb()->info("init: beginning downtimes dump");
+  neb_logger->info("init: beginning downtimes dump");
 
   std::multimap<
       time_t,
@@ -162,7 +161,7 @@ static void send_downtimes_list() {
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of downtimes dump");
+  neb_logger->info("init: end of downtimes dump");
 }
 
 /**
@@ -170,7 +169,7 @@ static void send_downtimes_list() {
  */
 static void send_host_dependencies_list() {
   // Start log message.
-  log_v2::neb()->info("init: beginning host dependencies dump");
+  neb_logger->info("init: beginning host dependencies dump");
 
   try {
     // Loop through all dependencies.
@@ -189,15 +188,15 @@ static void send_host_dependencies_list() {
       neb::callback_dependency(NEBCALLBACK_ADAPTIVE_DEPENDENCY_DATA, &nsadd);
     }
   } catch (std::exception const& e) {
-    log_v2::neb()->info(
-        "init: error occurred while dumping host dependencies: {}", e.what());
+    neb_logger->info("init: error occurred while dumping host dependencies: {}",
+                     e.what());
   } catch (...) {
-    log_v2::neb()->error(
+    neb_logger->error(
         "init: unknown error occurred while dumping host dependencies");
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of host dependencies dump");
+  neb_logger->info("init: end of host dependencies dump");
 
   return;
 }
@@ -207,7 +206,7 @@ static void send_host_dependencies_list() {
  */
 static void send_host_group_list() {
   // Start log message.
-  log_v2::neb()->info("init: beginning host group dump");
+  neb_logger->info("init: beginning host group dump");
 
   // Loop through all host groups.
   for (hostgroup_map::const_iterator
@@ -240,7 +239,7 @@ static void send_host_group_list() {
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of host group dump");
+  neb_logger->info("init: end of host group dump");
 }
 
 /**
@@ -248,7 +247,7 @@ static void send_host_group_list() {
  */
 static void send_severity_list() {
   /* Start log message. */
-  log_v2::neb()->info("init: beginning severity dump");
+  neb_logger->info("init: beginning severity dump");
 
   for (auto it = com::centreon::engine::severity::severities.begin(),
             end = com::centreon::engine::severity::severities.end();
@@ -262,7 +261,7 @@ static void send_severity_list() {
  */
 static void send_tag_list() {
   /* Start log message. */
-  log_v2::neb()->info("init: beginning tag dump");
+  neb_logger->info("init: beginning tag dump");
 
   for (auto it = com::centreon::engine::tag::tags.begin(),
             end = com::centreon::engine::tag::tags.end();
@@ -276,7 +275,7 @@ static void send_tag_list() {
  */
 static void send_host_list(neb_sender sender = neb::callback_host) {
   // Start log message.
-  log_v2::neb()->info("init: beginning host dump");
+  neb_logger->info("init: beginning host dump");
 
   // Loop through all hosts.
   for (host_map::iterator it{com::centreon::engine::host::hosts.begin()},
@@ -294,7 +293,7 @@ static void send_host_list(neb_sender sender = neb::callback_host) {
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of host dump");
+  neb_logger->info("init: end of host dump");
 }
 
 /**
@@ -309,7 +308,7 @@ static void send_pb_host_list() {
  */
 static void send_host_parents_list() {
   // Start log message.
-  log_v2::neb()->info("init: beginning host parents dump");
+  neb_logger->info("init: beginning host parents dump");
 
   try {
     // Loop through all hosts.
@@ -332,15 +331,15 @@ static void send_host_parents_list() {
       }
     }
   } catch (std::exception const& e) {
-    log_v2::neb()->error("init: error occurred while dumping host parents: {}",
-                         e.what());
+    neb_logger->error("init: error occurred while dumping host parents: {}",
+                      e.what());
   } catch (...) {
-    log_v2::neb()->error(
+    neb_logger->error(
         "init: unknown error occurred while dumping host parents");
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of host parents dump");
+  neb_logger->info("init: end of host parents dump");
 }
 
 /**
@@ -349,7 +348,7 @@ static void send_host_parents_list() {
  */
 static void send_service_dependencies_list() {
   // Start log message.
-  log_v2::neb()->info("init: beginning service dependencies dump");
+  neb_logger->info("init: beginning service dependencies dump");
 
   try {
     // Loop through all dependencies.
@@ -369,16 +368,16 @@ static void send_service_dependencies_list() {
       neb::callback_dependency(NEBCALLBACK_ADAPTIVE_DEPENDENCY_DATA, &nsadd);
     }
   } catch (std::exception const& e) {
-    log_v2::neb()->error(
+    neb_logger->error(
         "init: error occurred while dumping service dependencies: {}",
         e.what());
   } catch (...) {
-    log_v2::neb()->error(
+    neb_logger->error(
         "init: unknown error occurred while dumping service dependencies");
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of service dependencies dump");
+  neb_logger->info("init: end of service dependencies dump");
 }
 
 /**
@@ -386,7 +385,7 @@ static void send_service_dependencies_list() {
  */
 static void send_service_group_list() {
   // Start log message.
-  log_v2::neb()->info("init: beginning service group dump");
+  neb_logger->info("init: beginning service group dump");
 
   // Loop through all service groups.
   for (servicegroup_map::const_iterator
@@ -419,7 +418,7 @@ static void send_service_group_list() {
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of service groups dump");
+  neb_logger->info("init: end of service groups dump");
 }
 
 /**
@@ -427,7 +426,7 @@ static void send_service_group_list() {
  */
 static void send_service_list(neb_sender sender = neb::callback_service) {
   // Start log message.
-  log_v2::neb()->info("init: beginning service dump");
+  neb_logger->info("init: beginning service dump");
 
   // Loop through all services.
   for (service_map::const_iterator
@@ -446,7 +445,7 @@ static void send_service_list(neb_sender sender = neb::callback_service) {
   }
 
   // End log message.
-  log_v2::neb()->info("init: end of services dump");
+  neb_logger->info("init: end of services dump");
 }
 
 /**
@@ -460,20 +459,13 @@ static void send_pb_service_list() {
  *  Send the instance configuration loaded event.
  */
 static void send_instance_configuration() {
-  log_v2::neb()->info(
+  neb_logger->info(
       "init: sending initial instance configuration loading event");
-  std::shared_ptr<neb::instance_configuration> ic(
-      new neb::instance_configuration);
+  auto ic = std::make_shared<neb::instance_configuration>();
   ic->loaded = true;
   ic->poller_id = config::applier::state::instance().poller_id();
   neb::gl_publisher.write(ic);
 }
-
-/**************************************
- *                                     *
- *          Global Functions           *
- *                                     *
- **************************************/
 
 /**
  *  Send initial configuration to the global publisher.
