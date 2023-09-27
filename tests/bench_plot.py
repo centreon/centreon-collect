32e1c70e61eb82601c82aef638f3e8b487bdf88f
David Boucher <dboucher@centreon.com>
2023-09-27 11:34:31 -0400

    fix(tests): regressions on tests fixed.

--- tests/bench_plot.py
+++ tests/bench_plot.py
@@ -109,7 +109,7 @@ parser.add_argument('-b', '--bucket', default='centreon-collect-robot-report',
 args = parser.parse_args()
 
 if args.bucket is not None:
-    if !download_from_s3(args.unqlite_file, args.bucket):
+    if not download_from_s3(args.unqlite_file, args.bucket):
         exit()
 
 db = UnQLite(args.unqlite_file)
