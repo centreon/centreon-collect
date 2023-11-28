8b303c934e9afd8ff6662c9ee51bd5bc04c45342
David Boucher <dboucher@centreon.com>
2023-11-28 22:28:15 +0100

    cleanup(broker): muxer

--- broker/core/multiplexing/src/muxer.cc
+++ broker/core/multiplexing/src/muxer.cc
@@ -1,20 +1,20 @@
 /**
-* Copyright 2009-2013,2015-2017,2019-2023 Centreon
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* For more information : contact@centreon.com
-*/
+ * Copyright 2009-2013,2015-2017,2019-2023 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
 
 #include "com/centreon/broker/multiplexing/muxer.hh"
 
@@ -284,7 +284,6 @@ int32_t muxer::stop() {
   if (_read_handler)
     _read_handler = nullptr;
   _update_stats();
-  DEBUG(fmt::format("STOP muxer {} {}", static_cast<void*>(this), _name));
   return 0;
 }
 
