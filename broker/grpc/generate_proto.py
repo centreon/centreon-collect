c2f891414f971744c14ca6fdeef60835ac451fe2
David Boucher <dboucher@centreon.com>
2023-12-01 16:16:40 +0100

    fix(engine): updates due to the rebase

--- broker/grpc/generate_proto.py
+++ broker/grpc/generate_proto.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-** Copyright 2023 Centreon
-**
-** Licensed under the Apache License, Version 2.0(the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**
-** For more information : contact@centreon.com
+* Copyright 2023 Centreon
+*
+* Licensed under the Apache License, Version 2.0(the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* For more information : contact@centreon.com
 """
 
 from os import listdir
@@ -35,7 +35,7 @@ message CentreonEvent {
 """
 
 cc_file_begin_content = """
-/*
+/**
  * Copyright 2023 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -66,11 +66,7 @@ cc_file_begin_content = """
 
 using namespace com::centreon::broker;
 
-CCB_BEGIN()
-
-namespace grpc {
-
-
+namespace com::centreon::broker::grpc {
 namespace detail {
 
 /**
@@ -257,8 +253,6 @@ with open(args.cc_file, 'w') as fp:
     fp.write(cc_file_create_event_with_data_function_end)
     fp.write("""
 
-} //namespace grpc
+} // namespace com::centreon::broker::grpc
 
-CCB_END()
-             
 """)
