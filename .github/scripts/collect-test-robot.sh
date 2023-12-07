ac5d6e5958e22e62d89218db171a481901cfc739
David Boucher <dboucher@centreon.com>
2023-12-07 13:07:58 +0100

    wip(gha)

--- .github/scripts/collect-test-robot.sh
+++ .github/scripts/collect-test-robot.sh
@@ -5,41 +5,27 @@ set -x
 export RUN_ENV=docker
 
 test_file=$1
-database_type=$2
+distrib=$2
 
 if [[ "$test_file" =~ "unstable" ]] ; then
   exit 0
 fi
 
-if [ ${database_type} == 'mysql' ] && [ ! -f tests/${test_file}.mysql ]; then
-    echo > tests/log.html
-    echo '<?xml version="1.0" encoding="UTF-8"?>' > tests/output.xml
-    echo '<robot generator="Robot 6.0.2 (Python 3.9.14 on linux)" generated="20230517 15:35:12.235" rpa="false" schemaversion="3"></robot>' >> tests/output.xml
-    echo > tests/report.html
-    exit 0
-fi
-
 echo "###########################  start sshd ###########################"
 /usr/sbin/sshd -D  &
 
-if [ $database_type == 'mysql' ]; then
-    echo "########################### Start MySQL ######################################"
-    /usr/libexec/mysqldtoto --user=root &
+echo "########################### Start MariaDB ######################################"
+if [ "$distrib" != "bullseye" ]; then
+  mariadbd --socket=/var/lib/mysql/mysql.sock --user=root > /dev/null 2>&1 &
 else
-    echo "########################### Start MariaDB ######################################"
-    if [ "$distrib" = "ALMALINUX" ]; then
-      mariadbd --socket=/var/lib/mysql/mysql.sock --user=root > /dev/null 2>&1 &
-    else
-      mariadbd --socket=/run/mysqld/mysqld.sock --user=root > /dev/null 2>&1 &
-    fi
-    sleep 5
-
+  mariadbd --socket=/run/mysqld/mysqld.sock --user=root > /dev/null 2>&1 &
 fi
+sleep 5
 
 
 echo "########################## Install centreon collect ###########################"
 echo "Installation..."
-if [ "$distrib" = "ALMALINUX" ]; then
+if [ "$distrib" != "bullseye" ]; then
   dnf clean all
   rm -f ./*-selinux-*.rpm # avoid to install selinux packages which are dependent to centreon-common-selinux
   dnf install -y ./*.rpm
