28544cf1e54d0c4e9a0082669c0bfaf01cf96206
David Boucher <dboucher@centreon.com>
2024-01-03 05:26:41 -0500

    fix(broker): bam can be started before brokerrpc

--- broker/bam/inc/com/centreon/broker/bam/internal.hh
+++ broker/bam/inc/com/centreon/broker/bam/internal.hh
@@ -1,25 +1,26 @@
-/*
-** Copyright 2022 Centreon
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**
-** For more information : contact@centreon.com
-*/
+/**
+ * Copyright 2022-2023 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
 
 #ifndef CCB_BAM_INTERNAL_HH
 #define CCB_BAM_INTERNAL_HH
 
 #include "bbdo/bam.pb.h"
+#include "broker.pb.h"
 #include "com/centreon/broker/io/events.hh"
 #include "com/centreon/broker/io/protobuf.hh"
 #include "com/centreon/broker/namespace.hh"
@@ -75,8 +76,16 @@ using pb_dimension_truncate_table_signal =
     io::protobuf<DimensionTruncateTableSignal,
                  make_type(io::bam,
                            bam::de_pb_dimension_truncate_table_signal)>;
-
 }  // namespace bam
+
+/* We have to declare the pb_ba_info also here because we don't control the
+ * order things are created. If the bam stream is created before brokerrpc, its
+ * muxer will be declared with known events (so without pb_ba_info) and if we
+ * want pb_ba_info to be known, thenwe have to force its declaration. */
+namespace extcmd {
+using pb_ba_info =
+    io::protobuf<BaInfo, make_type(io::extcmd, extcmd::de_ba_info)>;
+}
 
 CCB_END()
 
