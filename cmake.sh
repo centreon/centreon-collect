18fecefb0fdd25bf3e85b4537250aa12e9b3c151
David Boucher <dboucher@centreon.com>
2024-01-18 06:14:47 -0500

    enh(cmake): work on build on alma8

--- cmake.sh
+++ cmake.sh
@@ -293,8 +293,8 @@ elif [ -r /etc/issue ] ; then
   fi
 fi
 
-if ! pip3 install conan==1.62.0 --upgrade --break-system-packages ; then
-  pip3 install conan==1.62.0 --upgrade
+if ! pip3.8 install conan==1.62.0 --upgrade --break-system-packages ; then
+  pip3.8 install conan==1.62.0 --upgrade
 fi
 
 if which conan ; then
