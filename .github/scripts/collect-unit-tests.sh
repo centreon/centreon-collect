38844e3a534f8f5f6be6c175ee8184970b9f6251
David Boucher <dboucher@centreon.com>
2024-01-31 15:31:25 +0000

    fix(gha): conan is dead

--- .github/scripts/collect-unit-tests.sh
+++ .github/scripts/collect-unit-tests.sh
@@ -8,8 +8,6 @@ cd /src
 export VCPKG_ROOT=/vcpkg
 export PATH=$VCPKG_ROOT:$PATH
 
-conan install .. -s compiler.cppstd=17 -s compiler.libcxx=libstdc++11 --build=missing
-
 OS_ID=$(cat /etc/issue | awk '{print $1}')
 if [[ "$OS_ID" == "Debian" || "$OS_ID" == "Ubuntu" ]] ; then
     CXXFLAGS="-Wall -Wextra" cmake -B build -DVCPKG_OVERLAY_PORTS=/overlays -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_BUILD_TYPE=Debug -DWITH_PREFIX=/usr -DWITH_PREFIX_BIN=/usr/sbin -DWITH_USER_BROKER=centreon-broker -DWITH_USER_ENGINE=centreon-engine -DWITH_GROUP_BROKER=centreon-broker -DWITH_GROUP_ENGINE=centreon-engine -DWITH_TESTING=On -DWITH_PREFIX_MODULES=/usr/share/centreon/lib/centreon-broker -DWITH_PREFIX_CONF_BROKER=/etc/centreon-broker -DWITH_PREFIX_LIB_BROKER=/usr/lib64/nagios -DWITH_PREFIX_CONF_ENGINE=/etc/centreon-engine -DWITH_PREFIX_LIB_ENGINE=/usr/lib64/centreon-engine -DWITH_PREFIX_LIB_CLIB=/usr/lib64/ -DWITH_RW_DIR=/var/lib/centreon-engine/rw -DWITH_VAR_DIR=/var/log/centreon-engine -DWITH_MODULE_SIMU=On -S .
