56b8953c5edf38682bf18f2497fed3463448a4bd
jean-christophe81 <98889244+jean-christophe81@users.noreply.github.com>
2024-02-07 11:49:08 +0100

    MON-20431 Add metrics name tests size (#839) (#998)
    REFS:MON-32950
    * Add metrics name tests size
    
    * Update library installation
    
    * fix tests
    
    ---------
    
    Co-authored-by: NITCHEU B <bnitcheu-ext@centreon.com>

--- tests/resources/Engine.py
+++ tests/resources/Engine.py
@@ -1881,23 +1881,32 @@ def external_command(func):
     return wrapper
 
 
-def process_service_check_result_with_metrics(hst: str, svc: str, state: int, output: str, metrics: int, config='config0'):
+def process_service_check_result_with_metrics(hst: str, svc: str, state: int, output: str, metrics: int, config='config0', metric_name='metric'):
     now = int(time.time())
     pd = [output + " | "]
     for m in range(metrics):
         v = math.sin((now + m) / 1000) * 5
-        pd.append(f"metric{m}={v}")
+        pd.append(f"{metric_name}{m}={v}")
+        logger.trace(f"{metric_name}{m}={v}")
     full_output = " ".join(pd)
     process_service_check_result(hst, svc, state, full_output, config)
 
+def process_service_check_result(hst: str, svc: str, state: int, output: str, config='config0', use_grpc=0, nb_check=1):
+    if use_grpc > 0:
+        port = 50001 + int(config[6:])
+        with grpc.insecure_channel(f"127.0.0.1:{port}") as channel:
+            stub = engine_pb2_grpc.EngineStub(channel)
+            for i in range(nb_check):
+                indexed_output = f"{output}_{i}"
+                stub.ProcessServiceCheckResult(engine_pb2.Check(
+                    host_name=hst, svc_desc=svc, output=indexed_output, code=state))
 
-def process_service_check_result(hst: str, svc: str, state: int, output: str, config='config0'):
-    now = int(time.time())
-    cmd = f"[{now}] PROCESS_SERVICE_CHECK_RESULT;{hst};{svc};{state};{output}\n"
-    f = open(
-        f"{VAR_ROOT}/lib/centreon-engine/{config}/rw/centengine.cmd", "w")
-    f.write(cmd)
-    f.close()
+    else:
+        now = int(time.time())
+        with open(f"{VAR_ROOT}/lib/centreon-engine/{config}/rw/centengine.cmd", "w") as f:
+            for i in range(nb_check):
+                cmd = f"[{now}] PROCESS_SERVICE_CHECK_RESULT;{hst};{svc};{state};{output}_{i}\n"
+                f.write(cmd)
 
 
 @external_command
