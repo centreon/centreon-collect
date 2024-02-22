72544a5c7273130cadef61d0fa746c4abb0e19e7
jean-christophe81 <98889244+jean-christophe81@users.noreply.github.com>
2024-02-22 16:32:29 +0100

    fix(engine): issue fixed between comments and fields in engine cfg files
    REFS: MON-15765
    
    Co-authored-by: David Boucher <dboucher@centreon.com>

--- engine/src/configuration/object.cc
+++ engine/src/configuration/object.cc
@@ -1,21 +1,21 @@
 /**
-* Copyright 2011-2014 Merethis
-*
-* This file is part of Centreon Engine.
-*
-* Centreon Engine is free software: you can redistribute it and/or
-* modify it under the terms of the GNU General Public License version 2
-* as published by the Free Software Foundation.
-*
-* Centreon Engine is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Centreon Engine. If not, see
-* <http://www.gnu.org/licenses/>.
-*/
+ * Copyright 2011-2014 Merethis
+ *
+ * This file is part of Centreon Engine.
+ *
+ * Centreon Engine is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * Centreon Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Centreon Engine. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 #include "com/centreon/engine/configuration/object.hh"
 #include "com/centreon/engine/configuration/anomalydetection.hh"
@@ -204,7 +204,7 @@ bool object::parse(std::string const& line) {
     key.assign(line, 0, pos);
     value.assign(line, pos + 1, std::string::npos);
   }
-  string::trim(value);
+  boost::algorithm::trim(value);
   if (!parse(key.c_str(), value.c_str()))
     return object::parse(key.c_str(), value.c_str());
   return true;
