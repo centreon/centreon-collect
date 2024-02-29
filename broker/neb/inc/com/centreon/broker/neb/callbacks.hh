da113db100e6cb9d7d93059c16aaa697a42d0d59
David Boucher <dboucher@centreon.com>
2024-02-29 21:56:43 +0100

    enh(broker): new event parent_host

--- broker/neb/inc/com/centreon/broker/neb/callbacks.hh
+++ broker/neb/inc/com/centreon/broker/neb/callbacks.hh
@@ -1,28 +1,26 @@
-/*
-** Copyright 2009-2013,2015, 2021-2022 Centreon
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**
-** For more information : contact@centreon.com
-*/
+/**
+ * Copyright 2009-2013,2015, 2021-2024 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
 
 #ifndef CCB_NEB_CALLBACKS_HH
 #define CCB_NEB_CALLBACKS_HH
 
+namespace com::centreon::broker::neb {
 
-namespace com::centreon::broker {
-
-namespace neb {
 extern unsigned gl_mod_flags;
 extern void* gl_mod_handle;
 
@@ -53,6 +51,7 @@ int callback_pb_process(int callback_type, void* data);
 int callback_program_status(int callback_type, void* data);
 int callback_pb_program_status(int callback_type, void* data);
 int callback_relation(int callback_type, void* data);
+int callback_pb_relation(int callback_type, void* data);
 int callback_service(int callback_type, void* data);
 int callback_service_check(int callback_type, void* data);
 int callback_pb_service_check(int callback_type, void* data);
@@ -68,8 +67,7 @@ int callback_tag(int callback_type, void* data) noexcept;
 int callback_pb_bench(int callback_type, void* data);
 
 void unregister_callbacks();
-}  // namespace neb
 
-}
+}  // namespace com::centreon::broker::neb
 
 #endif  // !CCB_NEB_CALLBACKS_HH
