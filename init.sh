8b202aa77e781613a644e1cb4807c5cd26e46179
David Boucher <dboucher@centreon.com>
2024-03-15 17:00:59 +0100

    cleanup(collect): comments added

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# A little script to source before compiling.
+# It just sets VCPKG_ROOT and PATH so that make/ninja won't fail.
 
+# To be sure the script is sourced.
 (return 0 2>/dev/null) && sourced=1 || sourced=0
 
 if (( sourced == 0 )) ; then
@@ -8,9 +11,11 @@ if (( sourced == 0 )) ; then
   exit 1
 fi
 
+# To be sure the script has not already been launched.
 if [[ "$PATH" =~ "vcpkg" ]] ; then
   echo "Already initialized"
 fi
 
+# The main purpose.
 export VCPKG_ROOT=$PWD/vcpkg
 export PATH=$VCPKG_ROOT:$PATH
