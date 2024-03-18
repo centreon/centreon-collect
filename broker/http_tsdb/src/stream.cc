e42f466523ba721551f9cd65b5e5d3ec6d5090d9
David Boucher <dboucher@centreon.com>
2024-03-18 16:24:17 +0100

    cleanup(broker)

--- broker/http_tsdb/src/stream.cc
+++ broker/http_tsdb/src/stream.cc
@@ -1,20 +1,20 @@
 /**
-* Copyright 2022 Centreon
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* For more information : contact@centreon.com
-*/
+ * Copyright 2022 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
 
 #include "com/centreon/broker/http_tsdb/stream.hh"
 #include "bbdo/storage/metric.hh"
@@ -190,8 +190,6 @@ bool stream::read(std::shared_ptr<io::data>& d, time_t) {
  * @param tree
  */
 void stream::statistics(nlohmann::json& tree) const {
-  time_t now = time(nullptr);
-
   auto extract_stat = [&](const char* label, const stat_average& data) {
     if (!data.empty()) {
       tree[label] = data.get_average();
