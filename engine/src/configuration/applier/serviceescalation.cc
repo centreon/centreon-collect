1b045441ce1a991f552a5f4ba669cbd01fd1e3bd
David Boucher <dboucher@centreon.com>
2024-06-04 09:47:01 +0200

    cleanup(engine): headers rewritten

--- engine/src/configuration/applier/serviceescalation.cc
+++ engine/src/configuration/applier/serviceescalation.cc
@@ -16,25 +16,6 @@
  * For more information : contact@centreon.com
  *
  */
-
-/**
- *
- * This file is part of Centreon Engine.
- *
- * Centreon Engine is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
- *
- * Centreon Engine is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Centreon Engine. If not, see
- * <http://www.gnu.org/licenses/>.
- */
-
 #include "com/centreon/engine/configuration/applier/serviceescalation.hh"
 #include "com/centreon/engine/broker.hh"
 #include "com/centreon/engine/config.hh"
