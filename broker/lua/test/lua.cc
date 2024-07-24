d64dd0eb5bb527aeea31fe241421a3c5090196c3
David Boucher <dboucher@centreon.com>
2024-07-24 18:35:52 +0200

    fix(broker/lua): Several fix in the lua Stream
    * Legacy OpenSSL md5 function replaced by the OpenSSL 3 version
    * improvement of hostgroups cache in streamconnector
    * improvement of servicegroups cache in streamconnector
    
    REFS: MON-34072 MON-15480

--- broker/lua/test/lua.cc
+++ broker/lua/test/lua.cc
@@ -1442,6 +1442,7 @@ TEST_F(LuaTest, ServiceGroupCacheTestName) {
   auto sg{std::make_shared<neb::service_group>()};
   sg->id = 28;
   sg->name = "centreon";
+  sg->enabled = true;
   _cache->write(sg);
 
   CreateScript(filename,
