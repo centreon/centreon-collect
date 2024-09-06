1fb2ef803a06dd12ca1af207205b9f6761500597
David Boucher <dboucher@centreon.com>
2024-09-06 17:42:57 +0200

    enh(deps): script improved so that the input is simplified

--- broker/deps.go
+++ broker/deps.go
@@ -13,14 +13,9 @@ import (
 // MaxDepth Max depth in search tree
 const MaxDepth = 3
 
-func findIncludes(file string, treated *[]string, depth int) {
+func findIncludes(file string, treated *[]string, depth int, outputs map[string]bool) {
   var myList []string
   file1 := filepath.Clean(file)
-  //file1, err := filepath.EvalSymLinks(file1)
-  //if err != nil {
-  //  fmt.Println("Error: " + err.Error())
-  //  os.Exit(1)
-  //}
     f, err := os.Open(file1)
     if err != nil {
       file1 = strings.TrimPrefix(file, "inc/")
@@ -64,17 +59,29 @@ func findIncludes(file string, treated *[]string, depth int) {
 
   scanner := bufio.NewScanner(f)
   r, _ := regexp.Compile("^#\\s*include\\s*([<\"])(.*)[>\"]")
+
   for scanner.Scan() {
     line := scanner.Text()
     match := r.FindStringSubmatch(line)
     if len(match) > 0 {
     /* match[0] is the global match, match[1] is '<' or '"' and match[2] is the file to include */
-      if match[1] == "\"" {
-        fmt.Printf("  \"%s\" -> \"%s\";\n", file, match[2])
-        myList = append(myList, match[2])
+      var output string
+      if depth == 1 {
+	if match[1] == "\"" {
+	  output = fmt.Sprintf("  \"%s\" -> \"%s\" [color=blue];\n", file, match[2])
+	  myList = append(myList, match[2])
+	} else {
+	  output = fmt.Sprintf("  \"%s\" -> \"%s\" [color=blue];\n", file, match[2])
+	}
       } else {
-        fmt.Printf("  \"%s\" -> \"%s\";\n", file, match[2])
+	if match[1] == "\"" {
+	  output = fmt.Sprintf("  \"%s\" -> \"%s\";\n", file, match[2])
+	  myList = append(myList, match[2])
+	} else {
+	  output = fmt.Sprintf("  \"%s\" -> \"%s\";\n", file, match[2])
+	}
       }
+      outputs[output] = true
     }
   }
   if err := scanner.Err(); err != nil {
@@ -90,7 +97,7 @@ func findIncludes(file string, treated *[]string, depth int) {
       }
     }
     if !found {
-      findIncludes(file2, treated, depth)
+      findIncludes(file2, treated, depth, outputs)
     }
   }
 }
@@ -115,8 +122,12 @@ func main() {
   fmt.Println("digraph deps {")
 
   var treated []string
+  outputs := make(map[string]bool)
   for _, file := range fileList {
-    findIncludes(file, &treated, 0)
+    findIncludes(file, &treated, 0, outputs)
+  }
+  for output := range outputs {
+    fmt.Println(output)
   }
   fmt.Println("}")
 }
