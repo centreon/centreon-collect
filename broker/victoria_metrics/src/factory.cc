aa5834e73909d068ae18a9ff864fb7f1a52dcacb
David Boucher <dboucher@centreon.com>
2024-10-03 11:49:51 +0200

    cleanup(collect): compilations warnings removed

--- broker/victoria_metrics/src/factory.cc
+++ broker/victoria_metrics/src/factory.cc
@@ -54,7 +54,8 @@ factory::factory()
 
 io::endpoint* factory::new_endpoint(
     config::endpoint& cfg,
-    const std::map<std::string, std::string>& global_params,
+    const std::map<std::string, std::string>& global_params
+    [[maybe_unused]],
     bool& is_acceptor,
     std::shared_ptr<persistent_cache>) const {
   is_acceptor = false;
