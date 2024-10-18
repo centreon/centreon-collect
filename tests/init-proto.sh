9092449c13d06da5d4619e61c9671af691e4bc13
David Boucher <dboucher@centreon.com>
2024-10-18 09:20:00 +0200

    fix(tests): init-proto.sh fixed to work with common.proto

--- tests/init-proto.sh
+++ tests/init-proto.sh
@@ -4,10 +4,9 @@ set -e
 
 cd resources
 python3 -m grpc_tools.protoc -I../../engine/enginerpc -I../../common/src --python_out=. --grpc_python_out=. engine.proto
-python3 -m grpc_tools.protoc -I../../broker/core/src -I../../common/src --python_out=. --grpc_python_out=. broker.proto
+python3 -m grpc_tools.protoc -I../../bbdo -I../../broker/core/src -I../../common/src --python_out=. --grpc_python_out=. broker.proto
 python3 -m grpc_tools.protoc -I../../common/src --python_out=. --grpc_python_out=. process_stat.proto
-for file in `ls ../../bbdo/*.proto`
-do
+for file in `ls ../../bbdo/*.proto` ; do
     python3 -m grpc_tools.protoc -I../../bbdo --python_out=. --grpc_python_out=. `basename $file`
 done
 python3 ../../broker/grpc/generate_proto.py -f grpc_stream.proto -c /tmp/not_used.cc -d ../../bbdo
