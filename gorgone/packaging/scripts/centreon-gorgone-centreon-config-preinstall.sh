e479a8b6c4c5faa467c9b93886cd16aa54b9a9c0
Kevin Duret <kduret@centreon.com>
2024-10-29 09:18:16 +0100

    fix(packaging): remove dependency to centreon-common

--- gorgone/packaging/scripts/centreon-gorgone-centreon-config-preinstall.sh
+++ gorgone/packaging/scripts/centreon-gorgone-centreon-config-preinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if ! getent passwd centreon > /dev/null 2>&1; then
+  useradd -g centreon -m -d /var/spool/centreon -r centreon 2> /dev/null
+fi
+
 if ! getent group centreon > /dev/null 2>&1; then
   groupadd -r centreon
 fi
