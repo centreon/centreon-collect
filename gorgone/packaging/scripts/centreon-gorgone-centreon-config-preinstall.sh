00517ccd3c51e8ecd3d4cc3981e3190c6d7a47ec
Kevin Duret <kduret@centreon.com>
2024-10-29 12:56:43 +0100

    fix

--- gorgone/packaging/scripts/centreon-gorgone-centreon-config-preinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! getent group centreon > /dev/null 2>&1; then
-  groupadd -r centreon
-fi
-
-if ! getent passwd centreon > /dev/null 2>&1; then
-  useradd -g centreon -m -d /var/spool/centreon -r centreon 2> /dev/null
-fi
