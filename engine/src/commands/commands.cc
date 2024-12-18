9814e149da63ef32b45af974b0f5a451d6f8c86d
David Boucher <dboucher@centreon.com>
2024-12-18 12:09:30 +0100

    enh(broker): AdaptiveHostStatus and AdaptiveServiceStatus added (#1869) (#1954)
    * enh(broker/grpc): Improvement of the grpc python generator script
    * enh(tests): new test on downtimes, to check duplicates in rrd
    * enh(broker): AdaptiveHostStatus and AdaptiveServiceStatus added
    * fix(engine): update_status() generalized with an argument
    * fix(broker): neb::callbacks works better with service_type and internal_id
    * enh(broker/lua): the lua stream must handle AdaptiveHostStatus and AdaptiveServiceStatus
    * enh(broker/unified_sql): work on adaptive status in unified_sql
    * cleanup(collect): headers removed when not needed
    * enh(cmake): cmake-vcpkg improved
    * fix(broker/bam): pb_adaptive_service_status are handled by bam now
    
    REFS: MON-151251

--- engine/src/commands/commands.cc
+++ engine/src/commands/commands.cc
@@ -1,6 +1,6 @@
 /**
  * Copyright 1999-2008           Ethan Galstad
- * Copyright 2011-2013,2015-2022 Centreon
+ * Copyright 2011-2013,2015-2024 Centreon
  *
  * This file is part of Centreon Engine.
  *
@@ -26,9 +26,7 @@
 #include "com/centreon/engine/broker.hh"
 #include "com/centreon/engine/checks/checker.hh"
 #include "com/centreon/engine/commands/processing.hh"
-#include "com/centreon/engine/comment.hh"
-#include "com/centreon/engine/configuration/applier/state.hh"
-#include "com/centreon/engine/downtimes/downtime.hh"
+#include "com/centreon/engine/common.hh"
 #include "com/centreon/engine/downtimes/downtime_finder.hh"
 #include "com/centreon/engine/downtimes/downtime_manager.hh"
 #include "com/centreon/engine/events/loop.hh"
@@ -540,8 +538,7 @@ int cmd_process_service_check_result(int cmd [[maybe_unused]],
     runtime_logger->warn(
         "Warning:  Passive check result was received for service '{}' on host "
         "'{}', but the host could not be found!",
-        fmt::string_view(svc_description.data(), svc_description.size()),
-        fmt::string_view(host_name.data(), host_name.size()));
+        svc_description, host_name);
     return ERROR;
   }
 
@@ -2622,7 +2619,7 @@ void acknowledge_host_problem(host* hst,
                 notifier::notification_option_none);
 
   /* update the status log with the host info */
-  hst->update_status();
+  hst->update_status(host::STATUS_ACKNOWLEDGEMENT);
 
   /* add a comment for the acknowledgement */
   auto com{std::make_shared<comment>(
@@ -2663,7 +2660,7 @@ void acknowledge_service_problem(service* svc,
                 notifier::notification_option_none);
 
   /* update the status log with the service info */
-  svc->update_status();
+  svc->update_status(service::STATUS_ACKNOWLEDGEMENT);
 
   /* add a comment for the acknowledgement */
   auto com{std::make_shared<comment>(
@@ -2679,7 +2676,7 @@ void remove_host_acknowledgement(host* hst) {
   hst->set_acknowledgement(AckType::NONE);
 
   /* update the status log with the host info */
-  hst->update_status();
+  hst->update_status(host::STATUS_ACKNOWLEDGEMENT);
 
   /* remove any non-persistant comments associated with the ack */
   comment::delete_host_acknowledgement_comments(hst);
@@ -2691,7 +2688,7 @@ void remove_service_acknowledgement(service* svc) {
   svc->set_acknowledgement(AckType::NONE);
 
   /* update the status log with the service info */
-  svc->update_status();
+  svc->update_status(host::STATUS_ACKNOWLEDGEMENT);
 
   /* remove any non-persistant comments associated with the ack */
   comment::delete_service_acknowledgement_comments(svc);
