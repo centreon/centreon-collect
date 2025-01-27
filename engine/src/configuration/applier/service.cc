2fb06a0d55fa4f498b17f9bceac13e601dba35e3
Sechkem <sechkem@ext.centreon.com>
2025-01-27 14:04:18 +0100

    fix(engine) : update the icon_id for service/host when modified (#2049) (#2068)
    * fix(engine) : update the icon_id for service/host when modified
    * fix(engine): update the icon_id for legacy_engine
    
    REFS: MON-157503

--- engine/src/configuration/applier/service.cc
+++ engine/src/configuration/applier/service.cc
@@ -512,6 +512,7 @@ void applier::service::modify_object(configuration::service const& obj) {
   s->set_acknowledgement_timeout(obj.acknowledgement_timeout() *
                                  config->interval_length());
   s->set_recovery_notification_delay(obj.recovery_notification_delay());
+  s->set_icon_id(obj.icon_id());
 
   // Contacts.
   if (obj.contacts() != obj_old.contacts()) {
@@ -717,6 +718,7 @@ void applier::service::modify_object(configuration::Service* old_obj,
   s->set_acknowledgement_timeout(new_obj.acknowledgement_timeout() *
                                  pb_config.interval_length());
   s->set_recovery_notification_delay(new_obj.recovery_notification_delay());
+  s->set_icon_id(new_obj.icon_id());
 
   // Contacts.
   if (!MessageDifferencer::Equals(new_obj.contacts(), old_obj->contacts())) {
