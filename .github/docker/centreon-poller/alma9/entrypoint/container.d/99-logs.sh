4ca635416692b292e8aaa65a07fd83d013d8871b
Kevin Duret <kduret@centreon.com>
2025-01-27 10:10:41 +0100

    fix

--- .github/docker/centreon-poller/alma9/entrypoint/container.d/99-logs.sh
+++ .github/docker/centreon-poller/alma9/entrypoint/container.d/99-logs.sh
@@ -5,4 +5,5 @@ echo "Centreon is ready"
 
 tail -f \
   /var/log/centreon-engine/centengine.log \
-  /var/log/centreon-broker/*.log
+  /var/log/centreon-broker/*.log \
+  /var/log/centreon-gorgone/gorgoned.log
