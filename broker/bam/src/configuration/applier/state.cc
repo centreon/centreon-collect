cfb2ea82f53c9bc29075b27c4421d55c8127d63f
David Boucher <dboucher@centreon.com>
2025-01-29 18:46:48 +0100

    enh(broker/bam): new logs to understand an issue on inherited downtimes

--- broker/bam/src/configuration/applier/state.cc
+++ broker/bam/src/configuration/applier/state.cc
@@ -255,27 +255,33 @@ void applier::state::load_from_cache(persistent_cache& cache) {
 
   std::shared_ptr<io::data> d;
   cache.get(d);
+  uint32_t count_idt = 0;
+  uint32_t count_ba = 0;
   while (d) {
     switch (d->type()) {
       case inherited_downtime::static_type():
         _ba_applier.apply_inherited_downtime(
             *std::static_pointer_cast<const pb_inherited_downtime>(
                 neb::bbdo2_to_bbdo3(d)));
+        count_idt++;
         break;
       case pb_inherited_downtime::static_type(): {
         const pb_inherited_downtime& dwn =
             *std::static_pointer_cast<const pb_inherited_downtime>(d);
         _ba_applier.apply_inherited_downtime(dwn);
+        count_idt++;
       } break;
       case pb_services_book_state::static_type(): {
         const ServicesBookState& state =
             std::static_pointer_cast<const pb_services_book_state>(d)->obj();
         _book_service.apply_services_state(state);
+        count_ba++;
       } break;
     }
     cache.get(d);
   }
-  _logger->debug("BAM: Inherited downtimes and BA states restored");
+  _logger->debug("BAM: {} Inherited downtimes and {} BA states restored",
+                 count_idt, count_ba);
 }
 
 /**
