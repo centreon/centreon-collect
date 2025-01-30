b9613a30251b37c359bab71453d4395d8f7df95e
jean-christophe81 <98889244+jean-christophe81@users.noreply.github.com>
2025-01-30 15:51:51 +0100

    Mon 158671 add grpc logs to agent otel (#2082)
    enh(agent): agent log grpc events, use of a more recent grpc recent (use of windows certificate store)
    enh(engine otel): grpc logger is alos driven by otel logger level

---
+++ agent/inc/com/centreon/agent/log.hh
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2024 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
+
+#ifndef CENTREON_AGENT_LOG_HH
+#define CENTREON_AGENT_LOG_HH
+
+namespace com::centreon::agent {
+
+extern std::shared_ptr<spdlog::logger> g_logger;
+
+void set_grpc_logger();
+
+}  // namespace com::centreon::agent
+
+#endif
