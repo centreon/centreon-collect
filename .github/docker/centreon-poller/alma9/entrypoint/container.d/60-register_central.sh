f80c35bad1b493d36c0f5a2c49af322b3481a066
Kevin Duret <kduret@centreon.com>
2025-02-11 15:16:31 +0100

    fix

--- .github/docker/centreon-poller/alma9/entrypoint/container.d/60-register_central.sh
+++ .github/docker/centreon-poller/alma9/entrypoint/container.d/60-register_central.sh
@@ -1,36 +1,47 @@
 #!/bin/sh
 
-WEB_ADDR=${WEB_ADDR:-web}
+WEB_HOST=${WEB_HOST:-web}
 WEB_GORGONE_PORT=${WEB_GORGONE_PORT:-8085}
 WEB_API_USERNAME=${WEB_API_USERNAME:-admin}
 WEB_API_PASSWORD=${WEB_API_PASSWORD:-Centreon\!2021}
 
-if getent hosts "${WEB_ADDR}"; then
+if getent hosts "${WEB_HOST}"; then
   # Set HOSTNAME if not already set
   : "${HOSTNAME:=$(hostname)}"
 
   API_RESPONSE=$(curl -s -X POST --insecure -H "Content-Type: application/json" \
       -d "{\"security\":{\"credentials\":{\"login\":\"${WEB_API_USERNAME}\", \"password\":\"${WEB_API_PASSWORD}\"}}}" \
-      "http://${WEB_ADDR}/centreon/api/latest/login")
+      "http://${WEB_HOST}/centreon/api/latest/login")
 
   API_TOKEN=$(echo "${API_RESPONSE}" | grep -o '"token":"[^"]*' | cut -d'"' -f4)
 
   PAYLOAD='{"name":"'"$HOSTNAME"'","hostname":"'"$HOSTNAME"'","type":"'"poller"'","address":"'"$HOSTNAME"'","parent_address":"'"127.0.0.1"'"}'
 
-  curl -s -X POST --insecure -i -H "Content-Type: application/json" -H "X-AUTH-TOKEN: ${API_TOKEN}" \
+  curl -s -X POST --insecure -H "Content-Type: application/json" -H "X-AUTH-TOKEN: ${API_TOKEN}" \
       -d "${PAYLOAD}" \
-      "http://${WEB_ADDR}/centreon/api/latest/platform/topology"
+      "http://${WEB_HOST}/centreon/api/latest/platform/topology"
 
-  CENTRAL_IP_ADDRESS=$(getent hosts "${WEB_ADDR}" | awk '{print $1;}')
-  curl -s -X POST --insecure -i -H "Content-Type: application/json" -H "centreon-auth-token: ${API_TOKEN}" \
+  CENTRAL_IP_ADDRESS=$(getent hosts "${WEB_HOST}" | awk '{print $1;}') || {
+    echo "Failed to resolve ${WEB_HOST} hostname"
+    exit 1
+  }
+
+  curl -s -X POST --insecure -H "Content-Type: application/json" -H "centreon-auth-token: ${API_TOKEN}" \
       -d "{\"linked_remote_master\":\"\",\"linked_remote_slaves\":[],\"open_broker_flow\":false,\"centreon_central_ip\":\"${CENTRAL_IP_ADDRESS}\",\"server_ip\":\"$HOSTNAME\",\"server_name\":\"$HOSTNAME\",\"server_type\":\"poller\"}" \
-      "http://${WEB_ADDR}/centreon/api/index.php?object=centreon_configuration_remote&action=linkCentreonRemoteServer"
+      "http://${WEB_HOST}/centreon/api/index.php?object=centreon_configuration_remote&action=linkCentreonRemoteServer"
 
-  until [ "$THUMBPRINT" != '' ]; do
+  MAX_RETRIES=120
+  retry_count=0
+  until [ "$THUMBPRINT" != '' ] || [ $retry_count -ge $MAX_RETRIES ]; do
     sleep 1
-    API_RESPONSE=$(curl -s -X GET -H "accept: application/json" "http://${WEB_ADDR}:${WEB_GORGONE_PORT}/api/internal/thumbprint")
+    API_RESPONSE=$(curl -s -X GET -H "accept: application/json" "http://${WEB_HOST}:${WEB_GORGONE_PORT}/api/internal/thumbprint")
     THUMBPRINT=$(echo $API_RESPONSE | grep -o '"thumbprint":\"[^\"]*' | cut -d'"' -f4)
+    retry_count=$((retry_count + 1))
   done
+  if [ -z "$THUMBPRINT" ]; then
+    echo "Failed to retrieve thumbprint after $MAX_RETRIES attempts"
+    exit 1
+  fi
 
   cat <<EOF > /etc/centreon-gorgone/config.d/40-gorgoned.yaml
 name:  gorgoned-$HOSTNAME
@@ -73,12 +84,12 @@ EOF
 
   until [ "$PING_OK" -gt "0" ] 2>/dev/null; do
     sleep 1
-    PING_OK=$(curl -s -X GET -H "accept: application/json" "http://${WEB_ADDR}:${WEB_GORGONE_PORT}/api/internal/constatus" | grep -o '"ping_ok":[0-9]*' | cut -d':' -f2)
+    PING_OK=$(curl -s -X GET -H "accept: application/json" "http://${WEB_HOST}:${WEB_GORGONE_PORT}/api/internal/constatus" | grep -o '"ping_ok":[0-9]*' | cut -d':' -f2)
     if [ "$PING_OK" -gt "0" ] 2>/dev/null; then
       for action in POLLERGENERATE CFGMOVE POLLERRESTART; do
         curl -X POST --insecure -H "Content-Type: application/json" -H "centreon-auth-token: ${API_TOKEN}" \
             -d "{\"action\":\"$action\",\"values\":\"$HOSTNAME\"}" \
-            "http://${WEB_ADDR}/centreon/api/index.php?action=action&object=centreon_clapi"
+            "http://${WEB_HOST}/centreon/api/index.php?action=action&object=centreon_clapi"
       done
     fi
   done
