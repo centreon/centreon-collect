e0a658f586cfd06d72051f891f6f947e22046434
David Boucher <dboucher@centreon.com>
2025-05-06 17:29:29 +0200

    wip(broker/unified_sql): does not compile

--- broker/unified_sql/src/database_configurator.cc
+++ broker/unified_sql/src/database_configurator.cc
@@ -224,7 +224,7 @@ absl::flat_hash_map<std::pair<uint64_t, uint16_t>, uint64_t> database_configurat
  * Data:
  *  FIELD                 & TYPE   & COL NAME    & C_TYPE & OPTIONS
  *  ---------------------------------------------------------------
- *  ${0}                  & uint64 & severity_id & uint64 & AU
+ *  ${0}                  & uint64 & tag_id      & uint64 & AU
  *  key::id               & uint64 & id          & uint64 &
  *  key::type             & uint32 & type        & uint32 &
  *  tag_name              & string & name        & string &
@@ -769,5 +769,133 @@ void database_configurator::_add_services_mysql(const ::google::protobuf::Repeat
   std::string query(fmt::format("INSERT INTO services VALUES {} ON DUPLICATE KEY UPDATE description=VALUES(description),action_url=VALUES(action_url),active_checks=VALUES(active_checks),check_command=VALUES(check_command),check_freshness=VALUES(check_freshness),check_interval=VALUES(check_interval),check_period=VALUES(check_period),default_active_checks=VALUES(default_active_checks),default_event_handler_enabled=VALUES(default_event_handler_enabled),default_flap_detection=VALUES(default_flap_detection),default_notify=VALUES(default_notify),default_passive_checks=VALUES(default_passive_checks),default_process_perfdata=VALUES(default_process_perfdata),display_name=VALUES(display_name),enabled=VALUES(enabled),event_handler=VALUES(event_handler),event_handler_enabled=VALUES(event_handler_enabled),first_notification_delay=VALUES(first_notification_delay),flap_detection=VALUES(flap_detection),flap_detection_on_critical=VALUES(flap_detection_on_critical),flap_detection_on_ok=VALUES(flap_detection_on_ok),flap_detection_on_unknown=VALUES(flap_detection_on_unknown),flap_detection_on_warning=VALUES(flap_detection_on_warning),freshness_threshold=VALUES(freshness_threshold),high_flap_threshold=VALUES(high_flap_threshold),icon_image=VALUES(icon_image),icon_image_alt=VALUES(icon_image_alt),low_flap_threshold=VALUES(low_flap_threshold),max_check_attempts=VALUES(max_check_attempts),notes=VALUES(notes),notes_url=VALUES(notes_url),notification_interval=VALUES(notification_interval),notification_period=VALUES(notification_period),notify=VALUES(notify),notify_on_critical=VALUES(notify_on_critical),notify_on_downtime=VALUES(notify_on_downtime),notify_on_flapping=VALUES(notify_on_flapping),notify_on_recovery=VALUES(notify_on_recovery),notify_on_unknown=VALUES(notify_on_unknown),notify_on_warning=VALUES(notify_on_warning),obsess_over_service=VALUES(obsess_over_service),passive_checks=VALUES(passive_checks),process_perfdata=VALUES(process_perfdata),retain_nonstatus_information=VALUES(retain_nonstatus_information),retain_status_information=VALUES(retain_status_information),retry_interval=VALUES(retry_interval),stalk_on_critical=VALUES(stalk_on_critical),stalk_on_ok=VALUES(stalk_on_ok),stalk_on_unknown=VALUES(stalk_on_unknown),stalk_on_warning=VALUES(stalk_on_warning)", fmt::join(values, ",")));
   mysql.run_query(query);
 }
+
+
+/** Database configuration
+ * Query: INSERT ON DUPLICATE KEY UPDATE
+ * Method: _add_service_resources
+ * Return: absl::flat_hash_map<std::pair<uint64_t, uint64_t>, uint64_t>
+ * Key: {host_id, service_id}
+ * Protobuf message: engine::configuration::Service
+ * Description: Add services into the resources database.
+ * Table: resources
+ * Data:
+ *  FIELD                 & TYPE   & COL NAME               & C_TYPE & OPTIONS
+ *  --------------------------------------------------------------------------
+ * ${0} & uint64 & resource_id & uint64_t & AU
+ * service_id & uint64 & id & uint64 & U
+ * host_id & uint64 & parent_id & uint64 & U
+ * ${NULL} & uint64 & internal_id & uint64 &
+ * ${0 // Maybe others values if meta-service, ba, ... } & uint32 & type & uint32 &
+ * max_check_attempts & uint32 & max_check_attempts & uint32 &
+ * ${poller_id} & uint64 & poller_id & uint64 &
+ * severity_id & uint64 & severity_id & uint64 & O
+ * service_description & string & name & string &
+ * ${NULL} & string & alias & string &
+ * host_name & string & parent_name & string &
+ * notes_url & string & notes_url & string &
+ * notes & string & notes & string &
+ * action_url & string & action_url & string &
+ * notifications_enabled & bool & notifications_enabled & bool &
+ * checks_passive & bool & passive_checks_enabled & bool &
+ * checks_active & bool & active_checks_enabled & bool &
+ * ${true} & bool & enabled & bool &
+ */
+/**
+ * @brief Add services into the resources database. (code for MariaDB).
+ *
+ * @param lst The list of messages to add/update.
+ */
+absl::flat_hash_map<std::pair<uint64_t, uint64_t>, uint64_t> database_configurator::_add_service_resources_mariadb(const ::google::protobuf::RepeatedPtrField<engine::configuration::Service>& lst) {
+  absl::flat_hash_map<std::pair<uint64_t, uint64_t>, uint64_t> retval;
+  uint64_t offset = 0;
+  std::string query("INSERT INTO resources (id,parent_id,internal_id,type,max_check_attempts,poller_id,severity_id,name,alias,parent_name,notes_url,notes,action_url,notifications_enabled,passive_checks_enabled,active_checks_enabled,enabled) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE internal_id=VALUES(internal_id),type=VALUES(type),max_check_attempts=VALUES(max_check_attempts),poller_id=VALUES(poller_id),severity_id=VALUES(severity_id),name=VALUES(name),alias=VALUES(alias),parent_name=VALUES(parent_name),notes_url=VALUES(notes_url),notes=VALUES(notes),action_url=VALUES(action_url),notifications_enabled=VALUES(notifications_enabled),passive_checks_enabled=VALUES(passive_checks_enabled),active_checks_enabled=VALUES(active_checks_enabled),enabled=VALUES(enabled)");
+  mysql_bulk_stmt stmt(query);
+  mysql& mysql = _stream->get_mysql();
+  if (!stmt.prepared())
+    mysql.prepare_statement(stmt);
+
+  auto bind = stmt.create_bind();
+
+  for (const auto& msg : lst) {
+    auto key = std::make_pair(msg.host_id(), msg.service_id());
+    retval.emplace(key, offset);
+    offset++;
+
+    bind->set_value_as_u64(0, msg.service_id());
+    bind->set_value_as_u64(1, msg.host_id());
+    bind->set_null_u64(2);
+    bind->set_value_as_u32(3, 0 /* Maybe others values if meta-service, ba, ... */);
+    bind->set_value_as_u32(4, msg.max_check_attempts());
+    bind->set_value_as_u64(5, poller_id);
+    if (msg.has_severity_id())
+      bind->set_value_as_u64(6, msg.severity_id());
+    else
+      bind->set_null_u64(6);
+    bind->set_value_as_str(7, common::truncate_utf8(msg.service_description(), get_centreon_storage_resources_col_size(centreon_storage_resources_name)));
+    bind->set_null_str(8);
+    bind->set_value_as_str(9, common::truncate_utf8(msg.host_name(), get_centreon_storage_resources_col_size(centreon_storage_resources_parent_name)));
+    bind->set_value_as_str(10, common::truncate_utf8(msg.notes_url(), get_centreon_storage_resources_col_size(centreon_storage_resources_notes_url)));
+    bind->set_value_as_str(11, common::truncate_utf8(msg.notes(), get_centreon_storage_resources_col_size(centreon_storage_resources_notes)));
+    bind->set_value_as_str(12, common::truncate_utf8(msg.action_url(), get_centreon_storage_resources_col_size(centreon_storage_resources_action_url)));
+    bind->set_value_as_bool(13, msg.notifications_enabled());
+    bind->set_value_as_bool(14, msg.checks_passive());
+    bind->set_value_as_bool(15, msg.checks_active());
+    bind->set_value_as_bool(16, true);
+    bind->next_row();
+  }
+  stmt.set_bind(std::move(bind));
+  
+  try {
+    std::promise<uint64_t> promise;
+    std::future<uint64_t> future = promise.get_future();
+    mysql.run_statement_and_get_int<uint64_t>(stmt, std::move(promise), mysql_task::int_type::LAST_INSERT_ID);
+    int first_id = future.get();
+    for (auto& [k, v] : retval)
+      v += first_id;
+  } catch (const std::exception& e) {
+      _logger->error("Error while executing <<{{}}>>: {{}}", query, e.what());
+  }
+  return retval;
+}
+
+
+
+/**
+ * @brief Add services into the resources database. (code for MySQL).
+ *
+ * @param lst The list of messages to add/update.
+ */
+absl::flat_hash_map<std::pair<uint64_t, uint64_t>, uint64_t> database_configurator::_add_service_resources_mysql(const ::google::protobuf::RepeatedPtrField<engine::configuration::Service>& lst) {
+  mysql& mysql = _stream->get_mysql();
+  absl::flat_hash_map<std::pair<uint64_t, uint64_t>, uint64_t> retval;
+  uint32_t offset = 0;
+
+  std::vector<std::string> values;
+  for (const auto& msg : lst) {
+    auto key = std::make_pair(msg.host_id(), msg.service_id());
+    retval.emplace(key, offset);
+    offset++;
+
+    std::string value(
+        fmt::format("({},{},NULL,{},{},{},{},'{}',NULL,'{}','{}','{}','{}',{},{},{},1)", msg.service_id(), msg.host_id(), 0 /* Maybe others values if meta-service, ba, ... */, msg.max_check_attempts(), poller_id, msg.severity_id(), misc::string::escape(msg.service_description(), get_centreon_storage_resources_col_size(centreon_storage_resources_name)), misc::string::escape(msg.host_name(), get_centreon_storage_resources_col_size(centreon_storage_resources_parent_name)), misc::string::escape(msg.notes_url(), get_centreon_storage_resources_col_size(centreon_storage_resources_notes_url)), misc::string::escape(msg.notes(), get_centreon_storage_resources_col_size(centreon_storage_resources_notes)), misc::string::escape(msg.action_url(), get_centreon_storage_resources_col_size(centreon_storage_resources_action_url)), msg.notifications_enabled(), msg.checks_passive(), msg.checks_active()));
+    values.emplace_back(value);
+  }
+  std::string query(fmt::format("INSERT INTO resources VALUES {} ON DUPLICATE KEY UPDATE internal_id=VALUES(internal_id),type=VALUES(type),max_check_attempts=VALUES(max_check_attempts),poller_id=VALUES(poller_id),severity_id=VALUES(severity_id),name=VALUES(name),alias=VALUES(alias),parent_name=VALUES(parent_name),notes_url=VALUES(notes_url),notes=VALUES(notes),action_url=VALUES(action_url),notifications_enabled=VALUES(notifications_enabled),passive_checks_enabled=VALUES(passive_checks_enabled),active_checks_enabled=VALUES(active_checks_enabled),enabled=VALUES(enabled)", fmt::join(values, ",")));
+  
+  try {
+    std::promise<int> promise;
+    std::future<int> future = promise.get_future();
+    mysql.run_query_and_get_int(query, std::move(promise), mysql_task::int_type::LAST_INSERT_ID);
+    int first_id = future.get();
+    for (auto& [k, v] : retval)
+      v += first_id;
+  } catch (const std::exception& e) {
+    _logger->error("Error while executing <<{{}}>>: {{}}", query, e.what());
+  }
+  return retval;
+
+}
+
 
 }  // namespace com::centreon::broker::unified_sql
