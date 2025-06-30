0439a150fa3d7207b561334f6c40c82062e07974
David Boucher <dboucher@centreon.com>
2025-06-30 15:53:35 +0200

    enh(broker/file): directory_watcher improved and tested (#2496)
    REFS: MON-153802

--- broker/core/inc/com/centreon/broker/file/directory_watcher.hh
+++ broker/core/inc/com/centreon/broker/file/directory_watcher.hh
@@ -1,59 +1,105 @@
-/*
-** Copyright 2015 Centreon
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-**
-** For more information : contact@centreon.com
-*/
+/**
+ * Copyright 2025 Centreon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
 
 #ifndef CCB_FILE_DIRECTORY_WATCHER_HH
 #define CCB_FILE_DIRECTORY_WATCHER_HH
 
-#include "com/centreon/broker/file/directory_event.hh"
+#include <sys/inotify.h>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+#include <cstdio>
 
-namespace com::centreon::broker {
+namespace com::centreon::broker::file {
 
-namespace file {
+constexpr size_t BUF_LEN = 4096;
+
 /**
  *  @class directory_watcher directory_watcher.hh
  * "com/centreon/broker/file/directory_watcher.hh"
  *  @brief Directory watcher.
  *
  *  Watch over directories for files modifications.
+ *  This watcher can't be used by multiple threads.
  */
 class directory_watcher {
+  std::shared_ptr<boost::asio::io_context> _io_context;
+  /* Watch descriptor */
+  int _wd;
+
+  boost::asio::posix::stream_descriptor _sd;
+  char _buffer[BUF_LEN];
+  size_t _bytes_read = 0;
+
+  std::shared_ptr<spdlog::logger> _logger;
+
  public:
-  directory_watcher();
+  class iterator {
+    directory_watcher* _watcher;
+    size_t _offset = 0;
+    inotify_event* _event = nullptr;
+    std::pair<uint32_t, std::string_view> _current;
+
+    inline void _update_current() {
+      if (_offset < _watcher->_bytes_read) {
+        _current.first = _event->mask;
+        _current.second = _event->name;
+      } else {
+        _current = {0, ""};
+      }
+    }
+
+   public:
+    iterator(directory_watcher* watcher, size_t offset = 0)
+        : _watcher(watcher),
+          _offset(offset),
+          _event{
+              reinterpret_cast<inotify_event*>(_watcher->_buffer + _offset)} {
+      _update_current();
+    }
+
+    std::pair<uint32_t, std::string_view>& operator*() { return _current; }
+
+    iterator& operator++() {
+      _offset += sizeof(inotify_event) + _event->len;
+      _event = reinterpret_cast<inotify_event*>(_watcher->_buffer + _offset);
+      _update_current();
+      return *this;
+    }
+
+    bool operator!=(const iterator& other) const {
+      return _offset != other._offset;
+    }
+    bool operator==(const iterator& other) const {
+      return _offset == other._offset;
+    }
+  };
+
+  directory_watcher(const std::string& to_watch_dir,
+                    uint32_t mask,
+                    bool non_blocking);
+  directory_watcher(const directory_watcher&) = delete;
+  directory_watcher& operator=(const directory_watcher&) = delete;
   ~directory_watcher();
 
-  void add_directory(std::string const& directory);
-  void remove_directory(std::string const& directory);
-  bool is_watched(std::string const& directory);
-
-  std::vector<directory_event> get_events();
-  void set_timeout(uint32_t msecs);
-
- private:
-  directory_watcher(directory_watcher const& o);
-  directory_watcher& operator=(directory_watcher const& o);
-
-  int _inotify_instance_id;
-  uint32_t _timeout;
-  std::map<std::string, int> _path_to_id;
-  std::map<int, std::string> _id_to_path;
+  iterator watch();
+  iterator begin() { return iterator(this); }
+  iterator end() { return iterator(this, _bytes_read); }
 };
-}  // namespace file
-
-}
+}  // namespace com::centreon::broker::file
 
 #endif  // !CCB_FILE_DIRECTORY_WATCHER_HH
--- broker/core/src/file/directory_watcher.cc
+++ broker/core/src/file/directory_watcher.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Centreon
+ * Copyright 2025 Centreon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,17 +18,11 @@
 
 #include "com/centreon/broker/file/directory_watcher.hh"
 
-#include <alloca.h>
 #include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/inotify.h>
-#include <sys/ioctl.h>
-#include <sys/select.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <boost/asio/posix/stream_descriptor.hpp>
+#include <boost/system/detail/error_code.hpp>
 
+#include "com/centreon/common/pool.hh"
 #include "com/centreon/exceptions/msg_fmt.hh"
 #include "common/log_v2/log_v2.hh"
 
@@ -38,174 +32,71 @@ using namespace com::centreon::broker::file;
 using log_v2 = com::centreon::common::log_v2::log_v2;
 
 /**
- * Default constructor.
+ * @brief Construct a new directory watcher object
+ *
+ * @param to_watch_dir the directory to watch
+ * @param mask the mask of events to watch for
+ * @param non_blocking if true, the inotify instance will be non-blocking
  */
-directory_watcher::directory_watcher() : _timeout(0) {
-  if ((_inotify_instance_id = ::inotify_init()) == -1) {
-    int err = errno;
+directory_watcher::directory_watcher(const std::string& to_watch_dir,
+                                     uint32_t mask,
+                                     bool non_blocking)
+    : _io_context(com::centreon::common::pool::io_context_ptr()),
+      _sd{asio::posix::stream_descriptor(*_io_context, inotify_init())},
+      _logger{log_v2::instance().get(log_v2::CORE)} {
+  int fd = _sd.native_handle();
+  if (fd < 0) {
     throw msg_fmt("directory_watcher: couldn't create inotify instance: '{}'",
-                  ::strerror(err));
+                  ::strerror(errno));
   }
+
+  if (non_blocking) {
+    boost::system::error_code ec;
+    _sd.non_blocking(true, ec);
+    if (ec) {
+      _logger->error(
+          "directory_watcher: couldn't set inotify instance to non-blocking: "
+          "{}",
+          ec.message());
+    }
+  }
+
+  _wd = inotify_add_watch(fd, to_watch_dir.c_str(), mask);
+  if (_wd < 0)
+    throw msg_fmt(
+        "directory_watcher: failed to add inotify watch on directory {}",
+        to_watch_dir);
 }
 
 /**
  *  Destructor.
  */
 directory_watcher::~directory_watcher() {
-  ::close(_inotify_instance_id);
+  std::cout << "directory_watcher destructor" << std::endl;
+  boost::system::error_code ec;
+  auto ec1 = _sd.close(ec);
+  if (ec1) {
+    _logger->error("Error while closing the directory watcher: {}",
+                   ec1.message());
+  }
+
+  int fd = _sd.native_handle();
+  inotify_rm_watch(fd, _wd);
+  close(fd);
 }
 
 /**
- *  Add a directory to the list of watched directories.
+ * @brief Watch for events on the watched directory and return an iterator over
+ * the events.
  *
- *  @param[in] directory  The directory to add.
+ * @return directory_watcher::iterator an iterator over the events
  */
-void directory_watcher::add_directory(std::string const& directory) {
-  int id =
-      ::inotify_add_watch(_inotify_instance_id, directory.c_str(),
-                          IN_CREATE | IN_MODIFY | IN_DELETE | IN_DELETE_SELF);
-  if (id == -1) {
-    int err = errno;
-    throw msg_fmt("directory_watcher: couldn't add directory: '{}'",
-                  ::strerror(err));
-  }
-
-  char* real_path = ::realpath(directory.c_str(), nullptr);
-  _path_to_id[real_path] = id;
-  _id_to_path[id] = real_path;
-  ::free(real_path);
-}
-
-/**
- *  @brief Remove a directory to the list of watched directories.
- *
- *  Do nothing if the directory was not watched.
- *
- *  @param[in] directory  The directory to remove.
- */
-void directory_watcher::remove_directory(std::string const& directory) {
-  char* real_path = ::realpath(directory.c_str(), nullptr);
-  std::map<std::string, int>::iterator it(_path_to_id.find(real_path));
-  ::free(real_path);
-  if (it == _path_to_id.end())
-    return;
-
-  if (::inotify_rm_watch(_inotify_instance_id, it->second) == -1) {
-    int err = errno;
-    throw msg_fmt("directory_watcher: couldn't remove directory: '{}'",
-                  ::strerror(err));
-  }
-
-  _id_to_path.erase(it->second);
-  _path_to_id.erase(it);
-}
-
-/**
- *  Is this directory watched?
- *
- *  @param[in] directory  The directory.
- *
- *  @return  True if this directory is watched.
- */
-bool directory_watcher::is_watched(std::string const& directory) {
-  return (_path_to_id.find(directory) != _path_to_id.end());
-}
-
-/**
- *  @brief Get the events of the watched directories.
- *
- *  This will blocks until new events are available.
- */
-std::vector<directory_event> directory_watcher::get_events() {
-  std::vector<directory_event> ret;
-
-  // Wait for at least one event.
-  fd_set set;
-  struct timeval tv;
-  FD_ZERO(&set);
-  FD_SET(_inotify_instance_id, &set);
-  tv.tv_sec = _timeout / 1000;
-  tv.tv_usec = (_timeout % 1000) * 1000;
-  ::select(_inotify_instance_id + 1, &set, nullptr, nullptr,
-           _timeout != 0 ? &tv : nullptr);
-
-  if (!FD_ISSET(_inotify_instance_id, &set))
-    return (ret);
-
-  auto logger = log_v2::instance().get(log_v2::CORE);
-
-  // Get the events
-  int buf_size;
-  if (ioctl(_inotify_instance_id, FIONREAD, &buf_size) == -1) {
-    int err = errno;
-    throw msg_fmt("directory_watcher: couldn't read events: '{}'",
-                  ::strerror(err));
-  }
-  logger->debug("file: directory watcher getting events of size {}", buf_size);
-  char* buf = (char*)alloca(buf_size);
-  int len = ::read(_inotify_instance_id, buf, buf_size);
-  if (len == -1) {
-    int err = errno;
-    throw msg_fmt("directory_watcher: couldn't read events: '{}'",
-                  ::strerror(err));
-  }
-
-  // Iterate over all the events.
-  struct inotify_event const* event;
-  for (char* ev = buf; ev < buf + len;
-       ev += sizeof(struct inotify_event) + event->len) {
-    event = (const struct inotify_event*)buf;
-    directory_event::type event_type;
-    if (event->mask & IN_CREATE)
-      event_type = directory_event::created;
-    else if (event->mask & IN_MODIFY)
-      event_type = directory_event::modified;
-    else if (event->mask & IN_DELETE)
-      event_type = directory_event::deleted;
-    else if (event->mask & IN_DELETE_SELF)
-      event_type = directory_event::directory_deleted;
-    else
-      continue;
-
-    std::map<int, std::string>::const_iterator found_path(
-        _id_to_path.find(event->wd));
-    if (found_path == _id_to_path.end())
-      break;
-
-    std::string name = found_path->second + "/" + event->name;
-
-    // Check if it's a file or a directory.
-    directory_event::file_type ft = directory_event::other;
-    if (event_type != directory_event::deleted &&
-        event_type != directory_event::directory_deleted) {
-      struct stat st;
-      if (::lstat(name.c_str(), &st) == -1) {
-        const char* error = ::strerror(errno);
-        throw msg_fmt("directory_watcher: couldn't check the file type: '{}'",
-                      error);
-      }
-      ft = directory_event::other;
-      if (S_ISDIR(st.st_mode))
-        ft = directory_event::directory;
-      else if (S_ISREG(st.st_mode))
-        ft = directory_event::file;
-    }
-
-    ret.push_back(directory_event(name, event_type, ft));
-    logger->debug(
-        "file: directory watcher getting an event for path '{}' and type "
-        "{}",
-        name, static_cast<uint32_t>(event_type));
-  }
-
-  return ret;
-}
-
-/**
- *  Set the timeout to check event.
- *
- *  @param[in] msecs  The timeout, in milliseconds. 0 for none.
- */
-void directory_watcher::set_timeout(uint32_t msecs) {
-  _timeout = msecs;
+directory_watcher::iterator directory_watcher::watch() {
+  boost::system::error_code ec;
+  auto buffer = boost::asio::buffer(_buffer);
+  _bytes_read = _sd.read_some(buffer, ec);
+  iterator retval(this);
+  if (ec)
+    _logger->debug("Unable to read from inotify: {}", ec.message());
+  return retval;
 }
