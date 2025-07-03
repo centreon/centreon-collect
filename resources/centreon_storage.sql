76b5c9e2adc17a03d02d74e4c81bb9cd6fad191b
David Boucher <dboucher@centreon.com>
2025-07-03 09:17:35 +0200

    fix(broker/bam): mod_bam_reporting_timeperiods columns resized (#2512)
    REFS: MON-174922

--- resources/centreon_storage.sql
+++ resources/centreon_storage.sql
@@ -1516,13 +1516,13 @@ DROP TABLE IF EXISTS mod_bam_reporting_timeperiods;
 CREATE TABLE mod_bam_reporting_timeperiods (
   timeperiod_id int NOT NULL,
   name varchar(200) default NULL,
-  sunday varchar(200) default NULL,
-  monday varchar(200) default NULL,
-  tuesday varchar(200) default NULL,
-  wednesday varchar(200) default NULL,
-  thursday varchar(200) default NULL,
-  friday varchar(200) default NULL,
-  saturday varchar(200) default NULL,
+  sunday varchar(2048) default NULL,
+  monday varchar(2048) default NULL,
+  tuesday varchar(2048) default NULL,
+  wednesday varchar(2048) default NULL,
+  thursday varchar(2048) default NULL,
+  friday varchar(2048) default NULL,
+  saturday varchar(2048) default NULL,
 
   PRIMARY KEY (timeperiod_id)
 ) ENGINE=InnoDB CHARACTER SET utf8;
