2441564f8686e41c6bf083c048a75740d39bdbf7
Jean Christophe Roques <jcroques@centreon.com>
2025-08-28 17:18:16 +0200

    fix ut

--- broker/core/test/mysql/mysql.cc
+++ broker/core/test/mysql/mysql.cc
@@ -54,7 +54,8 @@ class DatabaseStorageTest : public ::testing::Test {
  public:
   void SetUp() override {
     try {
-      config::applier::init(com::centreon::common::BROKER, 0, "test_broker", 0);
+      config::applier::init(com::centreon::common::BROKER, "", 0, "test_broker",
+                            0);
     } catch (std::exception const& e) {
       (void)e;
     }
