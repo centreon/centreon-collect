b2db15b7ac443e5da74164a558b9e384d61769fd
Sechkem <sechkem@ext.centreon.com>
2025-08-28 16:49:33 +0100

    fix(config): Harmonize Linux centagent.json wording (#2597) (#2625)
    fix(config): Harmonize Linux centagent.json wording (#2597)
    
    REFS: MON-177564

--- agent/test/config_test.cc
+++ agent/test/config_test.cc
@@ -76,13 +76,14 @@ TEST(config, token) {
     "port":2500,
     "encryption":true,
     "compression": true,
-    "ca_name":"toto",
+    "ca_common_name":"toto",
     "token":"token1"
 })";
   f.close();
 
   config conf(_json_config_path);  // Declare and initialize conf
   ASSERT_EQ(conf.get_token(), "token1");
+  ASSERT_EQ(conf.get_ca_name(), "toto");
 }
 
 TEST(config, reversed_grpc_streaming_token) {
@@ -96,11 +97,56 @@ TEST(config, reversed_grpc_streaming_token) {
     "encryption":true,
     "compression": true,
     "reversed_grpc_streaming":true,
-    "ca_name":"toto",
+    "ca_common_name":"toto",
     "token":"token1"
 })";
   f.close();
 
   config conf(_json_config_path);  // Declare and initialize conf
   ASSERT_TRUE(conf.get_trusted_tokens().contains("token1"));
+}
+TEST(config, old_fields) {
+  ::remove(_json_config_path.c_str());
+  std::ofstream f(_json_config_path);
+  f << R"(
+{   
+    "host":"127.0.0.1",
+    "endpoint":"host1.domain2:4317",
+    "port":2500,
+    "encryption":true,
+    "compression": true,
+    "reversed_grpc_streaming":true,
+    "ca_name":"toto",
+    "ca_certificate":"/path/to/ca.crt",
+    "token":"token1"
+})";
+  f.close();
+
+  config conf(_json_config_path);  // Declare and initialize conf
+
+  ASSERT_EQ(conf.get_ca_certificate_file(), "/path/to/ca.crt");
+  ASSERT_EQ(conf.get_ca_name(), "toto");
+}
+
+TEST(config, new_fields) {
+  ::remove(_json_config_path.c_str());
+  std::ofstream f(_json_config_path);
+  f << R"(
+{   
+    "host":"127.0.0.1",
+    "endpoint":"host1.domain2:4317",
+    "port":2500,
+    "encryption":true,
+    "compression": true,
+    "reversed_grpc_streaming":true,
+    "ca_common_name":"toto",
+    "ca":"/path/to/ca.crt",
+    "token":"token1"
+})";
+  f.close();
+
+  config conf(_json_config_path);  // Declare and initialize conf
+
+  ASSERT_EQ(conf.get_ca_certificate_file(), "/path/to/ca.crt");
+  ASSERT_EQ(conf.get_ca_name(), "toto");
 }
