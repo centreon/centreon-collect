08e5837fe25f33dd6f31b28489d362197b6da396
Kevin Duret <kduret@centreon.com>
2025-09-12 15:41:49 +0200

    enh(ci): parallelize gorgone robot tests

--- .github/scripts/gorgone-test-robot.sh
+++ .github/scripts/gorgone-test-robot.sh
@@ -16,12 +16,12 @@ ulimit -S -n 524288
 
 # echo '/tmp/core.%p' > /proc/sys/kernel/core_pattern
 
-mysql -h mariadb -u root -ppassword -e "CREATE DATABASE \`centreon\`"
-mysql -h mariadb -u root -ppassword -e "CREATE DATABASE \`centreon-storage\`"
-mysql -h mariadb -u root -ppassword -e "GRANT ALL PRIVILEGES ON centreon.* TO 'centreon'@'%'"
-mysql -h mariadb -u root -ppassword -e "GRANT ALL PRIVILEGES ON  \`centreon-storage\`.* TO 'centreon'@'%'"
-mysql -h mariadb -u root -ppassword 'centreon' < centreon/centreon/www/install/createTables.sql
-mysql -h mariadb -u root -ppassword 'centreon-storage' < centreon/centreon/www/install/createTablesCentstorage.sql
+mysql -h host.docker.internal -u root -ppassword -e "CREATE DATABASE \`centreon\`"
+mysql -h host.docker.internal -u root -ppassword -e "CREATE DATABASE \`centreon-storage\`"
+mysql -h host.docker.internal -u root -ppassword -e "GRANT ALL PRIVILEGES ON centreon.* TO 'centreon'@'%'"
+mysql -h host.docker.internal -u root -ppassword -e "GRANT ALL PRIVILEGES ON  \`centreon-storage\`.* TO 'centreon'@'%'"
+mysql -h host.docker.internal -u root -ppassword 'centreon' < centreon/centreon/www/install/createTables.sql
+mysql -h host.docker.internal -u root -ppassword 'centreon-storage' < centreon/centreon/www/install/createTablesCentstorage.sql
 
 echo "##### Starting tests #####"
-robot -v 'DBHOST:mariadb' -v 'DBNAME:centreon' -v 'DBNAME_STORAGE:centreon-storage' -v 'DBUSER:centreon' gorgone/tests/robot/tests/${test_file}
+robot -v 'DBHOST:host.docker.internal' -v 'DBNAME:centreon' -v 'DBNAME_STORAGE:centreon-storage' -v 'DBUSER:centreon' gorgone/tests/robot/tests/${test_file}
