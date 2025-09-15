55a7629e8643e639b21bc1552e8b09fcbf254044
Kevin Duret <kduret@centreon.com>
2025-09-15 09:17:25 +0200

    enh(ci): parallelize gorgone robot tests

--- .github/scripts/gorgone-test-robot.sh
+++ .github/scripts/gorgone-test-robot.sh
@@ -11,17 +11,32 @@ if [ -f "/.venv/bin/activate" ]; then
   source /.venv/bin/activate
 fi
 
-ulimit -c unlimited
-ulimit -S -n 524288
+# Wait for the database to be up and running.
+set +e
+while true ; do
+  timeout 20 mysql -h${MYSQL_HOST} -uroot -ppassword -e 'SELECT User FROM user' mysql
+  retval=$?
+  if [ "$retval" = 0 ] ; then
+    echo 'DB server is running.'
+    break ;
+  else
+    echo 'DB server is not yet responding.'
+    sleep 1
+  fi
+done
+set -e
+
+# ulimit -c unlimited
+# ulimit -S -n 524288
 
 # echo '/tmp/core.%p' > /proc/sys/kernel/core_pattern
 
-mysql -h host.docker.internal -u root -ppassword -e "CREATE DATABASE \`centreon\`"
-mysql -h host.docker.internal -u root -ppassword -e "CREATE DATABASE \`centreon-storage\`"
-mysql -h host.docker.internal -u root -ppassword -e "GRANT ALL PRIVILEGES ON centreon.* TO 'centreon'@'%'"
-mysql -h host.docker.internal -u root -ppassword -e "GRANT ALL PRIVILEGES ON  \`centreon-storage\`.* TO 'centreon'@'%'"
-mysql -h host.docker.internal -u root -ppassword 'centreon' < centreon/centreon/www/install/createTables.sql
-mysql -h host.docker.internal -u root -ppassword 'centreon-storage' < centreon/centreon/www/install/createTablesCentstorage.sql
+mysql -h ${MYSQL_HOST} -u root -ppassword -e "CREATE DATABASE \`centreon\`"
+mysql -h ${MYSQL_HOST} -u root -ppassword -e "CREATE DATABASE \`centreon-storage\`"
+mysql -h ${MYSQL_HOST} -u root -ppassword -e "GRANT ALL PRIVILEGES ON centreon.* TO 'centreon'@'%'"
+mysql -h ${MYSQL_HOST} -u root -ppassword -e "GRANT ALL PRIVILEGES ON  \`centreon-storage\`.* TO 'centreon'@'%'"
+mysql -h ${MYSQL_HOST} -u root -ppassword 'centreon' < /usr/local/src/centreon/centreon/www/install/createTables.sql
+mysql -h ${MYSQL_HOST} -u root -ppassword 'centreon-storage' < /usr/local/src/centreon/centreon/www/install/createTablesCentstorage.sql
 
 echo "##### Starting tests #####"
-robot -v 'DBHOST:host.docker.internal' -v 'DBNAME:centreon' -v 'DBNAME_STORAGE:centreon-storage' -v 'DBUSER:centreon' gorgone/tests/robot/tests/${test_file}
+robot -v 'DBHOST:mariadb' -v 'DBNAME:centreon' -v 'DBNAME_STORAGE:centreon-storage' -v 'DBUSER:centreon' gorgone/tests/robot/tests/${test_file}
