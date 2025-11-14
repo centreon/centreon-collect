8961db45c469199b6d539e5aefa6a362df6af222
Tpo76 <fthepaut@centreon.com>
2025-11-14 13:07:56 -0500

    Fix runtime user

--- .github/docker/centreon-gorgone/bookworm/entrypoint/container.d/99-logs.sh
+++ .github/docker/centreon-gorgone/bookworm/entrypoint/container.d/99-logs.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 touch /tmp/docker.ready
-echo "Centreon is ready"
+echo "Centreon Gorgone is ready"
 
-exec "$@"
-
-# tail -f \
-#   /var/log/apache2/error.log \
-#   /var/log/php8.2-fpm-centreon-error.log
