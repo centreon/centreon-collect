4e10bd39ef5d3da9f5cdef8261915fbb772dbc97
Sechkem <sechkem@ext.centreon.com>
2025-12-12 14:33:20 +0100

    Fix(engine/broker) :broker could not delete membership of service to service group (#3000)
    * fix(engine/broker):  delete the relations before deleting the service
    * fix(engine/broker):  delete the relations before deleting the host
    REFS: MON-191814

--- tests/resources/Engine.py
+++ tests/resources/Engine.py
@@ -1808,6 +1808,88 @@ def ctn_create_service(index: int, host_id: int, cmd_id: int):
     return retval
 
 
+def ctn_remove_service(index: int, host_name: str, service_description: str):
+    """
+    Remove a service on the engine instance index, with the given service_id.
+
+    Args:
+        index (int): Index of the poller configuration (from 0).
+        service_id (int): The service ID of the service to remove.
+    """
+    filename = f"{ETC_ROOT}/centreon-engine/config{index}/services.cfg"
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    host_name_re = re.compile(rf"^\s*host_name\s+{host_name}\s*$")
+    service_description_re = re.compile(
+        rf"^\s*service_description\s+{service_description}\s*$")
+    service_begin = re.compile(r"^define service {$")
+    service_end = re.compile(r"^}$")
+    service_begin_idx = 0
+
+    while True:
+        if (service_begin_idx >= len(lines)):
+            break
+        if (service_begin.match(lines[service_begin_idx])):
+            found_host = False
+            found_service = False
+            for service_line_idx in range(service_begin_idx, len(lines)):
+                if (host_name_re.match(lines[service_line_idx])):
+                    found_host = True
+                if (service_description_re.match(lines[service_line_idx])):
+                    found_service = True
+                if found_host and found_service:
+                    for end_service_line in range(service_line_idx, len(lines)):
+                        if service_end.match(lines[end_service_line]):
+                            del lines[service_begin_idx:end_service_line + 1]
+                            break
+                    break
+                elif service_end.match(lines[service_line_idx]):
+                    service_begin_idx = service_line_idx
+                    break
+        else:
+            service_begin_idx = service_begin_idx + 1
+
+    with open(filename, "w") as f:
+        f.writelines(lines)
+
+
+def ctn_remove_host(index: int, host_name: str):
+    """
+    Remove a host from the hosts.cfg of the given engine instance.
+
+    Args:
+        index (int): Index of the poller configuration (from 0).
+        host_name (str): The host name to remove.
+    """
+    filename = f"{ETC_ROOT}/centreon-engine/config{index}/hosts.cfg"
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    host_name_re = re.compile(rf"^\s*host_name\s+{host_name}\s*$")
+    host_begin = re.compile(r"^\s*define host \{$")
+    host_end = re.compile(r"^\s*\}$")
+    host_begin_idx = 0
+
+    while host_begin_idx < len(lines):
+        if host_begin.match(lines[host_begin_idx]):
+            for host_line_idx in range(host_begin_idx, len(lines)):
+                if host_name_re.match(lines[host_line_idx]):
+                    for end_line_idx in range(host_line_idx, len(lines)):
+                        if host_end.match(lines[end_line_idx]):
+                            del lines[host_begin_idx:end_line_idx + 1]
+                            break
+                    break
+                elif host_end.match(lines[host_line_idx]):
+                    host_begin_idx = host_line_idx
+                    break
+        else:
+            host_begin_idx += 1
+
+    with open(filename, "w") as f:
+        f.writelines(lines)
+
+
 def ctn_create_anomaly_detection(index: int, host_id: int, dependent_service_id: int, metric_name: string, sensitivity: float = 0.0):
     """
     Create an anomaly detection on the engine instance with the given index.
