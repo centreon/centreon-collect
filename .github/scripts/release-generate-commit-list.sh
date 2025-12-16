e86442e8e7c3e333df5a0a559da53edb203f7d67
tuntoja <tuntoja@centreon.com>
2025-12-16 14:50:44 +0100

    fix

--- .github/scripts/release-generate-commit-list.sh
+++ .github/scripts/release-generate-commit-list.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Inputs
+# BASE_SHA is latest stable release bundle tag commit sha
+# HEAD_SHA is release or hotfix branch head sha
 BASE_SHA="$1"
 HEAD_SHA="$2"
 
+# Prepare list of commit-list
 mkdir -p artifacts/commit-lists
 
+# Generate list of commits for components
 while read -r component; do
   echo "Generating commit list for $component"
 
