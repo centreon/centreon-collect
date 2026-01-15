2d26702bf7231e13d0c2f9d6f83478b74c159e56
jean-christophe81 <98889244+jean-christophe81@users.noreply.github.com>
2026-01-15 16:26:26 +0100

    MON-191980 fix(lua): rework groups lua cache (#3071)
    * fix(lua): rework hostgroup cache
    
    * fix(lua): rework servicegroup cache
    
    * fix(broker): events are sent to muxer if engine is yet sending another events

--- broker/test/precomp_inc/precomp.hh
+++ broker/test/precomp_inc/precomp.hh
@@ -68,6 +68,8 @@
 #define BOOST_DATE_TIME_NO_LIB 1
 #include <boost/interprocess/containers/string.hpp>
 #include <boost/interprocess/managed_mapped_file.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index_container.hpp>
 
 #include <spdlog/fmt/ostr.h>
 #include <spdlog/spdlog.h>
