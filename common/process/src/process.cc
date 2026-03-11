038492053434a8c82c9895bc468c3d62ad8ad39e
Soufiane Echkem <sechkem@ext.centreon.com>
2026-03-11 14:14:38 +0000

    fix: lock stderr/stdout read

--- common/process/src/process.cc
+++ common/process/src/process.cc
@@ -514,7 +514,10 @@ void process<use_mutex>::_on_stdout_read(const boost::system::error_code& err,
   }
   if (!received.empty()) {
     _stdout_handler(received);
-    _stdout_read();
+    {
+      detail::lock<use_mutex> l(&_protect);
+      _stdout_read();
+    }
   }
 
   if (eof) {
@@ -586,7 +589,10 @@ void process<use_mutex>::_on_stderr_read(const boost::system::error_code& err,
 
   if (!received.empty()) {
     _stderr_handler(received);
-    _stderr_read();
+    {
+      detail::lock<use_mutex> l(&_protect);
+      _stderr_read();
+    }
   }
 
   if (eof) {
