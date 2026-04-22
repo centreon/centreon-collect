6f154932ffb8f9daf441b9d362e0b8054b0b0e84
Soufiane Echkem <sechkem@ext.centreon.com>
2026-04-22 10:35:22 +0100

    fix(engine) : not use string_view for hosts

--- engine/src/configuration/applier/hostescalation.cc
+++ engine/src/configuration/applier/hostescalation.cc
@@ -18,6 +18,9 @@
  */
 
 #include "com/centreon/engine/configuration/applier/hostescalation.hh"
+#include <spdlog/spdlog.h>
+#include <fstream>
+#include <string>
 #include "com/centreon/engine/broker.hh"
 #include "com/centreon/engine/config.hh"
 #include "com/centreon/engine/configuration/applier/state.hh"
@@ -80,8 +83,8 @@ void applier::hostescalation::expand_objects(configuration::State& s) {
   std::list<std::unique_ptr<Hostescalation> > resolved;
   for (auto& he : *s.mutable_hostescalations()) {
     if (he.hostgroups().data().size() > 0) {
-      absl::flat_hash_set<std::string_view> host_names;
-      for (auto& hname : he.hosts().data())
+      absl::flat_hash_set<std::string> host_names;
+      for (const auto& hname : he.hosts().data())
         host_names.emplace(hname);
       for (auto& hg_name : he.hostgroups().data()) {
         auto found_hg =
@@ -105,9 +108,19 @@ void applier::hostescalation::expand_objects(configuration::State& s) {
         fill_string_group(e->mutable_hosts(), n);
       }
     } else if (he.hosts().data().size() > 0) {
-      /* No hostgroup: escalation targets host_name directly, pass through. */
-      resolved.emplace_back(std::make_unique<Hostescalation>());
-      resolved.back()->CopyFrom(he);
+      absl::flat_hash_set<std::string> host_names;
+      for (const auto& hname : he.hosts().data())
+        host_names.emplace(hname);
+
+      he.mutable_hostgroups()->clear_data();
+      he.mutable_hosts()->clear_data();
+
+      for (auto& host : host_names) {
+        resolved.emplace_back(std::make_unique<Hostescalation>());
+        auto& e = resolved.back();
+        e->CopyFrom(he);
+        e->mutable_hosts()->add_data(host);
+      }
     } else
       throw engine_error()
           << "Could not expand host escalation: neither hostgroups nor hosts "
