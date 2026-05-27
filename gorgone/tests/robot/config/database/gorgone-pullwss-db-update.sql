4273a51132a046749f37ce4a53ebd66a42278f0a
EvanAdam <eadam@centreon.com>
2026-05-27 10:09:48 +0200

    delete(gorgone): Allow gorgone test to run, this should not be needed in production and should be removed before a merge.
    Refs:MON-196790

--- gorgone/tests/robot/config/database/gorgone-pullwss-db-update.sql
+++ gorgone/tests/robot/config/database/gorgone-pullwss-db-update.sql
@@ -1,7 +1,7 @@
 -- ------------------------------------------------------------
--- upgrade script for gorgone to add pull and pullwss communication types, and new auth token field (optionnal for now)
+-- upgrade script for gorgone to add pull and pullwss communication types, and new uid
 -- Adds enum value '3' and '4' to gorgone_communication_type
--- Adds new optional field gorgone_auth_token
+-- Adds new field uid for more uniqueness of poller identification
 -- ------------------------------------------------------------
 
 -- 1. Update enum to allow '4'
@@ -9,7 +9,6 @@ ALTER TABLE `nagios_server`
     MODIFY `gorgone_communication_type` ENUM('1','2','3', '4')
         NOT NULL DEFAULT '1';
 
--- 2. Add authentication token field (optional)
 ALTER TABLE `nagios_server`
-    ADD COLUMN `gorgone_auth_token` VARCHAR(255) DEFAULT NULL
-        AFTER `gorgone_port`;
+    ADD COLUMN `uid` BIGINT UNSIGNED DEFAULT NULL COMMENT 'Snowflake 64-bit unique identifier',
+    ADD UNIQUE KEY `uniq_uid` (`uid`)
