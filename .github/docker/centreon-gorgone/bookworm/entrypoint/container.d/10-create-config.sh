a48be5e2652f133b0fe82435080c151140169cf8
Tpo76 <fthepaut@centreon.com>
2026-06-10 16:20:14 +0200

    remove bookworm + reorganise directory + clean up

--- .github/docker/centreon-gorgone/bookworm/entrypoint/container.d/10-create-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Config is now static in the image (config.d/40-gorgoned.yaml).
-# Dynamic params are injected at startup via env vars (GORGONE_UID, GORGONE_TOKEN,
-# CENTRAL_HOST, CENTRAL_PORT) using gorgone's load_env_config mechanism.
--- .github/docker/centreon-gorgone/bookworm/entrypoint/container.d/99-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-touch /tmp/docker.ready
-echo "Centreon Gorgone is ready"
-
