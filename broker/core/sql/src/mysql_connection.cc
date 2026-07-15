a23689a182b06511005f1b5bb850be3d6824690a
jean-christophe81 <98889244+jean-christophe81@users.noreply.github.com>
2026-07-15 10:58:44 +0200

    fix(broker): mysql connection  deadlock
    MON-199106-mysql_connection-deadlock (#3530) (#3554)
    
    * fix mysql_connection dead_lock
    
    * force usage of right formatter of opentelemetry messages
    
    * remove warnings
    
    * fix alma8 ut compile

--- broker/core/sql/src/mysql_connection.cc
+++ broker/core/sql/src/mysql_connection.cc
@@ -922,6 +922,7 @@ void mysql_connection::_run() {
       }
     }
   }
+  lck.lock();
   _clear_connection();
   _state = finished;
   _start_condition.notify_all();
@@ -1065,6 +1066,8 @@ void mysql_connection::_process_while_empty_task(
   }
 
   if (_tasks_list.empty()) {
+    lock.unlock();
+    std::unique_lock<std::mutex> l(_start_m);
     _state = finished;
     _start_condition.notify_all();
   } else {
