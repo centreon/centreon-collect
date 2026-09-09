3ff6d54413c776ec9dd90d1726c0f1a6bbed0918
Sechkem <sechkem@ext.centreon.com>
2026-09-09 09:19:20 +0100

    fix(packaging): do not stop services during package upgrades. (#3685)
    * fix(packaging): do not stop services during package upgrades.
    
    REFS: MON-204490

--- gorgone/packaging/scripts/centreon-gorgone-preremove.sh
+++ gorgone/packaging/scripts/centreon-gorgone-preremove.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-systemctl stop gorgoned.service ||:
+# Shared by the rpm %preun (el8, el9, el10) and the deb prerm (bookworm,
+# trixie, jammy, noble). Only a real removal may stop the service: on an
+# upgrade the new package's postinstall restarts gorgoned unconditionally.
+case "$1" in
+  1 | [2-9] | [1-9][0-9]* | upgrade | failed-upgrade | deconfigure)
+    ;;
+  *)
+    systemctl stop gorgoned.service ||:
+    ;;
+esac
